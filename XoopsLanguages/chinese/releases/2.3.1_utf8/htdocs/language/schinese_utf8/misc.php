968e701bd889a82c2619bc398c78c1aca7a02c2f
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2008-11-10 09:00:36 +0000

    Language Files for XOOPS 2.3.1

---
+++ XoopsLanguages/chinese/releases/2.3.1_utf8/htdocs/language/schinese_utf8/misc.php
@@ -0,0 +1,24 @@
+<?php
+// $Id: misc.php 2 2005-11-02 18:23:29Z skalpa $
+define("_MSC_YOURNAMEC","您的姓名：");
+define("_MSC_YOUREMAILC","您的邮件：");
+define("_MSC_FRIENDNAMEC","朋友的姓名：");
+define("_MSC_FRIENDEMAILC","朋友的邮件：");
+define("_MSC_RECOMMENDSITE","推荐本站给朋友");
+// %s is your site name
+define("_MSC_INTSITE","不错的网站：%s");
+define("_MSC_REFERENCESENT","推荐信已经发给您的朋友了，谢谢！");
+define("_MSC_ENTERYNAME","请输入您的姓名");
+define("_MSC_ENTERFNAME","请输入您朋友的姓名");
+define("_MSC_ENTERFMAIL","请输入您朋友的邮件");
+define("_MSC_NEEDINFO","您必须输入必填项目！");
+define("_MSC_INVALIDEMAIL1","邮件地址格式错误。");
+define("_MSC_INVALIDEMAIL2","请检查确认后再试。");
+
+define("_MSC_AVAVATARS","现有头像");
+
+define("_MSC_SMILIES","表情图");
+define("_MSC_CLICKASMILIE","点击表情图加入文章中。");
+define("_MSC_CODE","代码");
+define("_MSC_EMOTION","表情含义");
+?>
