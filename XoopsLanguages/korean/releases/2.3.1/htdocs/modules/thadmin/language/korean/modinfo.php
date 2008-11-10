b334ff68bca6c947c90e6430d9dcacb24f6c0437
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2008-11-10 07:41:55 +0000

    Language Files for XOOPS 2.3.1

---
+++ XoopsLanguages/korean/releases/2.3.1/htdocs/modules/thadmin/language/korean/modinfo.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @copyright   The Xoops Project http://sourceforge.net/projects/xoops/
+ * @license     http://www.gnu.org/licenses/gpl.txt GNU General Public License (GPL)
+ * @package     thadmin
+ */
+
+// Main
+define('_MI_TH_NAME', 'ThAdmin');
+define('_MI_TH_DESC', 'ThAdmin 은 관리화면용 테마를 관리할 수 있는 모듈입니다.');
+// Menu
+define('_MI_TH_HOME', 'Home');
+define('_MI_TH_HELP', '도움말');
+// Settings
+define('_MI_TH_ADMINTHEME', '기본테마');
+define('_MI_TH_ADMINTHEME_DESC', '관리화면용 기본테마를 선택해 주세요!');
+define('_MI_TH_CPANELEONLY', '경고를 표시?');
+define('_MI_TH_CPANELEONLY_DESC', '관리화면 인터페이스에서 경고용 메세지를 숨길 수 있습니다.');
+?>
