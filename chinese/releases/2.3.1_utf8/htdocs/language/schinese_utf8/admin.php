6977fc393235c9541c5ce4cbafc789c14a949d64
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2008-11-10 10:16:48 +0000

    Language Files for XOOPS 2.3.1

---
+++ chinese/releases/2.3.1_utf8/htdocs/language/schinese_utf8/admin.php
@@ -0,0 +1,15 @@
+<?php
+// $Id: admin.php 2 2005-11-02 18:23:29Z skalpa $
+//%%%%%%	File Name  admin.php 	%%%%%
+define("_AD_NORIGHT","您没有权限进入管理区");
+define("_AD_ACTION","操作");
+define("_AD_EDIT","编辑");
+define("_AD_DELETE","删除");
+define("_AD_LASTTENUSERS","最新10位注册会员");
+define("_AD_NICKNAME","昵称");
+define("_AD_EMAIL","电子邮件");
+define("_AD_AVATAR","个性头像");
+define("_AD_REGISTERED","注册日期"); //Registered Date
+define('_AD_PRESSGEN', '这是您首次进入管理区域，请点击下面的按钮继续。');
+define('_AD_LOGINADMIN', '以管理员身份登录...');
+?>
