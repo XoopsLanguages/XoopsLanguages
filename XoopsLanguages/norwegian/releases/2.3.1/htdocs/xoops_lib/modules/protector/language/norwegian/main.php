009aea05aadced5a19b8ee0033060bab2d0b60e1
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2008-11-10 10:38:54 +0000

    Language Files for XOOPS 2.3.1

---
+++ XoopsLanguages/norwegian/releases/2.3.1/htdocs/xoops_lib/modules/protector/language/norwegian/main.php
@@ -0,0 +1,7 @@
+<?php
+
+define('_MD_PROTECTOR_YOUAREBADIP','Du er registrert med en skadelig IP-adresse!') ;
+define('_MD_PROTECTOR_TURNJAVASCRIPTON','Slå JavaScript På') ;
+define('_MD_PROTECTOR_DENYBYRBL','Protector avviste ditt innlegg, da din IP-adresse er registrert i RBL') ;
+
+?>
