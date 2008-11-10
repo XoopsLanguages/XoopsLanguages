ab7c0b8f141475d147d1f80c0c7839f0c7a67f26
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2008-11-10 07:41:55 +0000

    Language Files for XOOPS 2.3.1

---
+++ korean/releases/2.3.1_utf8/htdocs/modules/thadmin/language/koreanutf/admin.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @copyright   The Xoops Project http://sourceforge.net/projects/xoops/
+ * @license     http://www.gnu.org/licenses/gpl.txt GNU General Public License (GPL)
+ * @package     ThAdmin
+ * @subpackage  language
+ */
+
+// General
+define('_AD_TH_PREFERENCES', '일반설정');
+define('_AD_TH_UPDATE', '업데이트');
+define('_AD_TH_TITLE', '관리'); 
+define('_AD_TH_NAVIGATION', '네비게이션');
+define('_AD_TH_ADMINHOME', '관리화면Home');
+define('_AD_TH_SITEHOME', '사이트Home');
+define('_AD_TH_LINKS', '유용한 링크');
+define('_AD_TH_LINKS_XOOPSPROJECT', 'XOOPS Project');
+define('_AD_TH_LINKS_XOOPSCOMMUNITY', 'XOOPS Community');
+define('_AD_TH_LINKS_LOCAL', 'Local Support');
+define('_AD_TH_LINKS_FORXOOPS', 'ForXoops');
+define('_AD_TH_LOGOUT', '로그아웃');
+define('_AD_TH_MODULES', '모듈');
+define('_AD_TH_MODULEADMIN' , '관리');
+define('_AD_TH_BLOCKS', '블록');
+define('_AD_TH_BLOCKADMIN', '관리');
+define('_AD_TH_USERS', '사용자');
+define('_AD_TH_MISCS', 'Tool');
+
+// CPanel Description
+define('_MD_CPANEL_OVERVIEW_DESC', '시스템 설정을 확인할 수 있다.');
+define('_MD_CPANEL_PHPEXTENSIONS_DESC', '로드된 php extension 을 확인할 수 있다.');
+define('_MD_AM_PREF_DESC', '사이트 및 설치된 모듈 관련 일반설정을 하실 수 있습니다.');
+define('_AD_TH_MODULES_DESC', '설치된 모든 모듈을 관리할 수 있다.');
+define('_AD_TH_BLOCKS_DESC', '모든 블록의 설정을 관리할 수 있다.');
+define('_AD_TH_USERS_DESC', '모든 사용자 관련 설정/작업을 할 수 있다.');
+define('_AD_TH_MISCS_DESC', '모든 tool 관련 설정/작업을 할 수 있다.');
+// Index
+define('_THADMIN_NAME', '이름:');
+define('_THADMIN_AUTHOR', '제작자:');
+define('_THADMIN_VERSION', '버전:');
+define('_THADMIN_ACTIVE', '활성화:');
+// Upload form
+define('_THADMIN_UPLOAD', '새 모듈을 업로드');
+define('_THADMIN_UPLOAD_FILE', '테마 파일 선택');
+define('_THADMIN_UPLOAD_DIRNAME', '테마 디렉토리명');
+define('_THADMIN_UPLOAD_INSTALL', '업로드후 설치');
+define('_THADMIN_UPLOAD_INSTALLATION', '설치 진행');
+define('_THADMIN_UPLOAD_UPLOADED', '새 테마가 지금 설치될 수 있습니다.');
+define('_THADMIN_UPLOAD_ERROR', '파일이 업로드되지 않았습니다.');
+?>
---
+++ slovenian/releases/2.3.1/upgrade/cnt-2.2.x-to-2.3.0/language/english.php
@@ -0,0 +1,10 @@
+<?php
+// $Id: english.php 2024 2008-08-31 03:24:12Z phppp $
+define( "_CONFIRM_UPGRADE_220", "
+The upgrade scripts will migrate data from existent 'profile' module.<br />
+Please don't uninstall the existent 'profile' module manually, otherwise corresponding data won't be migrated.<br /><br />
+Once the upgrade process is fully completed please <stong>go to module administration area to updade 'profile' module</strong>, by which profile data will be migrated completely.<br /><br />
+To cancel the upgrade, <a href='index.php'>click here</a>.<br /><br />
+Or to <a href='index.php?action=next&upd220=ok'>proceed</a>.
+");
+?>
