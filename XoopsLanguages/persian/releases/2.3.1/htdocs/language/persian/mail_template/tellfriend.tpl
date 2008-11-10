5b05b0ea8424f4babfd424ea1974f10e94aab907
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2008-11-10 23:31:36 +0000

    Persian/Farsi Language Files for XOOPS 2.3.1

---
+++ XoopsLanguages/persian/releases/2.3.1/htdocs/language/persian/mail_template/tellfriend.tpl
@@ -0,0 +1,25 @@
+Farsi:
+agar matne farsi ra nemibinid baraye didane matne farsi rooye in safhe click rast konid va
+Encoding --> Unicode(UTF-8) ra entekhab konid.
+سلام {FRIEND_NAME}
+دوست شما {YOUR_NAME} لینک سایت ما را برای شما فرستاده و دیدن آن را به شما توصیه کرده است:
+
+نام سایت: {SITENAME}
+آدرس سایت: {SITEURL}
+
+-----------
+{SITENAME} ({SITEURL})
+مدیر سایت
+پست الکترونیکی مدیر سایت: {ADMINMAIL}
+================================================================================
+English:
+Hello {FRIEND_NAME},
+Your friend {YOUR_NAME} liked our site and wanted to show it to you.
+
+Site Name: {SITENAME}
+Site URL:  {SITEURL}
+
+-----------
+{SITENAME} ({SITEURL})
+webmaster
+{ADMINMAIL}
---
+++ XoopsLanguages/persian/releases/2.3.1/htdocs/modules/thadmin/language/persian/admin.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @copyright   The Xoops Project http://sourceforge.net/projects/xoops/
+ * @license     http://www.gnu.org/licenses/gpl.txt GNU General Public License (GPL)
+ * @package     ThAdmin
+ * @subpackage  language
+ */
+
+// General
+define('_AD_TH_PREFERENCES', 'ویژگی ها');
+define('_AD_TH_UPDATE', 'به روز کردن');
+define('_AD_TH_TITLE', 'مدیریت'); 
+define('_AD_TH_NAVIGATION', 'ناوبری');
+define('_AD_TH_ADMINHOME', 'صفحه اصلی مدیریت');
+define('_AD_TH_SITEHOME', 'صفحه اول سایت');
+define('_AD_TH_LINKS', 'لینک های مفید');
+define('_AD_TH_LINKS_XOOPSPROJECT', 'پروژه زوپس');
+define('_AD_TH_LINKS_XOOPSCOMMUNITY', 'وب سایت زوپس');
+define('_AD_TH_LINKS_LOCAL', 'پشتیبانی های محلی');
+define('_AD_TH_LINKS_FORXOOPS', 'ForXoops');
+define('_AD_TH_LOGOUT', 'خروج');
+define('_AD_TH_MODULES', 'ماژول ها');
+define('_AD_TH_MODULEADMIN' , 'مدیریت ماژول ها');
+define('_AD_TH_BLOCKS', 'بلاک ها');
+define('_AD_TH_BLOCKADMIN', 'مدیریت بلاک ها');
+define('_AD_TH_USERS', 'کاربران');
+define('_AD_TH_MISCS', 'ابزار ها');
+
+// CPanel Description
+define('_MD_CPANEL_OVERVIEW_DESC', 'پیکربندی سیستم خود را ببینید');
+define('_MD_CPANEL_PHPEXTENSIONS_DESC', 'افزونه های php در حال کار را ببینید');
+define('_MD_AM_PREF_DESC', 'تمام تنظیمات پیشفرض برای سایت شما و ماژول های نصب شده');
+define('_AD_TH_MODULES_DESC', 'مدیریت تمام ماژول های نصب شده');
+define('_AD_TH_BLOCKS_DESC', 'مدیریت تمتم تنظیمات بلاک ها');
+define('_AD_TH_USERS_DESC', 'مدیریت تمام ابزار های کاربران');
+define('_AD_TH_MISCS_DESC', 'مدیریت تمام ابزار ها');
+// Index
+define('_THADMIN_NAME', 'نام:');
+define('_THADMIN_AUTHOR', 'سازنده:');
+define('_THADMIN_VERSION', 'نسخه:');
+define('_THADMIN_ACTIVE', 'فعال:');
+// Upload form
+define('_THADMIN_UPLOAD', 'بارگذاری ماژول جدید');
+define('_THADMIN_UPLOAD_FILE', 'انتخاب فایل قالب');
+define('_THADMIN_UPLOAD_DIRNAME', 'نام شاخه قالب');
+define('_THADMIN_UPLOAD_INSTALL', 'بعد از بارگذاری نصب کن');
+define('_THADMIN_UPLOAD_INSTALLATION', 'مراحل نصب');
+define('_THADMIN_UPLOAD_UPLOADED', 'قالب جدید را میتوانید هم اکنون نصب کنید');
+define('_THADMIN_UPLOAD_ERROR', 'هیچ فایلی بارگذاری نشده است');
+?>
