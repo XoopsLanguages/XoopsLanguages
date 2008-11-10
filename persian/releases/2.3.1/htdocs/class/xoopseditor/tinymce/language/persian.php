11179a85033287a66bf3d29cb4dac82b3fce5a5d
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2008-11-10 23:31:36 +0000

    Persian/Farsi Language Files for XOOPS 2.3.1

---
+++ persian/releases/2.3.1/htdocs/class/xoopseditor/tinymce/language/persian.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ *  TinyMCE adapter for XOOPS
+ *
+ * @copyright   The XOOPS project http://www.xoops.org/
+ * @license     http://www.fsf.org/copyleft/gpl.html GNU public license
+ * @package		core
+ * @subpackage	xoopseditor
+ * @since       2.3.0
+ * @author		Taiwen Jiang <phppp@users.sourceforge.net>
+ * @version		$Id$
+ */
+/*
+ * Assocated with editor_registry.php
+ */
+define("_XOOPS_EDITOR_TINYMCE", "ویرایشگر TinyMCE");
+define("_XOOPS_EDITOR_TINYMCE_LANGUAGE", "fa");
+// FONT LIST, FORMAT: "Name=value1,value2;Name=value"
+define("_XOOPS_EDITOR_TINYMCE_FONTS", "Arial=arial,helvetica,sans-serif;Courier New=courier new,courier,monospace;Georgia=georgia,times new roman,times,serif;Tahoma=tahoma,arial,helvetica,sans-serif;Times New Roman=times new roman,times,serif;Verdana=verdana,arial,helvetica,sans-serif;Impact=impact;WingDings=wingdings");
+?>
---
+++ persian/releases/2.3.1/htdocs/modules/system/language/persian/admin.php
@@ -0,0 +1,33 @@
+<?php
+// $Id: admin.php 1502 2008-04-26 11:52:21Z phppp $
+// Add Persian Language by Persain Xoops Project www.irxoops.org
+//%%%%%%	File Name  admin.php 	%%%%%
+define('_MD_AM_DBUPDATED','پایگاه داده با موفقیت به روز شد!');
+define('_MD_AM_DBERROR', 'پایگاه‌های‌داده به دلیل بعضی خطا‌ها به روز نشد!');
+
+define('_MD_AM_CONFIG','پیکربندی سیستم');
+
+
+// Admin Module Names
+define('_MD_AM_ADGS','گروه‌ها');
+define('_MD_AM_BANS','بنر‌ها');
+define('_MD_AM_BKAD','بلاک‌ها');
+define('_MD_AM_MDAD','ماژول‌ها');
+define('_MD_AM_SMLS','لبخندک‌ها');
+define('_MD_AM_RANK','نشان‌های اعضا(Ranks)');
+define('_MD_AM_USER','ویرایش اعضا');
+define('_MD_AM_FINDUSER', 'جستجوی اعضا');
+define('_MD_AM_PREF','ویژگی ها');
+define('_MD_AM_VRSN','نسخه');
+define('_MD_AM_MLUS', 'ارسال پیام به اعضا (mail users)');
+define('_MD_AM_IMAGES', 'مدیریت تصاویر(Image Manager)');
+define('_MD_AM_AVATARS', 'تصویر اعضا(Avatars)');
+define('_MD_AM_TPLSETS', 'تمپلیت‌ها');
+define('_MD_AM_COMMENTS', 'نظر‌ها');
+
+// Group permission phrases
+define('_MD_AM_PERMADDNG', 'سطح دسترسی %s برای %s برای گروه %s اضافه نشد');
+define('_MD_AM_PERMADDOK','سطح دسترسی %s برای %s برای گروه %s اضافه شد');
+define('_MD_AM_PERMRESETNG','سطح دسترسی گروه ها برای ماژول %s ریست نشد');
+define('_MD_AM_PERMADDNGP', 'تمام گزینه‌های والد باید انتخاب شوند');
+?>
