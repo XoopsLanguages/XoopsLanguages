74275af6043de0736b34e5c93d4333bad4bbe86e
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2008-11-10 09:00:36 +0000

    Language Files for XOOPS 2.3.1

---
+++ chinese/releases/2.3.1_utf8/htdocs/class/xoopseditor/tinymce/language/schinese_utf8.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ *  TinyMCE adapter for XOOPS
+ *
+ * @copyright   The XOOPS project http://www.xoops.org/
+ * @license     http://www.fsf.org/copyleft/gpl.html GNU public license
+ * @package		core
+ * @subpackage	xoopseditor
+ * @since       2.3.0
+ * @author		Taiwen Jiang <phppp@users.sourceforge.net>
+ * @version		$Id: schinese_utf8.php 2154 2008-09-22 02:38:32Z phppp $
+ */
+/*
+ * Assocated with editor_registry.php
+ */
+define("_XOOPS_EDITOR_TINYMCE", "可视化TinyMCE编辑器");
+
+// The value must be the same as /tinymce/jscripts/langs/your_language_code, for example, "en" for English, "fr" for French
+// For details, check http://tinymce.moxiecode.com/download_i18n.php
+define("_XOOPS_EDITOR_TINYMCE_LANGUAGE", "zh");
+
+// FONT LIST, FORMAT: "Name=value1,value2;Name=value"
+define("_XOOPS_EDITOR_TINYMCE_FONTS", "黑体=黑体;宋体=宋体;楷体=楷体_GB2312;Arial=arial,helvetica,sans-serif;Courier New=courier new,courier,monospace;Georgia=georgia,times new roman,times,serif;Tahoma=tahoma,arial,helvetica,sans-serif;Times New Roman=times new roman,times,serif;Verdana=verdana,arial,helvetica,sans-serif;Impact=impact;WingDings=wingdings");
+
+?>
---
+++ chinese/releases/2.3.1_utf8/htdocs/modules/system/language/schinese_utf8/admin/avatars.php
@@ -0,0 +1,12 @@
+<?php
+// $Id
+//%%%%%% Avatar Manager %%%%%
+define('_MD_AVATARMAN','头像管理');
+
+define('_MD_SYSAVATARS','系统自带头像');
+define('_MD_CSTAVATARS','用户自定义头像');
+define('_MD_ADDAVT','添加头像');
+define('_MD_USERS','有用户正在使用此头像');
+define('_MD_RUDELIMG','你确定要删除此头像？');
+define('_MD_FAILDEL', '未能删除 %s ');
+?>
