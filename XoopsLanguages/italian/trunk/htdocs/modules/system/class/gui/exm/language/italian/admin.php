8312f0a20c13bb07a14264668bc0ffc0d8396b77
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2008-11-11 06:35:35 +0000

    Updating Directory Structure

---
+++ XoopsLanguages/italian/trunk/htdocs/modules/system/class/gui/exm/language/italian/admin.php
@@ -0,0 +1,10 @@
+<?php
+// $Id$
+
+define('_AD_SYSOPTIONS','Opzioni di sistema');
+define('_AD_INSTALLEDMODULES','Moduli installati');
+
+define('_AD_XOOPSTHEMES','Temi di XOOPS');
+define('_AD_INTERESTSITES','Link');
+
+?>
