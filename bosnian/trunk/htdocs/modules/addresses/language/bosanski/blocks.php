d90bdb16d010e482462e38fe0caa2b94df7c55a1
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2008-11-12 01:07:33 +0000

    Updating Directory Structure for Bosnian module translations

--- bosnian/trunk/htdocs/modules/addresses/language/bosanski/blocks.php
+++
@@ -1,8 +0,0 @@
-<?php
-// $Id: blocks.php,v 1.6 2003/02/15 23:08:34 w4z004 Exp $
-// Blocks
-define("_MB_MYADDRESSES_DISP","Prika&#382;i");
-define("_MB_MYADDRESSES_LINKS","Adrese");
-define("_MB_MYADDRESSES_CHARS","Du&#382;ina naslova");
-define("_MB_MYADDRESSES_LENGTH"," slova(oznaka)");
-?>
