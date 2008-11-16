0e4faaef980fbda974df65497811ceeb3ea28560
mujak <mujak@c17157d5-08f2-41e7-a131-df055f6a0da3>
2008-11-16 17:07:19 +0000

    Shoutbox 4.03 new language

--- XoopsLanguages/bosnian/modules/shoutbox/language/bosanski/main.php
+++ XoopsLanguages/bosnian/modules/shoutbox/language/bosanski/main.php
@@ -1,3 +1,63 @@
+<<<<<<< .mine
+<?php
+//  ------------------------------------------------------------------------ //
+//                XOOPS - PHP Content Management System                      //
+//                    Copyright (c) 2000 XOOPS.org                           //
+//                       <http://www.xoops.org/>                             //
+//  ------------------------------------------------------------------------ //
+//  This program is free software; you can redistribute it and/or modify     //
+//  it under the terms of the GNU General Public License as published by     //
+//  the Free Software Foundation; either version 2 of the License, or        //
+//  (at your option) any later version.                                      //
+//                                                                           //
+//  You may not change or alter any portion of this comment or credits       //
+//  of supporting developers from this source code or any supporting         //
+//  source code which is considered copyrighted (c) material of the          //
+//  original comment or credit authors.                                      //
+//                                                                           //
+//  This program is distributed in the hope that it will be useful,          //
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of           //
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            //
+//  GNU General Public License for more details.                             //
+//                                                                           //
+//  You should have received a copy of the GNU General Public License        //
+//  along with this program; if not, write to the Free Software              //
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA //
+//  ------------------------------------------------------------------------ //
+//  Original Author: Alphalogic <alphafake@hotmail.com>					     //
+//  Original Author Website: http://www.alphalogic-network.de		         //
+//  ------------------------------------------------------------------------ //
+//  XOOPS Version made by: (XOOPS 1.3.x and 2.0.x version)			         //
+//  Jan304 <http://www.jan304.org>									         //
+//  ------------------------------------------------------------------------ //
+//  Author:     tank                                                         //
+//  Website:    http://www.customvirtualdesigns.com                          //
+//  E-Mail:     tanksplace@comcast.net                                       //
+//  Date:       10/05/2008                                                   //
+//  Module:     Shoutbox                                                     //
+//  File:       language/english/main.php                                    //
+//  Version:    4.01                                                         //
+//  ------------------------------------------------------------------------ //
+//  File:       language/bosanski/main.php                                   //
+//  Preveo na Bosanski: Saba* info@g-orahovica.com* http://g-orahovica.com*  //
+//  Change Log                                                               //
+//  ***                                                                      //
+//  Version 4.01 Initial CVD Release 10/05/2008                              //
+//  ***                                                                      //
+
+//%%%%%%	File Name popup.php 	%%%%%
+define("_MD_SHOUTBOX_POPUP_ENTERNAME","Upi&scaron;i Nik Ime: ");
+define("_MD_SHOUTBOX_POPUP_FORCE","Natjerati: ");
+define("_MD_SHOUTBOX_POPUP_USE_AUTOREFRESH","Auto-Aktulizacija? ");
+define("_MD_SHOUTBOX_POPUP_REFRESHTIME","Vrijeme Aktuliziranja: ");
+define("_MD_SHOUTBOX_POPUP_CONNECT","Spoji se");
+define("_MD_SHOUTBOX_POPUP_SHOUT","Po&scaron;alji");
+define("_MD_SHOUTBOX_POPUP_TITLE","Live Chat");
+define("_MD_SHOUTBOX_POPUP_CONNECTED",": spojen(a) kao ");
+define("_MD_SHOUTBOX_POPUP_REFRESH", "Aktuliziraj");
+define("_MD_SHOUTBOX_POPUP_NOSHOUTS", "Bez poruke!");
+define("_MD_SHOUTBOX_POPUP_AUTOFOCUS", "Auto-Fokus");
+?>  =======
 <?php
 //  ------------------------------------------------------------------------ //
 //                XOOPS - PHP Content Management System                      //
@@ -57,4 +117,4 @@ define("_MD_SHOUTBOX_POPUP_CONNECTED",": prijavljeni kao ");
 define("_MD_SHOUTBOX_POPUP_REFRESH", "Aktuliziraj");
 define("_MD_SHOUTBOX_POPUP_NOSHOUTS", "Poni&scaron;ti Poruku!");
 define("_MD_SHOUTBOX_POPUP_AUTOFOCUS", "Auto-Fokus");
-?>
+?>>>>>>>> .r2418
