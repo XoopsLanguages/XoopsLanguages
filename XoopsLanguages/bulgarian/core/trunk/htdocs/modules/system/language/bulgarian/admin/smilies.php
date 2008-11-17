81e4f20b1289dc3cb87f00d2191e4c2bb00eecb9
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2008-11-17 07:43:26 +0000

    Bulgarian Language Files for XOOPS 2.3.1

---
+++ XoopsLanguages/bulgarian/core/trunk/htdocs/modules/system/language/bulgarian/admin/smilies.php
@@ -0,0 +1,22 @@
+<?php
+// $Id: smilies.php 2026 2008-08-31 08:10:34Z phppp $
+//%%%%%%	Admin Module Name  Smilies 	%%%%%
+define('_AM_DBUPDATED',_MD_AM_DBUPDATED);
+
+define('_AM_SMILESCONTROL', 'Емотикони');
+define('_AM_CODE', 'Код');
+define('_AM_SMILIE', 'Емоция');
+define('_AM_ACTION', 'Действие');
+define('_AM_EDIT', 'Промяна');
+define('_AM_DEL', 'Изтриване');
+define('_AM_CNRFTSD', 'Невъзможно зареждане на емотиконите от базата.');
+define('_AM_ADDSMILE', 'Добавяне на емотикона');
+define('_AM_EDITSMILE', 'Промяна на емотикона');
+define('_AM_SMILECODE', 'Код за емотиконата:');
+define('_AM_SMILEURL', 'URL към снимка:');
+define('_AM_SMILEEMOTION', 'Описание:');
+define('_AM_ADD', 'Добавняне');
+define('_AM_SAVE', 'Запис');
+define('_AM_WAYSYWTDTS', 'ВНИМАНИЕ: Сигурни ли сте, че искате да изтриете снимката?');
+define('_AM_DISPLAYF', 'Да показва във форма?');
+?>
