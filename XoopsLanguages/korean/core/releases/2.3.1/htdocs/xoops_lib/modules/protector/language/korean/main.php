6e457ddd644dd7b30440bfed0487e17e5a012f20
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2008-11-18 16:37:36 +0000

    Updating Directory Structure

--- XoopsLanguages/korean/core/releases/2.3.1/htdocs/xoops_lib/modules/protector/language/korean/main.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define('_MD_PROTECTOR_YOUAREBADIP','님의 IP는 Protector모듈에 의해 Bad IP로 등록되어졌습니다.') ;
-define('_MD_PROTECTOR_TURNJAVASCRIPTON','JavaScript기능을 ON으로 설정') ;
-define('_MD_PROTECTOR_DENYBYRBL','님의 IP가 RBL에 등록되어져 있기때문에 Protector모듈이 님의 투고를 막았습니다.') ;
-
-?>
