8c79a865ef60be6c6cb10cb56e94daf98b28e04c
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2008-11-18 17:01:44 +0000

    Updating Directory Structure

--- persian/core/releases/2.3.1/htdocs/xoops_lib/modules/protector/language/persian/main.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define('_MD_PROTECTOR_YOUAREBADIP','شما توسط سیستم حفاظتی سایت در قسمت IP های غیر مجاز قرار گرفته اید.') ;
-define('_MD_PROTECTOR_TURNJAVASCRIPTON','JavaScript را روشن کنید') ;
-define('_MD_PROTECTOR_DENYBYRBL','سیستم حفاظتی سایت پست شما را قبول نمیکند, به این دلیل که IP شما در لیست IP های غیر مجاز قرار گرفته است') ;
-
-?>
