9008b650568059ee41508823354103a528f4ab63
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2008-11-18 22:36:13 +0000

    Updating Directory Structure

--- german/core/releases/2.3.1/htdocs/xoops_lib/modules/protector/language/german/main.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define('_MD_PROTECTOR_YOUAREBADIP','Diese IP wurden als Böse-IP vom Protector registriert.') ;
-define('_MD_PROTECTOR_TURNJAVASCRIPTON','JavaScript einschalten') ;
-define('_MD_PROTECTOR_DENYBYRBL','Protector hat den Beitrag gesperrt da die verwendete IP auf der Ausschlußliste steht') ;
-
-?>
