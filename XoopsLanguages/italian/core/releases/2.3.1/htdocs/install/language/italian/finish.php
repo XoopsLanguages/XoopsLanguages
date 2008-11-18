7a84f1490672c6f69393858e120367360cf05aed
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2008-11-18 16:25:03 +0000

    Updating Directory Structure

--- XoopsLanguages/italian/core/releases/2.3.1/htdocs/install/language/italian/finish.php
+++
@@ -1,9 +0,0 @@
-<?php
-// $Id: finish.php 2055 2008-09-05 06:26:48Z phppp $
-$content .=
-"<h3>Il tuo sito</h3>
-<p>Adesso puoi accedere alla  <a href='../index.php'>home page del tuo sito</a>.</p>
-<h3>Supporto</h3>
-<p>Visita <a href='http://xoops.sourceforge.net/' rel='external'>The XOOPS Project</a></p>
-";
-?>
