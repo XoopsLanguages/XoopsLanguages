8595a54fa8a0ed1f5228e05b61dc059a7d7eef52
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2008-11-18 22:04:18 +0000

    Updating Directory Structure

--- norwegian/core/releases/2.3.1/htdocs/xoops_lib/modules/protector/language/norwegian/main.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define('_MD_PROTECTOR_YOUAREBADIP','Du er registrert med en skadelig IP-adresse!') ;
-define('_MD_PROTECTOR_TURNJAVASCRIPTON','Slå JavaScript På') ;
-define('_MD_PROTECTOR_DENYBYRBL','Protector avviste ditt innlegg, da din IP-adresse er registrert i RBL') ;
-
-?>
