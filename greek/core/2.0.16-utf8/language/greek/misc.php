ebe1c3339bcc1ee3790c254ce751ee3b92b7feb3
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2008-11-19 02:43:20 +0000

    Greek Language Files for XOOPS 2.0.16

---
+++ greek/core/2.0.16-utf8/language/greek/misc.php
@@ -0,0 +1,30 @@
+<?php
+// $Id: misc.php,v 1.6 2003/02/12 11:36:33 okazu Exp $
+
+// Greek Translation by Yannis yannis@xoopsgreece.gr 27/11/05
+// reviewed by angelos@xoopsgreece.gr 15/03/2006
+// with Xoops Greece Translation Team (www.xoopsgreece.gr)
+
+define("_MSC_YOURNAMEC","Το όνομά σου: ");
+define("_MSC_YOUREMAILC","Το Email σου: ");
+define("_MSC_FRIENDNAMEC","Όνομα Φίλου: ");
+define("_MSC_FRIENDEMAILC","Email Φίλου: ");
+define("_MSC_RECOMMENDSITE","Πρότεινε αυτή τη σελίδα σε κάποιο φίλο");
+
+// %s is your site name
+define("_MSC_INTSITE","Μια ενδιαφέρουσα σελίδα: %s");
+define("_MSC_REFERENCESENT","Ευχαριστούμε, οι πληροφορίες για τη σελίδα μας στάλθηκαν στον φίλο σου!");
+define("_MSC_ENTERYNAME","Παρακαλούμε πληκτρολογίστε το όνομά σας");
+define("_MSC_ENTERFNAME","Παρακαλούμε πληκτρολογίστε το όνομα του φίλου σας");
+define("_MSC_ENTERFMAIL","Παρακαλούμε πληκτρολογίστε το email του φίλου σας");
+define("_MSC_NEEDINFO","Πρέπει να καταχωρήσετε τις υποχρεωτικές πληροφορίες!");
+define("_MSC_INVALIDEMAIL1","Το email που δώσατε δεν είναι έγκυρο.");
+define("_MSC_INVALIDEMAIL2","Παρακαλώ ελέγξτε τη διεύθυνση και ξαναπροσπαθήστε.");
+
+define("_MSC_AVAVATARS","Διαθέσιμα Εικονίδια");
+
+define("_MSC_SMILIES","Φατσούλες (Smilies)");
+define("_MSC_CLICKASMILIE","Κάντε κλικ σε κάποια φατσούλα για να την εισάγετε στο μήνυμά σας.");
+define("_MSC_CODE","Κωδικάς (Code)"); //Κώδικας
+define("_MSC_EMOTION","Emotion");
+?>
---
+++ greek/core/2.0.16-utf8/modules/system/language/greek/blocks.php
@@ -0,0 +1,45 @@
+<?php
+// $Id: blocks.php,v 1.15 2005/08/08 06:29:39 skalpa Exp $
+// Blocks
+define("_MB_SYSTEM_ADMENU","Μενού Διαχειριστή");
+define("_MB_SYSTEM_RNOW","Εγγραφή!");
+define("_MB_SYSTEM_LPASS","Ξεχάσατε το Password;");
+define("_MB_SYSTEM_SEARCH","Αναζήτηση");
+define("_MB_SYSTEM_ADVS","Προχωρημένη Αναζήτηση");
+define("_MB_SYSTEM_VACNT","Προβολή Λογαριασμού");
+define("_MB_SYSTEM_EACNT","Επεξεργασία Λογαριασμού");
+// RMV-NOTIFY
+define("_MB_SYSTEM_NOTIF", "Ειδοποιήσεις");
+define("_MB_SYSTEM_LOUT","Αποσύνδεση");
+define("_MB_SYSTEM_INBOX","Μηνύματα");
+define("_MB_SYSTEM_SUBMS","Submitted News");
+define("_MB_SYSTEM_WLNKS","Waiting Links");
+define("_MB_SYSTEM_BLNK","Broken Links");
+define("_MB_SYSTEM_MLNKS","Modified Links");
+define("_MB_SYSTEM_WDLS","Waiting Downloads");
+define("_MB_SYSTEM_BFLS","Broken Files");
+define("_MB_SYSTEM_MFLS","Modified Downloads");
+define("_MB_SYSTEM_HOME","Αρχική Σελίδα"); // link to home page in main menu block
+define("_MB_SYSTEM_RECO","Προτείνετέ μας");
+define("_MB_SYSTEM_PWWIDTH","Pop-Up Window Width");
+define("_MB_SYSTEM_PWHEIGHT","Pop-Up Window Height");
+define("_MB_SYSTEM_LOGO","Logo image file under %s directory");  // %s is your root image directory name
+define("_MB_SYSTEM_COMPEND", "Σχόλια");
+
+//define("_MB_SYSTEM_LOGGEDINAS", "Logged in as");
+define("_MB_SYSTEM_SADMIN","Show admin groups");
+define("_MB_SYSTEM_SPMTO","Send Private Message to %s");
+define("_MB_SYSTEM_SEMTO","Send Email to %s");
+
+define("_MB_SYSTEM_DISPLAY","Display %s members");
+define("_MB_SYSTEM_DISPLAYA","Display member avatars");
+define("_MB_SYSTEM_NODISPGR","Do not display users whose rank is:");
+
+define("_MB_SYSTEM_DISPLAYC","Display %s comments");
+define("_MB_SYSTEM_SECURE", "Secure Login");
+
+define("_MB_SYSTEM_NUMTHEME", "%s themes");
+define("_MB_SYSTEM_THSHOW", "Display screenshot image");
+define("_MB_SYSTEM_THWIDTH", "Screenshot image width");
+//define('_MB_SYSTEM_REMEMBERME', 'Remember me');
+?>
