11453bf52ca337987f3902b55bdfeeb63401a29a
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2008-11-19 02:43:20 +0000

    Greek Language Files for XOOPS 2.0.16

---
+++ XoopsLanguages/greek/core/2.0.16-utf8/language/greek/admin.php
@@ -0,0 +1,20 @@
+<?php
+// $Id: admin.php,v 1.5 2003/02/12 11:35:45 okazu Exp $
+//%%%%%%	File Name  admin.php 	%%%%%
+
+// Greek Translation by angelos@xoopsgreece.gr 15/03/2006
+// with Xoops Greece Translation Team (www.xoopsgreece.gr)
+
+define("_AD_NORIGHT","Δεν έχετε δικαίωμα πρόσβασης σε αυτή τη περιοχή ");
+define("_AD_ACTION","Action");
+define("_AD_EDIT","Επεξεργασία");
+define("_AD_DELETE","Διαγραφή");
+define("_AD_LASTTENUSERS","Οι τελευταίοι 10 εγγεγραμμένοι χρήστες");
+define("_AD_NICKNAME","Ψευδώνυμο");
+define("_AD_EMAIL","Email");
+define("_AD_AVATAR","Εικονίδιο");
+define("_AD_REGISTERED","Εγγεγραμμένος από"); //Registered Date
+define('_AD_PRESSGEN', 'Αυτή είναι η πρώτη φορά που κάνετε χρήση του τομέα διαχείρισης. Κάντε click στο κουμπί για να προχωρήσετε.');
+define('_AD_LOGINADMIN', 'Πραγματοποιείται η σύνδεση σας...');
+
+?>
---
+++ XoopsLanguages/greek/core/2.0.16-utf8/language/greek/pmsg.php
@@ -0,0 +1,48 @@
+<?php
+// $Id: pmsg.php,v 1.7 2004/12/26 19:11:54 onokazu Exp $
+//%%%%%%	File Name readpmsg.php 	%%%%%
+
+// Greek Translation by angelos@xoopsgreece.gr 15/03/2006
+// with Xoops Greece Translation Team (www.xoopsgreece.gr)
+
+define("_PM_DELETED","Το μήνυμα σας (ή τα μηνύματα σας) έχουν διαγραφεί");
+define("_PM_PRIVATEMESSAGE","Προσωπικά Μηνύματα");
+define("_PM_INBOX","Εισερχόμενα");
+define("_PM_FROM","Από");
+define("_PM_YOUDONTHAVE","Δεν έχετε κανένα Προσωπικό Μήνυμα");
+define("_PM_FROMC","Από: ");
+define("_PM_SENTC","Ημερομηνία αποστολής: "); // The date of message sent
+define("_PM_PROFILE","Προφίλ");
+
+// %s is a username
+define("_PM_PREVIOUS","Προηγούμενο μήνυμα");
+define("_PM_NEXT","Επόμενο μήνυμα");
+
+//%%%%%%	File Name pmlite.php 	%%%%%
+define("_PM_SORRY","Συγνώμη αλλά δεν είστε εγγεγραμμένος χρήστης του δυκτιακού τόπου μας.");
+define("_PM_REGISTERNOW","Γραφτείτε τώρα!");
+define("_PM_GOBACK","Επιστροφή");
+define("_PM_USERNOEXIST","Ο Χρήστης που επιλέξατε δεν υπάρχει στη βάση δεδομένων μας.");
+define("_PM_PLZTRYAGAIN","Παρακαλώ ελέγξτε το όνομα και προσπαθήστε ξανά.");
+define("_PM_MESSAGEPOSTED","Το μήνυμά σας εστάλη");
+define("_PM_CLICKHERE","Μπορείτε να πατήσετε εδώ για να δείτε τα Προσωπικά σας Μηνύματα");
+define("_PM_ORCLOSEWINDOW","ή πατήστε εδώ για να κλείσει το παράθυρο.");
+define("_PM_USERWROTE","Ο/Η %s έγραψε:");
+define("_PM_TO","Προς: ");
+define("_PM_SUBJECTC","Θέμα: ");
+define("_PM_MESSAGEC","Μήνυμα: ");
+define("_PM_CLEAR","Σβήσιμο");
+define("_PM_CANCELSEND","Ακύρωση αποστολής");
+define("_PM_SUBMIT","Υποβολή");
+
+//%%%%%%	File Name viewpmsg.php 	%%%%%
+define("_PM_SUBJECT","Θέμα");
+define("_PM_DATE","Ημερομηνία");
+define("_PM_NOTREAD","Δεν έχει διαβαστεί");
+define("_PM_SEND","Αποστολή");
+define("_PM_DELETE","Διαγραφή");
+define("_PM_REPLY", "Απάντηση");
+define("_PM_PLZREG","Σας παρακαλούμε να γραφτείτε πρώτα για να μπορέσετε να στείλετε προσωπικά μηνύματα!");
+
+define("_PM_ONLINE", "Online");
+?>
