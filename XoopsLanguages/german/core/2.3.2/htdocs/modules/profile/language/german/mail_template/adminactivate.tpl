cc956f6f1612fb933186eb179bff377fe768720a
dhcst <dhcst@c17157d5-08f2-41e7-a131-df055f6a0da3>
2008-11-27 08:51:17 +0000

    made a copy

---
+++ XoopsLanguages/german/core/2.3.2/htdocs/modules/profile/language/german/mail_template/adminactivate.tpl
@@ -0,0 +1,12 @@
+Hallo {X_UNAME},
+
+der neue User {USERNAME} ({USEREMAIL}) 
+hat sich soeben auf {SITENAME} registrieren lassen.
+Klicken Sie bitte auf den folgenden Link um den 
+Account des Users zu aktivieren:
+
+{USERACTLINK}
+
+-----------
+{SITENAME} ({SITEURL}) 
+Der Administrator
---
+++ XoopsLanguages/german/core/2.3.2/htdocs/modules/system/language/german/admin/users.php
@@ -0,0 +1,59 @@
+<?php
+// german v 1.4 2008/10/12 alfred $
+//%%%%%%	Admin Module Name  Users 	%%%%%
+define('_AM_DBUPDATED',_MD_AM_DBUPDATED);
+
+define('_AM_AYSYWTDU','Soll das Mitglied %s wirklich gelöscht werden?');
+define('_AM_BYTHIS','Es werden alle Daten des Mitglieds gelöscht wenn Sie fortfahren.');
+define('_AM_YES','Ja');
+define('_AM_NO','Nein');
+define('_AM_YMCACF','Es müssen alle Felder ausgefüllt werden');
+define('_AM_CNRNU','Neues Mitglied konnte nicht registriert werden.');
+define('_AM_EDEUSER','Mitglied bearbeiten/löschen');
+define('_AM_NICKNAME','Mitgliedsname');
+define('_AM_MODIFYUSER','Mitglied verändern');
+define('_AM_DELUSER','Mitglied löschen');
+define('_AM_GO','Los!');
+define('_AM_ADDUSER','Mitglied hinzufügen');
+define('_AM_NAME','Name');
+define('_AM_EMAIL','E-Mail');
+define('_AM_OPTION','Option');
+define('_AM_AVATAR','Avatar');
+define('_AM_THEME','Theme');
+define('_AM_AOUTVTEAD','E-Mail-Adresse für alle sichtbar');
+define('_AM_URL','Homepage');
+define('_AM_ICQ','ICQ');
+define('_AM_AIM','AIM');
+define('_AM_YIM','YIM');
+define('_AM_MSNM','MSN');
+define('_AM_LOCATION','Wohnort');
+define('_AM_OCCUPATION','Beruf');
+define('_AM_INTEREST','Interessen');
+define('_AM_RANK','Rang');
+define('_AM_NSRA','Kein Spezialrang vorhanden');
+define('_AM_NSRID','Keine Spezialränge in der Datenbank vorhanden');
+define('_AM_ACCESSLEV','Gruppen-Level');
+define('_AM_SIGNATURE','Signatur');
+define('_AM_PASSWORD','Passwort');
+define('_AM_INDICATECOF','* Pflichtfelder die ausgefüllt werden müssen');
+define('_AM_NOTACTIVE','Dieses Mitglied wurde noch nicht aktiviert. Soll das Mitglied aktiviert werden?');
+define('_AM_UPDATEUSER','Mitglied aktualisieren');
+define('_AM_USERINFO','Mitgliedsinfo');
+define('_AM_USERID','Mitglieds-ID');
+define('_AM_RETYPEPD','Passwort wiederholen');
+define('_AM_CHANGEONLY','(nur bei Änderungen)');
+define('_AM_USERPOST','Beiträ;ge des Mitglieds');
+define('_AM_STORIES','Artikel');
+define('_AM_COMMENTS','Kommentare');
+define('_AM_PTBBTSDIYT','Zum Synchronisieren der Beiträge den Button drücken und die Daten werden aktualisiert');
+define('_AM_SYNCHRONIZE','Synchronisieren');
+define('_AM_USERDONEXIT','Mitglied existiert nicht!');
+define('_AM_STNPDNM','Falsches Passwort eingegeben, gehen Sie zurück und versuchen Sie es erneut');
+define('_AM_CNGTCOM','Konnte nicht die Gesamtzahl der Kommentare ermitteln');
+define('_AM_CNGTST','Konnte nicht die Gesamtzahl der Artikel ermitteln');
+define('_AM_CNUUSER','Mitglied kann nicht aktualisiert werden');
+define('_AM_CNGUSERID','Konnte Mitglieds-ID nicht finden');
+define('_AM_LIST','Auflisten');
+define('_AM_NOUSERS', 'Kein Mitglied ausgewählt');
+define("_AM_CNRNU2","Der User kann nicht zur Gruppe %s hinzugef�gt werden.");
+?>
