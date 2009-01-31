cb6712559fa2aa757192a9600683fe3b43521f07
mujak <mujak@c17157d5-08f2-41e7-a131-df055f6a0da3>
2009-01-31 22:39:26 +0000

    news 1.63 language bosanski

---
+++ bosnian/modules/language/bosanski/newsletter.php
@@ -0,0 +1,17 @@
+<?php
+$newslettertemplate=<<<contentend
+Naslov: %title%
+Tema: %topic_title%
+Napisao: %publisher%
+Objavljena: %published%
+Ocjena: %rating%
+Citana: %reads%
+
+Sadrzaj:
+%hometext%
+
+Procitaj vise: %link%
+ ----------------------------------------------------------------------------
+
+contentend;
+?>
