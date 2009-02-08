bb8433f5a9c7f345b6f0718442da6823e6c1272e
dhcst <dhcst@c17157d5-08f2-41e7-a131-df055f6a0da3>
2009-02-08 12:10:33 +0000

    upgrade lang files for installer

--- german/core/2.3.3/htdocs/install/language/german/finish.php
+++ german/core/2.3.3/htdocs/install/language/german/finish.php
@@ -1,7 +1,9 @@
 <?php
-// $Id: finish.php 1376 2008-03-05 04:50:32Z phppp $
-// $Id: finish.php german V2.3 08. Juli 2008
-// Code. UTF-8 by Muki
+// $Id: finish.php 2768 2009-02-08 12:40 dhcst $
+// _LANGCODE: de
+// _CHARSET : UTF-8
+// Translator: XOOPS Translation Team
+
 $content .=
 "<h3>Ihre Seite</h3>
 <p>Die Installation sollte erfolgreich gewesen sein, wechseln Sie nun auf Ihre <a href='../index.php'>Startseite</a>.</p>
@@ -9,4 +11,15 @@ $content .=
 <p>Besuchen Sie den Deutschen Support <a href='http://www.myxoops.org'>myXOOPS.org</a>, die englischsprachigen Seiten
 <a href='http://www.xoops.org'>XOOPS.org</a> oder auf Sourceforge <a href='http://xoops.sourceforge.net/'>The XOOPS Project</a>.</p>
 <p>Viel Spass mit Ihrer Installation</p>";
+
+$content .=
+"<h3>Sicherheitskonfiguration</h3>
+<p>Der Installer wird versuchen Ihre Website sicher zu machen. Prüfen Sie folgendes:
+<div class='confirmMsg'>
+Die Datei <em>mainfile.php</em> muss schreibgeschützt sein.<br />
+Entfernen Sie den Ordner <em>{$installer_modified}</em> (oder <em>install</em> wenn dieser nicht automatisch umbenannt wurde) vom Ihrem Server.
+</div>
+</p>
+";
+
 ?>
--- german/core/2.3.3/htdocs/install/language/german/welcome.php
+++ german/core/2.3.3/htdocs/install/language/german/welcome.php
@@ -1,18 +1,21 @@
 <?php
-// $Id: welcome.php 1784 2008-05-26 05:15:28Z phppp $
-// $Id: welcome.php german V2.3 08. Juli 2008
-// Code. UTF-8 by Muki
+// $Id: welcome.php 2717 2009-02-08 12:56 dhcst $
+// _LANGCODE: de
+// _CHARSET : UTF-8
+// Translator: XOOPS Translation Team
 
 $content .= '
-<h3>Was ist XOOPS?</h3>
 <p>
-<b>XOOPS</b> ist ein dynamisches OO (Objekt Orientiertes) Open Source Portalskript geschrieben in PHP. Unterstützt wird es mit einer Anzahl von Datenbanken (aktuell nur mySQL), XOOPS ist somit ein ideales CMS für den Aufbau von kleineren und größeren Communities
+    <acronym title="eXtensible Object-Oriented Portal System">XOOPS</acronym> ist ein 
+    dynamisches OO (Objekt Orientiertes) Open Source Portalskript geschrieben in PHP. 
+    XOOPS ist somit ein ideales CMS für den Aufbau von kleineren und größeren Communities.
 </p>
 <p>
-XOOPS ist freigegeben unter den Bedingungen der <a href="http://www.gnu.org/copyleft/gpl.html">GNU General Public License (GPL)</a> und ist frei zu verwenden und zu ändern.
-Es ist frei, so lange Änderungen, wie Sie durch die Bestimungen der GPL genannt sind, erhalten bleiben.
+    XOOPS ist freigegeben unter den Bedingungen der 
+    <a href="http://www.gnu.org/copyleft/gpl.html">GNU General Public License (GPL)</a> 
+    und ist frei zu verwenden und zu ändern. Es ist frei, so lange Änderungen, 
+    wie Sie durch die Bestimungen der GPL genannt sind, erhalten bleiben.
 </p>
-
 <h3>Anforderungen</h3>
 <ul>
 	<li>WWW Server (<a href="http://www.apache.org/" rel="external">Apache</a>, IIS, Roxen, etc)</li>
@@ -20,14 +23,15 @@ Es ist frei, so lange Änderungen, wie Sie durch die Bestimungen der GPL genannt
 	<li><a href="http://www.mysql.com/" rel="external">MySQL</a> 3.23 or higher (4.1 oder höher empfohlen)</li>
 </ul>
 <h3>Vorbereitungen</h3>
-<ul>
-<li>Setup des HTTP-Servers, PHP und der Datenbankrechte.</li>
-<li>Erstellen Sie eine Datenbank für Ihre XOOPS-Seite.</li>
-<li>Bereiten Sie ein Userkonto vor und vergeben Sie dem User die Rechte zur Datenbank.</li>
-<li>Das Verzeichnis uploads/ sowie die Datei mainfile.php müssen beschreibbar sein.</li>
-<li>Aus Sicherheitsgründen wird empfohlen, die Verzeichnisse <em>xoops_lib</em> und <em>xoops_data</em> ausserhalb des <a href="http://phpsec.org/projects/guide/3.html" rel="external">document root</a> zu platzieren, oder aber zumindest die OrdnerNamen zu ändern.</li>
-<li>Die Verzeichnisse <em>xoops_data/</em>, <em>xoops_data/configs/</em>, <em>xoops_data/caches/</em>, <em>xoops_data/caches/xoops_cache/</em>, <em>xoops_data/caches/smarty_cache/</em> und <em>xoops_data/caches/smarty_compile/</em> müssen beschreibbar sein.</li>
-<li>Schalten Sie Cookies und JavaScript in Ihrem Browser ein.</li>
-</ul>
+<ol>
+    <li>Setup des HTTP-Servers, PHP und der Datenbankrechte.</li>
+    <li>Erstellen Sie eine Datenbank für Ihre XOOPS-Seite.</li>
+    <li>Bereiten Sie ein Userkonto vor und vergeben Sie dem User die Rechte zur Datenbank.</li>
+    <li>Die folgenden Verzeichnisse und Dateien müssen beschreibbar sein: %s</li>
+    <li>Sollten Sie das Modul <em>protector</em> installieren, <div class="confirmMsg">ersetzen Sie die Datei <em>mainfile.dist.php</em> in Ihrem XOOPS Root Verzeichnis mit der <em>mainfile.dist.php.protector</em>.</div></li>
+    <li>Für mehr Sicherheit sollten die folgenden Verzeichnisse ausserhalb des <a href="http://phpsec.org/projects/guide/3.html" rel="external">Document root</a> sein und umbenannt werden: %s</li>
+    <li>Erstellen Sie folgende verzeichnisse und machen diese beschreibbar: %s</li>
+    <li>Schalten Sie Cookies und JavaScript in Ihrem Browser ein.</li>
+</ol>
 ';
 ?>
