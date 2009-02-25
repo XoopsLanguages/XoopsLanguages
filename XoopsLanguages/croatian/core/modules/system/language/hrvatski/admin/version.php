876414b47192c1c9011682ab03a438f0008b0225
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2009-02-25 02:32:43 +0000

    Updating Croatian Directory Structure

--- XoopsLanguages/croatian/core/modules/system/language/hrvatski/admin/version.php
+++
@@ -1,8 +0,0 @@
-<?php
-// $Id: version.php 2 2005-11-02 18:23:29Z skalpa $
-// prijevod na hrvatski: Ivan Kosak (www.xoopshr.org) 14.2.2009
-//%%%%%%	Admin Module Name  Version 	%%%%%
-define("_AM_DBUPDATED",_MD_AM_DBUPDATED);
-
-
-?>
