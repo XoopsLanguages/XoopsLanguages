1259a5e609e0235ffba2635934b284605cb6e665
sadistiko <sadistiko@c17157d5-08f2-41e7-a131-df055f6a0da3>
2009-03-02 10:08:48 +0000

    added videotube 1.84 module traslation

---
+++ XoopsLanguages/croatian/modules UTF-8/videotube 1.84/videotube/language/hrvatski/admin.php
@@ -0,0 +1,189 @@
+<?php
+//
+// _LANGCODE: hr
+// _CHARSET : UTF-8
+// Translator: XoopsHR.org / Dražen Punčec & Ivan Kosak / 2.3.2009.
+//
+//  ------------------------------------------------------------------------ //
+//                XOOPS - PHP Content Management System                      //
+//                    Copyright (c) 2000 XOOPS.org                           //
+//                       <http://www.xoops.org/>                             //
+//  ------------------------------------------------------------------------ //
+//  This program is free software; you can redistribute it and/or modify     //
+//  it under the terms of the GNU General Public License as published by     //
+//  the Free Software Foundation; either version 2 of the License, or        //
+//  (at your option) any later version.                                      //
+//                                                                           //
+//  You may not change or alter any portion of this comment or credits       //
+//  of supporting developers from this source code or any supporting         //
+//  source code which is considered copyrighted (c) material of the          //
+//  original comment or credit authors.                                      //
+//                                                                           //
+//  This program is distributed in the hope that it will be useful,          //
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of           //
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            //
+//  GNU General Public License for more details.                             //
+//                                                                           //
+//  You should have received a copy of the GNU General Public License        //
+//  along with this program; if not, write to the Free Software              //
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA //
+//  ------------------------------------------------------------------------ //
+//  Author:     tank                                                         //
+//  Website:    http://www.customvirtualdesigns.com                          //
+//  E-Mail:     tanksplace@comcast.net                                       //
+//  Date:       01/18/2009                                                   //
+//  Module:     Video Tube                                                   //
+//  File:       languages/english/admin.php                                  //
+//  Version:    1.84                                                         //
+//  ------------------------------------------------------------------------ //
+//  Change Log                                                               //
+//  ***                                                                      //
+//  Version 1.0  Initial Release 04/14/2008                                  //
+//  ***                                                                      //
+//  Version 1.1  04/23/2008                                                  //
+//  Bug Fix: Added status messages for Admin Deny Submission                 //
+//  Note: E-mail contact address change                                      //
+//  ***                                                                      //
+//  Version 1.4  05/21/2008                                                  //
+//  Add new definitions as necessary to support current release              //
+//  ***                                                                      //
+//  Version 1.63  08/23/2008                                                 //
+//  Add new definitions as necessary to support current release              //
+//  ***                                                                      //
+//  Version 1.64  09/06/2008                                                 //
+//  Add new definitions as necessary to support current release              //
+//  ***                                                                      //
+//  Version 1.65  09/12/2008                                                 //
+//  Add new definitions as necessary to support current release              //
+//  ***                                                                      //
+//  Version 1.84  01/18/2009                                                 //
+//  Add new definitions as necessary to support current release              //
+//  ***                                                                      //
+
+define("_AD_VP_ADMINTITLE","Video Tube - Administracija");
+define("_AD_VP_PUBLISHED_HEADER","Video Tube - Lista objavljenih videa");
+define("_AD_VP_SUBMISSION_HEADER","Video Tube - Pregled podnesenih videa");
+define("_AD_VP_PREFERENCES","Prednosti");
+define("_AD_VP_ID_CHEADING","ID");
+define("_AD_VP_CODE_CHEADING","Kod");
+define("_AD_VP_TITLE_CHEADING","Naslov");
+define("_AD_VP_ARTIST_CHEADING","Izvođač");
+define("_AD_VP_CATEGORY_CHEADING","Kategorija");
+define("_AD_VP_SUBMITTER_CHEADING","Podnositelj");
+define("_AD_VP_EDIT_CHEADING","Uredi");
+define("_AD_VP_DELETE_CHEADING","Obriši");
+define("_AD_VP_VIEW_CHEADING","Pregled");
+define("_AD_VP_APPROVE_CHEADING","Odobri");
+define("_AD_VP_DENY_CHEADING","Ospori");
+define("_AD_VP_EDIT","Uredi");
+define("_AD_VP_DELETE","Obriši");
+define("_AD_VP_VIEW","Pregled");
+define("_AD_VP_APPROVE","Odobri");
+define("_AD_VP_DENY","Ospori");
+define("_AD_VP_CANCEL","Odustani");
+define("_AD_VP_DEL_REALLY","Da li sigurno želite obrisati ovaj video?");
+define("_AD_CONFIG","Video Tube postavke");
+define("_AD_GENERALCONF","Prednosti");
+define("_AD_VP_ADDHEADER","Video Tube - Dodaj video");
+define("_AD_VP_EDITHEADER","Video Tube - Uredi video");
+define("_AD_VP_ID","Video ID");
+define("_AD_VP_CODE","Video Kod");
+define("_AD_VP_TITLE","Video Naslov");
+define("_AD_VP_ARTIST","Video Izvođač");
+define("_AD_VP_EMBEDCODE","Unesi kod");
+define("_AD_VP_FULLEMBEDCODE","Unesi cijeli kod");
+define("_AD_VP_DESC","Opis");
+define("_AD_VP_THUMB","Thumb URL");
+define("_AD_VP_SERVICENAME","Ime servisa");
+define("_AD_VP_CATEGORY","Kategorija");
+define("_AD_VP_ADDSUBMITADD","Dodaj video Video");
+define("_AD_VP_ADDSUBMITRESET","Resetiraj");
+define("_AD_VP_ADDSUCCESS","Uspješno dodan video");
+define("_AD_VP_ADDERROR","Greška pri dodavanju vedea");
+define("_AD_VP_APPROVEALLSUCCESS","Uspješno objavljeni video");
+define("_AD_VP_APPROVEALLERROR","Greška pri objavljivanju videa");
+define("_AD_VP_DELSUCCESS","Video uspješno obrisan");
+define("_AD_VP_DELERROR","Greška pri brisanju videa");
+define("_AD_VP_EDITSUBMITADD","Potvrdi promjene");
+define("_AD_VP_EDITSUBMITCANCEL","Odustani");
+define("_AD_VP_EDITSUCCESS","Promjene videa su uspješno snimljene");
+define("_AD_VP_EDITERROR","Greška pri uređivanju videa");
+define("_AD_VP_PUBLISHSUCCESS","Video uspješno objavljen");
+define("_AD_VP_PUBLISHERROR","Greška pri objavljivanja videa");
+define("_AD_VP_DENY_REALLY","Želi te li osporiti ovaj video?");
+define("_AD_VP_DENYSUCCESS","Uspješno osporen video");
+define("_AD_VP_DENYERROR","Greška pri osporavanju videa");
+
+define("_AD_VP_CATEGORY_HEADER","Lista video kategorije");
+define("_AD_VP_CATEDIT","Uredi");
+define("_AD_VP_CATEDITSUBMITADD","Potvri promjene u kategoriji");
+define("_AD_VP_CATEDITSUBMITCANCEL","Odustani");
+define("_AD_VP_CATEDITSUCCESS","Promjene kategorije su uspješno snimljene");
+define("_AD_VP_CATEDITERROR","Greška pri uređivanju kategorije");
+define("_AD_VP_CATTITLE","Naslov kategorije");
+define("_AD_VP_CATEDITHEADER","Video Tube - Uredi kategorije");
+define("_AD_VP_DEL_CAT_REALLY","Sigurno želite obrisati kategoriju?");
+define("_AD_VP_CATREFID","Ref");
+define("_AD_VP_CATCID","Cat ID");
+define("_AD_VP_CATDELDENY","Kategorija mora biti prazna da bi se obrisala.");
+define("_AD_VP_DELCATSUCCESS","Uspješno brisanje kategorije");
+define("_AD_VP_DELCATERROR","Greška pri brisanju kategorije");
+define("_AD_VP_CATREORDERSUCCESS","Uspješna promjena kategorije");
+define("_AD_VP_CATREORDERERROR","Greška pri promjeni kategorije");
+define("_AD_VP_CATADDSUBMITADD","Dodaj kategoriju");
+define("_AD_VP_CATADDSUBMITRESET","Resetiraj");
+define("_AD_VP_CATADDSUCCESS","Uspješno dodavanje kategorije");
+define("_AD_VP_CATADDERROR","Greška pri dodavanju kategorije");
+
+define("_AD_VP_CATADDHEADER","Video Tube - Dodaj novu kategoriju");
+define("_AD_VP_CATMODHEADER","Video Tube - Promijeni kategoriju");
+define("_AD_VP_CATEGORY_OVERVIEW_HEADER","Video Tube - Pregled kategorije");
+define("_AD_VP_CATEGORY_TITLE","Naslov kategorije");
+define("_AD_VP_CATEGORY_WEIGHT","Veličina zaslona");
+define("_AD_VP_PARENT_CATEGORY","Roditeljska kategorija");
+define("_AD_VP_MODIFY","Promijeni");
+define("_AD_VP_MOVE","Pomakni");
+define("_AD_VP_RESET","Resetiraj");
+
+define("_AD_VP_CATPID_CHEADING","PID");
+define("_AD_VP_CATID_CHEADING","ID reference");
+define("_AD_VP_CATCID_CHEADING","CID");
+define("_AD_VP_CATTITLE_CHEADING","Naslov");
+define("_AD_VP_CATWEIGHT_CHEADING","Veličina");
+define("_AD_VP_CATVIDEOS_CHEADING","Video filmovi");
+
+define("_AD_VP_CAT_MOVE_REQUEST","Premjesti sve video filmove iz odabrane kategorije u određenu kategoriju");
+define("_AD_VP_CATPID_MOVE","Odaberi PID kategorije");
+define("_AD_VP_CATID_MOVE","Odaberi ID Suca kategorije");
+define("_AD_VP_CATCID_MOVE","Odaberi CID kategorije");
+define("_AD_VP_CATTITLE_MOVE","Odaberi naslov kategorije");
+define("_AD_VP_CATVIDEO_ASSIGNED","Namjenjeni videi za ovu kategoriju");
+define("_AD_VP_CATSUBS_ASSIGNED","Pod kategorija namjenjena ovoj kategoriji");
+define("_AD_VP_CAT_MOVE_DEST","Odabir odredišta kategorije");
+define("_AD_VP_CATMOVEDEST","Odaberi odredište kategorije");
+define("_AD_VP_CATMOVE","Premjesti Videe");
+define("_AD_VL_CATDELETE","Obriši kategoriju");
+
+define("_AD_VP_MOVEDUPLERROR","Premjesti zahtjev greške događaja.<br />Izabrana kategorija i odredište kategorija mora se razlikovati od kategorije.");
+define("_AD_VP_MOVENOVIDEOSERROR","Nema videa za pomak.<br />Izabrana kategorija ne sadržava niti jedan video.");
+define("_AD_VP_CATDELVIDEMPTYERROR","Kategorija ne može biti obrisana ako sadržava videe.<br />Molim uklonite prvo video filmove iz kategorie.");
+define("_AD_VP_CATDELSUBEMPTYERROR","Kategorija ne može biti izbrisana ako sadržava pod-kategorije.<br />Molim izbrišite prvo kategorije.");
+define("_AD_VP_CATMOVESUCCESS","Videi su uspješno premješteni iz izabrane kategorije u određenu kategoriju.");
+define("_AD_VP_CATMOVEERROR","Greška pri premještanju iz kategorije u određenu kategoriju.");
+define("_AD_VL_CATDELETECONFIRM","Sigurno želite obrisati ovu kategoriju?");
+define("_AD_VP_CATDELSUCCESS","Uspješno brisanje kategorije");
+define("_AD_VP_CATDELERROR","Greška pri brisanju kategorije.");
+
+define("_AD_VP_VIDEOREPORT_HEADER","Video izvješća");
+define("_AD_VP_REASON_CHEADING","Razlog izvješća");
+define("_AD_VP_NUMREPORTS_CHEADING","# izvješća");
+define("_AD_VP_DELETEREPORT_CHEADING","Brisanje SAMO izvješća");
+define("_AD_VP_DELETEREPORT_VIDEO_CHEADING","Brisanje izvješća i Videa");
+define("_AD_VP_DELREPORT_REALLY","Sigurno želite obrisati video izvješće?");
+define("_AD_VP_DELREPORTSUCCESS","Uspješno brisanje video izvješća");
+define("_AD_VP_DELREPORTERROR","Greška pri brisanju video izvješća");
+define("_AD_VP_DELREPORTVIDEO_REALLY","Sigurno želite obristi oba video izvješća?");
+define("_AD_VP_DELREPORTVIDEOSUCCESS","Uspješno brisanje oba video izvješća");
+define("_AD_VP_DELREPORTVIDEOERROR","Greška pri brisanju video izvješća i videa");
+
+?>
---
+++ XoopsLanguages/croatian/modules UTF-8/videotube 1.84/videotube/language/hrvatski/blocks.php
@@ -0,0 +1,100 @@
+<?php
+//
+// _LANGCODE: hr
+// _CHARSET : UTF-8
+// Translator: XoopsHR.org / Dražen Punčec & Ivan Kosak / 2.3.2009.
+//
+//  ------------------------------------------------------------------------ //
+//                XOOPS - PHP Content Management System                      //
+//                    Copyright (c) 2000 XOOPS.org                           //
+//                       <http://www.xoops.org/>                             //
+//  ------------------------------------------------------------------------ //
+//  This program is free software; you can redistribute it and/or modify     //
+//  it under the terms of the GNU General Public License as published by     //
+//  the Free Software Foundation; either version 2 of the License, or        //
+//  (at your option) any later version.                                      //
+//                                                                           //
+//  You may not change or alter any portion of this comment or credits       //
+//  of supporting developers from this source code or any supporting         //
+//  source code which is considered copyrighted (c) material of the          //
+//  original comment or credit authors.                                      //
+//                                                                           //
+//  This program is distributed in the hope that it will be useful,          //
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of           //
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            //
+//  GNU General Public License for more details.                             //
+//                                                                           //
+//  You should have received a copy of the GNU General Public License        //
+//  along with this program; if not, write to the Free Software              //
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA //
+//  ------------------------------------------------------------------------ //
+//  Author:     tank                                                         //
+//  Website:    http://www.customvirtualdesigns.com                          //
+//  E-Mail:     tanksplace@comcast.net                                       //
+//  Date:       09/14/2008                                                   //
+//  Module:     Video Tube                                                   //
+//  File:       languages/english/blocks.php                                 //
+//  Version:    1.7 RC1                                                      //
+//  ------------------------------------------------------------------------ //
+//  Change Log                                                               //
+//  ***                                                                      //
+//  Version 1.0  Initial Release 04/14/2008                                  //
+//  ***                                                                      //
+//  Version 1.1  04/23/2008                                                  //
+//  Note: E-mail contact address change                                      //
+//  ***                                                                      //
+//  Version 1.62  08/09/2008                                                 //
+//  NEW: Add video wall blocks                                               //
+//  ***                                                                      //
+//  Version 1.65  09/12/2008                                                 //
+//  Bug Fix: Add Video Size for Video Play blocks                            //
+//  ***                                                                      //
+//  Version 1.7 RC1  09/14/2008                                              //
+//  Bug Fix: Add variables for Featured blocks                               //
+//  ***                                                                      //
+
+// Blocks
+
+define("_BL_VP_MV_NUMVIDEOS","Broj Videa u bloku");
+define("_BL_VP_RD_NUMVIDEOS","Broj Videa u bloku");
+define("_BL_VP_LT_NUMVIDEOS","Broj Videa u bloku");
+define("_BL_VP_MV_VIDEOWALL","Dimenzija Matrice");
+define("_BL_VP_RD_VIDEOWALL","Dimenzija Matrice");
+define("_BL_VP_LT_VIDEOWALL","Dimenzija Matrice");
+define("_BL_VP_MV_VIDEOSIZE","Veličina videa u postotku");
+define("_BL_VP_RD_VIDEOSIZE","Veličina videa u postotku");
+define("_BL_VP_LT_VIDEOSIZE","Veličina videa u postotku");
+define("_BL_VP_FTP_VIDEOSIZE","Veličina videa u postotku");
+
+define("_BL_VP_FTH_VIDEO1","Video 1 - Video ID");
+define("_BL_VP_FTH_VIDEO2","Video 2 - Video ID");
+define("_BL_VP_FTH_VIDEO3","Video 3 - Video ID");
+define("_BL_VP_FTH_VIDEO4","Video 4 - Video ID");
+define("_BL_VP_FTH_VIDEO5","Video 5 - Video ID");
+
+define("_BL_VP_FTV_VIDEO1","Video 1 - Video ID");
+define("_BL_VP_FTV_VIDEO2","Video 2 - Video ID");
+define("_BL_VP_FTV_VIDEO3","Video 3 - Video ID");
+define("_BL_VP_FTV_VIDEO4","Video 4 - Video ID");
+define("_BL_VP_FTV_VIDEO5","Video 5 - Video ID");
+
+define("_BL_VP_FTP_VIDEO1","Video ID");
+
+// Additional text provided in v1.81
+
+define("_BL_VP_SUBMITTED","objavio");
+define("_BL_VP_ON","na");
+define("_BL_VP_AT","kod");
+define("_BL_VP_VIEWS","Pregledano");
+define("_BL_VP_DISPLAYMORE","Prikaži više informacija");
+define("_BL_VP_DISPLAYLESS","Prikaži manje informacija");
+define("_BL_VP_VIDEOTITLE","Video naslov");
+define("_BL_VP_LOCALVIEWS","Lokalni pregledi");
+define("_BL_VP_LOCALSUBMITTER","Lokalni podnositelj");
+define("_BL_VP_SUBMITDATE","Datum");
+define("_BL_VP_DURATION","Trajanje");
+define("_BL_VP_PUBDATE","Datum objave");
+define("_BL_VP_AUTHOR","Autor");
+define("_BL_VP_TOTALVIEWS","Pregledano");
+
+?>
