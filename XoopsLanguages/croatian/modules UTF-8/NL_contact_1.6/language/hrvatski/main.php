22bdcf625cb190b818401e09e5da4d6304f2aa0e
sadistiko <sadistiko@c17157d5-08f2-41e7-a131-df055f6a0da3>
2009-03-02 15:50:31 +0000

    added NL_contact 1.6 module translation

---
+++ XoopsLanguages/croatian/modules UTF-8/NL_contact_1.6/language/hrvatski/main.php
@@ -0,0 +1,32 @@
+<?php
+// _LANGCODE: hr
+// _CHARSET : UTF-8
+// Translator: XoopsHR.org / Dražen Punčec / 2.3.2009.
+define("_CT_NAME","Ime");
+define("_CT_EMAIL","Email");
+define("_CT_URL","URL");
+define("_CT_ICQ","ICQ");
+define("_CT_COMPANY","Poduzeće");
+define("_CT_LOCATION","Lokacija");
+define("_CT_COMMENTS","Komentari");
+define("_CT_SUBMIT","Pošalji");
+define("_CT_YOURMESSAGE","Vaša poruka:");
+define("_CT_WEBMASTER","Webmaster");
+define("_CT_HELLO","Pozdrav %s,");
+define("_CT_THANKYOUCOMMENTS","Zahvaljujemo na kontaktu %s.");
+define("_CT_SENTTOWEBMASTER","Poruka je poslana na %s.");
+define("_CT_SUBMITTED","%s je poslao sljedeće informacije:");
+define("_CT_MESSAGESENT","Poruka za %s je poslana");
+define("_CT_CONFIRM", "Potvrda emaila");
+define("_CT_SENDCONFIRMEMAIL", "Pošalji potvrdni email na %s");
+define("_CT_SENDCONFIRM", "Slanje potvrdnog emaila vama!" );
+define("_CT_SENTASCONFIRM","Vaš komentar je poslan na: %s kao potvrda emaila.");
+define("_CT_ADDRESS", "Adresa");
+define("_CT_SECURITY_CODE", "Sigurnosni kod" );
+define("_CT_SECURITY_TYPE", "Unesi sigurnosni kod" );
+define("_CT_ERROR_CONDITIONHEAD", "Greška");
+define("_CT_ERROR_CONDITION", "<b>Molimo ispravite grešku!</b><br />" );
+define("_CT_ERROR_BADSECVALUE_MSG", "Neispravan sigurnosni kod!<br />");
+define("_CT_ERROR_BADEMAIL_MSG", "Provjera zapisa email adrese!<br />");
+define("_CT_NO_SPAM", "Nema SPAM! Sigurnosna provjera");
+?>
