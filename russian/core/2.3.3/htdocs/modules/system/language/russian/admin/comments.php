ad0e09dabd73a8c23214f24cd182b97e5c8d4de2
algalochkin <algalochkin@c17157d5-08f2-41e7-a131-df055f6a0da3>
2009-03-10 14:12:27 +0000

    Core 2.3.3: make Russian translation structure. Stage 23 A russian folder as copy from english folder is created

---
+++ russian/core/2.3.3/htdocs/modules/system/language/russian/admin/comments.php
@@ -0,0 +1,16 @@
+<?php
+// $Id$
+// _LANGCODE: en
+// _CHARSET : UTF-8
+// Translator: XOOPS Translation Team
+
+//%%%%%% Comment Manager %%%%%
+define('_MD_AM_COMMMAN','Comment Manager');
+
+define('_MD_AM_LISTCOMM','List Comments');
+define('_MD_AM_ALLMODS','All modules');
+define('_MD_AM_ALLSTATUS','Any status');
+define('_MD_AM_MODULE','Module');
+define('_MD_AM_COMFOUND','%s comment(s) found.');
+
+?>
---
+++ russian/core/2.3.3/htdocs/modules/system/language/russian/admin/version.php
@@ -0,0 +1,10 @@
+<?php
+// $Id$
+// _LANGCODE: en
+// _CHARSET : UTF-8
+// Translator: XOOPS Translation Team
+
+//%%%%%%	Admin Module Name  Version 	%%%%%
+define("_AM_DBUPDATED",_MD_AM_DBUPDATED);
+
+?>
