124b5b42d01ff13fd272017aeff7d46e36e2a596
algalochkin <algalochkin@c17157d5-08f2-41e7-a131-df055f6a0da3>
2009-03-10 13:21:38 +0000

    Core 2.3.3: make Russian translation structure. Stage 8 A class/xoopseditor/fckeditor/language folder is created russian.php as copy from english.php

---
+++ russian/core/2.3.3/htdocs/class/xoopseditor/fckeditor/language/russian.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ *  FCKeditor adapter for XOOPS
+ *
+ * @copyright   The XOOPS project http://www.xoops.org/
+ * @license     http://www.fsf.org/copyleft/gpl.html GNU public license
+ * @package     class
+ * @subpackage  xoopseditor
+ * @since       2.3.0
+ * @author      Taiwen Jiang <phppp@users.sourceforge.net>
+ * @version     $Id: english.php 2117 2008-09-20 04:27:56Z phppp $
+ */
+/*
+ * Associated with editor_registry.php
+ */
+define("_XOOPS_EDITOR_FCKEDITOR", "FCK Editor");
+define("_XOOPS_EDITOR_FCKEDITOR_FONTLIST",'Arial;Comic Sans MS;Courier New;Tahoma;Times New Roman;Verdana');
+define("_XOOPS_EDITOR_FCKEDITOR_LANGUAGE", "en");
+?>
