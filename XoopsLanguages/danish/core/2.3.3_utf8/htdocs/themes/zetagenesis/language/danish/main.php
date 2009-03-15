2e4c3ae38550c9c8b7ccdf247141767327e75dc8
anderssk <anderssk@c17157d5-08f2-41e7-a131-df055f6a0da3>
2009-03-15 17:47:19 +0000

    Upload language files for 2.3.3

---
+++ XoopsLanguages/danish/core/2.3.3_utf8/htdocs/themes/zetagenesis/language/danish/main.php
@@ -0,0 +1,18 @@
+<?php
+
+// Theme language definitions
+
+// _LANGCODE: da
+// _CHARSET : UTF-8
+// Translator: Anderssk (Anders Kristiansen)
+
+/* ------------------- userbar.html ------------------- */
+define("_THEME_REGISTER",           "Bruger registrering");
+define("_THEME_INBOX",              "Vis dine beskeder");
+define("_THEME_INBOX_NOTREAD",      "Besked(er) ulæst");
+define("_THEME_NOTIFICATIONS",      "Vis dine notifikationer");
+define("_THEME_PROFILE",            "Vis din profil");
+define("_THEME_ADMINISTRATION",     "Administration");
+define("_THEME_LOGOUT",             "Klik for at logge ud");
+
+?>
---
+++ XoopsLanguages/danish/core/2.3.3_utf8/upgrade/cnt-2.2.x-to-2.3.0/language/danish.php
@@ -0,0 +1,13 @@
+<?php
+// $Id: danish.php
+// _LANGCODE: da
+// _CHARSET : UTF-8
+// Translator: Anderssk (Anders Kristiansen)
+define( "_CONFIRM_UPGRADE_220", "
+Dette script vil migrerer data fra det eksisterende 'profile' modul.<br />
+Vær venlig ikke at afinstallerer det eksisterende 'profile' modul manuelt, ellers bliver eksisterende data ikke migreret.<br /><br />
+Når opgraderingen er helt færdig <stong>gå da i modul administration og opgrader 'profile' modulet</strong>, da data først herefter vil blive migreret helt.<br /><br />
+For at fortryde opgraderingen, <a href='index.php'>klik her</a>.<br /><br />
+ellers gå <a href='index.php?action=next&upd220=ok'>videre</a>.
+");
+?>
