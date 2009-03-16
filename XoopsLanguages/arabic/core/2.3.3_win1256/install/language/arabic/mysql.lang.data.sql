4bc3dfa49a5108c63234ca5b23039c5d898ebd1c
arabxoops <arabxoops@c17157d5-08f2-41e7-a131-df055f6a0da3>
2009-03-16 01:36:41 +0000

    Added lang folder for install

---
+++ XoopsLanguages/arabic/core/2.3.3_win1256/install/language/arabic/mysql.lang.data.sql
@@ -0,0 +1,33 @@
+#
+# Dumping data for table `ranks`
+#
+
+INSERT INTO ranks VALUES (1, '⁄÷Ê ÃœÌœ', 0, 20, 0, 'rank3e632f95e81ca.gif');
+INSERT INTO ranks VALUES (2, '⁄÷Ê ‰‘ÿ', 21, 40, 0, 'rank3dbf8e94a6f72.gif');
+INSERT INTO ranks VALUES (3, '⁄÷Ê ›⁄«·', 41, 70, 0, 'rank3dbf8e9e7d88d.gif');
+INSERT INTO ranks VALUES (4, '⁄÷Ê „„Ì“', 71, 150, 0, 'rank3dbf8ea81e642.gif');
+INSERT INTO ranks VALUES (5, '⁄÷Ê „Â„', 151, 10000, 0, 'rank3dbf8eb1a72e7.gif');
+INSERT INTO ranks VALUES (6, '„‘—›', 0, 0, 1, 'rank3dbf8edf15093.gif');
+INSERT INTO ranks VALUES (7, '„œÌ— «·„Êﬁ⁄', 0, 0, 1, 'rank3dbf8ee8681cd.gif');
+
+#
+# Dumping data for table `smiles`
+#
+
+INSERT INTO smiles VALUES (1, ':-D', 'smil3dbd4d4e4c4f2.gif', '”⁄Ìœ Ãœ«', 1);
+INSERT INTO smiles VALUES (2, ':-)', 'smil3dbd4d6422f04.gif', '„» ”„', 1);
+INSERT INTO smiles VALUES (3, ':-(', 'smil3dbd4d75edb5e.gif', 'Õ“Ì‰', 1);
+INSERT INTO smiles VALUES (4, ':-o', 'smil3dbd4d8676346.gif', '„ ›«∆·', 1);
+INSERT INTO smiles VALUES (5, ':-?', 'smil3dbd4d99c6eaa.gif', '„” €—»', 1);
+INSERT INTO smiles VALUES (6, '8-)', 'smil3dbd4daabd491.gif', '„—Õ', 1);
+INSERT INTO smiles VALUES (7, ':lol:', 'smil3dbd4dbc14f3f.gif', '„» ”„', 1);
+INSERT INTO smiles VALUES (8, ':-x', 'smil3dbd4dcd7b9f4.gif', '„Ã‰Ê‰', 1);
+INSERT INTO smiles VALUES (9, ':-P', 'smil3dbd4ddd6835f.gif', '„÷Õﬂ', 1);
+INSERT INTO smiles VALUES (10, ':oops:', 'smil3dbd4df1944ee.gif', '⁄›Ê«', 0);
+INSERT INTO smiles VALUES (11, ':cry:', 'smil3dbd4e02c5440.gif', 'Õ“Ì‰ Ãœ«', 0);
+INSERT INTO smiles VALUES (12, ':evil:', 'smil3dbd4e1748cc9.gif', '„Ã‰Ê‰ Ãœ«', 0);
+INSERT INTO smiles VALUES (13, ':roll:', 'smil3dbd4e29bbcc7.gif', '⁄ÌÊ‰Ì', 0);
+INSERT INTO smiles VALUES (14, ';-)', 'smil3dbd4e398ff7b.gif', '„÷Õﬂ', 0);
+INSERT INTO smiles VALUES (15, ':pint:', 'smil3dbd4e4c2e742.gif', '“““““““', 0);
+INSERT INTO smiles VALUES (16, ':hammer:', 'smil3dbd4e5e7563a.gif', '„ÿ—ﬁ…', 0);
+INSERT INTO smiles VALUES (17, ':idea:', 'smil3dbd4e7853679.gif', '·œÌ ›ﬂ—…', 0);
---
+++ XoopsLanguages/arabic/core/2.3.3_win1256/install/language/arabic/welcome.php
@@ -0,0 +1,37 @@
+<?php
+// $Id$
+// _LANGCODE: ar
+// _CHARSET : windows-1256
+// Translator: Arab XOOPS Team
+
+$content .= '
+<p>
+	הÙÇד ÅÏÇÑÉ ÇבדÍÊזל <acronym title="eXtensible Object-Oriented Portal System">XOOPS</acronym> וז הÙÇד דÝÊזÍ ÇבדÕÏÑ
+	ÊדÊ ÈÑדÌÊו ÈבÛÉ PHP. ז םÊÚÈÑ זÓםבÉ דהÇÓÈÉ
+	בÊØזםÑ ז ÈהÇÁ דזÇÞÚ ÇבÅהÊÑהÊ Çב‗ÈםÑÉ ז ÇבÕÛםÑÉ ז ÔÈ‗ÇÊ ÇבÅהÊÑÇהÊ ÇבדÍבםÉ ÈÏÇÎב ÇבÔÑ‗ÇÊ ז ÇבדזÇÞÚ ÇבÊÌÇÑםÉ ז ÇבדÏזהÇÊ ז ÇבדÒםÏ.
+</p>
+<p>
+	הÙÇד ÅÏÇÑÉ ÇבדÍÊזל XOOPS ÎÇÖÚ בבÔÑזØ ÇבדÐ‗זÑÉ Ýם ÇבÕÝÍÉ ÇבÊÇבםÉ
+	<a href="http://www.gnu.org/copyleft/gpl.html" rel="external">GNU General Public License - GPL</a>
+	ז ב‗ ÍÑםÉ ÇÓÊÎÏÇדו ז ÊÚÏםבו.
+	םד‗ה‗ ÅÚÇÏÉ ÊזÒםÚו ÈדÇ Ãה‗ דבÊÒד ÈÔÑזØ ÇבÊזÒםÚ ÇבדÊÚÇÑÝ ÚבםוÇ.
+</p>
+<h3>דÊØבÈÇÊ ÊÑ‗םÈ ÇבדÌבÉ</h3>
+<ul>
+	<li>ÎÇÏד זםÈ: <a href="http://www.apache.org/" rel="external">Apache</a> ז IIS ז Roxen ז ÛםÑוÇ</li>
+	<li>ÈםÆÉ <a href="http://www.php.net/" rel="external">PHP</a>: ÇבÅÕÏÇÑÉ 4.3.0 Ãז ÃÚבל ז םזÕל ÈÇבÅÕÏÇÑÉ 5.2 ÝדÇ ÝזÞ</li>
+	<li>ÞÇÚÏÉ ÈםÇהÇÊ <a href="http://www.mysql.com/" rel="external">MySQL</a>: ÇבÅÕÏÇÑÉ 32.3 Ãז ÃÚבל ז םזÕל ÈÇבÅÕÏÇÑÉ 4.1 ÝדÇ ÝזÞ</li>
+</ul>
+<h3>ÞÈב ÊÑ‗םÈ ÇבדÌבÉ</h3>
+<ol>
+<li>ÊÃ‗Ï דה Ãה ÈÑדÌÉ Èם ÃÊÔ Èם ÊÝÊÍ ‗ב דדםÒÇÊוÇ בדזÞÚ‗</li>
+<li>ÅהÔÇÁ ÞÇÚÏÉ ÈםÇהÇÊ בבדÌבÉ</li>
+	<li>ÃהÔÃ ÍÓÇÈ דÓÊÎÏד בו ‗Çדב ÇבÕבÇÍםÇÊ Úבל ÞÇÚÏÉ ÇבÈםÇהÇÊ.</li>
+    <li>ÇÌÚב ÇבדÌבÏÇÊ ז ÇבדבÝÇÊ ÇבÊÇבםÉ ÞÇÈבÉ בב‗ÊÇÈÉ: %s</li>
+	<li>ÅÐÇ ‗הÊ ÊÑםÏ ÊÑ‗םÈ ÈÑהÇדÌ ÇבÍÇÑÓ protector, <div class="confirmMsg">ÝÇÓÊÈÏב ÇבדבÝ <em>mainfile.dist.php</em> ÈÇבדבÝ <em>extras/mainfile.dist.php.protector</em> ØÈÚÇ דÚ ÅÚÇÏÉ ÊÓדםÊו Åבל <em>htdocs/mainfile.dist.php</em>.</div></li>
+	<li>בÖדÇה ÃדÇה Ã‗ÈÑ בדזÞÚ‗ ההÕÍ‗ ÈהÞב ÇבדÌבÏםה <em>xoops_lib</em> ÇבÎÇÕ Èד‗ÊÈÇÊ ÒזזÈÓ ז <em>xoops_data</em> ÇבÎÇÕ ÈÍÝÙ ÈםÇהÇÊ ÒזזÈÓ ÎÇÑÌ דÌבÏ <a href="http://phpsec.org/projects/guide/3.html" rel="external">document root</a>, Ãז ÍÊל ÊÛםםÑ ÇÓדודÇ: %s</li>
+    <li>ÊÃ‗Ï דה Ãה ÇבדÌבÏÇÊ ÇבÊÇבםÉ ÞÇÈבÉ בב‗ÊÇÈÉ: %s</li>
+<li>ÊÝÚםב ÇבÌÇÝÇ Ó‗ÑםÈÊ זÇבדבÝÇÊ ÇבדÄÞÊו Çב‗ז‗םÒ Ýם דÊÕÝÍ‗</li>
+</ol>
+';
+?>
