5fa58d032c66a3223f7c23c48018d54d57076fb2
arabxoops <arabxoops@users.noreply.github.com>
2009-03-16 01:36:41 +0000

    Added lang folder for install

---
+++ arabic/core/2.3.3_win1256/install/language/arabic/finish.php
@@ -0,0 +1,24 @@
+<?php
+// $Id$
+// _LANGCODE: ar
+// _CHARSET : windows-1256
+// Translator: Arab XOOPS Team
+
+$content .=
+"<h3>гжёЏя</h3>
+<p> «б¬д <a href='../index.php'>≈÷џЎ ед«</a> бг‘«еѕ… «б’ЁЌ… «б—∆н”н… бгжёЏя </p>
+<h3>«бѕЏг «бЁдн</h3>
+<p>—«ћЏ <a href='http://xoops.sourceforge.net/' rel='external'>The XOOPS Project</a></p>
+";
+
+$content .=
+"<h3>≈ћ—«Ѕ«  «б√г«д</h3>
+<p>гЏ«бћ «б Ћ»н  ”жЁ нЌ«жб  Џѕнб гжёЏя б≈Џ »«—«  √гдн…. б–бя нћ» Џбня «б Ќёё гд «б «бн:
+<div class='confirmMsg'>
+гбЁ <em>mainfile.php</em> нћ» √д няжд ббё—«Ѕ… ЁёЎ.<br />
+Ќ–Ё гћбѕ <em>{$installer_modified}</em> (√ж <em>install</em> Ён Ќ«б… √де бг   г ≈Џ«ѕ…  ”гн е »ж«”Ў… гЏ«бћ «б Ћ»н )  гд гжёЏя.
+</div>
+</p>
+";
+
+?>
