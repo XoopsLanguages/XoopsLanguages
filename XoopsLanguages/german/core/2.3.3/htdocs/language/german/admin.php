3cae67ba51642f214361adb0f2573dccd8bb9c3a
dhcst <dhcst@c17157d5-08f2-41e7-a131-df055f6a0da3>
2009-03-21 14:07:01 +0000

    fixed typo

--- XoopsLanguages/german/core/2.3.3/htdocs/language/german/admin.php
+++ XoopsLanguages/german/core/2.3.3/htdocs/language/german/admin.php
@@ -5,7 +5,7 @@
 // Translator: XOOPS Translation Team
 
 //%%%%%%    File Name  admin.php    %%%%%
-define("_AD_NORIGHT","Die erforderlichen Userrechte um diesen Bereich zu betreten sind nicht vorhanden.");
+define("_AD_NORIGHT","Die erforderlichen Userrechte für diesen Bereich sind nicht vorhanden.");
 define("_AD_ACTION","Aktion:");
 define("_AD_EDIT","Bearbeiten");
 define("_AD_DELETE","Löschen");
@@ -17,9 +17,9 @@ define("_AD_REGISTERED","Registriert am:"); //Registered Date
 define('_AD_PRESSGEN', 'Dies ist das erste Mal, dass Du den Admin-Bereich betrittst. Bitte klicke auf \"Abschicken\" um fortzufahren.');
 define('_AD_LOGINADMIN', 'EINLOGGEN...');
 
-define("_AD_WARNINSTALL2","WARNUNG: Das Verzeichnis %s existiert noch auf dem Server. Entfernen Sie bitte dieses Verzeichnis aus Sicherheitsgründen.");
-define("_AD_WARNINWRITEABLE","WARNUNG: Die Datei %s ist nicht schreibgeschützt auf dem Server. <br />Bitte setzen Sie die Zugriffsrechte richtig. Ansonsten wird das zu einem Sicherheitsrisiko!<br /> (unter Unix (444), unter Win32 (schreibgeschützt))");
-define("_AD_WARNINNOTWRITEABLE","WARNUNG: Verzeichnis %s ist nicht beschreibbar vom Server. <br />Bitte ändern Sie die Schreibrechte dieses Ordners.<br /> für Unix (777), für Windows (readable)");
+define("_AD_WARNINGINSTALL","WARNUNG: Das Verzeichnis %s existiert noch auf dem Server. Entfernen Sie bitte dieses Verzeichnis aus Sicherheitsgründen.");
+define("_AD_WARNINGWRITEABLE","WARNUNG: Die Datei %s ist nicht schreibgeschützt auf dem Server. <br />Bitte setzen Sie die Zugriffsrechte richtig. Ansonsten wird das zu einem Sicherheitsrisiko!<br /> (unter Unix (444), unter Win32 (schreibgeschützt))");
+define("_AD_WARNINGNOTWRITEABLE","WARNUNG: Verzeichnis %s ist nicht beschreibbar vom Server. <br />Bitte ändern Sie die Schreibrechte dieses Ordners.<br /> für Unix (777), für Windows (readable)");
 define("_AD_WARNINGXOOPSLIBINSIDE","WARNUNG: Verzeichnis %s ist innerhalb des DocumentRoot!<br />In diesem Ordner befinden sich sensible Daten. Dieser sollte daher vor Zugriffen aus dem Web geschützt werden.");
 
 ?>
