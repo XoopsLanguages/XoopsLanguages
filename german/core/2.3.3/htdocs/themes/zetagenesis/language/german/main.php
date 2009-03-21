c3990e80335e43baa91b4ac8b62909d0ddef5680
dhcst <dhcst@c17157d5-08f2-41e7-a131-df055f6a0da3>
2009-03-21 14:19:45 +0000

    add theme lang

---
+++ german/core/2.3.3/htdocs/themes/zetagenesis/language/german/main.php
@@ -0,0 +1,19 @@
+<?php
+
+// Theme language definitions
+
+// $Id: main.php 2857 2009-03-21 11:12:11Z dhcst $
+// _LANGCODE: de
+// _CHARSET : UTF-8
+// Translator: XOOPS üTranslation Team
+
+/* ------------------- userbar.html ------------------- */
+define("_THEME_REGISTER",           "User Registration");
+define("_THEME_INBOX",              "Mitteilungen anzeigen");
+define("_THEME_INBOX_NOTREAD",      "Nachricht(en) ungelesen");
+define("_THEME_NOTIFICATIONS",      "Benachrichtigungen");
+define("_THEME_PROFILE",            "Profil ansehen");
+define("_THEME_ADMINISTRATION",     "Administration");
+define("_THEME_LOGOUT",             "Abmelden");
+
+?>
