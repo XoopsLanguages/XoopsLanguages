6fa991a615803a8f28e5515184ec7070355f9945
Anders Kristiansen <anderssk@users.noreply.github.com>
2009-03-27 17:02:51 +0000

    Xdonations upload

---
+++ danish/modules/xdonations/xdonations 1.9/language/danish/modinfo.php
@@ -0,0 +1,47 @@
+<?php
+/************************************************************************/
+/* Donations - Paypal financial management module for Xoops 2           */
+/* Copyright (c) 2004 by Xoops2 Donations Module Dev Team			    */
+/* http://dev.xoops.org/modules/xfmod/project/?group_id=1060			*/
+/* $Id: modinfo.php,v 1.3 2004/10/15 17:58:57 blackdeath_csmc Exp $      */
+/************************************************************************/
+/*                                                                      */
+/* Based on NukeTreasury for PHP-Nuke - by Dave Lawrence AKA Thrash     */
+/* NukeTreasury - Financial management for PHP-Nuke                     */
+/* Copyright (c) 2004 by Dave Lawrence AKA Thrash                       */
+/*                       thrash@fragnastika.com                         */
+/*                       thrashn8r@hotmail.com                          */
+/*                                                                      */
+/************************************************************************/
+/*                                                                      */
+/* This program is free software; you can redistribute it and/or modify */
+/* it under the terms of the GNU General Public License as published by */
+/* the Free Software Foundation; either version 2 of the License.       */
+/*                                                                      */
+/* This program is distributed in the hope that it will be useful, but  */
+/* WITHOUT ANY WARRANTY; without even the implied warranty of           */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU     */
+/* General Public License for more details.                             */
+/*                                                                      */
+/* You should have received a copy of the GNU General Public License    */
+/* along with this program; if not, write to the Free Software          */
+/* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  */
+/* USA                                                                  */
+/************************************************************************/
+
+// Module Info
+
+define("_MI_DM_NAME","Donationer");
+
+// A brief description of this module
+define("_MI_DM_DESC","Paypal financial management module for XOOPS2");
+
+// Names of blocks for this module (Not all module has blocks)
+define("_MI_DM_BNAME1","Donat-O-Meter");
+define("_MI_DM_BNAME1_DESC","Grafisk visning af donationer");
+define("_MI_DM_BNAME2","Giv en donation!");
+define("_MI_DM_BNAME2_DESC","Tillad dine brugeren at donere til din side med Paypal");
+define("_MI_DM_BNAME3","Tidligere Donorer");
+define("_MI_DM_BNAME3_DESC","En liste over tidligere donorer");
+define("_MI_DM_BUTTON_URL","https://www.paypal.com/en_US/i/btn/x-click-but21.gif");
+?>
