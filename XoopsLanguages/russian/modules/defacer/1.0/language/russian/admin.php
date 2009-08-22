7d760726ebbcc4b0a88f9c889e4e72881a414bcf
andrey3761 <andrey3761@c17157d5-08f2-41e7-a131-df055f6a0da3>
2009-08-22 08:59:53 +0000

    Defacer: The Russian translation is added.

---
+++ XoopsLanguages/russian/modules/defacer/1.0/language/russian/admin.php
@@ -0,0 +1,46 @@
+<?php
+// _LANGCODE: ru 
+// _CHARSET : UTF-8 
+// Translator: Deka87
+
+define("_AM_DEFACER_MODULEADMIN","Администрирование");
+define("_AM_DEFACER_GENERALSET","Общие настройки");
+
+define("_AM_DEFACER_ID","ID");
+define("_AM_DEFACER_MODULE","Модуль");
+define("_AM_DEFACER_TITLE","Заголовок");
+define("_AM_DEFACER_URL","URL");
+define("_AM_DEFACER_STATUS","Статус");
+define("_AM_DEFACER_QUICKLAUNCH","Быстрый запуск");
+
+define("_AM_DEFACER_CHANGESTATUS","Нажмите, чтобы изменить статус");
+define("_AM_DEFACER_DBUPDATED","База данных обновлена");
+define("_AM_DEFACER_ERROR","Извините, произошла ошибка");
+define("_AM_DEFACER_RUDEL","Вы уверены, что хотите это удалить?");
+define("_AM_DEFACER_NOTFOUND","Не найдено");
+
+define("_AM_DEFACER_MODDEACTIVE","Модуль не активирован");
+define("_AM_DEFACER_SELECTMODULE_ERR","Пожалуйста, сперва выберите модуль");
+define("_AM_DEFACER_SELECTPAGE_ERR","Пожалуйста, сперва выберите страницу");
+
+define("_AM_DEFACER_PAGEMAN","Менеджер страниц");
+define("_AM_DEFACER_PAGE","Страница");
+define("_AM_DEFACER_PAGE_MODULE","Модуль страницы");
+define("_AM_DEFACER_PAGE_TITLE","Заголовок страницы");
+define("_AM_DEFACER_PAGE_URL","URL страницы");
+define("_AM_DEFACER_PAGE_URL_DESC","Описание URL'a страницы");
+define("_AM_DEFACER_PAGE_DISPLAY","Отображение страницы");
+
+define("_AM_DEFACER_THEMEMAN","Менеджер тем");
+define("_AM_DEFACER_THEME","Тема");
+
+define("_AM_DEFACER_METAMAN","Менеджер мета тэгов");
+define("_AM_DEFACER_META_SITENAME","Название сайта");
+define("_AM_DEFACER_META_PAGETITLE","Заголовок страницы");
+define("_AM_DEFACER_META_SLOGAN","Слоган сайта");
+define("_AM_DEFACER_META_KEYWORDS","Ключевые слова");
+define("_AM_DEFACER_META_DESCRIPTION","Описание");
+
+define("_AM_DEFACER_PERMISSIONMAN","Менеджер доступа");
+define("_AM_DEFACER_PERMISSION_GROUPS","Разрешенные группы");
+?>
---
+++ XoopsLanguages/russian/modules/defacer/1.0/language/russian/modinfo.php
@@ -0,0 +1,26 @@
+<?php
+// _LANGCODE: ru 
+// _CHARSET : UTF-8 
+// Translator: Deka87
+
+define("_MI_DEFACER_MD_NAME","Defacer");
+define("_MI_DEFACER_MD_DSC","Этот модуль позволяет изменять тему, мета данные и права доступа к любой странице вашего сайта .");
+
+define("_MI_DEFACER_PAGEMANAGER","Менеджер страниц");
+define("_MI_DEFACER_THEMEMANAGER","Менеджер тем");
+define("_MI_DEFACER_METAMANAGER","Менеджер мета данных");
+define("_MI_DEFACER_PERMISSIONMANAGER","Менеджер доступа");
+define("_MI_DEFACER_ABOUT","О модуле");
+
+define("_MI_DEFACER_DISDEFACER","Отключить Defacer");
+define("_MI_DEFACER_DISDEFACER_DSC","Это отключит все функции модуля");
+
+define("_MI_DEFACER_DISTHEMES","Отключить смену тем");
+define("_MI_DEFACER_DISTHEMES_DSC","Это полностью отключит смену тем в модуле");
+
+define("_MI_DEFACER_DISMETAS","Отключить смену мета данных");
+define("_MI_DEFACER_DISMETAS_DSC","Это полностью отключит смену мета данных в модуле");
+
+define("_MI_DEFACER_DISPERMISSIONS","Отключить смену прав доступа");
+define("_MI_DEFACER_DISPERMISSIONS_DSC","Это полностью отключит смену прав доступа в модуле");
+?>
