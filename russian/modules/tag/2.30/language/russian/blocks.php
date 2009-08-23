2b27fff1e6475397e7e27128dc316bc3b9c3abc8
andrey3761 <andrey3761@c17157d5-08f2-41e7-a131-df055f6a0da3>
2009-08-23 18:37:14 +0000

    Module Tag 2.30, 1.60: Russian translation is added.

---
+++ russian/modules/tag/2.30/language/russian/blocks.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Tag management for XOOPS
+ *
+ * @copyright	The XOOPS project http://www.xoops.org/
+ * @license		http://www.fsf.org/copyleft/gpl.html GNU public license
+ * @author		Taiwen Jiang (phppp or D.J.) <php_pp@hotmail.com>
+ * @since		1.00
+ * @version		$Id: blocks.php 2292 2008-10-12 04:53:18Z phppp $
+ * @package		tag
+ */
+
+if (!defined('XOOPS_ROOT_PATH')) { exit(); }
+
+define("TAG_MB_ITEMS", "Число для показа");
+define("TAG_MB_FONTSIZE_MAX", "Максимальный размер шрифта (%) для отображения");
+define("TAG_MB_FONTSIZE_MIN", "Минимальный размер шрифта (%) для отображения");
+define("TAG_MB_TIME_DURATION", "Продолжительность времени для статистики, в днях");
+define("TAG_MB_SORT", "Критерий сортировки");
+define("TAG_MB_ALPHABET", "Алфавит");
+define("TAG_MB_COUNT", "Количество элементов");
+define("TAG_MB_TIME", "Время");
+?>
