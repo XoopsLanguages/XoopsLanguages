49d74a710208b1cd2159457801cbd9655ea48ff7
andrey3761 <andrey3761@c17157d5-08f2-41e7-a131-df055f6a0da3>
2009-08-26 20:00:22 +0000

    Module tad_uploader: Russian translation is added.

---
+++ russian/modules/tad_uploader/1.2/language/russian/admin.php
@@ -0,0 +1,43 @@
+<?php
+
+// $Id: admin.php,v 1.2 2009/08/25 01:20:25 radio-hobby $
+// _CHARSET : UTF-8
+// _LANGCODE: ru
+// Translator: 2009-03-12 MACTEP
+
+//power.php
+define("_AM_CAT_DB_ERROR1","Не могу получить данные каталога");
+define("_AM_CAT_SET_ACCESS_POWER","Видимые папки для каждой группы");
+define("_AM_CAT_SET_UPLOAD_POWER","Папки загрузки для каждой группы");
+
+//index.php
+define("_AM_CAT_CREATE_FOLDER","Создать Новую Папку");
+define("_AM_CAT_FOLDER_NAME","Название Папки");
+define("_AM_CAT_FOLDER_DESC","Описание");
+define("_AM_CAT_FATHER_FOLDER","Родительская папка");
+define("_AM_CAT_FOLDER_SORT","Приоритет");
+define("_AM_CAT_ENABLE","Состояние");
+define("_AM_CAT_SHARE","Ресурс");
+define("_AM_CAT_SAVE","Сохранить");
+define("_AM_CAT_DB_ERROR2","Не могу заменить данные каталога");
+define("_AM_CAT_DB_ERROR3","Не могу получить данные страницы");
+define("_AM_CAT_PAGE_BAR1","(Всего %s) Страниц %s：");
+define("_AM_CAT_FUNCTION","Функция");
+define("_AM_CAT_DEL_CONFIRM","Удалить папку? Эта операция удалит все файлы в папке!");
+define("_AM_CAT_LIST_ALL_FILES","Отобразить все файлы","%s file(s) in total");
+define("_AM_CAT_AUTHOR","автор");
+define("_AM_CAT_FILE_COUNTER","Счетчик");
+define("_AM_CAT_EDIT","Редактировать");
+define("_AM_CAT_DEL","Удалить");
+
+define("_MD_CAT_MENU","Меню");
+define("_MD_CAT_PREV","Назад");
+define("_MD_CAT_NEXT","Далее");
+define("_MD_CAT_FIRST","В начало");
+define("_MD_CAT_LAST","В конец");
+define("_MD_TO_MOD","Перейти в модуль");
+
+define("_MI_CAT_ADMENU1", "Папка");
+define("_MI_CAT_ADMENU2", "Разрешение");
+
+?>
---
+++ russian/modules/tad_uploader/1.2/language/russian/blocks.php
@@ -0,0 +1,10 @@
+<?php
+
+// $Id: blocks.php,v 1.2 2009/08/25 01:20:25 radio-hobby $
+// _CHARSET : UTF-8
+// _LANGCODE: ru
+// Translator: 2009-03-12 MACTEP
+
+define("_MB_CAT_CATALOG_B_EDIT_1_BITEM0","Количество отображаемых файлов");
+define("_MB_CAT_DB_ERROR2","Не могу получить данные catalog_file");
+?>
