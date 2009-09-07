e8f4d5380c8596ef13358a79df4c4e40ed0a3829
andrey3761 <andrey3761@c17157d5-08f2-41e7-a131-df055f6a0da3>
2009-09-07 23:15:50 +0000

    Core 2.4.0 Beta 2: The Russian translation is added.

---
+++ XoopsLanguages/russian/core/2.4.0/htdocs/modules/system/class/gui/oxygen/language/russian/admin.php
@@ -0,0 +1,76 @@
+<?php
+// $Id$
+// _LANGCODE: ru
+// _CHARSET : UTF-8
+// Translator: 2009-03-12 andrey3761
+
+define('_AD_SYSOPTIONS','Конфигурация системы');
+define('_AD_INSTALLEDMODULES','Установленные модули');
+
+define('_AD_XOOPSTHEMES','Тема');
+define('_AD_XOOPSMODULES','Модули');
+define('_AD_INTERESTSITES','Ссылки');
+define('_AD_LOCALSUPPORT','Локальная поддержка');
+define('_XOOPS_NEWS','Новости XOOPS');
+define('_XOOPS_WEBSITE','XOOPS');
+define('_SOURCEFORGE','Sourceforge');
+
+define('_MD_OXYGEN_VERSION','Версия');
+define('_MD_OXYGEN_VERSION_XOOPS','Версия XOOPS');
+define('_MD_OXYGEN_VERSION_PHP','Версия PHP');
+define('_MD_OXYGEN_VERSION_MYSQL','Версия Mysql');
+define('_MD_OXYGEN_Server_API','Server API');
+define('_MD_OXYGEN_OS','ОС');
+define('_MD_OXYGEN_ABOUT','О Xoops');
+define('_MD_OXYGEN_ABOUT_TEXT','XOOPS - это динамический OO (Объектно-Ориентированный) основанный на открытом исходном коде скрипт портала, написанный на PHP. XOOPS поддерживает различные базы данных, XOOPS - это идеальный инструмент для создания небольших высоко-динамичных сообществ вэб-сайтов, внутренних порталов компании, корпоративных порталов, web-публикаторов и многого другого. Прочитайте <a href="http://www.xoops.org/modules/wfchannel/" rel="external">Всё о XOOPS</a> чтобы узнать дополнительные детали.');
+define('_MD_OXYGEN_XOOPS_LINKS','Xoops ссылки');
+
+define('_MD_OXYGEN_XOOPSPROJECT','Проект XOOPS');
+define('_MD_OXYGEN_LOCALSUPPORT','Xoops локальная поддержка');
+define('_MD_OXYGEN_XOOPSCORE','Ядро XOOPS');
+define('_MD_OXYGEN_XOOPSTHEME','Темы для XOOPS');
+define('_MD_OXYGEN_XOOPSWIKI','Xoops Wiki');
+define('_MD_OXYGEN_XOOPSBOOKS','Книги о XOOPS');
+define('_MD_OXYGEN_NEWMODULE','Новые модули');
+define('_MD_OXYGEN_XOOPSFAQ','Xoops FAQ');
+define('_MD_OXYGEN_CODESVN','Code Svn');
+define('_MD_OXYGEN_REPORTBUG','Report Bug');
+define('_MD_OXYGEN_XGIFTSHOP','Xoops Gift Shop');
+define('_MD_OXYGEN_XDONATIONS','Make a Donation');
+define('_MD_OXYGEN_XDONATIONS_TEXT','<h2>We Need Your Help - Make a Donation!</h2>
+If you like the XOOPS system, and would like to give something in return, you can make a donation to the XOOPS Foundation! Simply use the PayPal payment system to transfer your donation to the Foundation account.<br />
+Your donation will be used to promote and advance the use and development of the XOOPS system. Guaranteed.<br />
+The XOOPS Foundation is a registered not-for-profit organisation in the USA, dedicated to promote XOOPS!<br />
+All donators will get the Friend of XOOPS rank here on this website. Thanks to the Friends of XOOPS for their donations and continued support<br />
+For Make a Donation and more info please visite <a href="http://www.xoops.org/modules/xdonations/" rel="external">here</a>');
+
+define('_MD_OXYGEN_SITEPREF','Конфигурация');
+define('_MD_OXYGEN_GENERAL','Основыне настройки');
+define('_MD_OXYGEN_USERSETTINGS','Настройка информации о пользователях');
+define('_MD_OXYGEN_METAFOOTER','Мета теги и нижний колонтитул');
+define('_MD_OXYGEN_CENSOR','Настройка параметров цензуры слов');
+define('_MD_OXYGEN_SEARCH','Настройка поиска');
+define('_MD_OXYGEN_MAILER','Настройка почты');
+define('_MD_OXYGEN_AUTHENTICATION','Настройка аутентификации');
+
+//add for styles name
+define('_MD_LANG_SILVER','Серебрянный');
+define('_MD_LANG_DARK','Тёмный');
+define('_MD_LANG_ORANGE','Оранжевый');
+
+// help
+define('_MD_AM_BANS_HELP','you can manage your <br> banners from here');
+define('_MD_AM_BKAD_HELP','with blocks you can <br> add many thing for <br> your users');
+define('_MD_AM_ADGS_HELP','you can add some groups <br> and manage permission <br> for ecah group');
+define('_MD_AM_IMAGES_HELP','you can add your image <br> here and make some <br> Category for yourself');
+define('_MD_AM_MDAD_HELP','at here you can install <br> and unistall your xoops <br> modules. do you know <br> what module is?');
+define('_MD_AM_PREF_HELP','xoops core and all of your <br> modules have a preferences <br> for manage module options');
+define('_MD_AM_SMLS_HELP','you and your users can use <br> this smilies in your posts ,<br> pms , comments and ...');
+define('_MD_AM_RANK_HELP','you can add ranks <br> for your users . ');
+define('_MD_AM_USER_HELP','whit this options you <br> can add new user or <br> edit old  user and userinfo ,<br> change groupss and many <br> other things');
+define('_MD_AM_FINDUSER_HELP','sometimes you need see <br>information about some users,<br> use this options for find<br> his/her');
+define('_MD_AM_MLUS_HELP','if you need send mail <br>or mp for your users ,<br> you can use this option');
+define('_MD_AM_AVATARS_HELP','add some avatars for your<br> users , they can use this <br>avatars on them profiles');
+define('_MD_AM_TPLSETS_HELP','if you need add some changes<br> in core or modules template use <br>this options');
+define('_MD_AM_COMMENTS_HELP','your users add some Comments <br>on your website , for your news , <br>photos , weblog post links and ...<br> manage user comments from here');
+?>
