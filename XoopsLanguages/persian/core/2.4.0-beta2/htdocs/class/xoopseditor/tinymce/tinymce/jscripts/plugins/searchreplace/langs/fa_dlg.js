761b197a1fed0fd3bc7a6a1d37bae001d5fce6b0
voltan1 <voltan1@c17157d5-08f2-41e7-a131-df055f6a0da3>
2009-09-07 17:26:36 +0000

    add persian translation for xoops 2.4.0-beta2

---
+++ XoopsLanguages/persian/core/2.4.0-beta2/htdocs/class/xoopseditor/tinymce/tinymce/jscripts/plugins/searchreplace/langs/fa_dlg.js
@@ -0,0 +1,26 @@
+/**
+* Translation of TinyMCE for Persian users
+*
+* @copyright	      http://www.impresscms.org/ The ImpressCMS Project 
+* @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+* @package	      Translations
+* @since		 3.0.6.2
+* @author		Sina Asghari <pesian_stranger@users.sourceforge.net>
+* @version		$Id$
+*/
+tinyMCE.addI18n('fa.searchreplace_dlg',{
+searchnext_desc:"جستجوی مجدد",
+notfound:"جستجو کامل شد. محتوای موردنظر پیدا نشد.",
+search_title:"جستجو",
+replace_title:"جستجو/تعویض",
+allreplaced:"تمام رشته جستجو خطور کرده تعویض شدند.",
+findwhat:"جستجوی چه چیزی؟",
+replacewith:"تعویض با",
+direction:"جهت",
+up:"بالا",
+down:"پایین",
+mcase:"همسانی حروف",
+findnext:"جستجوی&nbsp;بعدی",
+replace:"تعویض",
+replaceall:"تعویض&nbsp;همه‌ی موارد",
+});
