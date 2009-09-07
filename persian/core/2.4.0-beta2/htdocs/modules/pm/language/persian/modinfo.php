1cb0279caf991696addc5b0d49986c4baf1e0d7f
voltan1 <voltan1@c17157d5-08f2-41e7-a131-df055f6a0da3>
2009-09-07 17:26:36 +0000

    add persian translation for xoops 2.4.0-beta2

---
+++ persian/core/2.4.0-beta2/htdocs/modules/pm/language/persian/modinfo.php
@@ -0,0 +1,29 @@
+<?php
+// $Id$
+// _LANGCODE: fa
+// _CHARSET : UTF-8
+// Translator: XOOPS Translation Team
+
+define("_PM_MI_NAME", "پیام شخصی");
+define("_PM_MI_DESC", "ماژولی برای فرستادن پیام شخصی میان اعضای سایت");
+
+define("_PM_MI_INDEX", "صفحه اول");
+define("_PM_MI_PRUNE", "هرس کردن پیام ها");
+
+define("_PM_MI_LINK_TITLE", "لینک پیام شخصی");
+define("_PM_MI_LINK_DESCRIPTION", "یک لینک برای فرستادن پیام شخصی به عضو سایت نشان می‌دهد");
+define("_PM_MI_MESSAGE", "نوشتن یک پیام به");
+
+define("_PM_MI_PRUNESUBJECT", "عنوان پیام شخصی هرس کردن");
+define("_PM_MI_PRUNESUBJECT_DESC", "این عنوان پیام شخصی است که به کاربران سایت بعد از اینکه پی ام‌ها هرس شدند ارسال می‌شود.");
+define("_PM_MI_PRUNEMESSAGE", "متن پیام شخصی هرس کردن");
+define("_PM_MI_PRUNEMESSAGE_DESC", "این پیام ، متن پیامی است که پس از هرس کردن پیام‌های شخصی و پاک شده تعدادی از پیام‌های کاربران برای آنها فرستاده می‌شود. برای نشان دادن تعداد پیام‌های پاک شده از  {PM_COUNT} استفاده کنید که خودکار تعداد پیام‌های حذف شده به جای این علامت قرار خواهد گرفت.");
+define("_PM_MI_PRUNESUBJECTDEFAULT", "پی ام‌های شما در فرایند پاکسازی پیام‌های شخصی پاک شده است");
+define("_PM_MI_PRUNEMESSAGEDEFAULT", "در هنگام پاکسازی پیام‌های شخصی ما {PM_COUNT} پیام از پیام‌های ارسالی شما را برای کم کردن حجم سایت پاک کردیم");
+
+define("_PM_MI_MAXSAVE", "حداکثر پی ام در قسمت ذخیره ");
+define("_PM_MI_MAXSAVE_DESC", "");
+
+define("_PM_MI_PERPAGE", "تعداد پی ام در هر صفحه");
+define("_PM_MI_PERPAGE_DESC", "");
+?>
