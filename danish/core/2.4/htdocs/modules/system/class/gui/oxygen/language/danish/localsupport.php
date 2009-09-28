719f0fe9414f938124af0049b56b560f35292bb4
Anders Kristiansen <anderssk@users.noreply.github.com>
2009-09-28 11:14:20 +0000

    Add local sites

--- danish/core/2.4/htdocs/modules/system/class/gui/oxygen/language/danish/localsupport.php
+++ danish/core/2.4/htdocs/modules/system/class/gui/oxygen/language/danish/localsupport.php
@@ -3,36 +3,21 @@
 
 $menu = array();
 
-// sample for English  support
-/*
+
 $menu[] = array(
-    'link'      => 'http://www.xoops.org',
-    'title'     => 'XOOPS',
+    'link'      => 'http://www.xoopsnordic.org',
+    'title'     => 'XOOPS Nordic',
     'absolute'  => 1,
     'icon'      => XOOPS_URL . '/modules/system/class/gui/oxygen/images/xoops.png'
 );
 
 $menu[] = array(
-    'link'      => 'http://www.xoops.org',
-    'title'     => 'XOOPS',
+    'link'      => 'http://www.danordesign.com',
+    'title'     => 'XOOPS Hosting',
     'absolute'  => 1,
     'icon'      => XOOPS_URL . '/modules/system/class/gui/oxygen/images/xoops.png'
 );
 
-$menu[] = array(
-    'link'      => 'http://www.xoops.org/modules/library/',
-    'title'     => _AD_XOOPSTHEMES,
-    'absolute'  => 1,
-    'icon'      => XOOPS_URL . '/modules/system/class/gui/oxygen/images/tweb.png'
-);
-
-$menu[] = array(
-    'link'      => 'http://www.xoops.org/modules/modules-search/',
-    'title'     => _AD_XOOPSMODULES,
-    'absolute'  => 1,
-    'icon'      => XOOPS_URL . '/modules/system/class/gui/oxygen/images/xoops.png'
-);
-*/
 
 return $menu;
 ?>
