5a8baa6824c614543aed5348b4db12df407f7e53
Anders Kristiansen <anderssk@users.noreply.github.com>
2009-09-28 11:03:24 +0000

    Add translation af exm admin theme

---
+++ danish/core/2.4/htdocs/modules/system/class/gui/exm/language/danish/admin.php
@@ -0,0 +1,10 @@
+<?php
+// $Id$
+
+define('_AD_SYSOPTIONS','System indstillinger');
+define('_AD_INSTALLEDMODULES','Installerede moduler');
+
+define('_AD_XOOPSTHEMES','XOOPS Temaer');
+define('_AD_INTERESTSITES','Links');
+
+?>
