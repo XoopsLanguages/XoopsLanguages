8d3cf5117c88e6d31a23eae39beaf19d17e39b4d
voltan1 <voltan1@c17157d5-08f2-41e7-a131-df055f6a0da3>
2009-10-28 21:12:58 +0000

    Add xoops 2.4.0 persian language

---
+++ persian/core/2.4.0/htdocs/class/xoopseditor/tinymce/tinymce/jscripts/plugins/xhtmlxtras/langs/fa_dlg.js
@@ -0,0 +1,42 @@
+/**
+* Translation of TinyMCE for Persian users
+*
+* @copyright	      http://www.impresscms.org/ The ImpressCMS Project 
+* @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+* @package	      Translations
+* @since		 3.0.6.2
+* @author		Sina Asghari <pesian_stranger@users.sourceforge.net>
+* @version		$Id$
+*/
+tinyMCE.addI18n('fa.xhtmlxtras_dlg',{
+attribute_label_title:"عنوان",
+attribute_label_id:"شناسه",
+attribute_label_class:"کلاس",
+attribute_label_style:"ترکیب‌بندی",
+attribute_label_cite:"ذکر",
+attribute_label_datetime:"تاریخ/زمان",
+attribute_label_langdir:"جهت متن",
+attribute_option_ltr:"چپ به راست",
+attribute_option_rtl:"راست به چپ",
+attribute_label_langcode:"زبان",
+attribute_label_tabindex:"فهرست تب",
+attribute_label_accesskey:"کد دسترسی",
+attribute_events_tab:"اتفاقات",
+attribute_attrib_tab:"صفات",
+general_tab:"عمومی",
+attrib_tab:"صفات",
+events_tab:"رویدادها",
+fieldset_general_tab:"تنظیمات عمومی",
+fieldset_attrib_tab:"صفات عنصر",
+fieldset_events_tab:"رویدادهای عنصر",
+title_ins_element:"درج عنصر",
+title_del_element:"حذف عنصر",
+title_acronym_element:"سرنام عنصر",
+title_abbr_element:"اختصار عنصر",
+title_cite_element:"ذکر عنصر",
+remove:"حذف",
+insert_date:"درج تاریخ/زمان کنونی",
+option_ltr:"چپ به راست",
+option_rtl:"راست به چپ",
+attribs_title:"درج/ویرایش عنصر",
+});
