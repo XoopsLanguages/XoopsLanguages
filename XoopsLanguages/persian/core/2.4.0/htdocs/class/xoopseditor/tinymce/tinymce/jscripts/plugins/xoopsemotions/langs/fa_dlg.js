16328bd746ce106bce497e94b9c4d3e2c691a830
voltan1 <voltan1@c17157d5-08f2-41e7-a131-df055f6a0da3>
2009-10-28 21:12:58 +0000

    Add xoops 2.4.0 persian language

---
+++ XoopsLanguages/persian/core/2.4.0/htdocs/class/xoopseditor/tinymce/tinymce/jscripts/plugins/xoopsemotions/langs/fa_dlg.js
@@ -0,0 +1,10 @@
+/**
+ * @version     $Id: en_dlg.js 2804 2009-02-16 01:16:49Z dugris $
+ */
+
+tinyMCE.addI18n('fa.xoopsemotions_dlg',{
+    title : 'وارد کردن لبخند های زوپس',
+    tab_emotionsbrowser: 'لبخند ها',
+    tab_emotionsadmin: 'اضافه کردن لبخند',
+    error_noemotions: 'هیچ لبخندی در پایگاه داده ها موجود نیست!!!'
+});
