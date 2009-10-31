97490c972af1fda5fbfe81f3499aa1ee59a5cbc0
andrey3761 <andrey3761@c17157d5-08f2-41e7-a131-df055f6a0da3>
2009-10-31 21:13:12 +0000

    Language isearch added.

---
+++ XoopsLanguages/russian/modules/isearch/1.9/language/russian/admin.php
@@ -0,0 +1,73 @@
+<?php
+//  ------------------------------------------------------------------------ //
+//                       ISEARCH - MODULE FOR XOOPS 2                        //
+//                  Copyright (c) 2005-2006 Instant Zero                     //
+//                     <http://xoops.instant-zero.com/>                      //
+// ------------------------------------------------------------------------- //
+//  This program is free software; you can redistribute it and/or modify     //
+//  it under the terms of the GNU General Public License as published by     //
+//  the Free Software Foundation; either version 2 of the License, or        //
+//  (at your option) any later version.                                      //
+//                                                                           //
+//  You may not change or alter any portion of this comment or credits       //
+//  of supporting developers from this source code or any supporting         //
+//  source code which is considered copyrighted (c) material of the          //
+//  original comment or credit authors.                                      //
+//                                                                           //
+//  This program is distributed in the hope that it will be useful,          //
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of           //
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            //
+//  GNU General Public License for more details.                             //
+//                                                                           //
+//  You should have received a copy of the GNU General Public License        //
+//  along with this program; if not, write to the Free Software              //
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA //
+//  ------------------------------------------------------------------------ //
+
+// _LANGCODE: ru
+// _CHARSET : UTF-8
+// Translator: 2009/04/05 andrey3761
+
+define("_AM_ISEARCH_DBUPDATED","База данных успешно обновлена");
+define("_AM_ISEARCH_GENERALSET", "Настройки модуля" );
+define("_AM_ISEARCH_GOTOMOD","Перейти к модулю");
+define("_AM_ISEARCH_MODULEADMIN","Администрирование модуля");
+define("_AM_ISEARCH_STATS","Статистика поиска (%d слов записанно)");
+
+define("_AM_ISEARCH_ID","ID");
+define("_AM_ISEARCH_KEYWORD","Искомое слово");
+define("_AM_ISEARCH_KEYWORDS","Искомые слова за день");
+define("_AM_ISEARCH_USER","Пользователь");
+define("_AM_ISEARCH_DATE","Дата");
+define("_AM_ISEARCH_MOST_SEARCH","Наиболее часто запрашиваемые слова");
+define("_AM_ISEARCH_HITS","Запросов");
+define("_AM_ISEARCH_BIGGEST_USERS","Активные в поиске пользователи");
+define("_AM_ISEARCH_DAY_STATS","Ежедневная статистика");
+define("_AM_ISEARCH_USE","Запросов");
+
+define("_AM_ISEARCH_PRUNE","Prune");
+define("_AM_ISEARCH_PRUNE_DATE","Prune keywords before : ");
+define("_AM_ISEARCH_PRUNE_KEYONLY","or only remove this word : ");
+define("_AM_ISEARCH_PRUNE_DESC","If you don't enter a word the the script will prune by date");
+define("_AM_ISEARCH_PRUNE_CONFIRM","Do you confirm the deletion of %u elements ?");
+define("_AM_ISEARCH_NOTHING_PRUNE","Нечего удалять");
+define("_AM_ISEARCH_EXPORT","Экспорт");
+define('_AM_ISEARCH_EXPORT_BETWEEN', 'Экспортировать поиск, сделанный между ');
+define('_AM_ISEARCH_EXPORT_AND'," и ");
+define("_AM_ISEARCH_DATE_FORMAT","Формат даты (в формате PHP)");
+define("_AM_ISEARCH_DELIMITER","Разделитель полей");
+define("_AM_ISEARCH_EXPORT_ERROR","Ошибка, не возможно создать файл экспорта %s");
+define('_AM_ISEARCH_EXPORT_READY', "Ваш экспортный файл доступен.<br /><a href='%s'>Нажмите здесь для загрузки</a>.<br />Не забывайте <a href='%s'>удалить</a> файл после закачки.");
+define('_AM_ISEARCH_DELETED_OK',"Файл успешно удалён");
+define('_AM_ISEARCH_DELETED_PB',"Ошибка, невозможно удалить файл");
+define("_AM_ISEARCH_NOTHING_TO_EXPORT","Нечего экспортировать!");
+define("_AM_ISEARCH_DELETE","Удалить");
+define("_AM_ISEARCH_BLACKLIST","Чёрный список");
+define("_AM_ISEARCH_ACTION","Действие");
+define("_AM_ISEARCH_AREYOUSURE","Вы действительно уверены?");
+define('_AM_ISEARCH_BLACKLIST_DESC',"Слова в этом списке не будут записаны во время поиска");
+define('_AM_ISEARCH_BLACKLIST_ADD',"Добавить");
+define('_AM_ISEARCH_BLACKLIST_ADD_DSC',"Добавить слова в список<br />(одно слово в сроке)");
+define('_AM_ISEARCH_IP',"IP");
+define('_AM_ISEARCH_FILTER_BY',"Фильтр");
+?>
---
+++ XoopsLanguages/russian/modules/isearch/1.9/language/russian/main.php
@@ -0,0 +1,34 @@
+<?php
+//  ------------------------------------------------------------------------ //
+//                       ISEARCH - MODULE FOR XOOPS 2                        //
+//                  Copyright (c) 2005-2006 Instant Zero                     //
+//                     <http://xoops.instant-zero.com/>                      //
+// ------------------------------------------------------------------------- //
+//  This program is free software; you can redistribute it and/or modify     //
+//  it under the terms of the GNU General Public License as published by     //
+//  the Free Software Foundation; either version 2 of the License, or        //
+//  (at your option) any later version.                                      //
+//                                                                           //
+//  You may not change or alter any portion of this comment or credits       //
+//  of supporting developers from this source code or any supporting         //
+//  source code which is considered copyrighted (c) material of the          //
+//  original comment or credit authors.                                      //
+//                                                                           //
+//  This program is distributed in the hope that it will be useful,          //
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of           //
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            //
+//  GNU General Public License for more details.                             //
+//                                                                           //
+//  You should have received a copy of the GNU General Public License        //
+//  along with this program; if not, write to the Free Software              //
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA //
+//  ------------------------------------------------------------------------ //
+
+// _LANGCODE: ru
+// _CHARSET : UTF-8
+// Translator: 2009/04/05 andrey3761
+
+define('_ISEARCH_DATE',"Дата");
+define('_ISEARCH_KEYWORD',"Искомое слово");
+define('_ISEARCH_PAGE',"Страница");
+?>
