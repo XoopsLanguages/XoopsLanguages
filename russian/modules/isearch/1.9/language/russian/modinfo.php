d7fbcc2ad81376adfab21e69cd782e8609711339
andrey3761 <andrey3761@c17157d5-08f2-41e7-a131-df055f6a0da3>
2009-10-31 21:13:12 +0000

    Language isearch added.

---
+++ russian/modules/isearch/1.9/language/russian/modinfo.php
@@ -0,0 +1,54 @@
+<?php
+//  ------------------------------------------------------------------------ //
+//                       ISEARCH - MODULE FOR XOOPS 2                        //
+//                  Copyright (c) 2005-2006 Instant Zero                     //
+//                     <http://xoops.instant-zero.com/>                      //
+// ------------------------------------------------------------------------- //
+//  This program is free software; you can redistribute it and/or modify     //
+//  it under the terms of the GNU General Public License as published by     //
+//  the Free Software Foundation; either version 2 of the License, or        //
+//  (at your option) any later version.                                      //
+//                                                                           //
+//  You may not change or alter any portion of this comment or credits       //
+//  of supporting developers from this source code or any supporting         //
+//  source code which is considered copyrighted (c) material of the          //
+//  original comment or credit authors.                                      //
+//                                                                           //
+//  This program is distributed in the hope that it will be useful,          //
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of           //
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            //
+//  GNU General Public License for more details.                             //
+//                                                                           //
+//  You should have received a copy of the GNU General Public License        //
+//  along with this program; if not, write to the Free Software              //
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA //
+//  ------------------------------------------------------------------------ //
+
+// _LANGCODE: ru
+// _CHARSET : UTF-8
+// Translator: 2009/10/31 andrey3761
+
+define('_MI_ISEARCH_NAME',"iSearch");
+define('_MI_ISEARCH_DESC',"При помощи этого модуля вы будете знать, что пользователи ищут на вашем сайте.");
+
+define('_MI_ISEARCH_ADMMENU1',"Статистика");
+define('_MI_ISEARCH_ADMMENU2',"Prune");
+define('_MI_ISEARCH_ADMMENU3',"Экспорт");
+define('_MI_ISEARCH_ADMMENU4',"Чёрный список");
+
+define('_MI_ISEARCH_OPT0',"Число поисковых запросов на главной странице модуля");
+define('_MI_ISEARCH_OPT0_DSC',"Сколько поисковых запросов пользователь может видеть на странице (0=ничего не показывать)");
+
+define('_MI_ISEARCH_OPT1',"Группы, для которых статистика не ведётся");
+define('_MI_ISEARCH_OPT1_DSC',"Все поиски, сделанные пользователями этой группы не будут записываться в статистику");
+
+define('_MI_ISEARCH_OPT2',"Число ключевых слов, видимых в администрировании");
+define('_MI_ISEARCH_OPT2_DSC',"");
+
+define('_MI_ISEARCH_OPT3', "Списко IP, для которых статистика не ведётся");
+define('_MI_ISEARCH_OPT3_DSC', "По одному IP на строку.");
+
+define('_MI_ISEARCH_BNAME1',"Последний поиск");
+define('_MI_ISEARCH_BNAME2',"Biggest users of the search");
+define('_MI_ISEARCH_BNAME3',"Статистика");
+?>
