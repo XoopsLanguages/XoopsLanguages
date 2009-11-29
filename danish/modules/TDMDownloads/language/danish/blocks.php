7a74525079448df7d2bd0eed4f55d0277d09bcb6
Anders Kristiansen <anderssk@users.noreply.github.com>
2009-11-29 20:02:05 +0000

    Initial upload

---
+++ danish/modules/TDMDownloads/language/danish/blocks.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * ****************************************************************************
+ *  - TDMDownloads By TDM   - TEAM DEV MODULE FOR XOOPS
+ *  - GNU Licence Copyright (c)  (www.xoops.org)
+ *
+ * La licence GNU GPL, garanti à l'utilisateur les droits suivants
+ *
+ * 1. La liberté d'exécuter le logiciel, pour n'importe quel usage,
+ * 2. La liberté de l' étudier et de l'adapter à ses besoins,
+ * 3. La liberté de redistribuer des copies,
+ * 4. La liberté d'améliorer et de rendre publiques les modifications afin
+ * que l'ensemble de la communauté en bénéficie.
+ *
+ * @copyright   http://www.tdmxoops.net
+ * @license     http://www.fsf.org/copyleft/gpl.html GNU public license
+ * @author		TDM (G.Mage); TEAM DEV MODULE
+ *
+ * ****************************************************************************
+ */
+define("_MB_TDMDOWNLOADS_ALLCAT", "Alle kategorier");
+define("_MB_TDMDOWNLOADS_CATTODISPLAY", "VÃ¦lg viste kategorier");
+define("_MB_TDMDOWNLOADS_CHARS", "Titel lÃ¦ngde");
+define("_MB_TDMDOWNLOADS_DISP", "Vis");
+define("_MB_TDMDOWNLOADS_FILES", "Filer");
+define("_MB_TDMDOWNLOADS_LENGTH", "Tegn");
+?>
+<?php // Translation done by xtransam & anderssk - 2009-08-30 20:35 ?>
---
+++ danish/modules/TDMDownloads/language/danish/mail_template/category_newfile_notify.tpl
@@ -0,0 +1,23 @@
+Hej {X_UNAME},
+
+Filen "{FILE_NAME}" er tilføjet i "{CATEGORY_NAME}" på {X_SITENAME}.
+
+Du kan se filen her:
+{FILE_URL}
+
+Du kan se hele kategorien her:
+{CATEGORY_URL}
+
+-----------
+
+Du modtager denne besked fordi du har valgt at blive orienteret når nye filer tilføjes.
+
+Hvis dette er en fejl eller hvis du ikke ønsker at modtage flere tilsvarende orienteringer, så bedes du opdatere dine Orienteringsvalg ved at besøge linket herunder:
+{X_UNSUBSCRIBE_URL}
+
+Vær venlig at lade være med at svare på denne besked.
+
+-----------
+{X_SITENAME} ({X_SITEURL}) 
+webmaster
+{X_ADMINMAIL}
