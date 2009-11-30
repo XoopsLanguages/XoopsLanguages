1c132ec21792be92659096e0ffcf30ded4610b90
andrey3761 <andrey3761@c17157d5-08f2-41e7-a131-df055f6a0da3>
2009-11-30 18:43:48 +0000

    Core 2.4.2: The Russian translation is added.

---
+++ XoopsLanguages/russian/core/2.4.2/htdocs/themes/zetagenesis/language/russian/main.php
@@ -0,0 +1,51 @@
+<?php
+
+// Theme language definitions
+
+// $Id: main.php 2857 2009-02-22 18:12:11Z dugris $
+// _LANGCODE: ru
+// _CHARSET : UTF-8
+// Translator: 2009-11-30 andrey3761
+
+/* ------------------- userbar.html ------------------- */
+define("_THEME_REGISTER",           "Регистрация");
+define("_THEME_INBOX",              "Личные сообщения");
+define("_THEME_INBOX_NOTREAD",      "Непрочитанные сообщения");
+define("_THEME_NOTIFICATIONS",      "Оповещения");
+define("_THEME_PROFILE",            "Профиль");
+define("_THEME_ADMINISTRATION",     "Администрирование");
+define("_THEME_LOGOUT",             "Выход");
+
+define("THEME_WELLCOME",				"Добро пожаловать");
+define("THEME_EDITPROFILE",				"Редактировать профиль");
+define("THEME_NOTIFICATION",			"Просмотр уведомлений");
+define("THEME_MESSAGE",					"Просмотр сообщений");
+define("THEME_YOUHAVE",					"Вы имеете");
+define("THEME_NOTREAD",					"непрочитанных сообщений");
+define("THEME_MYACCOUNT",				"Мой аккаунт");
+define("THEME_SEARCH",					"Поиск");
+define("THEME_DESC_SEARCH",				"Поиск по сайту");
+define("THEME_KEYWORDS",				"Ключевые слова");
+define("THEME_UPTOP",					"Top page");
+define("THEME_USER",					"Пользователи");
+
+/* footer admin */
+define("THEME_CPHOME",					"Администрирование");
+define("THEME_SYSOPTIONS",				"Опции");
+define("THEME_BANS",					"Управление банерами");
+define("THEME_BKAD",					"Управление блоками");
+define("THEME_ADGS",					"Управление группами");
+define("THEME_PREF",					"Конфигурация");
+define("THEME_INSTALLEDMODULES",		"Управление модулями");
+define("THEME_IMAGES",					"Картинки");
+define("THEME_MDAD",					"Модули");
+define("THEME_SMLS",					"Управление смайликами");
+define("THEME_RANK",					"Ранг пользователей");
+define("THEME_EUSER",					"Редактирование пользователей");
+define("THEME_FINDUSER",				"Поиск пользователей");
+define("THEME_MLUS",					"Отправить по почте");
+define("THEME_AVATARS",					"Управление аватарами");
+define("THEME_TPLS",					"Управление шаблонами");
+define("THEME_COMMENTS",				"Управление коментариями");
+
+?>
