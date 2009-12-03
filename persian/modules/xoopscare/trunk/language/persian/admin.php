a62a68d4d4695ed97af78223c169ae62d2753d13
voltan1 <voltan1@c17157d5-08f2-41e7-a131-df055f6a0da3>
2009-12-03 12:22:56 +0000

    add xoopscore module

---
+++ persian/modules/xoopscare/trunk/language/persian/admin.php
@@ -0,0 +1,56 @@
+<?php
+//  ------------------------------------------------------------------------ //
+//                      XOOPSCARE - MODULE FOR XOOPS 2                		 //
+//                  Copyright (c) 2007, 2008 Instant Zero                    //
+//                     <http://www.instant-zero.com/>                        //
+// ------------------------------------------------------------------------- //
+//  This program is free software; you can redistribute it and/or modify     //
+//  it under the terms of the GNU General Public License as published by     //
+//  the Free Software Foundation; either version 2 of the License, or        //
+//  (at your option) any later version.                                      //
+//                                                                           //
+//  You may not change or alter any portion of this comment or credits       //
+//  of supporting developers from this source code or any supporting         //
+//  source code which is considered copyrighted (c) material of the          //
+//  original comment or credit authors.                                      //
+//                                                                           //
+//  This program is distributed in the hope that it will be useful,          //
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of           //
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            //
+//  GNU General Public License for more details.                             //
+//                                                                           //
+//  You should have received a copy of the GNU General Public License        //
+//  along with this program; if not, write to the Free Software              //
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA //
+//  ------------------------------------------------------------------------ //
+
+define("_AM_XC_ADMINISTRATION","مدیریت");
+define("_AM_XC_GENERALSET","ویژگی های کلی");
+define("_AM_XC_DBMAINTAIN","تعمیر دیتابیس");
+define("_AM_XC_QUERIES","کوری ها");
+define("_AM_XC_CACHE_TPL","شاخه های Cache و Templates_c");
+define("_AM_XC_SPAM_CMT","نظر های اسپم");
+define("_AM_XC_BANNED_IP","IP های بسته شده");
+define("_AM_XC_SPMSTATE_1","در جای خود نگه دار");
+define("_AM_XC_SPMSTATE_2","از سایت  خارج کن");
+define("_AM_XC_SPMSTATE_3","مخفی کن");
+define("_AM_XC_SPMSTATE_4","حذف کن");
+define("_AM_XC_DOIT_NOW","الان این کار را انجام دهید");
+define("_AM_XC_SESSIONS","سشن ها (sessions)");
+define("_AM_XC_DONT_FORGET","فراموش نکنید که قبل از انجام این کار ویژگی های ماژول را ذخیره کنید");
+define("_AM_XC_SAVE","ذخیره");
+define("_AM_XC_HELP1","اگر از کرون برای کار ها استفاده می کنید، باید یک پسورد برای استفاده از اسکریپت مورد نظر بنویسید. اگر از کرون استفاده نمیکنید،نیازی به وارد کردن پسورد ندارید");
+define("_AM_XC_HELP2","اگر آدرس فیزیکی فایلی را وارد کنید، این فایل برای ثبت وقایع تمام کار هایی که ماژول انجام می دهد به کار خواهد رفت");
+define("_AM_XC_HELP3","توصیه می شود از این گزینه استفاده کنید، این گزینه دیتابیس شما را verify, repair, analyze و optimize خواهد کرد (0 = استفاده نشود)");
+define("_AM_XC_HELP4","از این گزینه برای اجرا کردن کوری های دیتابیس به صورت زمان بندی شده استفاده کنید (0 = استفاده نشود)");
+define("_AM_XC_HELP5","مراقب باشید که چه کد هایی را در این قسمت  به عنوان کوری مینویسید! یادتان نرود که هر کدام را با سمی کالون (;) ببندید و هر کوری را در یک خط بنویسید");
+define("_AM_XC_HELP6","به جز index.html و adminmenu.php تمام چیز های دیگر پاک خواهند شد (0 = استفاده نشود)");
+define("_AM_XC_HELP7","هر نظری که دارای %s باشد به عنوان اسپم شناسایی خواهد شد و با آن برخورد می شود (0 = استفاده نشود)");
+define("_AM_XC_HELP8","آیا می خواهید IP  کاربرانی که نظر هایشان به عنوان اسپم شناسایی شده است بسته شود؟");
+define("_AM_XC_HELP9","انتخاب کنید که چه عملی روی نظر هایی که به عنوان اسپم شناخته شده اند انجام شود؟");
+define("_AM_XC_HELP10","این قسمت جدول session  های زوپس را از دیتابیس خالی خواهد کرد (0 = استفاده نشود)");
+define("_AM_XC_HELP11","میتوانید از این قسمت برای اجرای چند خط php  استفاده کنید (0 = استفاده نشود)");
+define("_AM_XC_HELP12","از تگ های باز و بسته کردن php  در این قسمت استفاده نکنید.");
+define("_AM_XC_PHP","کد های Php");
+define("_AM_XC_OPERATION_MADE","عملیات به پایان رسید");
+?>
---
+++ persian/modules/xoopscare/trunk/language/persian/modinfo.php
@@ -0,0 +1,65 @@
+<?php
+//  ------------------------------------------------------------------------ //
+//                      XOOPSCARE - MODULE FOR XOOPS 2                		 //
+//                  Copyright (c) 2007, 2008 Instant Zero                    //
+//                     <http://www.instant-zero.com/>                        //
+// ------------------------------------------------------------------------- //
+//  This program is free software; you can redistribute it and/or modify     //
+//  it under the terms of the GNU General Public License as published by     //
+//  the Free Software Foundation; either version 2 of the License, or        //
+//  (at your option) any later version.                                      //
+//                                                                           //
+//  You may not change or alter any portion of this comment or credits       //
+//  of supporting developers from this source code or any supporting         //
+//  source code which is considered copyrighted (c) material of the          //
+//  original comment or credit authors.                                      //
+//                                                                           //
+//  This program is distributed in the hope that it will be useful,          //
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of           //
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            //
+//  GNU General Public License for more details.                             //
+//                                                                           //
+//  You should have received a copy of the GNU General Public License        //
+//  along with this program; if not, write to the Free Software              //
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA //
+//  ------------------------------------------------------------------------ //
+
+define("_MI_XC_ADMENU1","ویژگی ها");
+define("_MI_XC_ADMENU2","وقایع ثبت شده");
+define("_MI_XC_ADMENU3","اضافه");
+
+define("_MI_XC_NAME","مراقب زوپس");
+define("_MI_XC_DESC","ماژولی برای مراقبت از سایت زوپس شما");
+define("_MI_XC_LOGFILE","فایل ثبت وقایع");
+define("_MI_XC_LOGFILE_DSC","یک نام انتخاب کرده و آدرس آن را بنویسید تا کار هایی که ماژول انجام می دهد در آن ثبت شود");
+define("_MI_XC_CRON_PASS","پسورد کرون");
+define("_MI_XC_CRON_PASS_DSC","پسوردی را که در لینک مورد استفاده در کرون خود قرار می دهید وارد کنید");
+define("_MI_XC_MAINTAIN","تعمیر دیتابیس هر");
+define("_MI_XC_MAINTAIN_DSC","این بخش دیتابیس سایت شما را هر ... روز verify, repair, analyze و optimize می کند.");
+define("_MI_XC_QUERIES","اجرای کوری ها");
+define("_MI_XC_QUERIES_DSC","اجرای کوری ها هر ... روز");
+define("_MI_XC_QUERIES_LST","فهرست کوری ها");
+define("_MI_XC_QUERIES_LST_DSC","کوری ها را برای اجرا وارد کنید");
+define("_MI_XC_TPL","شاخه های Cache و Templates_c");
+define("_MI_XC_TPL_DSC","خالی کردن شاخه های cache و templates_c هر ... روز");
+define("_MI_XC_SPAMCMNT","نظر های اسپم");
+define("_MI_XC_SPAMCMNT_DSC","حذف کردن نظر های اسپم هر ... روز");
+define("_MI_XC_SPAMSTATE","چه کاری  روی نظر های اسپم انجام شود؟");
+define("_MI_XC_SPAMSTATE_DSC","نگه داشتن در سایت، خارج کردن از سایت، مخفی کردن، حذف کردن نظر های اسپم");
+define("_MI_XC_SPAMBLOCK","کسانی که اسپم ارسال کرده اند بسته شوند؟");
+define("_MI_XC_SPAMBLOCK_DSC","آیا می خواهید IP  کسانی که  نظر های اسپم ارسال کرده اند بسته شود؟");
+define("_MI_XC_SESSIONS","خالی کردن sessions");
+define("_MI_XC_SESSIONS_DSC"," خالی کردن سشن ها هر ... روز");
+define("_MI_XC_MAINTIAN_L","آخرین زمانی که دیتابیس تعمیر شده است");
+define("_MI_XC_QUERIES_L","آخرین  زمانی که کوری ها اجرا شده اند");
+define("_MI_XC_TPL_L","آخرین زمانی که شاخه های cache و templates_c خالی شده اند");
+define("_MI_XC_SPAMCMNT_L","آخرین زمانی که نظر های اسپم حذف شده اند");
+define("_MI_XC_SESSIONS_L","آخرین زمانی که جدول سشن ها خالی شده است");
+define("_MI_XC_PHP","اجرای کد های Php");
+define("_MI_XC_PHP_DSC","کد های Php  را تایپ کنید تا هر ...  روز اجرا شوند");
+define("_MI_XC_PHP_L","آخرین زمانی که کد های php  اجرا شده اند");
+define("_MI_XC_PHP_SCRIPT","کد های php  برای اجرا");
+define("_MI_XC_PHP_SCRIPT_DSC","کد ها را برای اجرا تایپ کنید");
+define("_MI_XC_HELP","راهنما");
+define("_MI_XC_BNAME1","نگهداری از وب سایت");
+?>
