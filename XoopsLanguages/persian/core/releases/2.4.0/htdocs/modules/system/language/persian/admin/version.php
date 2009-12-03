e1930ed830536823d04e254187c17c9223c51fce
voltan1 <voltan1@c17157d5-08f2-41e7-a131-df055f6a0da3>
2009-12-03 12:17:38 +0000

    add persian language for modules ( part 5 )

---
+++ XoopsLanguages/persian/core/releases/2.4.0/htdocs/modules/system/language/persian/admin/version.php
@@ -0,0 +1,11 @@
+<?php
+// $Id$
+// _LANGCODE: fa
+// _CHARSET : UTF-8
+// Translator: XOOPS Translation Team
+
+//%%%%%%	Admin Module Name  Version 	%%%%%
+define("_AM_DBUPDATED",_MD_AM_DBUPDATED);
+
+
+?>
