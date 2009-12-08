71675d311305bdeab39ef4832602ea5cdcede4b1
Michael Albertsen <culex@culex.dk>
2009-12-08 16:50:15 +0000

    missing main.php translation

---
+++ danish/modules/onlinehistory/language/danish/main.php
@@ -0,0 +1,9 @@
+<?php
+
+define("_MA_ONLINEHISTORY_NAME","Online Historie");
+define("_MA_ONLINEHISTORY_USERNAME","Brugernavn");
+define("_MA_ONLINEHISTORY_DATE","Dat0");
+define("_MA_ONLINEHISTORY_IP","IP-Adresse");
+define("_MA_ONLINEHISTORY_WHO","Hvem..");
+define("_MA_ONLINEHISTORY_TIMELIFE","Viser sidste %s time(r)");
+?> 
