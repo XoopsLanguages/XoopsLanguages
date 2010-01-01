524009369e4f86f15ad04d426e90965ffb8a9840
andrey3761 <andrey3761@c17157d5-08f2-41e7-a131-df055f6a0da3>
2010-01-01 12:27:16 +0000

    XOOPS 2.4.3: Russian translation is added.

---
+++ russian/core/2.4.3/htdocs/modules/system/class/gui/exm/language/russian/admin.php
@@ -0,0 +1,13 @@
+<?php
+// $Id$
+// _LANGCODE: ru
+// _CHARSET : UTF-8
+// Translator: 2009-12-28 andrey3761
+
+define('_AD_SYSOPTIONS','Конфигурация системы');
+define('_AD_INSTALLEDMODULES','Установленные модули');
+
+define('_AD_XOOPSTHEMES','XOOPS темы');
+define('_AD_INTERESTSITES','Ссылки');
+
+?>
