c1d7132465825df158dd6bdc237836be5a76d365
Michael Albertsen <culex@culex.dk>
2010-01-02 12:16:13 +0000

    Tilføjet sprogfiler dansk for 2.4.3 final. Alle filer er kopi fra 2.4.2 pånær.
    language/danish/banners.php
    define('_BANNERS_NO_LOGIN_DATA', 'Ingen login data registreret');
    define('_BANNERS_NO_REFERER', 'Ingen henvisning registreret');
    define('_BANNERS_NO_ID', 'Ingen gyldig ID registreret');

---
+++ danish/core/2.4.3/htdocs/modules/system/class/gui/oxygen/language/danish/localsupport.php
@@ -0,0 +1,23 @@
+<?php
+// $Id$
+
+$menu = array();
+
+
+$menu[] = array(
+    'link'      => 'http://www.xoopsnordic.org',
+    'title'     => 'XOOPS Nordic',
+    'absolute'  => 1,
+    'icon'      => XOOPS_URL . '/modules/system/class/gui/oxygen/images/xoops.png'
+);
+
+$menu[] = array(
+    'link'      => 'http://www.danordesign.com',
+    'title'     => 'XOOPS Hosting',
+    'absolute'  => 1,
+    'icon'      => XOOPS_URL . '/modules/system/class/gui/oxygen/images/xoops.png'
+);
+
+
+return $menu;
+?>
