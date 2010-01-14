e4cf7257f69cb031a635dcf5a1d6a61ff171a3f9
andrey3761 <andrey3761@c17157d5-08f2-41e7-a131-df055f6a0da3>
2010-01-14 23:25:36 +0000

    zetadigme: Russian translation is added.

---
+++ russian/themes/zetadigme/language/russian/admin.php
@@ -0,0 +1,62 @@
+<?php
+// $Id: admin.php 28 2009-12-06 01:09:24Z kris_fr $
+// _LANGCODE: ru
+// _CHARSET : UTF-8
+// Translator: 2010-01-15 andrey3761
+
+define('_AD_SYSOPTIONS','Конфигурация системы');
+define('_AD_INSTALLEDMODULES','Установленные модули');
+
+define('_AD_XOOPSTHEMES','Темы XOOPS');
+define('_AD_XOOPSMODULES','Модули XOOPS');
+define('_AD_INTERESTSITES','Xoops проект');
+define('_AD_LOCALSUPPORT','Техподдержка');
+define('_XOOPS_WEBSITE','XOOPS.ORG');
+define('_SOURCEFORGE','Sourceforge');
+define('_AD_XDONATIONS','Пожертвование');
+
+define("THEME_WELLCOME",				"Добро пожаловать");
+define("THEME_PROFILE",					"Просмотр профиля");
+define("THEME_EDITPROFILE",				"Редактирование профиля");
+define("THEME_NOTIFICATION",			"Просмотр уведомлений");
+define("THEME_MESSAGE",					"Просмотр сообщений");
+define("THEME_YOUHAVE",					"You have");
+define("THEME_NOTREAD",					"message non read");
+define("THEME_MYACCOUNT",				"Мой аккаунт");
+define("THEME_SEARCH",					"Поиск");
+define("THEME_DESC_SEARCH",				"Search on");
+define("THEME_KEYWORDS",				"Put the keywords");
+define("THEME_UPTOP",					"Top page");
+define("THEME_USER",					"Пользователи");
+
+define('_MD_VERSION','Версия');
+define('_MD_VERSION_XOOPS','Версия XOOPS');
+define('_MD_VERSION_PHP','Версия PHP');
+define('_MD_VERSION_MYSQL','Версия mySQL');
+define('_MD_Server_API','Server API');
+define('_MD_OS','ОС');
+define('_MD_ABOUT','О XOOPS');
+define('_MD_ABOUT_TEXT','XOOPS - это динамический OO (Объектно-Ориентированный) основанный на открытом исходном коде скрипт портала, написанный на PHP. XOOPS поддерживает различные базы данных, XOOPS - это идеальный инструмент для создания небольших высоко-динамичных сообществ вэб-сайтов, внутренних порталов компании, корпоративных порталов, web-публикаторов и многого другого. Прочитайте <a href="http://www.xoops.org/modules/wfchannel/" rel="external">Всё о XOOPS</a> чтобы узнать дополнительные детали.');
+define('_MD_XOOPS_LINKS','XOOPS ссылки');
+
+define('_MD_XOOPSPROJECT','XOOPS проект');
+define('_MD_LOCALSUPPORT','XOOPS техподдержка');
+define('_MD_XOOPSCORE','XOOPS ядро');
+define('_MD_XOOPSTHEME','XOOPS темы');
+define('_MD_XOOPSWIKI','XOOPS вики');
+define('_MD_XOOPSBOOKS','XOOPS книги');
+define('_MD_NEWMODULE','Новые модули');
+define('_MD_XOOPSFAQ','XOOPS фак');
+define('_MD_CODESVN','Code SVN');
+define('_MD_REPORTBUG','Сообщения об ошибках');
+define('_MD_XGIFTSHOP','XOOPS Gift Shop');
+define('_MD_XDONATIONS','Помощь проекту');
+define('_MD_XDONATIONS_TEXT','<h2>We Need Your Help - Make a Donation!</h2>
+If you like the XOOPS system, and would like to give something in return, you can make a donation to the XOOPS Foundation! Simply use the PayPal payment system to transfer your donation to the Foundation account.<br />
+Your donation will be used to promote and advance the use and development of the XOOPS system. Guaranteed.<br />
+The XOOPS Foundation is a registered not-for-profit organisation in the USA, dedicated to promote XOOPS!<br />
+All donators will get the Friend of XOOPS rank here on this website. Thanks to the Friends of XOOPS for their donations and continued support<br />
+For Make a Donation and more info please visite <a href="http://www.xoops.org/modules/xdonations/" rel="external">here</a>');
+define('_MD_MOVETOBLUE','Установите официальные модули');
+define('_MD_MOVETOBLUE_LINK','http://www.xoops.org/modules/repository/');
+?>
---
+++ russian/themes/zetadigme/language/russian/localsupport.php
@@ -0,0 +1,38 @@
+<?php
+// $Id: localsupport.php 28 2009-12-06 01:09:24Z kris_fr $
+
+$menu = array();
+
+// sample for English  support
+/*
+$menu[] = array(
+    'link'      => 'http://www.xoops.org',
+    'title'     => 'XOOPS',
+    'absolute'  => 1,
+    'icon'      => XOOPS_URL . '/modules/system/class/gui/paradigme/icons/xoops.png'
+);
+
+$menu[] = array(
+    'link'      => 'http://www.xoops.org',
+    'title'     => 'XOOPS',
+    'absolute'  => 1,
+    'icon'      => XOOPS_URL . '/modules/system/class/gui/paradigme/icons/xoops.png'
+);
+
+$menu[] = array(
+    'link'      => 'http://www.xoops.org/modules/library/',
+    'title'     => _AD_XOOPSTHEMES,
+    'absolute'  => 1,
+    'icon'      => XOOPS_URL . '/modules/system/class/gui/paradigme/icons/tweb.png'
+);
+
+$menu[] = array(
+    'link'      => 'http://www.xoops.org/modules/modules-search/',
+    'title'     => _AD_XOOPSMODULES,
+    'absolute'  => 1,
+    'icon'      => XOOPS_URL . '/modules/system/class/gui/paradigme/icons/xoops.png'
+);
+*/
+
+return $menu;
+?>
