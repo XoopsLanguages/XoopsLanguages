ac898864bd8675f3b31b544fa53937d8dc8eccf2
anderssk <anderssk@c17157d5-08f2-41e7-a131-df055f6a0da3>
2010-01-17 21:12:27 +0000

    Translation of xoopspool 1.3

---
+++ XoopsLanguages/danish/modules/xoopspoll/xoopspool_1.3_danish_UTF-8/xoopspool/language/danish/blocks.php
@@ -0,0 +1,9 @@
+<?php
+// $Id: blocks.php,v 1.5 2003/02/12 11:39:13 okazu Exp $
+// Blocks
+// Translated by Anders Kristiansen (anderssk)
+
+define("_PL_WILLEXPIRE","Afstemningen vil lukke den");
+define("_PL_HASEXPIRED","Afstemningen blev lukket den");
+
+?>
---
+++ XoopsLanguages/danish/modules/xoopspoll/xoopspool_1.3_danish_UTF-8/xoopspool/language/english/modinfo.php
@@ -0,0 +1,24 @@
+<?php
+// $Id: modinfo.php,v 1.8 2004/12/26 19:12:13 onokazu Exp $
+// Module Info
+
+// The name of this module
+define("_MI_POLLS_NAME","Polls");
+
+// A brief description of this module
+define("_MI_POLLS_DESC","Shows a poll/survey block");
+
+// Names of blocks for this module (Not all module has blocks)
+define("_MI_POLLS_BNAME1","Polls");
+
+// Names of admin menu items
+define("_MI_POLLS_ADMENU1","List Polls");
+define("_MI_POLLS_ADMENU2","Add Poll");
+
+//Module properties
+define("_MI_POLL_LIMITBYIP","Restrict voting from the same IP");
+define("_MI_POLL_LIMITBYIPD","");
+define("_MI_POLL_LIMITBYUID","Restricting voting from the same User");
+define("_MI_POLL_LIMITBYUIDD","");
+
+?>
