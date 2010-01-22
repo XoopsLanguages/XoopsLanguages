eab7af13e2314e67a4d5c6bd12969e6f19d13f4d
voltan1 <voltan1@c17157d5-08f2-41e7-a131-df055f6a0da3>
2010-01-22 17:30:58 +0000

    add persian language for xoops 2.4.3

---
+++ XoopsLanguages/persian/core/releases/2.4.3/htdocs/language/persian/banners.php
@@ -0,0 +1,78 @@
+<?php
+/*
+ You may not change or alter any portion of this comment or credits
+ of supporting developers from this source code or any supporting source code
+ which is considered copyrighted (c) material of the original comment or credit authors.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+/**
+* Xoops Language
+*
+* @copyright The XOOPS Project http://sourceforge.net/projects/xoops/
+* @license http://www.fsf.org/copyleft/gpl.html GNU public license
+* @package kernel
+* @subpackage Xoops Banners Language
+* @since 2.0.0
+* @author Kazumi Ono <onokazu@xoops.org>
+* @version $Id$
+*/
+defined('XOOPS_ROOT_PATH') or die('Restricted access');
+// _LANGCODE: fa
+// _CHARSET : UTF-8
+// Translator: XOOPS Translation Team
+define('_BANNERS_MANAGEMENT', 'مدیریت تبلیغات');
+define('_BANNERS_WELCOMEBACK', 'خوش آمدید: %s');
+define('_BANNERS_LOGGEDOUT', 'از بخش مدیریت تبلیغات خارج شده اید');
+define('_BANNERS_LOGOUT', 'خروج از مدیریت تبلیغات');
+define('_BANNERS_LOGIN_TITLE','آمار تبلیغات');
+define('_BANNERS_LOGIN_LOGIN','ورود :');
+define('_BANNERS_LOGIN_INCORRECT', 'نام کاربری یا واژه رمز صحیح نیست');
+define('_BANNERS_LOGIN_PASS','رمزعبور :');
+define('_BANNERS_LOGIN_INFO','لطفا اطلاعات مشتری را وارد کنید');
+define('_BANNERS_LOGIN_OK','تایید');
+define('_BANNERS_ID','شماره‌ی بنر:');
+define('_BANNERS_TITLE','بنر‌های فعال فعلی برای %s');
+define('_BANNERS_URL','تغییر آدرس بنر:');
+define('_BANNERS_NOTHINGFOUND', 'هیچ اطلاعاتی یافت نشد');
+define('_BANNERS_NO', '#');
+define('_BANNERS_FOW_IN','در ادامه بنر‌های فعال شما دیده می‌شوند ');
+define('_BANNERS_IMP_MADE','بازدید انجام شده');
+define('_BANNERS_IMP_TOTAL','کل بازدید');
+define('_BANNERS_IMP_LEFT','بازدید باقی‌مانده');
+define('_BANNERS_CLICKS','کلیک ها');
+define('_BANNERS_PER_CLICKS','% کلیک');
+define('_BANNERS_FUNCTIONS','توابع');
+define('_BANNERS_STATS','پست‌الکترونیکی آمار');
+define('_BANNERS_SHOWBANNER', 'نمایش بنر');
+define('_BANNERS_SEND_STATS','<a href=\'%s\' title=\'E-mail Stats\'/>پست‌الکترونیکی آمار</a> را برای این بنرها ارسال کن');
+define('_BANNERS_POINTS','این بنر به <a href=\'%s\' title=\'\'>این آدرس</a> می‌رود');
+define('_BANNERS_UNLIMITED','نا‌محدود');
+define('_BANNERS_FINISHED','بنر در تاریخ %s خاتمه پیدا می‌کند');
+define('_BANNERS_STARTED','تاریخ شروع');
+define('_BANNERS_ENDED','تاریخ پایان');
+define('_BANNERS_MAIL_SUBJECT','آمار بنر‌های شما در %s');
+define('_BANNERS_MAIL_MESSAGE', 'آمار تبلیغات در دسترس برای بنر انتخاب شده در %s :\n\n\n
+نام مشتری: %s\nشماره بنر: %s\n
+تصویر بنر: %s\n
+آدرس بنر: %s\n\n
+تعداد بازدید خریداری‌شده: %s\n
+تعداد بازدید انجام‌شده: %s\n
+داد بازدید باقی‌مانده: %s\n
+تعداد کلیک انجام‌شده: %s\n
+درصد کلیک به تعداد بازدید: %f \n\n\n
+گزارش در تاریخ مقابل ایجاد شد: %s');
+define('_BANNERS_MAIL_NOT_OK', 'ما با یک خطا در ارسال ایمیل به شما مواجه شده ایم. لطفا با مدیر سایت تماس بگیرید و این مشکل را گزارش دهید.');
+define('_BANNERS_MAIL_OK','آمار بنر شما به آدرس پست‌الکترونیکی‌تان ارسال شد.');
+define('_BANNERS_MAIL_ERROR','هیچ پست‌الکترونیکی برای این مشتری اختصاص نیافته‌است %s.<br />لطفا با مدیر سایت تماس بگیرید');
+define('_BANNERS_DBUPDATED', 'گزینه ها ویرایش شدند و پایگاه داده ها به روز شد');
+define('_BANNERS_DBERROR', 'پایگاه داده ها به روز نشده است و یک خطا روی داده است!');
+define('_BANNERS_CHANGE', 'تغییر');
+
+define('_BANNERS_NO_LOGIN_DATA', 'هیچ اطلاعات ورودی شناسایی نشد');
+define('_BANNERS_NO_REFERER', 'هیچ اجاع دهنده ای شناسایی نشد');
+define('_BANNERS_NO_ID', 'هیچ ID معتبری شناسایی نشد');
+?>
---
+++ XoopsLanguages/persian/core/releases/2.4.3/htdocs/language/persian/comment.php
@@ -0,0 +1,66 @@
+<?php
+/*
+ You may not change or alter any portion of this comment or credits
+ of supporting developers from this source code or any supporting source code 
+ which is considered copyrighted (c) material of the original comment or credit authors.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+/**
+ *  Xoops Language
+ *
+ * @copyright       The XOOPS Project http://sourceforge.net/projects/xoops/
+ * @license         http://www.fsf.org/copyleft/gpl.html GNU public license
+ * @package         kernel
+ * @subpackage      Xoops Comment Language
+ * @since           2.0.0
+ * @author          Kazumi Ono <onokazu@xoops.org>
+ * @version         $Id$
+ */
+defined('XOOPS_ROOT_PATH') or die('Restricted access');
+
+// _LANGCODE: fa
+// _CHARSET : UTF-8
+// Translator: XOOPS Translation Team
+define('_CM_TITLE','عنوان');
+define('_CM_MESSAGE','پیام');
+define('_CM_DOSMILEY','امکان گذاشتن شکلک');
+define('_CM_DOHTML','اجازه HTML');
+define('_CM_DOAUTOWRAP','شکستن خود به خود خط‌ها');
+define('_CM_DOXCODE','اجازه‌ی کدهای کلیدی');
+define('_CM_REFRESH','باز‌بینی (refresh)');
+define('_CM_PENDING','معلق');
+define('_CM_HIDDEN','مخفی');
+define('_CM_ACTIVE','فعال');
+define('_CM_STATUS','وضعیت');
+define('_CM_POSTCOMMENT','ارسال نظر');
+define('_CM_REPLIES','پاسخ‌ها');
+define('_CM_PARENT','بالاسری');
+define('_CM_TOP','بالا');
+define('_CM_BOTTOM','پایین');
+define('_CM_ONLINE','آن‌لاین');
+define('_CM_POSTED','فرستاده‌شده در تاریخ'); // Posted date
+define('_CM_UPDATED', 'به روز‌شده در تاریخ');
+define('_CM_THREAD','شاخه');
+define('_CM_POSTER','فرستنده');
+define('_CM_JOINED','عضویت از');
+define('_CM_POSTS','پیام');
+define('_CM_FROM','از');
+define('_CM_COMDELETED', 'نظرها پاک شد');
+define('_CM_COMDELETENG', 'موفق به پاک‌سازی نظرها نشدیم');
+define('_CM_DELETESELECT' , 'پاک‌سازی تمامی زیر نظرها');
+define('_CM_DELETEONE' , 'نه، تنها این نظر را حذف‌کن');
+define('_CM_DELETEALL', 'بله، همه را حذف‌کن');
+define('_CM_THANKSPOST', 'با تشکر از پیام شما');
+define('_CM_NOTICE', 'بی‌شک دیدگاه هر کس نشانه‌ی تفکر اوست، ما در برابر نظر دیگران مسئول نیستیم');
+define('_CM_COMRULES','شرایط نظر');
+define('_CM_COMAPPROVEALL','هر نظری پذیرفته ‌شود');
+define('_CM_COMAPPROVEUSER','تنها نظرهای کاربران عضو پذیرفته ‌شود');
+define('_CM_COMAPPROVEADMIN','همه‌ی نظرها نیاز به تایید مدیر سایت دارند');
+define('_CM_COMANONPOST','اجازه‌ی فرستنده ناشناس');
+define('_CM_COMNOCOM','غیر فعال‌کردن نظر‌خواهی');
+
+?>
