8d0de1cad2c92a23f8408e7193a9ec563a50e79a
voltan1 <voltan1@c17157d5-08f2-41e7-a131-df055f6a0da3>
2010-01-22 17:30:58 +0000

    add persian language for xoops 2.4.3

---
+++ persian/core/releases/2.4.3/htdocs/language/persian/uploader.php
@@ -0,0 +1,52 @@
+<?php
+/*
+ You may not change or alter any portion of this comment or credits
+ of supporting developers from this source code or any supporting source code 
+ which is considered copyrighted (c) material of the original comment or credit authors.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+/**
+ *  Xoops Language
+ *
+ * @copyright       The XOOPS Project http://sourceforge.net/projects/xoops/
+ * @license         http://www.fsf.org/copyleft/gpl.html GNU public license
+ * @package         kernel
+ * @subpackage      Xoops Uploader Language
+ * @since           2.0.0
+ * @author          Kazumi Ono <onokazu@xoops.org>
+ * @version         $Id$
+ */
+defined('XOOPS_ROOT_PATH') or die('Restricted access');
+
+// $Id$
+// _LANGCODE: fa
+// _CHARSET : UTF-8
+// Translator: XOOPS Translation Team
+
+define('_ER_UP_MIMETYPELOAD', 'خطا در هنگام بارگذاری نوع پرونده');
+define('_ER_UP_FILENOTFOUND', 'هیچ پرونده‌ای پیدا نشد');
+define('_ER_UP_INVALIDFILESIZE', 'اندازه‌ی پرونده‌ی نامعتبر');
+define('_ER_UP_FILENAMEEMPTY', 'نام پرونده خالی است');
+define('_ER_UP_NOFILEUPLOADED', 'هیچ پرونده‌ای بارگذاری نشد');
+define('_ER_UP_ERROROCCURRED', 'یک خطا روی داده: خطای #%s');
+define('_ER_UP_UPLOADDIRNOTSET', 'شاخه‌ی بارگذاری تنظیم نشده');
+define('_ER_UP_FAILEDOPENDIR', 'خطا در بازکردن شاخه: %s');
+define('_ER_UP_FAILEDOPENDIRWRITE', 'خطا در بازکردن شاخه با دسترسی نوشتن: %s');
+define('_ER_UP_FILESIZETOOLARGE', 'پرونده خیلی بزرگ است (بیشترین اندازه %u بایت): %u bytes');
+define('_ER_UP_FILEWIDTHTOOLARGE', 'عرض پرونده از اندازه‌ی مشخص‌شده بیشتر است (بیشترین اندازه‌ی %u پیکسل): %u پیکسل');
+define('_ER_UP_FILEHEIGHTTOOLARGE', 'ارتفاع پرونده از اندازه‌ی مشخص شده بیشتر است (بیشترین اندازه‌ی %u پیکسل): %u پیکسی');
+define('_ER_UP_MIMETYPENOTALLOWED', 'این نوع پرونده نامعتبر است: %s');
+define('_ER_UP_FAILEDUPLOADFILE', 'عدم موفقیت در بارگذاری پرونده: %s');
+define('_ER_UP_FAILEDFETCHIMAGESIZE', 'خطا در پیداکردن اندازه‌ی تصویر %s ،حداکثر اندازه تصویر نادیده گرفته شد..');
+define('_ER_UP_UNKNOWNFILETYPEREJECTED', 'پرونده‌های نامشخص رد می‌شوند');
+define('_ER_UP_ERRORSRETURNED', 'خطا در هنگام بارگذاری فایل : %s');
+define('_ER_UP_INVALIDIMAGEFILE', 'تصویر نامعتبر');
+define('_ER_UP_SUSPICIOUSREFUSED', 'تصور مشکوک بارگذاری نمی‌شود');
+define('_ER_UP_INVALIDFILENAME', 'نام پرونده نامعتبر است');
+define('_ER_UP_FAILEDSAVEFILE', 'عدم موفقیت در ذخیره‌ی پرونده در %s');
+
+?>
---
+++ persian/core/releases/2.4.3/htdocs/modules/system/class/gui/oxygen/language/persian/admin.php
@@ -0,0 +1,84 @@
+<?php
+// $Id$
+
+define('_AD_SYSOPTIONS','گزینه‌های سیستم');
+define('_AD_INSTALLEDMODULES','نصب ماژول ها');
+
+define('_AD_XOOPSTHEMES','قالب‌های زوپس');
+define('_AD_XOOPSMODULES','ماژول های زوپس');
+define('_AD_INTERESTSITES','لینک ها');
+define('_AD_LOCALSUPPORT','پشتیبانی محلی');
+define('_XOOPS_NEWS','اخبار زوپس');
+define('_XOOPS_WEBSITE','سایت زوپس');
+define('_SOURCEFORGE','پروژه زوپس');
+
+define('_MD_OXYGEN_VERSION','نسخه');
+define('_MD_OXYGEN_VERSION_XOOPS','نسخه زوپس');
+define('_MD_OXYGEN_VERSION_PHP','نسخه php');
+define('_MD_OXYGEN_VERSION_MYSQL','نسخه mysql');
+define('_MD_OXYGEN_Server_API','API کارگذار');
+define('_MD_OXYGEN_OS','سیستم عامل');
+define('_MD_OXYGEN_ABOUT','درباره زوپس');
+define('_MD_OXYGEN_ABOUT_TEXT','برنامه مدیریت محتوای زوپس, CMS قدرتمندی‌است كه با زبان PHP نوشته شده و با بانك اطلاعای MySQL همخوانی دارد، شما با استفاده از این برنامه‌ی كاملا رایگان، قادر خواهید بود برای شركت و یا گروه خود، مراكز دانلود، انجمن‌های بحث و گفتگو، مراكز و وبلاگ های خبری ، وبلاگ ها و وب سایت های شخصی ، فروشگاه و ویترین معرفی محصولات و خدمات ، گالری تصاویر یا ویدیو و... ایجاد كنید. برای کسب اطلاعات بیشتر صفحه <a href="http://www.xoops.org/modules/wfchannel/" rel="external">درباره زوپس</a> را مطالعه فرمایید.');
+define('_MD_OXYGEN_XOOPS_LINKS','لینک های زوپس');
+
+define('_MD_OXYGEN_XOOPSPROJECT','پروژه زوپس');
+define('_MD_OXYGEN_LOCALSUPPORT','سایت های پشتیبان محلی');
+define('_MD_OXYGEN_XOOPSCORE','هسته زوپس');
+define('_MD_OXYGEN_XOOPSTHEME','قالب های زوپس');
+define('_MD_OXYGEN_XOOPSWIKI','دانشنامه زوپس');
+define('_MD_OXYGEN_XOOPSBOOKS','کتاب های زوپس');
+define('_MD_OXYGEN_NEWMODULE','ماژول های جدید');
+define('_MD_OXYGEN_XOOPSFAQ','پرسش های همگانی');
+define('_MD_OXYGEN_CODESVN','مخازن SVN');
+define('_MD_OXYGEN_REPORTBUG','گزارش ایرادات');
+define('_MD_OXYGEN_XGIFTSHOP','فروشگاه هدایای زوپس');
+define('_MD_OXYGEN_XDONATIONS','کمک به پروژه زوپس');
+define('_MD_OXYGEN_XDONATIONS_TEXT','<h2>We Need Your Help - Make a Donation!</h2>
+اگر شما به سیستم زوپس علاقه دارید, و دوست دارید در برگشت چیز های دریافت کنید, شما میتوانید به بنیاد زوپس کمک کنید! با استفاده از حساب PayPal مبلغ مورد نظر خود را به حساب بنیاد زوپس منتقل کنید.<br>
+تمام پرداخت های شما برای ترویج و پیشرفت استفاده و توسعه زوپس هزینه خواهد شد.<br>
+بنیاد زوپس به عنوان یک سازمان بدون منفعت مالی در آمریکا ثبت شده است, و در راستای تقویت زوپس فعالیت میکند!<br>
+تمام کمک کنندگان در وب سایت رسمی سایت زوپس رنک دوستان زوپس را دریافت خواهند کرد. از تمام دوستان زوپس به خاطر حمایت های مالی و دنباله دارشان سپاسگذاریم<br>
+برای کمک به زوپس و کسب اطلاعات بیشتر به  <a href="http://www.xoops.org/modules/xdonations/" rel="external">اینجا</a>مراجعه کنید.');
+
+define('_MD_OXYGEN_SITEPREF','ویژگی ها');
+define('_MD_OXYGEN_GENERAL','تنضیمات اصلی');
+define('_MD_OXYGEN_USERSETTINGS','تنظیمات کاربران');
+define('_MD_OXYGEN_METAFOOTER','کلمات کلیدی و فوتر');
+define('_MD_OXYGEN_CENSOR','سانسور کلمات');
+define('_MD_OXYGEN_SEARCH','گزینه های جستجو');
+define('_MD_OXYGEN_MAILER','تنظیمات میل');
+define('_MD_OXYGEN_AUTHENTICATION','گزینه‌های تایید سندیت');
+
+//add for styles name
+
+define('_MD_LANG_SILVER','نقره ای');
+define('_MD_LANG_DARK','تیره');
+define('_MD_LANG_ORANGE','نارنجی');
+
+// help
+define('_MD_AM_BANS_HELP','در اینجا میتوانید بنر های <br>تبلیغاتی سایت را مدیریت کنید');
+define('_MD_AM_BKAD_HELP','با استفاده از بلاک ها شما <br>میتوانید قسمت های زیادی را <br>برای کاربران خود اضافه کنید');
+define('_MD_AM_ADGS_HELP','شما میتوانید گروه های جدید اضافه کنید <br>و میزان دسترسی هر گروه را به<br> بخش های مختلف سایت مشخص کنید');
+define('_MD_AM_IMAGES_HELP','شما میتوانید تصاویر مورد نیاز<br> خود را اینجا اضافه کنید <br>و آنها را مدیریت کنید');
+define('_MD_AM_MDAD_HELP','در اینجا شما میتوانید ماژول های زوپس <br>خود را نصب کنید و یا آنها را مدیریت کنید .<br> میدانید ماژول ها به چه درد میخورد؟');
+define('_MD_AM_PREF_HELP','هسته زوپس و تمام ماژول هایی که نصب کرداید<br> دارای بخشی برای تنضیمات کلی هستند !!!<br> میتوانید گزینه های ماژول ها را مدیریت کنید');
+define('_MD_AM_SMLS_HELP','شما و کاربرانتان میتوانید <br>از شکلک های این بخش در تمام <br>قسمت های سایت استفاده کنید !!!<br> شکلک جدید اضافه کنید.!!!');
+define('_MD_AM_RANK_HELP','در اینجا میتوانید برای کاربران<br> نشان ( رنک ) اضافه کنید ');
+define('_MD_AM_USER_HELP','در این قسمت میتوانید کاربران جدید اضافه کنید<br> و یا اطلاعات کاربران خود را ویرایش کنید<br> . دسترسی ها و گروه های آنها را عوض کنید');
+define('_MD_AM_FINDUSER_HELP','بعضی وقت ها لازم دارید <br>به اطلاعات کاربران خود دسترسی پیدا کنید !!<br> از اینجا میتوانید کاربران خود را پیدا کنید');
+define('_MD_AM_MLUS_HELP','برای ارسال ایمیل و پیام شخصی گروهی<br> به اعضا از این گزینه استفاده کنید');
+define('_MD_AM_AVATARS_HELP','چند آواتور برای کاربران خود اضافه کنید<br> تا از آنها در سایت استفاده کنند');
+define('_MD_AM_TPLSETS_HELP','در این قسمت میتوانید اگو ( تمپلیت )<br> ماژول های خود را ویرایش کنید');
+define('_MD_AM_COMMENTS_HELP','کاربران شما میتوانند در بخش های مختلف سایت<br> مثل گالری و اخبار و ... نظر ارسال کنند .<br> نظر های آنها را دراین قسمت مدیریت کنید');
+
+//Add 10 des
+define('_MD_OXYGEN_XOOPS_LICENSE','مجوز زوپس');
+define('_MD_OXYGEN_KEY','کلید زوپس');
+define('_MD_OXYGEN_LICENSE','مجوز');
+define('_MD_OXYGEN_RSS','خوراک');
+define('_MD_OXYGEN_POWERED_BY','Powered by');
+define('_MD_OXYGEN_SOURCEFORGE_TEXT','XOOPS Code hosted on SourceForge');
+define('_MD_OXYGEN_ADMINISTRATION','مدیریت زوپس');
+
+?>
