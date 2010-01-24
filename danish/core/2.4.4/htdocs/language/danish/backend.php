79ea9fe4707ee4d3ead07237e89e1fe06c4b94ef
Anders Kristiansen <anderssk@users.noreply.github.com>
2010-01-24 09:43:01 +0000

    2.4.4 incl. logger.php

---
+++ danish/core/2.4.4/htdocs/language/danish/backend.php
@@ -0,0 +1,13 @@
+<?php
+// $Id: admin.php 2752 2009-02-02 13:48:42Z phppp $
+
+// _LANGCODE: da
+// _CHARSET : UTF-8
+// Translator: Anders Kristiansen (anderssk) 
+defined('XOOPS_ROOT_PATH') or die('Begrænset adgang');
+// RSS feed URLs
+
+return array(
+    'http://www.xoops.org/backend.php',
+    );
+?>
