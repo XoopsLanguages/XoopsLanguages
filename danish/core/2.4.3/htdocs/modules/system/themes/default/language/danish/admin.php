e989db500a1a3a4ffea3bc9f759ee6ccf42051cc
Michael Albertsen <culex@culex.dk>
2010-01-26 20:10:55 +0000

    dk language for system/themes/default/language/danish

---
+++ danish/core/2.4.3/htdocs/modules/system/themes/default/language/danish/admin.php
@@ -0,0 +1,76 @@
+<?php
+// $Id$
+define('_AD_SYSOPTIONS','System Indstillinger');
+define('_AD_INSTALLEDMODULES','Installerede moduler');
+
+define('_AD_XOOPSTHEMES','XOOPS Themes');
+define('_AD_XOOPSMODULES','XOOPS Moduler');
+define('_AD_INTERESTSITES','Links');
+define('_AD_LOCALSUPPORT','Lokal Support');
+define('_XOOPS_NEWS','Xoops Nyheder');
+define('_XOOPS_WEBSITE','XOOPS');
+define('_SOURCEFORGE','Sourceforge');
+
+define('_MD_OXYGEN_VERSION','Version');
+define('_MD_OXYGEN_VERSION_XOOPS','XOOPS Version');
+define('_MD_OXYGEN_VERSION_PHP','PHP Version');
+define('_MD_OXYGEN_VERSION_MYSQL','mySQL Version');
+define('_MD_OXYGEN_Server_API','Server API');
+define('_MD_OXYGEN_OS','OS');
+define('_MD_OXYGEN_ABOUT','Om XOOPS');
+define('_MD_OXYGEN_ABOUT_TEXT','XOOPS er et udvidbart, OO (Objekt orienteret), let at anvende, dynamisk web system skrevet i PHP. XOOPS er det ideelle værktøj til at udvikle små til større dynamiske community hjemmesider, Intra virksomheds portaler, forretningshjemmesider, weblogs og meget mere. Læs <a href="http://www.xoops.org/modules/wfchannel/" rel="external">Alt om XOOPS</a> siden for flere detaljer.');
+define('_MD_OXYGEN_XOOPS_LINKS','XOOPS Links');
+
+define('_MD_OXYGEN_XOOPSPROJECT','XOOPS Projektet');
+define('_MD_OXYGEN_LOCALSUPPORT','XOOPS Lokal supports');
+define('_MD_OXYGEN_XOOPSCORE','XOOPS Core');
+define('_MD_OXYGEN_XOOPSTHEME','XOOPS Themaer');
+define('_MD_OXYGEN_XOOPSWIKI','XOOPS Wiki');
+define('_MD_OXYGEN_XOOPSBOOKS','XOOPS Bøger');
+define('_MD_OXYGEN_NEWMODULE','New Moduler');
+define('_MD_OXYGEN_XOOPSFAQ','XOOPS FAQ');
+define('_MD_OXYGEN_CODESVN','Code SVN');
+define('_MD_OXYGEN_REPORTBUG','Indberet fejl');
+define('_MD_OXYGEN_XGIFTSHOP','XOOPS merchandise');
+define('_MD_OXYGEN_XDONATIONS','Donér til XOOPS');
+define('_MD_OXYGEN_XDONATIONS_TEXT','<h2>Vi har brug for din hjælp - Giv et bidrag! </H2><br/>Hvis du kan lide XOOPS\' system, og vil gerne give noget til gengæld, kan du give en donation til XOOPS instituttet! Du skal blot bruge PayPal betalingssystem til at overføre din donation til instituttets konto. <br/>Dit bidrag vil blive brugt til at fremme - og fremme brugen og udviklingen af XOOPS systemet. Garanteret. <br/>XOOPS Instituttet er en registreret ikke-for-profit organisation i USA, dedikeret til at fremme XOOPS! <br/>Alle donorer vil få den ven af XOOPS rang her på denne hjemmeside. Takket være Venner af XOOPS for deres donationer og fortsatte støtte <br/>
+For Make a Donation og mere info bedes du visite <a href="http://www.xoops.org/modules/xdonations/" rel="external"> her </a>');
+define('_MD_OXYGEN_SITEPREF','Præferencer');
+define('_MD_OXYGEN_GENERAL','Generalle indstillinger');
+define('_MD_OXYGEN_USERSETTINGS','Bruger info indstillinger');
+define('_MD_OXYGEN_METAFOOTER','Meta Tags og Footer');
+define('_MD_OXYGEN_CENSOR','Censur indstillinger');
+define('_MD_OXYGEN_SEARCH','Søge options');
+define('_MD_OXYGEN_MAILER','Mail Setup');
+define('_MD_OXYGEN_AUTHENTICATION','Godkendelses indstillinger');
+
+//Add for styles name
+define('_MD_LANG_SILVER','Sølv');
+define('_MD_LANG_DARK','Mørk');
+define('_MD_LANG_ORANGE','Orange');
+
+//help
+define ( '_MD_AM_BANS_HELP', 'Administrer dine bannere <br> herfra');
+define ( '_MD_AM_BKAD_HELP', 'Styr system og modul blokke <br>, der giver ekstra info til dig eller dine brugere');
+define ( '_MD_AM_ADGS_HELP', 'Tilføj/Rediger grupper og <br> administrere tilladelser <br> for hver gruppe');
+define ( '_MD_AM_IMAGES_HELP', 'Opret kategori for dine billeder <br> og tilføje billeder i kategorier');
+define ( '_MD_AM_MDAD_HELP', 'Installere og afinstallere din XOOPS moduler. <br> Tjek på modulstatus');
+define ( '_MD_AM_PREF_HELP', 'Administrer Xoops og individuelle <br> modulers indstillinger');
+define ( '_MD_AM_SMLS_HELP', 'Administrer smiliesikoner, der skal anvendes i <br> dine indlæg, PMS, kommentarer, osv.');
+define ( '_MD_AM_RANK_HELP', 'Administrer brugerrang');
+define ( '_MD_AM_USER_HELP', 'Tilføj/Rediger bruger og <br> hans/hendes oplysninger');
+define ( '_MD_AM_FINDUSER_HELP', 'Find en bruger, baseret på de foreliggende <br> detaljer, fx e-mail, brugernavn');
+define ( '_MD_AM_MLUS_HELP', 'Send e-mail eller PM til udvalgte brugere');
+define ( '_MD_AM_AVATARS_HELP', 'Tilføj avatarer til dine brugere, <br> der kan anvendes i deres profiler');
+define ( '_MD_AM_TPLSETS_HELP', 'Ændre/tilpas Xoops skabeloner');
+define ( '_MD_AM_COMMENTS_HELP', 'Administrer bruger kommentarer');
+
+//Add 10 des
+define ( '_MD_OXYGEN_XOOPS_LICENSE', 'Xoops Licens');
+define ( '_MD_OXYGEN_KEY', 'Xoops Key');
+define ( '_MD_OXYGEN_LICENSE', 'Licens');
+define ( '_MD_OXYGEN_RSS', 'RSS feed');
+define ( '_MD_OXYGEN_POWERED_BY', 'Powered by');
+define ( '_MD_OXYGEN_SOURCEFORGE_TEXT', 'Xoops Code på SourceForge');
+define ( '_MD_OXYGEN_ADMINISTRATION', 'Xoops administration');
+?>
