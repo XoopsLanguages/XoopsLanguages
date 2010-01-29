dabe481ec4a9dcdf9de6f098a412bae165837480
voltan1 <voltan1@c17157d5-08f2-41e7-a131-df055f6a0da3>
2010-01-29 11:20:37 +0000

    Update to xoopspoll 1.3

--- XoopsLanguages/persian/modules/xoopspoll/trunk/language/persian/blocks.php
+++ XoopsLanguages/persian/modules/xoopspoll/trunk/language/persian/blocks.php
@@ -1,7 +1,6 @@
 <?php
-// $Id: blocks.php,v 1.3 2003/10/23 13:16:41 wellwine Exp $
-// Blocks
-define('_BL_POLL_EDIT_SHOWRESULTS', 'بعد از اتمام نظر سنجی آیا برای کسی که رای داده است نتایج نشان داده شوند؟ (0=نه, 1=بله)');
-define('_BL_POLL_EDIT_SHOWPERCENT', 'درصد در نتایج نشان داده شود؟ (0=نه, 1=بله)');
-define('_BL_POLL_EDIT_SHOWBAR', 'ستون ها در نتایج نشان داده شود (0=نه, 1=بله)'); 
+// $Id: blocks.php,v 1.8 2004/12/26 19:12:13 onokazu Exp $// Blocks
+define("_PL_WILLEXPIRE","نظرسنجی در تاریخ زیر بسته خواهد شد");
+define("_PL_HASEXPIRED","نظرسنجی در تاریخ زیر بسته شده است");
+
 ?>
--- XoopsLanguages/persian/modules/xoopspoll/trunk/language/persian/main.php
+++ XoopsLanguages/persian/modules/xoopspoll/trunk/language/persian/main.php
@@ -1,22 +1,24 @@
 <?php
 // $Id: main.php,v 1.1 2003/10/14 12:15:34 wellwine Exp $
+
 //%%%%%%    File Name pollresults.php     %%%%%
 define("_PL_TOTALVOTES","همه رای ها: %s");
 define("_PL_TOTALVOTERS","همه رای دهنده ها: %s");
 
 //%%%%%%    File Name index.php     %%%%%
-define("_PL_POLLSLIST","فهرست نظر سنجی ها");
+define("_PL_POLLSLIST","فهرست نظرسنجی ها");
 define("_PL_ALREADYVOTED", "متاسفانه، شما یک بار رای داده اید.");
 define("_PL_THANKSFORVOTE","با تشکر از رای شما");
-define("_PL_SORRYEXPIRED", "متاسفانه تاریخ شرکت در این نظر سنجی به اتمام رسیده است.");
-define("_PL_YOURPOLLAT", "%s، نظر سنجی شما در سایت %s"); // 1st %s is user name, 2nd %s is site name
+define("_PL_SORRYEXPIRED", "متاسفانه تاریخ شرکت در این نظرسنجی به اتمام رسیده است.");
+define("_PL_YOURPOLLAT", "%s، نظرسنجی شما در سایت %s"); // 1st %s is user name, 2nd %s is site name
 define("_PL_PREV", "قبلی");
 define("_PL_NEXT", "بعدی");
-define("_PL_POLLQUESTION", "سوال نظر سنجی");
+define("_PL_POLLQUESTION", "سوال نظرسنجی");
 define("_PL_VOTERS", "همه رای دهنده ها");
 define("_PL_VOTES", "همه رای ها");
-define("_PL_EXPIRATION", "تاریخ به پایان رسیدن نظر سنجی");
+define("_PL_EXPIRATION", "تاریخ به پایان رسیدن نظرسنجی");
 define("_PL_EXPIRED", "به اتمام رسیده است");
+define("_PL_MUSTLOGIN", "برای شرکت در نظرسنجی بایدبه سایت وارد شوید.");
 
 //%%%%%%    File Name xoopspollrenderer.php     %%%%%
 // %s represents date
