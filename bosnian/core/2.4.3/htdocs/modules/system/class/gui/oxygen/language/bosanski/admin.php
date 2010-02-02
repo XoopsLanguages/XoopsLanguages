715f7a2bd0de3f16f98bc7ed24c16b045e076468
mujak8 <Mujakazi@users.noreply.github.com>
2010-02-02 00:35:43 +0000

    xoops 2.4.3 komplet prevod bosanski

---
+++ bosnian/core/2.4.3/htdocs/modules/system/class/gui/oxygen/language/bosanski/admin.php
@@ -0,0 +1,83 @@
+<?php
+// $Id$
+
+define('_AD_SYSOPTIONS','Sistemske opcije');
+define('_AD_INSTALLEDMODULES','Instalirani Moduli');
+
+define('_AD_XOOPSTHEMES','Theme');
+define('_AD_XOOPSMODULES','Moduli');
+define('_AD_INTERESTSITES','Linkovi');
+define('_AD_LOCALSUPPORT','Local Support');
+define('_XOOPS_NEWS','Xoops vijesti');
+define('ـXOOPS_WEBSITE','XOOPS');
+define('ـSOURCEFORGE','Sourceforge');
+
+define('_MD_OXYGEN_VERSION','Verzija');
+define('_MD_OXYGEN_VERSION_XOOPS','Xoops Verzija');
+define('_MD_OXYGEN_VERSION_PHP','PHP Verzija');
+define('_MD_OXYGEN_VERSION_MYSQL','Mysql Verzija');
+define('_MD_OXYGEN_Server_API','Server API');
+define('_MD_OXYGEN_OS','OS');
+define('_MD_OXYGEN_ABOUT','O nama');
+define('_MD_OXYGEN_ABOUT_TEXT','g-orahovica.com, je spontano nastala 2006 prvo kao g-orahovica.net, ali je od aprila 2007 promjenila server i naziv domene na g-orahovica.com. Ovaj portal se privatno financira od vlasnika i namijenjen je za mjestane Orahovice ma gdje bili. <a href="http://www.g-orahovica.com/modules/contact/" rel="external">Kontakt forma</a> , ukoliko imate pitanja, pitajte nas.');
+define('_MD_OXYGEN_XOOPS_LINKS','Xoops Linkovi');
+
+define('_MD_OXYGEN_XOOPSPROJECT','Xoops Projekt');
+define('_MD_OXYGEN_LOCALSUPPORT','Xoops Local supports');
+define('_MD_OXYGEN_XOOPSCORE','Xoops Core');
+define('_MD_OXYGEN_XOOPSTHEME','Xoops Teme');
+define('_MD_OXYGEN_XOOPSWIKI','Xoops Wiki');
+define('_MD_OXYGEN_XOOPSBOOKS','Xoops Books');
+define('_MD_OXYGEN_NEWMODULE','Novi Moduli');
+define('_MD_OXYGEN_XOOPSFAQ','Xoops FAQ');
+define('_MD_OXYGEN_CODESVN','Code Svn');
+define('_MD_OXYGEN_REPORTBUG','Report Bug');
+define('_MD_OXYGEN_XGIFTSHOP','Xoops Gift Shop');
+define('_MD_OXYGEN_XDONATIONS','Budi nam Donator');
+define('_MD_OXYGEN_XDONATIONS_TEXT','<h2>&#381;eli&scaron; li nam pomo&#263;i - Uplati Donaciju!</h2>
+Webstranica se privatno financira od strane vlasnika i da bi ste mu pomogli, uplatite preko PayPal malu donaciju! Osim PayPal-a se mo&#382;ete i na druge na&#269;ine nov&#269;ane uplate izvr&scaron;iti.<br> 
+Za vi&scaron;e informacija u vezi Donacija pitajte putem kontakt formulara <a href="http://g-orahovica.com/modules/contact/ rel="external">Ovdje</a><br>
+Sa va&scaron;im Donacijama opstanak webstranice se osigurava i ima svjetliju budu&#263;nost!<br>
+Svi Donatori &#263;e automatski dobiti statut Donatora!<br>
+PayPal uplate izvr&scaron;ite  <a href="http://www.g-orahovica.com/modules/xdonations/" rel="external">ovdje</a>');
+
+define('_MD_OXYGEN_SITEPREF','Pode&scaron;avanja');
+define('_MD_OXYGEN_GENERAL','Generalna pode&scaron;avanja');
+define('_MD_OXYGEN_USERSETTINGS','Pode&scaron;avanja info &#268;lanova');
+define('_MD_OXYGEN_METAFOOTER','Meta Tagovi i dna stranice');
+define('_MD_OXYGEN_CENSOR','Opcija cenzurisanja rije&#269;i');
+define('_MD_OXYGEN_SEARCH','Opcija pretrage');
+define('_MD_OXYGEN_MAILER','Mail Setup');
+define('_MD_OXYGEN_AUTHENTICATION','Opcija autorizacije');
+
+//add for styles name 
+define('_MD_LANG_SILVER','Srebrni');
+define('_MD_LANG_DARK','Crni');
+define('_MD_LANG_ORANGE','Orange');
+
+//help
+define('_MD_AM_BANS_HELP','Manage your banners<br>from here');
+define('_MD_AM_BKAD_HELP','Manage system and module blocks<br>that provide extra info to you or your users');
+define('_MD_AM_ADGS_HELP','Add/Edit groups and <br>manage permissions <br> for each group');
+define('_MD_AM_IMAGES_HELP','Create Category for your images<br>and add images within category');
+define('_MD_AM_MDAD_HELP','Install and unistall your XOOPS modules.<br>Check on module status ');
+define('_MD_AM_PREF_HELP','Manage XOOPS and individual <br>modules preferences');
+define('_MD_AM_SMLS_HELP','Manage smilies icons to be used in <br>your posts, PMs, comments, etc.');
+define('_MD_AM_RANK_HELP','Manage user ranks');
+define('_MD_AM_USER_HELP','Add/Edit user and <br>his/her information');
+define('_MD_AM_FINDUSER_HELP','Find a user, based on available <br>details, e.g. email, user name');
+define('_MD_AM_MLUS_HELP','Send email or PM to selected users');
+define('_MD_AM_AVATARS_HELP','Add avatars for your users,<br>to be used in their profiles');
+define('_MD_AM_TPLSETS_HELP','Modify/Customize XOOPS templates');
+define('_MD_AM_COMMENTS_HELP','Manage user comments');
+
+//Add 10 des
+define('_MD_OXYGEN_XOOPS_LICENSE','XOOPS License');
+define('_MD_OXYGEN_KEY','XOOPS Key');
+define('_MD_OXYGEN_LICENSE','License');
+define('_MD_OXYGEN_RSS','Rss feed');
+define('_MD_OXYGEN_POWERED_BY','Powered by');
+define('_MD_OXYGEN_SOURCEFORGE_TEXT','XOOPS Code hosted on SourceForge');
+define('_MD_OXYGEN_ADMINISTRATION','XOOPS Administration');
+
+?>
