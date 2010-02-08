64e4549739c693a7060ebbaa914ebfec95a6eb45
anderssk <anderssk@c17157d5-08f2-41e7-a131-df055f6a0da3>
2010-02-08 19:42:07 +0000

    Files from 2.4.4 add to release 2.5.0-alpha1

---
+++ XoopsLanguages/danish/core/2.5.0-alpha1/htdocs/class/calendar/JSCal2/JSCal2/src/js/lang/da.js
@@ -0,0 +1,61 @@
+Calendar.LANG("da", "Danish", {
+
+        fdow: 1,                // first day of week for this locale; 0 = Sunday, 1 = Monday, etc.
+
+        goToday: "Til i dag",
+
+        today: "I dag",         // appears in bottom bar
+
+        wk: "uge",
+
+        weekend: "0,6",         // 0 = Sunday, 1 = Monday, etc.
+
+        AM: "am",
+
+        PM: "pm",
+
+        mn : [ "Januar",
+               "Februar",
+               "Marts",
+               "April",
+               "Maj",
+               "Juni",
+               "Juli",
+               "August",
+               "September",
+               "Oktober",
+               "November",
+               "December" ],
+
+        smn : [ "Jan",
+                "Feb",
+                "Mar",
+                "Apr",
+                "Maj",
+                "Jun",
+                "Jul",
+                "Aug",
+                "Sep",
+                "Okt",
+                "Nov",
+                "Dec" ],
+
+        dn : [ "Søndag",
+               "Mandag",
+               "Tirsdag",
+               "Onsdag",
+               "Torsdag",
+               "Fredag",
+               "Lørdag",
+               "Søndag" ],
+
+        sdn : [ "Sø",
+                "Ma",
+                "Ti",
+                "On",
+                "To",
+                "Fr",
+                "Lø",
+                "Sø" ]
+
+});
