34758bf03a73b4c1b0dc484c960bf15a639d7fbe
anderssk <anderssk@c17157d5-08f2-41e7-a131-df055f6a0da3>
2010-02-09 13:33:17 +0000

    avatars.php updated to 2.5.0-alpha1

--- XoopsLanguages/danish/core/2.5.0-alpha1/htdocs/modules/system/language/danish/admin/avatars.php
+++ XoopsLanguages/danish/core/2.5.0-alpha1/htdocs/modules/system/language/danish/admin/avatars.php
@@ -1,12 +1,33 @@
 <?php
-// $Id
-//%%%%%% Avatar Manager %%%%%
-define('_MD_AVATARMAN', 'Brugerikon kontrol');
+/**
+ * @copyright   The XOOPS Project http://sourceforge.net/projects/xoops/
+ * @license     http://www.fsf.org/copyleft/gpl.html GNU public license
+ * _LANGCODE    da
+ * _CHARSET     UTF-8
+ * _TRANSLATER	Anders Kristiansen (anderssk)
+ */
+// Navigation
+define( '_AM_SYSTEM_AVATAR_MANAGER', 'Avatar indstillinger' );
+define( '_AM_SYSTEM_AVATAR_MAIN', 'Avatar menu' );
+define( '_AM_SYSTEM_AVATAR_SYSTEM', 'System Avatars' );
+define( '_AM_SYSTEM_AVATAR_CUSTOM', 'Egne Avatars' );
+define( '_AM_SYSTEM_AVATAR_ADD', 'Tilføj Avatar' );
+define( '_AM_SYSTEM_AVATAR_EDIT', 'Redigér Avatar' );
 
-define('_MD_SYSAVATARS', 'Systembaserede brugerikoner');
-define('_MD_CSTAVATARS', 'Tilføjede brugerikoner');
-define('_MD_ADDAVT', 'Tilføj brugerikon');
-define('_MD_USERS', 'Brugere, der anvender denne brugerikon');
-define('_MD_RUDELIMG', 'Er du siker på, at du vil slette dette brugerikon?');
-define('_MD_FAILDEL', 'Det lykkedes ikke at slette brugerikon %s fra databasen');
+// Infos
+define( '_AM_SYSTEM_AVATAR_ERROR', 'Fejl' );
+define( '_AM_SYSTEM_AVATAR_USERS', 'Brugere der benytter denne avatar' );
+define( '_AM_SYSTEM_AVATAR_USE_FILE', 'Vælg filer i: %s' );
+define( '_AM_SYSTEM_AVATAR_UPLOAD', 'Upload:' );
+// Messages
+define( '_AM_SYSTEM_AVATAR_FAILDEL', 'Det lykkedes ikke at slette brugerikon %s fra databasen' );
+define( '_AM_SYSTEM_AVATAR_DELETE', 'Er du siker på, at du vil slette dette avartar billed?' );
+
+// Tips
+define( '_AM_SYSTEM_AVATAR_TIPS', '
+<ul id="newsticker" class="newsticker">
+<li>Vedligehold alle system og egne avatars</li>
+<li>Medlemmer kan valgfrit ændre deres egen personlige ikon som kaldes avatar.<br />Denne mulighed kan fjernes i Bruger indstillinger.</li>
+</ul>' );
+
 ?>
