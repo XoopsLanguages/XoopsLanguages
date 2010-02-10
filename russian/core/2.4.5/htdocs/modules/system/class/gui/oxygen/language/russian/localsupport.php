774a99f63df4ee6cf3a9eb37f7e89b7b40d9cc50
andrey3761 <andrey3761@c17157d5-08f2-41e7-a131-df055f6a0da3>
2010-02-10 20:56:25 +0000

    XOOPS 2.4.5 Beta : Russian translation is added.

---
+++ russian/core/2.4.5/htdocs/modules/system/class/gui/oxygen/language/russian/localsupport.php
@@ -0,0 +1,45 @@
+<?php
+// $Id$
+
+$menu = array();
+
+// sample for french support
+/*
+$menu[] = array(
+    'link'      => 'http://www.afux.org',
+    'title'     => 'AFUX',
+    'absolute'  => 1,
+    'icon'      => XOOPS_URL . '/modules/system/class/gui/oxygen/images/xoops.png'
+);
+
+$menu[] = array(
+    'link'      => 'http://www.frxoops.org',
+    'title'     => 'Xoops France',
+    'absolute'  => 1,
+    'icon'      => XOOPS_URL . '/modules/system/class/gui/oxygen/images/xoops.png'
+);
+
+$menu[] = array(
+    'link'      => 'http://www.frxoops.org/modules/library/',
+    'title'     => _AD_XOOPSTHEMES,
+    'absolute'  => 1,
+    'icon'      => XOOPS_URL . '/modules/system/class/gui/oxygen/images/tweb.png'
+);
+
+$menu[] = array(
+    'link'      => 'http://www.frxoops.org/modules/modules-search/',
+    'title'     => _AD_XOOPSMODULES,
+    'absolute'  => 1,
+    'icon'      => XOOPS_URL . '/modules/system/class/gui/oxygen/images/xoops.png'
+);
+*/
+
+$menu[] = array(
+    'link'      => 'http://xoops2.ru',
+    'title'     => 'Русскоязычная техподдержка',
+    'absolute'  => 1,
+    'icon'      => XOOPS_URL . '/modules/system/class/gui/oxygen/images/xoops.png'
+);
+
+return $menu;
+?>
