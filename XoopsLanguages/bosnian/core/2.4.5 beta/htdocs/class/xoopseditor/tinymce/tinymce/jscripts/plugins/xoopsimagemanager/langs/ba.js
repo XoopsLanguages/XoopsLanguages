38e1f937a5ec6f4498d2ced1f1cacec2219cd5a2
mujak8 <mujak8@c17157d5-08f2-41e7-a131-df055f6a0da3>
2010-02-13 17:39:25 +0000

    Xoops 2.4.5 komplet prevod na bosanski jezik

---
+++ XoopsLanguages/bosnian/core/2.4.5 beta/htdocs/class/xoopseditor/tinymce/tinymce/jscripts/plugins/xoopsimagemanager/langs/ba.js
@@ -0,0 +1,14 @@
+/**
+ * @version     $Id: en.js 3146 2009-04-16 17:50:38Z luciorota $
+ */
+
+tinyMCE.addI18n('en.xoopsimagemanager',{
+    desc : 'Xoops Advanced Imagemanager',
+    delta_width : '0',
+    delta_height : '0'
+});
+tinyMCE.addI18n('en.xoopsimagebrowser',{
+    desc : 'Xoops Imagebrowser',
+    delta_width : '0',
+    delta_height : '0'
+});
---
+++ XoopsLanguages/bosnian/core/2.4.5 beta/htdocs/modules/profile/language/bosanski/mail_template/emailchanged.tpl
@@ -0,0 +1,11 @@
+Halo {X_UNAME},
+
+Tvoja E-Mail-Adresa na {SITENAME} 
+je uspješno promijenjena. 
+
+Ukoliko imaš neko pitanje, 
+kontaktiraj  nas.
+
+-------------------------------------------------
+{SITENAME} ({SITEURL}) 
+{ADMINMAIL}
