50427ffaa238da15be8c6be16d3abee15e833ffd
mujak8 <Mujakazi@users.noreply.github.com>
2010-02-13 17:51:51 +0000

    delete unimportant folder

--- bosnian/modules/language/bosanski/blocks.php
+++
@@ -1,8 +0,0 @@
-<?php
-// $Id: blocks.php,v 1.9 2004/12/26 19:11:57 onokazu Exp $
-// Blocks
-define("_MB_MYLINKS_DISP","Prika&#382;i");
-define("_MB_MYLINKS_LINKS","Linkovi");
-define("_MB_MYLINKS_CHARS","Du&#382;ina naslova");
-define("_MB_MYLINKS_LENGTH"," karaktera(oznaka, slova)");
-?>
