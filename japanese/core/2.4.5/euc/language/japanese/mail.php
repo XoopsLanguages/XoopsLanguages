1d8dd627025f1fcfdca4b8ca6d1bce185947f066
ROUTE286 <ROUTE286@c17157d5-08f2-41e7-a131-df055f6a0da3>
2010-03-04 07:43:11 +0000

    Commit the version 2010/03/04 of Google Code (XOOPS Project: 2-8-6)

--- japanese/core/2.4.5/euc/language/japanese/mail.php
+++ japanese/core/2.4.5/euc/language/japanese/mail.php
@@ -27,9 +27,9 @@ defined('XOOPS_ROOT_PATH') or die('Restricted access');
 // Translator: XOOPS Translation Team
 define('_MAIL_MSGBODY', 'メッセージ本文が入っていません');
 define('_MAIL_FAILOPTPL', 'テンプレートファイルが開けません');
-define('_MAIL_FNAMENG', '名前が入っていりません');
-define('_MAIL_FEMAILNG', 'E-mailが入っていません);
-define('_MAIL_SENDMAILNG', '%s さん宛のメールが送信できませんでした");
+define('_MAIL_FNAMENG', '名前が入っていません');
+define('_MAIL_FEMAILNG', 'E-mailが入っていません');
+define('_MAIL_SENDMAILNG', '%s さん宛のメールが送信できませんでした');
 define('_MAIL_MAILGOOD', '%s さん宛へメールを送信しました');
 define('_MAIL_SENDPMNG', '%s さん宛のプライベートメッセージが送信できませんでした’);
 define('_MAIL_PMGOOD', '%s さん宛へプライベートメッセージを送信しました');
