8d07afddfa3f70e55627bff365a561e07e4b8427
ROUTE286 <ROUTE286@c17157d5-08f2-41e7-a131-df055f6a0da3>
2010-04-09 07:57:50 +0000

    delete a procedure to change mainfile.php when use protect in installation wizard

--- XoopsLanguages/japanese/core/2.4.5/utf-8/install/language/japanese/welcome.php
+++ XoopsLanguages/japanese/core/2.4.5/utf-8/install/language/japanese/welcome.php
@@ -6,32 +6,31 @@
 
 $content .= '
 <p>
-	<acronym title="eXtensible Object-Oriented Portal System">XOOPS</acronym> は、
-	オブジェクト指向ベースで作成された、PHPによるオープンソースのポータルサイト作成ソフトです。
-	規模の大小を問わず、ダイナミックなコミュニティーサイトや、イントラネット用のポータルサイト、企業のポータルサイト、ウェブログ、そういったものの作成に最適です。
+    <acronym title="eXtensible Object-Oriented Portal System">XOOPS</acronym> は、
+    オブジェクト指向ベースで作成された、PHP によるオープンソースのポータルサイト作成ソフトです。
+    規模の大小を問わず、ダイナミックなコミュニティサイトや、イントラネット用のポータルサイト、企業のポータルサイト、ウェブログ、そういったものの作成に最適です。
 </p>
 <p>
-	XOOPSは、
-	<a href="http://www.gnu.org/copyleft/gpl.html" target="_blank">GNU 一般公衆利用許諾契約書(GPL)</a>（参考：<a href="http://www.opensource.jp/gpl/gpl.ja.html" target="_blank">日本語訳</a>）
-	というライセンスでリリースされており、自由に使用し、また改変できます。
-	再配布もGPLの条項にしたがっていれば、自由に行うことができます。
+    XOOPSは、
+    <a href="http://www.gnu.org/copyleft/gpl.html" target="_blank">GNU 一般公衆利用許諾契約書(GPL)</a>（参考：<a href="http://www.opensource.jp/gpl/gpl.ja.html" target="_blank">日本語訳</a>）
+    というライセンスでリリースされており、自由に使用し、また改変できます。
+    再配布もGPLの条項にしたがっていれば、自由に行うことができます。
 </p>
 <h3>必要なソフトウエア</h3>
 <ul>
-	<li>WWWサーバ (<a href="http://www.apache.org/" rel="external">Apache</a>, IIS, Roxen, など)</li>
-	<li><a href="http://www.php.net/" rel="external">PHP</a> 4.3.0 以降 (5.2 以降 推奨)</li>
-	<li><a href="http://www.mysql.com/" rel="external">MySQL</a> 3.23 以降 (4.1 以降 推奨)</li>
+    <li>WWWサーバ (<a href="http://www.apache.org/" rel="external">Apache</a>, IIS など)</li>
+    <li><a href="http://www.php.net/" rel="external">PHP</a> 4.3.0 以降 (5.2 以降 推奨)</li>
+    <li><a href="http://www.mysql.com/" rel="external">MySQL</a> 3.23 以降 (4.1 以降 推奨)</li>
 </ul>
 <h3>準備</h3>
 <ol>
-	<li>WWWサーバ、PHP、データベースサーバを適切にセットアップする。</li>
-	<li>XOOPS用にデータベースを１つ準備する。</li>
-	<li>上記のデータベースにアクセスできる、データベースサーバのユーザアカウントを準備する。</li>
-	<li>以下のディレクトリとファイルを書き込み可能にする: %s</li>
-	<li><em>protector</em>モジュールをインストールする必要があるなら、<div class="confirmMsg">XOOPSルートディレクトリにある <em>mainfile.dist.php</em> ファイルを <em>/extras/mainfile.dist.php.protector</em> に置き換えてください。</div></li>
-    <li>セキュリティの観点から <a href="http://phpsec.org/projects/guide/3.html" rel="external">ドキュメントルート</a> 以下から２つのディレクトリをその外に移動し、フォルダ名を変更することを強く推奨します。: %s</li>
-    <li>以下のディレクトリを作成し(まだないのなら) 書き込み可能にする: %s</li>
-    <li>ブラウザのクッキーとJavaScriptをオンにする。</li>
+    <li>WWWサーバ、PHP、データベースサーバを適切にセットアップする。</li>
+    <li>XOOPS 用にデータベースを１つ準備する。</li>
+    <li>上記のデータベースにアクセスできる、データベースサーバのユーザアカウントを準備する。</li>
+    <li>下のフォルダ・ファイルを書き込み可能にする: %s</li>
+    <li>セキュリティの観点から <a href="http://phpsec.org/projects/guide/3.html" rel="external">DocumentRoot</a> 下から２つのフォルダをその外に移動し、フォルダ名を変更することを強く推奨します。: %s</li>
+    <li>以下のフォルダを作成し(まだないのなら) 書き込み可能にする: %s</li>
+    <li>ブラウザの クッキー と JavaScript をオンにする。</li>
 </ol>
 ';
 ?>
