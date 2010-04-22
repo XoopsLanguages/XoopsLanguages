e5487c1f71bb4693e744f33999a46dc40e8123a6
Michael Albertsen <culex@culex.dk>
2010-04-22 07:34:31 +0000

    Danish language for Wishcraft spiders 1.15

---
+++ danish/modules/spiders/language/danish/modinfo.php
@@ -0,0 +1,55 @@
+<?php
+// $Author: wishcraft $
+//  ------------------------------------------------------------------------ //
+//                XOOPS - PHP Content Management System                      //
+//                    Copyright (c) 2000 XOOPS.org                           //
+//                       <http://www.xoops.org/>                             //
+//  ------------------------------------------------------------------------ //
+//  This program is free software; you can redistribute it and/or modify     //
+//  it under the terms of the GNU General Public License as published by     //
+//  the Free Software Foundation; either version 2 of the License, or        //
+//  (at your option) any later version.                                      //
+//                                                                           //
+//  You may not change or alter any portion of this comment or credits       //
+//  of supporting developers from this source code or any supporting         //
+//  source code which is considered copyrighted (c) material of the          //
+//  original comment or credit authors.                                      //
+//                                                                           //
+//  This program is distributed in the hope that it will be useful,          //
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of           //
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            //
+//  GNU General Public License for more details.                             //
+//                                                                           //
+//  You should have received a copy of the GNU General Public License        //
+//  along with this program; if not, write to the Free Software              //
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA //
+//  ------------------------------------------------------------------------ //
+// Author: Simon Roberts (AKA wishcraft)                                     //
+// URL: http://www.chronolabs.org.au                                         //
+// Project: The XOOPS Project                                                //
+// ------------------------------------------------------------------------- //
+
+/**
+ * @translation     xoops nordic (http://www.xoopsnordic.org)
+ * @specification   _LANGCODE: dk
+ * @specification   _CHARSET: UTF-8 without bom
+ * @Translator      Culex (culex@culex.dk) - (www.culex.dk)
+**/
+
+	define('_MI_SPIDERS_DIRNAME','spiders');
+	
+	define('_MI_SPIDERS_NAME','Robotstyring');
+	define('_MI_SPIDERS_DESCRIPTION','Dette module er for robotstyring');	
+	define('_MI_SPIDERS_LOGON', 'Logon Session');
+	define('_MI_SPIDERS_LOGONDESC', 'Når en robot er opdaget log den ind under dens brugernavn.');	
+	
+	define('_MI_SPIDERS_MM1', 'Statistik');
+	define('_MI_SPIDERS_MM2', 'Senest her rapport');	
+	define('_MI_SPIDERS_ADMINMENU1', 'Styr Robotter');
+	define('_MI_SPIDERS_ADMINMENU2', 'Tilføj Robot');
+	define('_MI_SPIDERS_ADMINMENU3', 'Import Robotdefinitioner');		
+	
+	define('_MI_SPIDERS_GROUP_TYPE', 'Spider');
+	define('_MI_SPIDERS_GROUP_NAME', 'Robots, Crawlers & Spiders');
+	define('_MI_SPIDERS_GROUP_DESCRIPTION', 'Robots, Crawlers & Spiders som skanner din webside og har tilladelse.');	
+?>
