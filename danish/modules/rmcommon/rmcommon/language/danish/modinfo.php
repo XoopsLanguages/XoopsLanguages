3cd5ed31a0178b9591bb27757e7e46883544bdf7
Michael Albertsen <culex@culex.dk>
2010-04-22 07:47:33 +0000

    Danish language files for RmCommon 2.05 Alpha

---
+++ danish/modules/rmcommon/rmcommon/language/danish/modinfo.php
@@ -0,0 +1,55 @@
+<?php
+// $Id: modinfo.php 219 2010-02-14 08:01:45Z i.bitcero $
+// --------------------------------------------------------------
+// Red México Common Utilities
+// A framework for Red México Modules
+// Author: Eduardo Cortés <i.bitcero@gmail.com>
+// Email: i.bitcero@gmail.com
+// License: GPL 2.0
+// --------------------------------------------------------------
+
+/**
+ * @translation     xoops nordic (http://www.xoopsnordic.org)
+ * @specification   _LANGCODE: dk
+ * @specification   _CHARSET: UTF-8 without bom
+ * @Translator      Culex (culex@culex.dk) - (www.culex.dk)
+**/
+
+// Konfigurationer
+define ('_MI_RMC_LANG', 'Sprog der anvendes');
+define ('_MI_RMC_IMAGESTORE', 'Arranger billeder efter dato');
+define ('_MI_RMC_EDITOR', 'Vælg standard editor');
+define('_MI_RMC_EDITOR_VISUAL','Visual Editor');
+define('_MI_RMC_EDITOR_HTML','HTML Editor');
+define('_MI_RMC_EDITOR_XOOPS','XoopsCode Editor');
+define('_MI_RMC_EDITOR_SIMPLE','Simple Editor');
+define ('_MI_RMC_IMGCATSNUMBER', 'Max antal for billed kategorisliste.');
+define ('_MI_RMC_IMGSNUMBER', 'Antal billeder per side i Billedemanager');
+define ('_MI_RMC_SECREY', 'Secret Key');
+define ('_MI_RMC_SECREYD', 'Giv en hemmelig nøgle, der bruges til at kode information.');
+
+define ('_MI_RMC_DOHTML', 'Tillad HTML i tekst');
+define ('_MI_RMC_DOSMILE', 'Tillad smilies i tekst');
+define ('_MI_RMC_DOXCODE', 'Tillad XoopsCode');
+define ('_MI_RMC_DOIMAGE', 'Tillad billeder i tekst');
+define ('_MI_RMC_DOBR', 'Auto tilføje linjeskift i tekst');
+
+define ('_MI_RMC_MODSNUMBER', 'antal (nummer) Moduler på dashboard');
+
+// Kommentarer
+define ('_MI_RMC_ENABLECOMS', 'Aktiver kommentarer');
+define ('_MI_RMC_ANONCOMS', 'Tillad anonyme brugere at skrive kommentarer');
+define ('_MI_RMC_APPROVEREG', 'automatisk godkende kommentarer fra registrerede brugere');
+define ('_MI_RMC_APPROVEANON', 'automatisk godkende kommentarer fra anonyme brugere');
+define ('_MI_RMC_ALLOWEDIT', 'Tillad brugere at redigere deres kommentarer');
+define ('_MI_RMC_EDITLIMIT', 'Frist for at redigere en kommentar. (i timer)');
+
+// Admin Menu
+define ('_MI_RMC_MENUDASH', 'Dashboard');
+define ('_MI_RMC_MENUPLUGS', 'Plugins');
+define ('_MI_RMC_MENUIMG', 'Billeder');
+define ('_MI_RMC_OPTICATS', 'Kategorier');
+define ('_MI_RMC_OPTINEWCAT', 'Ny kategori');
+define ('_MI_RMC_OPTINEWIMG', 'Nyt Billede');
+define ('_MI_RMC_OPTINEWIMGS', 'Opret Billeder');
+
