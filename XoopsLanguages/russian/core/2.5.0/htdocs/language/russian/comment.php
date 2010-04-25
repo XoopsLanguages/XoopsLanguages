255ca7db9ecbf910403328a9931934f4b3b59f2e
andrey3761 <andrey3761@c17157d5-08f2-41e7-a131-df055f6a0da3>
2010-04-25 18:52:37 +0000

    XOOPS 2.5.0 Alfa 2 : Russian translation is added.

---
+++ XoopsLanguages/russian/core/2.5.0/htdocs/language/russian/comment.php
@@ -0,0 +1,66 @@
+<?php
+/*
+ You may not change or alter any portion of this comment or credits
+ of supporting developers from this source code or any supporting source code 
+ which is considered copyrighted (c) material of the original comment or credit authors.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+/**
+ *  Xoops Language
+ *
+ * @copyright       The XOOPS Project http://sourceforge.net/projects/xoops/
+ * @license         http://www.fsf.org/copyleft/gpl.html GNU public license
+ * @package         kernel
+ * @subpackage      Xoops Comment Language
+ * @since           2.0.0
+ * @author          Kazumi Ono <onokazu@xoops.org>
+ * @version         $Id: comment.php 3152 2009-04-18 13:30:57Z catzwolf $
+ */
+defined('XOOPS_ROOT_PATH') or die('Restricted access');
+
+// _LANGCODE: ru
+// _CHARSET : UTF-8
+// Translator: andrey3761
+
+define('_CM_TITLE','Наименование');
+define('_CM_MESSAGE','Сообщение');
+define('_CM_DOSMILEY','Разрешить смайлики');
+define('_CM_DOHTML','Разрешить HTML тэги');
+define('_CM_DOAUTOWRAP','Автоматически переносить строки');
+define('_CM_DOXCODE','Разрешить BB-коды');
+define('_CM_REFRESH','Обновить');
+define('_CM_PENDING','Ожидающий');
+define('_CM_HIDDEN','Скрытый');
+define('_CM_ACTIVE','Активный');
+define('_CM_STATUS','Статус');
+define('_CM_POSTCOMMENT','Отправить комментарий');
+define('_CM_REPLIES','Ответы');
+define('_CM_PARENT','Родительский');
+define('_CM_TOP','Вверх');
+define('_CM_BOTTOM','Вниз');
+define('_CM_ONLINE','Пользователь активен!');
+define('_CM_POSTED','Отправлено'); // дата сообщения
+define('_CM_UPDATED', 'Обновлено');
+define('_CM_THREAD','Нити');
+define('_CM_POSTER','Отправитель');
+define('_CM_JOINED','Дата регистрации');
+define('_CM_POSTS','Сообщений');
+define('_CM_FROM','От');
+define('_CM_COMDELETED', 'Комментарий удален.');
+define('_CM_COMDELETENG', 'Не могу удалить комментарий.');
+define('_CM_DELETESELECT', 'Удалить все дочерние комментарии?');
+define('_CM_DELETEONE', 'Нет, удалить только этот комментарий');
+define('_CM_DELETEALL', 'Да, удалить все');
+define('_CM_THANKSPOST', 'Благодарим за сообщение!');
+define('_CM_NOTICE', "Комментарии принадлежат их авторам. Мы не несем ответственности за их содержание.");
+define('_CM_COMRULES','Правила для комментариев');
+define('_CM_COMAPPROVEALL','Комментарии всегда одобряются');
+define('_CM_COMAPPROVEUSER','Комментарии от зарегистрированных пользователей всегда одобряются');
+define('_CM_COMAPPROVEADMIN','Все комментарии нуждаются в одобрении администратором');
+define('_CM_COMANONPOST','Разрешить анонимным пользователям создавать комментарии?');
+define('_CM_COMNOCOM','Запретить комментарии');
+?>
---
+++ XoopsLanguages/russian/core/2.5.0/htdocs/modules/system/language/russian/admin/avatars.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @copyright   The XOOPS Project http://sourceforge.net/projects/xoops/
+ * @license     http://www.fsf.org/copyleft/gpl.html GNU public license
+ * _LANGCODE    en
+ * _CHARSET     UTF-8
+ * Translator   2010-04-22 andrey3761
+ */
+
+// Navigation
+define( '_AM_SYSTEM_AVATAR_MANAGER', 'Управление аватарами' );
+define( '_AM_SYSTEM_AVATAR_MAIN', 'Главная' );
+define( '_AM_SYSTEM_AVATAR_SYSTEM', 'Системные аватары' );
+define( '_AM_SYSTEM_AVATAR_CUSTOM', 'Аватары пользователей' );
+define( '_AM_SYSTEM_AVATAR_ADD', 'Добавить аватар' );
+define( '_AM_SYSTEM_AVATAR_EDIT', 'Редактировать аватар' );
+define( '_AM_SYSTEM_AVATAR_DELETE', 'Удалить аватар' );
+
+// Infos
+define( '_AM_SYSTEM_AVATAR_ERROR', 'Ошибки' );
+define( '_AM_SYSTEM_AVATAR_USERS', 'Пользователей, использующих этот аватар' );
+define( '_AM_SYSTEM_AVATAR_USE_FILE', 'Выбрать файлы: %s' );
+define( '_AM_SYSTEM_AVATAR_UPLOAD', 'Загрузить:' );
+// Messages
+define( '_AM_SYSTEM_AVATAR_FAILDEL', 'Не удалось удалить аватар %s из базы данных' );
+define( '_AM_SYSTEM_AVATAR_DELETE', 'Вы уверены, что хотите удалить этот аватар?' );
+
+// Tips
+define( '_AM_SYSTEM_AVATAR_TIPS', '
+<ul id="newsticker" class="newsticker">
+<li>Управление всеми системными или выборочными аватарами</li>
+<li>Site members can optionally fashion their own online personal called avatars.<br />This option can be set off from module User preferences.</li>
+</ul>' );
+
+?>
