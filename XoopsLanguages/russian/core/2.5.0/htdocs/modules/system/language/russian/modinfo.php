cb80426255c30b0ec5aa9df87b6d78237854f297
andrey3761 <andrey3761@c17157d5-08f2-41e7-a131-df055f6a0da3>
2010-05-01 15:53:03 +0000

    XOOPS 2.5.0 Alfa 2 : Russian translation is added.

--- XoopsLanguages/russian/core/2.5.0/htdocs/modules/system/language/russian/modinfo.php
+++ XoopsLanguages/russian/core/2.5.0/htdocs/modules/system/language/russian/modinfo.php
@@ -2,89 +2,89 @@
 // $Id: modinfo.php 4208 2010-01-25 23:10:18Z kraven30 $
 // _LANGCODE: en
 // _CHARSET : UTF-8
-// Translator: XOOPS Translation Team
+// Translator: 2010-05-01 andrey3761
 
 // Module Info
 
 // The name of this module
-define("_MI_SYSTEM_NAME","System");
+define("_MI_SYSTEM_NAME", "Система");
 
 // A brief description of this module
-define("_MI_SYSTEM_DESC","For administration of core settings of the site.");
+define("_MI_SYSTEM_DESC", "Для администрирования ядра и настроек сайта.");
 
 // Names of blocks for this module (Not all module has blocks)
-define("_MI_SYSTEM_BNAME2","User Menu");
-define("_MI_SYSTEM_BNAME3","Login");
-define("_MI_SYSTEM_BNAME4","Search");
-define("_MI_SYSTEM_BNAME5","Waiting Contents");
-define("_MI_SYSTEM_BNAME6","Main Menu");
-define("_MI_SYSTEM_BNAME7","Site Info");
-define('_MI_SYSTEM_BNAME8', "Who's Online");
-define('_MI_SYSTEM_BNAME9', "Top Posters");
-define('_MI_SYSTEM_BNAME10', "New Members");
-define('_MI_SYSTEM_BNAME11', "Recent Comments");
+define("_MI_SYSTEM_BNAME2", "Меню пользователя");
+define("_MI_SYSTEM_BNAME3", "Вход пользователей");
+define("_MI_SYSTEM_BNAME4", "Поиск");
+define("_MI_SYSTEM_BNAME5", "В ожидании");
+define("_MI_SYSTEM_BNAME6", "Основное меню");
+define("_MI_SYSTEM_BNAME7", "Информация сайта");
+define('_MI_SYSTEM_BNAME8', "Кто онлайн");
+define('_MI_SYSTEM_BNAME9', "Лучшие отправители");
+define('_MI_SYSTEM_BNAME10', "Новые участники");
+define('_MI_SYSTEM_BNAME11', "Последние комментарии");
 // RMV-NOTIFY
-define('_MI_SYSTEM_BNAME12', "Notification Options");
-define('_MI_SYSTEM_BNAME13', "Themes");
+define('_MI_SYSTEM_BNAME12', "Настройки оповещения");
+define('_MI_SYSTEM_BNAME13', "Темы оформления");
 
 // Names of admin menu items
-define("_MI_SYSTEM_ADMENU1","Banners");
-define("_MI_SYSTEM_ADMENU2","Blocks");
-define("_MI_SYSTEM_ADMENU3","Groups");
-define("_MI_SYSTEM_ADMENU5","Modules");
-define("_MI_SYSTEM_ADMENU6","Preferences");
-define("_MI_SYSTEM_ADMENU7","Smilies");
-define("_MI_SYSTEM_ADMENU9","Ranks");
-define("_MI_SYSTEM_ADMENU10","Users");
-define("_MI_SYSTEM_ADMENU11","Mail");
-define("_MI_SYSTEM_ADMENU12", "Find Users");
-define("_MI_SYSTEM_ADMENU13", "Images");
-define("_MI_SYSTEM_ADMENU14", "Avatars");
-define("_MI_SYSTEM_ADMENU15", "Templates");
-define("_MI_SYSTEM_ADMENU16", "Comments");
-define("_MI_SYSTEM_ADMENU17", "File Manager");
-define("_MI_SYSTEM_ADMENU18", "Maintenance");
+define("_MI_SYSTEM_ADMENU1", "Баннеры");
+define("_MI_SYSTEM_ADMENU2", "Блоки");
+define("_MI_SYSTEM_ADMENU3", "Группы");
+define("_MI_SYSTEM_ADMENU5", "Модули");
+define("_MI_SYSTEM_ADMENU6", "Конфигурация сайта");
+define("_MI_SYSTEM_ADMENU7", "Смайлики");
+define("_MI_SYSTEM_ADMENU9", "Ранги пользователей");
+define("_MI_SYSTEM_ADMENU10", "Редактирование пользователей");
+define("_MI_SYSTEM_ADMENU11", "E-mail для пользователей");
+define("_MI_SYSTEM_ADMENU12", "Поиск пользователей");
+define("_MI_SYSTEM_ADMENU13", "Рисунки");
+define("_MI_SYSTEM_ADMENU14", "Аватары");
+define("_MI_SYSTEM_ADMENU15", "Шаблоны");
+define("_MI_SYSTEM_ADMENU16", "Комментарии");
+define("_MI_SYSTEM_ADMENU17", "Менеджер файлов");
+define("_MI_SYSTEM_ADMENU18", "Техническое обслуживание");
 
 //Preference
-define("_MI_SYSTEM_PREFERENCE_BREAK_GENERAL","General Settings");
-define("_MI_SYSTEM_PREFERENCE_TIPS","help online?");
-define("_MI_SYSTEM_PREFERENCE_TIPS_DSC","This gives you tips and online help");
-define("_MI_SYSTEM_PREFERENCE_ICONS","Icons");
-define("_MI_SYSTEM_PREFERENCE_BREADCRUMB","Breadcrumb");
+define("_MI_SYSTEM_PREFERENCE_BREAK_GENERAL","Основыне настройки");
+define("_MI_SYSTEM_PREFERENCE_TIPS","онлайн помощь?");
+define("_MI_SYSTEM_PREFERENCE_TIPS_DSC","Это включает краткие указания и </br>встроенные подсказки");
+define("_MI_SYSTEM_PREFERENCE_ICONS","Иконки");
+define("_MI_SYSTEM_PREFERENCE_BREADCRUMB","Навигационная панель");
 
-define("_MI_SYSTEM_PREFERENCE_BREAK_ACTIVE","Active Section");
-define("_MI_SYSTEM_PREFERENCE_ACTIVE_AVATARS","Active Avatars");
-define("_MI_SYSTEM_PREFERENCE_ACTIVE_BANNERS","Active Banners");
+define("_MI_SYSTEM_PREFERENCE_BREAK_ACTIVE","Активация секций");
+define("_MI_SYSTEM_PREFERENCE_ACTIVE_AVATARS","Аватары");
+define("_MI_SYSTEM_PREFERENCE_ACTIVE_BANNERS","Банеры");
 define("_MI_SYSTEM_PREFERENCE_ACTIVE_BLOCKSADMIN","");
-define("_MI_SYSTEM_PREFERENCE_ACTIVE_COMMENTS","Active Comments");
-define("_MI_SYSTEM_PREFERENCE_ACTIVE_FILEMANAGER","Active File Manager");
+define("_MI_SYSTEM_PREFERENCE_ACTIVE_COMMENTS","Комментарии");
+define("_MI_SYSTEM_PREFERENCE_ACTIVE_FILEMANAGER","Менеджер файлов");
 define("_MI_SYSTEM_PREFERENCE_ACTIVE_GROUPS","");
-define("_MI_SYSTEM_PREFERENCE_ACTIVE_IMAGES","Active Images Manager");
-define("_MI_SYSTEM_PREFERENCE_ACTIVE_MAILUSERS","Active Mail Users");
+define("_MI_SYSTEM_PREFERENCE_ACTIVE_IMAGES","Менеджер рисунков");
+define("_MI_SYSTEM_PREFERENCE_ACTIVE_MAILUSERS","Отправка e-mail пользователям");
 define("_MI_SYSTEM_PREFERENCE_ACTIVE_MODULESADMIN","");
 define("_MI_SYSTEM_PREFERENCE_ACTIVE_PREFERENCES","");
-define("_MI_SYSTEM_PREFERENCE_ACTIVE_SMILIES","Active Smilies");
+define("_MI_SYSTEM_PREFERENCE_ACTIVE_SMILIES","Смайлики");
 define("_MI_SYSTEM_PREFERENCE_ACTIVE_TPLSETS","");
-define("_MI_SYSTEM_PREFERENCE_ACTIVE_USERRANK","Active User Ranks");
-define("_MI_SYSTEM_PREFERENCE_ACTIVE_USERS","Active Users");
-define("_MI_SYSTEM_PREFERENCE_ACTIVE_MAINTENANCE","Active Maintenance");
+define("_MI_SYSTEM_PREFERENCE_ACTIVE_USERRANK","Ранги пользователей");
+define("_MI_SYSTEM_PREFERENCE_ACTIVE_USERS","Пользователи");
+define("_MI_SYSTEM_PREFERENCE_ACTIVE_MAINTENANCE","Техническое обслуживание");
 
-define("_MI_SYSTEM_PREFERENCE_BREAK_PAGER","Number of rows to display in the administration");
-define("_MI_SYSTEM_PREFERENCE_AVATARS_PAGER","Number of avatars to display per page");
-define("_MI_SYSTEM_PREFERENCE_BANNERS_PAGER","Number of banners to display per page");
-define("_MI_SYSTEM_PREFERENCE_COMMENTS_PAGER","Number of comments to display per page");
-define("_MI_SYSTEM_PREFERENCE_GROUPS_PAGER","Number of groups to display per page");
-define("_MI_SYSTEM_PREFERENCE_IMAGES_PAGER","Number of images to display per page");
-define("_MI_SYSTEM_PREFERENCE_SMILIES_PAGER","Number of smilies to display per page");
-define("_MI_SYSTEM_PREFERENCE_USERRANKS_PAGER","Number of ranks to display per page");
-define("_MI_SYSTEM_PREFERENCE_USERS_PAGER","Number of users to display per page");
+define("_MI_SYSTEM_PREFERENCE_BREAK_PAGER","Количество отображаемых строк в системных секциях при администрировании");
+define("_MI_SYSTEM_PREFERENCE_AVATARS_PAGER","Количество аватаров на страницу");
+define("_MI_SYSTEM_PREFERENCE_BANNERS_PAGER","Количество банеров на страницу");
+define("_MI_SYSTEM_PREFERENCE_COMMENTS_PAGER","Количество коментариев на страницу");
+define("_MI_SYSTEM_PREFERENCE_GROUPS_PAGER","Количество групп на страницу");
+define("_MI_SYSTEM_PREFERENCE_IMAGES_PAGER","Количество картинок на страницу");
+define("_MI_SYSTEM_PREFERENCE_SMILIES_PAGER","Количество смайликов на страницу");
+define("_MI_SYSTEM_PREFERENCE_USERRANKS_PAGER","Количество рангов на страницу");
+define("_MI_SYSTEM_PREFERENCE_USERS_PAGER","Количество пользователей на страницу");
 
-define("_MI_SYSTEM_PREFERENCE_BREAK_EDITOR","Editor Settings");
-define("_MI_SYSTEM_PREFERENCE_BLOCKS_EDITOR","Editor for blocks:");
+define("_MI_SYSTEM_PREFERENCE_BREAK_EDITOR","Управление редакторами");
+define("_MI_SYSTEM_PREFERENCE_BLOCKS_EDITOR","Редактор для блоков:");
 define("_MI_SYSTEM_PREFERENCE_BLOCKS_EDITOR_DSC","");
-define("_MI_SYSTEM_PREFERENCE_COMMENTS_EDITOR","Editor for comments:");
+define("_MI_SYSTEM_PREFERENCE_COMMENTS_EDITOR","Редактор для комментариев:");
 define("_MI_SYSTEM_PREFERENCE_COMMENTS_EDITOR_DSC","");
-define("_MI_SYSTEM_PREFERENCE_GENERAL_EDITOR","Editor for all modules:");
+define("_MI_SYSTEM_PREFERENCE_GENERAL_EDITOR","Редактор для всех модулей:");
 define("_MI_SYSTEM_PREFERENCE_GENERAL_EDITOR_DSC","");
 define("_MI_SYSTEM_PREFERENCE_ANONPOST","");
 define("_MI_SYSTEM_PREFERENCE_REDIRECT","");
