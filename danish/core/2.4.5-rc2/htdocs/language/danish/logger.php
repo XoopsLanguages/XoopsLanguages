0e9c80d105d72b9308e8ac4629190a234b1ad238
Anders Kristiansen <anderssk@users.noreply.github.com>
2010-05-26 20:35:43 +0000

    add translation for 2.4.5-rc2

---
+++ danish/core/2.4.5-rc2/htdocs/language/danish/logger.php
@@ -0,0 +1,59 @@
+<?php
+/*
+ You may not change or alter any portion of this comment or credits
+ of supporting developers from this source code or any supporting source code
+ which is considered copyrighted (c) material of the original comment or credit authors.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+/**
+ *  Xoops Language
+ *
+ * @copyright       The XOOPS Project http://sourceforge.net/projects/xoops/
+ * @license         http://www.fsf.org/copyleft/gpl.html GNU public license
+ * @package         kernel
+ * @subpackage      Xoops Logger Language
+ * @since           2.4.0
+ * @author          trabis <lusopoemas@gmail.com>
+ * @version         $Id: logger.php 0000 01/08/2009 10:02:39 trabis $
+ */
+defined('XOOPS_ROOT_PATH') or die('Restricted access');
+
+// _LANGCODE: da
+// _CHARSET : UTF-8
+// Translator: Anders Kristiansen (anderssk)
+define('_LOGGER_DEBUG', "Debug");
+define('_LOGGER_INCLUDED_FILES', "Inkluderede filer");
+define('_LOGGER_FILES', "%s filer");
+define('_LOGGER_MEM_USAGE', "Hukemmelse brug");
+define('_LOGGER_MEM_ESTIMATED', "%s [Skønnet]");
+define('_LOGGER_NONE', "Ingen");
+define('_LOGGER_ALL', "Alle");
+define('_LOGGER_ERRORS', "Fejl");
+define('_LOGGER_DEPRECATED', "Deprecated");
+define('_LOGGER_QUERIES', "Forespørgelser");
+define('_LOGGER_BLOCKS', "Blokke");
+define('_LOGGER_EXTRA', "Ekstra");
+define('_LOGGER_TIMERS', "Timere");
+
+
+define('_LOGGER_TIMETOLOAD', "%s tog %s sekunder at indlæse.");
+
+define('_LOGGER_TOTAL', "Total");
+
+define('_LOGGER_NOT_CACHED', "Ikke cached");
+define('_LOGGER_CACHED', "Cached (regenereres hvert %s sekund)");
+
+define('_LOGGER_UNKNOWN', "Ukendt");
+define('_LOGGER_E_USER_NOTICE', "Bemærk");
+define('_LOGGER_E_USER_WARNING', "Advarsel");
+define('_LOGGER_E_USER_ERROR', "Fejl");
+define('_LOGGER_E_NOTICE', "Bemærk");
+define('_LOGGER_E_WARNING', "Advarsel");
+define('_LOGGER_E_STRICT', "Streng");
+define('_LOGGER_FILELINE', "%s i filen %s linie %s");
+
+?>
