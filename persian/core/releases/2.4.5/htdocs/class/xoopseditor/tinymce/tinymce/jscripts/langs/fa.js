874c15fff33f90bf6c08c54bfcc91fe85284a0c1
voltan1 <voltan1@c17157d5-08f2-41e7-a131-df055f6a0da3>
2010-07-11 11:06:52 +0000

    Add Persian language for XOOPS 2.4.5

---
+++ persian/core/releases/2.4.5/htdocs/class/xoopseditor/tinymce/tinymce/jscripts/langs/fa.js
@@ -0,0 +1,164 @@
+/**
+* Translation of TinyMCE for Persian users
+*
+* @copyright	http://www.impresscms.org/ The ImpressCMS Project 
+* @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+* @package	    Translations
+* @since		3.0.6.2
+* @author		Sina Asghari <pesian_stranger@users.sourceforge.net>
+* @version		$Id$
+*/
+tinyMCE.addI18n({fa:{
+common:{
+edit_confirm:"آیا شما مایل به استفاده از حالت WYSIWYG برای این فضای متنی هستید؟",
+apply:"تایید",
+insert:"درج",
+update:"به‌روز رسانی",
+cancel:"لغو",
+close:"بستن",
+browse:"کاوش",
+class_name:"کلاسه",
+not_set:"-- تنظیم نشده --",
+clipboard_msg:"کپی/بریدن/چسباندن در Mozilla Firefox قابل دستیابی نیستند.\nآیا شما اطلاعات بیشتری در مورد این برآمد می‌خواهید؟",
+clipboard_no_support:"در حال حاضر مرورگر اینترنتیان پشتیبانی نمی‌شود، از صفحه کلید خود برای اینکار استفاده کنید.",
+popup_blocked:"با عرض پوزش ، اما ما اطلاع یافته‌ایم که مدافع پنجره بازشوی شما (Popup-Blocker) ، پنجره‌ای که عملیات کاربردی را ارائه می‌دهد را غیرفعال کرده است. شما نیاز به غیر فعال کردن مدافع پنجره بازشو، جهت تنظیم استفاده کامل از این ابزار را در این سایت دارید.",
+invalid_data:"خطا: اشتباهی در ارزش‌ها رخ داده است، این قسمت با رنگ قرمز مشخص شده‌اند.",
+more_colors:"رنگ‌های بیشتر",
+},
+contextmenu:{
+align:"محل قرارگیری",
+left:"چپ چین",
+center:"وسط چین",
+right:"راست چین",
+full:"سراسری",
+},
+insertdatetime:{
+date_fmt:"%Y-%m-%d",
+time_fmt:"%H:%M:%S",
+insertdate_desc:"درج تاریخ",
+inserttime_desc:"درج زمان",
+months_long:"ژانویه,فوریه,مارس,آوریل,مِی,ژوئن,ژوییه,اوت,سپتامبر,اکتبر,نوامبر,دسامبر",
+months_short:"Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec",
+day_long:"یکشنبه,دوشنبه,سه‌شنبه,چهارشنبه,پنج‌شنبه,جمعه,شنبه,یکشنبه",
+day_short:"ی,د,س,چ,پ,ج,ش,ی",
+},
+print:{
+print_desc:"چاپ کردن",
+},
+preview:{
+preview_desc:"پیش‌نمایش",
+},
+directionality:{
+ltr_desc:"چپ به راست",
+rtl_desc:"راست به چپ",
+},
+layer:{
+insertlayer_desc:"ساختن لایه‌ای جدید",
+forward_desc:"حرکت به سمت جلو",
+backward_desc:"حرکت به سمت عقب",
+absolute_desc:"تغییر به جایگاه ثابت",
+content:"صفحه جدید",
+},
+save:{
+save_desc:"ذخیره",
+cancel_desc:"لغو تمامی تغییرات",
+},
+nonbreaking:{
+nonbreaking_desc:"درج نویسه‌ی غیر-قابل شکستن",
+},
+iespell:{
+iespell_desc:"اجرای غلط یاب",
+download:"ماژول ieSpell پیدا نشد. آیا می‌خواهید نصبش کنید؟",
+},
+advhr:{
+advhr_desc:"قوانین افقی",
+},
+emotions:{
+emotions_desc:"شکلک‌ها",
+},
+searchreplace:{
+search_desc:"جستجو",
+replace_desc:"جستجو/جایگزینی",
+},
+advimage:{
+image_desc:"اضافه/ویرایش کردن تصاویر",
+},
+advlink:{
+link_desc:"اضافه/ویرایش کردن لینک",
+},
+xhtmlxtras:{
+cite_desc:"ذکر کردن",
+abbr_desc:"اختصار",
+acronym_desc:"سرنام",
+del_desc:"حذف",
+ins_desc:"درج",
+attribs_desc:"اضافه/ویرایش کردن عنوان",
+},
+style:{
+desc:"ویرایش ترکیب‌بندی پرونده‌های CSS",
+},
+paste:{
+paste_text_desc:"چسباندن از متن معمولی",
+paste_word_desc:"چسباندن از Word",
+selectall_desc:"انتخاب همه",
+},
+paste_dlg:{
+text_title:"برای چسباندن متن درون پنجره از CTRL+V بر روی صفحه کلیدتان استفاده نمائید.",
+text_linebreaks:"نگهداری خطوط شکسته",
+word_title:"برای چسباندن متن درون پنجره از CTRL+V بر روی صفحه کلیدتان استفاده نمائید.",
+},
+table:{
+desc:"درج جدول جدید",
+row_before_desc:"درج سطر قبل از",
+row_after_desc:"درج سطر بعد از",
+delete_row_desc:"حذف ستون",
+col_before_desc:"درج ستون قبل از",
+col_after_desc:"درج ستون بعد از",
+delete_col_desc:"حذف ستون",
+split_cells_desc:"تقسیم سلول‌های جدول",
+merge_cells_desc:"ادغام سلول‌های جدول",
+row_desc:"مشخصات سطرهای جدول",
+cell_desc:"مشخصات خانه‌های جدول",
+props_desc:"مشخصات جدول",
+paste_row_before_desc:"چسباندن سطر جدول در قبل",
+paste_row_after_desc:"چسباندن سطر جدول در بعد",
+cut_row_desc:"بریدن سطر جدول",
+copy_row_desc:"کپی‌برداری از سطر جدول",
+del:"حذف جدول",
+row:"سطر",
+col:"ستون",
+cell:"خانه",
+},
+autosave:{
+unload_msg:"تغییراتی که انجام داده‌اید، با بیرون رفتن از این صفحه از بین می‌روند.",
+},
+fullscreen:{
+desc:"تغییر به حالت تمام صفحه",
+},
+media:{
+desc:"درج/ویرایش رسانه جاسازی شده",
+edit:"ویرایش رسانه‌ی اضافه شده",
+},
+fullpage:{
+desc:"ویژگی‌های اسناد",
+},
+template:{
+desc:"درج محتوای از پیش تعیین شده در الگو",
+},
+visualchars:{
+desc:"روشن/خاموش کردن نویسه‌های تصویری.",
+},
+spellchecker:{
+desc:"رفتن به حالت غلط یاب",
+menu:"تنظیمات غلط یاب",
+ignore_word:"نادیده گرفتن واژه",
+ignore_words:"نادیده گرفتن همه",
+langs:"زبان‌ها",
+wait:"لطفا صبر کنید...",
+sug:"پیشنهادها",
+no_sug:"بدون پیشنهاد",
+no_mpell:"هیچ غلط املایی پیدا نشد.",
+},
+pagebreak:{
+desc:"درج محدود کننده صفحات.",
+}}});
---
+++ persian/core/releases/2.4.5/htdocs/class/xoopseditor/tinymce/tinymce/jscripts/plugins/xoopsmlcontent/langs/fa_dlg.js
@@ -0,0 +1,10 @@
+tinyMCE.addI18n('fa.xoopsmlcontent_dlg',{
+	title : 'درج محتوای چند زبانه',
+	subtitle : 'زبان مورد نظرتان را انتخاب کنید، سپس متن خود را در اینجا بچسبانید:',
+	sellang : 'انتخاب زبان',
+    chooselang : 'شما باید یک زبان انتخاب کنید',
+    maxstring : ' کارکتر از %maxchar% ورودی',
+    alertmaxstring : 'شما خک اکنون از بیشترین حد کارکتر های مجاز استفاده کرده اید',
+    delta_width : 100,
+    delta_height : 100
+});
