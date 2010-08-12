05fe5c9f7a921713a6001b748cd25f07f4c35883
voltan1 <voltan1@c17157d5-08f2-41e7-a131-df055f6a0da3>
2010-08-12 18:52:31 +0000

    Add persian language of contact module 1.7 by ￼ vahid

---
+++ persian/modules/contact/trunk/language/persian/main.php
@@ -0,0 +1,29 @@
+﻿<?php
+define("_CT_NAME","نام");
+define("_CT_EMAIL","ایمیل");
+define("_CT_URL","آدرس وب");
+define("_CT_ICQ","ICQ");
+define("_CT_COMPANY","شرکت");
+define("_CT_LOCATION","موقعیت");
+define("_CT_COMMENTS","پیام شما");
+define("_CT_SUBMIT","فرستادن");
+define("_CT_YOURMESSAGE","پیام شما :‏");
+define("_CT_WEBMASTER","وبمستر");
+define("_CT_HELLO","سلام‏‏ %s,");
+define("_CT_THANKYOUCOMMENTS","از تماس شما متشکریم %s.");
+define("_CT_SENTTOWEBMASTER","پیام شما فرستاده شد به %s.");
+define("_CT_SUBMITTED","%s اطلاعات زیر فرستاده شد:");
+define("_CT_MESSAGESENT","پیام به %s فرستاده شد‏‏");
+define("_CT_CONFIRM", "ایمیل تایید");
+define("_CT_SENDCONFIRMEMAIL", "فرستادن تائیدیه ایمیل به %s");
+define("_CT_SENDCONFIRM", "فرستادن تائیدیه ایمیل برای شما!" );
+define("_CT_SENTASCONFIRM","پیام شما فرستاده شد به: %sعنوان ایمیل تاییدیه.");
+define("_CT_ADDRESS", "آدرس");
+define("_CT_SECURITY_CODE", "کد امنیتی" );
+define("_CT_SECURITY_TYPE", "کد امنیتی را وارد کنید" );
+define("_CT_ERROR_CONDITIONHEAD", "وضعیت خطا");
+define("_CT_ERROR_CONDITION", "<b>لطغا خطا را تصحیح کنید!‏</b><br />" );
+define("_CT_ERROR_BADSECVALUE_MSG", "کد امنیتی اشتباه وارد شده است!‏<br />");
+define("_CT_ERROR_BADEMAIL_MSG", "ایمیلی را که وارد کردید چک کنید<br />");
+define("_CT_NO_SPAM", "بررسی امنیتی (اسپم نیست)‏");
+?>
