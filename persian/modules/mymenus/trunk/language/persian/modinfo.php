39fa1f7b7889fcee1d55fb3170256e1e7672b888
voltan1 <voltan1@c17157d5-08f2-41e7-a131-df055f6a0da3>
2010-08-17 04:29:24 +0000

    Add persian language by aslani606

---
+++ persian/modules/mymenus/trunk/language/persian/modinfo.php
@@ -0,0 +1,27 @@
+<?php
+//  Author: Trabis
+//  URL: http://www.xuups.com
+//  E-Mail: lusopoemas@gmail.com
+//  Translate by Eshtehard.net
+// Official Xoops Support Site For Persians
+//  Translate by irxoops.org
+
+// Module Info
+
+define('_MI_MYMENUS_MD_NAME', "منوهای من");
+define('_MI_MYMENUS_MD_DESC', "منو برای سایت شما");
+
+// Blocks
+define('_MI_MYMENUS_BLK', "بلاک منو");
+define('_MI_MYMENUS_BLK_DSC', "");
+
+// Admin menu
+define('_MI_MYMENUS_MENUSMANAGER', "مدیریت منوها");
+define('_MI_MYMENUS_MENUMANAGER', "مدیریت لینک های منوها");
+define('_MI_MYMENUS_ABOUT', "درباره ");
+
+//Configs
+define('_MI_MENUS_CONF_ASSIGN_METHOD', "روش انتساب");
+define('_MI_MENUS_CONF_ASSIGN_METHOD_DSC', "روش تخصیص css و جی اس به هدر");
+define('_MI_MENUS_CONF_ASSIGN_METHOD_XOOPSTPL', "XoopsTpl");
+define('_MI_MENUS_CONF_ASSIGN_METHOD_XOTHEME', "XoTheme");
