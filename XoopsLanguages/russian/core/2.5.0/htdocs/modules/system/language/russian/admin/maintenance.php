6746085149f68f760d0e6b5b6ecadc28b3f3c74a
andrey3761 <andrey3761@c17157d5-08f2-41e7-a131-df055f6a0da3>
2010-10-10 15:49:45 +0000

    maintenance.php translate

--- XoopsLanguages/russian/core/2.5.0/htdocs/modules/system/language/russian/admin/maintenance.php
+++ XoopsLanguages/russian/core/2.5.0/htdocs/modules/system/language/russian/admin/maintenance.php
@@ -4,69 +4,69 @@
  * @license     http://www.fsf.org/copyleft/gpl.html GNU public license
  * _LANGCODE    ru
  * _CHARSET     UTF-8
- * Translator   2010-04-25 andrey3761
+ * Translator   2010-10-10 andrey3761
  * @version     $Id$
  */
 
 //Nav
 define("_AM_SYSTEM_MAINTENANCE_NAV_MANAGER", "Техническое обслуживание");
-define("_AM_SYSTEM_MAINTENANCE_NAV_LIST","All maintenance");
+define("_AM_SYSTEM_MAINTENANCE_NAV_LIST","Все виды обслуживания");
 define("_AM_SYSTEM_MAINTENANCE_NAV_DUMP","Дамп");
 
-define("_AM_SYSTEM_MAINTENANCE_SESSION","Empty the sessions table");
-define("_AM_SYSTEM_MAINTENANCE_SESSION_OK","Session maintenance : OK");
-define("_AM_SYSTEM_MAINTENANCE_SESSION_NOTOK","Session maintenance : Error");
-define("_AM_SYSTEM_MAINTENANCE_CACHE","Clean cache folder");
-define("_AM_SYSTEM_MAINTENANCE_CACHE_OK","Cache maintenance : OK");
-define("_AM_SYSTEM_MAINTENANCE_CACHE_NOTOK","Cache maintenance : Error");
-define("_AM_SYSTEM_MAINTENANCE_TABLES","Tables maintenance");
-define("_AM_SYSTEM_MAINTENANCE_TABLES_OK","Cache maintenance : OK");
-define("_AM_SYSTEM_MAINTENANCE_TABLES_NOTOK","Cache maintenance : Error");
-define("_AM_SYSTEM_MAINTENANCE_QUERY_DESC","Optimize, Check, Repair and Analyze your tables");
-define("_AM_SYSTEM_MAINTENANCE_QUERY_OK","Maintain database : OK");
-define("_AM_SYSTEM_MAINTENANCE_QUERY_NOTOK","Maintain database : Error");
-define("_AM_SYSTEM_MAINTENANCE_CHOICE1","Optimize table(s)");
-define("_AM_SYSTEM_MAINTENANCE_CHOICE2","Check table(s)");
-define("_AM_SYSTEM_MAINTENANCE_CHOICE3","Repair table(s)");
-define("_AM_SYSTEM_MAINTENANCE_CHOICE4","Analyze table(s)");
+define("_AM_SYSTEM_MAINTENANCE_SESSION","Очистить таблицу с сессиями");
+define("_AM_SYSTEM_MAINTENANCE_SESSION_OK","Обслуживание сессий : OK");
+define("_AM_SYSTEM_MAINTENANCE_SESSION_NOTOK","Обслуживание сессий : Error");
+define("_AM_SYSTEM_MAINTENANCE_CACHE","Очистить папку с кэшем");
+define("_AM_SYSTEM_MAINTENANCE_CACHE_OK","Обслуживание кэша : OK");
+define("_AM_SYSTEM_MAINTENANCE_CACHE_NOTOK","Обслуживание кэша : Error");
+define("_AM_SYSTEM_MAINTENANCE_TABLES","Обслуживание таблиц");
+define("_AM_SYSTEM_MAINTENANCE_TABLES_OK","Обслуживание таблиц : OK");
+define("_AM_SYSTEM_MAINTENANCE_TABLES_NOTOK","Обслуживание таблиц : Error");
+define("_AM_SYSTEM_MAINTENANCE_QUERY_DESC","Оптимизация, Проверка, Востановление и Анализ ваших таблиц");
+define("_AM_SYSTEM_MAINTENANCE_QUERY_OK","Обслуживание базы данных : OK");
+define("_AM_SYSTEM_MAINTENANCE_QUERY_NOTOK","Обслуживание базы данных : Error");
+define("_AM_SYSTEM_MAINTENANCE_CHOICE1","Оптимизация таблиц(ы)");
+define("_AM_SYSTEM_MAINTENANCE_CHOICE2","Проверка таблиц(ы)");
+define("_AM_SYSTEM_MAINTENANCE_CHOICE3","Востановление таблиц(ы)");
+define("_AM_SYSTEM_MAINTENANCE_CHOICE4","Анализ таблиц(ы)");
 define("_AM_SYSTEM_MAINTENANCE_TABLES_DESC", 
-"ANALYZE TABLE analyzes and stores the key distribution for a table. During the analysis, the table is locked with a read lock.<br />
-CHECK TABLE checks a table or tables for errors.<br />
-OPTIMIZE TABLE to reclaim the unused space and to defragment the data file.<br />
-REPAIR TABLE repairs a possibly corrupted table.");
+"АНАЛИЗ ТАБЛИЦ анализирует и сохраняет распределение ключей для таблицы. Во время проверки таблица блокируется с блокировкой на чтение.<br />
+ПРОВЕРКА ТАБЛИЦ проверяет одну или несколько таблиц на наличие ошибок.<br />
+ОПТИМИЗАЦИЯ ТАБЛИЦ убирает неиспользуемое пространство и дефрагментирует файлы таблицы.<br />
+ВОСТАНОВЛЕНИЕ ТАБЛИЦ востанавливает возможно повреждённые таблицы.");
 
-define("_AM_SYSTEM_MAINTENANCE_RESULT","Result");
-define("_AM_SYSTEM_MAINTENANCE_RESULT_NO_RESULT","Not Result");
-define("_AM_SYSTEM_MAINTENANCE_RESULT_CACHE","Clean Cache task");
-define("_AM_SYSTEM_MAINTENANCE_RESULT_SESSION","Clean sessions table task");
-define("_AM_SYSTEM_MAINTENANCE_RESULT_QUERY","Database task");
-define("_AM_SYSTEM_MAINTENANCE_ERROR_MAINTENANCE","No choice for maintenance");
+define("_AM_SYSTEM_MAINTENANCE_RESULT","Результат");
+define("_AM_SYSTEM_MAINTENANCE_RESULT_NO_RESULT","Нет результата");
+define("_AM_SYSTEM_MAINTENANCE_RESULT_CACHE","Задание очистки кэша");
+define("_AM_SYSTEM_MAINTENANCE_RESULT_SESSION","Задание очистки таблицы сессий");
+define("_AM_SYSTEM_MAINTENANCE_RESULT_QUERY","Задание базы данных");
+define("_AM_SYSTEM_MAINTENANCE_ERROR_MAINTENANCE","Не выбрано задание для обслуживания");
 
-define("_AM_SYSTEM_MAINTENANCE_TABLES1","Tables");
-define("_AM_SYSTEM_MAINTENANCE_TABLES_OPTIMIZE","Optimize");
-define("_AM_SYSTEM_MAINTENANCE_TABLES_CHECK","Check"); 
-define("_AM_SYSTEM_MAINTENANCE_TABLES_REPAIR","Repair");
-define("_AM_SYSTEM_MAINTENANCE_TABLES_ANALYZE","Analyze");
+define("_AM_SYSTEM_MAINTENANCE_TABLES1","Таблицы");
+define("_AM_SYSTEM_MAINTENANCE_TABLES_OPTIMIZE","Оптимизированно");
+define("_AM_SYSTEM_MAINTENANCE_TABLES_CHECK","Проверенно"); 
+define("_AM_SYSTEM_MAINTENANCE_TABLES_REPAIR","Востановленно");
+define("_AM_SYSTEM_MAINTENANCE_TABLES_ANALYZE","Проанализированно");
 
 //Dump
-define("_AM_SYSTEM_MAINTENANCE_DUMP","Dump");
-define("_AM_SYSTEM_MAINTENANCE_DUMP_TABLES_OR_MODULES","Select tables or modules");
-define("_AM_SYSTEM_MAINTENANCE_DUMP_DROP","Add command DROP TABLE IF EXISTS 'tables' in the dump");
-define("_AM_SYSTEM_MAINTENANCE_DUMP_OR", "OR");
-define("_AM_SYSTEM_MAINTENANCE_DUMP_AND", "AND");
-define("_AM_SYSTEM_MAINTENANCE_DUMP_ERROR_TABLES_OR_MODULES", "You must select the tables or modules");
-define("_AM_SYSTEM_MAINTENANCE_DUMP_NO_TABLES", "No tables");
-define("_AM_SYSTEM_MAINTENANCE_DUMP_TABLES", "Tables");
-define("_AM_SYSTEM_MAINTENANCE_DUMP_STRUCTURES", "Structures");
-define("_AM_SYSTEM_MAINTENANCE_DUMP_NB_RECORDS", "Numbers of records");
-define("_AM_SYSTEM_MAINTENANCE_DUMP_FILE_CREATED", "File created");
-define("_AM_SYSTEM_MAINTENANCE_DUMP_RESULT", "Result");
-define("_AM_SYSTEM_MAINTENANCE_DUMP_RECORDS", "record(s)");
+define("_AM_SYSTEM_MAINTENANCE_DUMP","Дамп");
+define("_AM_SYSTEM_MAINTENANCE_DUMP_TABLES_OR_MODULES","Выберите таблицы или модули");
+define("_AM_SYSTEM_MAINTENANCE_DUMP_DROP","Добавить команду DROP TABLE IF EXISTS 'tables' в дамп");
+define("_AM_SYSTEM_MAINTENANCE_DUMP_OR", "или");
+define("_AM_SYSTEM_MAINTENANCE_DUMP_AND", "и");
+define("_AM_SYSTEM_MAINTENANCE_DUMP_ERROR_TABLES_OR_MODULES", "Вы должны выбрать таблицы или модули");
+define("_AM_SYSTEM_MAINTENANCE_DUMP_NO_TABLES", "Нет таблиц");
+define("_AM_SYSTEM_MAINTENANCE_DUMP_TABLES", "Таблицы");
+define("_AM_SYSTEM_MAINTENANCE_DUMP_STRUCTURES", "Структуры");
+define("_AM_SYSTEM_MAINTENANCE_DUMP_NB_RECORDS", "Количество записей");
+define("_AM_SYSTEM_MAINTENANCE_DUMP_FILE_CREATED", "Файл создан");
+define("_AM_SYSTEM_MAINTENANCE_DUMP_RESULT", "Результат");
+define("_AM_SYSTEM_MAINTENANCE_DUMP_RECORDS", "запись(ей)");
 
 // Tips
 define( '_AM_SYSTEM_MAINTENANCE_TIPS', 
 '<ul>
-<li>You can do a simple maintenance of your XOOPS Installation: clear your cache and session table, and do maintenance of your tables</li>
+<li>Вы можете сделать простое обслуживание системы XOOPS: очистить кэш и таблицу сессий, обслуживать таблицы базы данных</li>
 </ul>' );
 
 ?>
