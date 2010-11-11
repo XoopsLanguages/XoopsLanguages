02d8adae0dd78fab7840951c51c7ce92397dad08
voltan1 <voltan1@c17157d5-08f2-41e7-a131-df055f6a0da3>
2010-11-11 08:51:23 +0000

    Add persian traslation for xoops 2.5.0

---
+++ persian/core/releases/2.5.0/upgrade/upd-2.0.18-to-2.3.0/language/persian.php
@@ -0,0 +1,24 @@
+<?php
+// $Id$
+// Add Persian Language by Persain Xoops Project www.irxoops.org
+define( "LEGEND_XOOPS_PATHS", "شاخه فیزیکی زوپس" );
+define( "LEGEND_DATABASE", "تنظیم کارکتر پایگاه داده ها" );
+
+define( "XOOPS_LIB_PATH_LABEL", "شاخه کتابخانه های زوپس" );
+define( "XOOPS_LIB_PATH_HELP", "مسیر شاخه کتابخانه زوپس بودن  بدون داشتن اسلش، همسازی بعدی. برای امنیت بیشتر این شاخه را به خارج از " . XOOPS_ROOT_PATH . " منتقل کنید" );
+define( "XOOPS_DATA_PATH_LABEL", "شاخه فایل‌های داده زوپس" );
+define( "XOOPS_DATA_PATH_HELP", "مسیر شاخه فایل های داده زوپس (قابل نوشتن) بدون داشتن اسلش، همسازی بعدی. برای امنیت بیشتر این شاخه را به خارج از " . XOOPS_ROOT_PATH . " منتقل کنید." );
+
+define( "DB_COLLATION_LABEL", "کارکتر پایگاه داده ها تنظیم و تطبیق داده شد" );
+define( "DB_COLLATION_HELP",  "از نسخه MySQL 4.12 از سیستم تنظیم و تطابق دستی کارکتر ها پشتیبانی میشود. هرچند این عمل خیلی پیچیده است, در نتیجه اگر شما دقیقا نمیدانید که چه میخواهید و این گزینه چکار میکند  لطفا وضعیت موجود را تغییر نداده و چیز دیگری را انتخاب نکنید.");
+define( "DB_COLLATION_NOCHANGE",  "تغییر نده");
+
+define( "XOOPS_PATH_FOUND", "مسیر پیدا شد." );
+define( "ERR_COULD_NOT_ACCESS", "شاخه تعیین شده قابل  دسترسی نیست. لطفا برسی کنید که آیا این شاخه در سرور موجود و قابل خواند است یا خیر." );
+define( "CHECKING_PERMISSIONS", "برسی دسترسی نوشتن در شاخه ها و فایل ها ..." );
+define( "ERR_NEED_WRITE_ACCESS", "سرویس دهنده نیاز دارد که دسترسی نوشت را برای فایل ها و شاخه های زیر بگیرد<br />(i.e. <em>دسترسی 777 برای directory_name</em>در سرور UNIX/LINUX)" );
+define( "IS_NOT_WRITABLE", "%s قابل نوشتن نیست." );
+define( "IS_WRITABLE", "%s قابل نوشتن است." );
+define( "ERR_COULD_NOT_WRITE_MAINFILE", "خطا در نوشتن محتوا در mainfile.php, محتوای موجود را دستی در mainfile.php بنویسید." );
+
+?>
