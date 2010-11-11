7ae52669701291f1a150890685804912c37d71ba
voltan1 <voltan1@c17157d5-08f2-41e7-a131-df055f6a0da3>
2010-11-11 09:41:29 +0000

    Add persian traslation for xoops 2.5.0

---
+++ XoopsLanguages/persian/core/releases/2.5.0/htdocs/modules/system/language/persian/admin/banners.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * @copyright   The XOOPS Project http://sourceforge.net/projects/xoops/
+ * @license     http://www.fsf.org/copyleft/gpl.html GNU public license
+ * _LANGCODE    fa
+ * _CHARSET     UTF-8
+ * @version     $Id$
+ */
+
+//%%%%%%        Admin Module Name  Banners         %%%%%
+define("_AM_SYSTEM_BANNERS_DBUPDATED",_AM_SYSTEM_DBUPDATED);
+
+//Nav
+define("_AM_SYSTEM_BANNERS_NAV_MANAGER","مدیریت بنر‌ها");
+define("_AM_SYSTEM_BANNERS_NAV_MAIN","فهرست بنر‌ها و مشتری‌ها");
+define("_AM_SYSTEM_BANNERS_NAV_EDITBNR","ویرایش بنر");
+define("_AM_SYSTEM_BANNERS_NAV_DELETEBNR","حذف بنر");
+define("_AM_SYSTEM_BANNERS_NAV_DELETEFINISHBNR","حذف بنر پایان یافته");
+define("_AM_SYSTEM_BANNERS_NAV_ADDBNR","اضافه کردن بنر جدید");
+define("_AM_SYSTEM_BANNERS_NAV_EDITADVCLI","ویرایش مشتری‌ها");
+define("_AM_SYSTEM_BANNERS_NAV_ADDNWCLI","اضافه کردن مشتری جدید");
+define("_AM_SYSTEM_BANNERS_NAV_DELETECLI","حذف بنر");
+
+define("_AM_SYSTEM_BANNERS_CURACTBNR","بنر‌های فعال در حال حاضر");
+define("_AM_SYSTEM_BANNERS_BANNERID","شناسه (ID) بنر");
+define("_AM_SYSTEM_BANNERS_IMPRESION","نشان‌گذاری");
+define("_AM_SYSTEM_BANNERS_IMPLEFT","نشان‌گذاری را کنار بگذار");
+define("_AM_SYSTEM_BANNERS_CLICKS","بازدید‌ها");
+define("_AM_SYSTEM_BANNERS_NCLICKS","% بار دیده شده");
+define("_AM_SYSTEM_BANNERS_CLINAME","نام شاخه‌ی بنر");
+define("_AM_SYSTEM_BANNERS_FUNCTION","تابع‌ها");
+define("_AM_SYSTEM_BANNERS_UNLIMIT","نا‌محدود");
+define("_AM_SYSTEM_BANNERS_VIEW","نمایش بنر");
+define("_AM_SYSTEM_BANNERS_EDIT","ویرایش");
+define("_AM_SYSTEM_BANNERS_DELETE","حذف");
+define("_AM_SYSTEM_BANNERS_FINISHBNR","بنر‌هایی که زمان نمایش آنها خاتمه یافته");
+define("_AM_SYSTEM_BANNERS_IMPD","نشان‌گذاری");
+define("_AM_SYSTEM_BANNERS_STARTDATE","تاریخ شروع");
+define("_AM_SYSTEM_BANNERS_ENDDATE","تاریخ خاتمه");
+define("_AM_SYSTEM_BANNERS_ADVCLI","تبلیغات شاخه");
+define("_AM_SYSTEM_BANNERS_ACTIVEBNR","بنرهای فعال");
+define("_AM_SYSTEM_BANNERS_CONTNAME","نام تماس");
+define("_AM_SYSTEM_BANNERS_CONTMAIL","پست‌الکترونیکی تماس");
+define("_AM_SYSTEM_BANNERS_CLINAMET","نام شاخه:");
+define("_AM_SYSTEM_BANNERS_ADDNWBNR","اضافه‌کردن بنر جدید");
+define("_AM_SYSTEM_BANNERS_IMPPURCHT","نشان‌گذاری‌های خریداری شده:");
+define("_AM_SYSTEM_BANNERS_IMGURLT","آدرس تصویر:");
+define("_AM_SYSTEM_BANNERS_CLICKURLT","آدرس پیوند:");
+define("_AM_SYSTEM_BANNERS_ADDBNR","اضافه کردن بنر");
+define("_AM_SYSTEM_BANNERS_ADDNWCLI","اضافه کردن شاخه جدید");
+define("_AM_SYSTEM_BANNERS_CONTNAMET","نام تماس:");
+define("_AM_SYSTEM_BANNERS_CONTMAILT","پست الکترونیکی تماس:");
+define("_AM_SYSTEM_BANNERS_CLILOGINT","شناسه ورود شاخه:");
+define("_AM_SYSTEM_BANNERS_CLIPASST","واژه رمز شاخه:");
+define("_AM_SYSTEM_BANNERS_ADDCLI","اضافه کردن شاخه");
+define("_AM_SYSTEM_BANNERS_DELEBNR","حذف بنر");
+define("_AM_SYSTEM_BANNERS_SUREDELE","آیا مطمئنید که می‌خواهید این بنر را حذف کنید");
+define("_AM_SYSTEM_BANNERS_NO","نه");
+define("_AM_SYSTEM_BANNERS_YES","بله");
+define("_AM_SYSTEM_BANNERS_EDITBNR","ویرایش بنر");
+define("_AM_SYSTEM_BANNERS_ADDIMPT","اضافه کردن نشان‌گذاری‌های بیشتر:");
+define("_AM_SYSTEM_BANNERS_PURCHT","خریداری شده:");
+define("_AM_SYSTEM_BANNERS_MADET","ساخته شده:");
+define("_AM_SYSTEM_BANNERS_CHGBNR","عوض‌کردن بنر");
+define("_AM_SYSTEM_BANNERS_DELEADC","حذف کردن تبلیغات شاخه");
+define("_AM_SYSTEM_BANNERS_SUREDELCLI","شما شاخه‌ی <b>%s</b> و همه‌ی بنرهای موجود در آن را حذف کردید.");
+define("_AM_SYSTEM_BANNERS_NOBNRRUN","در حال حاضر این شاخه دارای هیچ بنری نیست");
+define("_AM_SYSTEM_BANNERS_WARNING","هشدار!!!");
+define("_AM_SYSTEM_BANNERS_ACTBNRRUN","این شاخه دارای بنر‌های فعال زیر است که در سایت قرار دارند:");
+define("_AM_SYSTEM_BANNERS_SUREDELBNR","آیا مطمئنید که می‌خواهید این شاخه و همه‌ی بنر‌های موجود در آن را حذف کنید؟");
+define("_AM_SYSTEM_BANNERS_EDITADVCLI","ویرایش تبلیغات شاخه");
+define("_AM_SYSTEM_BANNERS_EXTINFO","اطلاعات بیشتر:");
+define("_AM_SYSTEM_BANNERS_CHGCLI","عوض‌کردن شاخه");
+define("_AM_SYSTEM_BANNERS_USEHTML","کد‌های Html فعال شود؟");
+define("_AM_SYSTEM_BANNERS_CODEHTML","کد Html:");
+
+// Tips
+define("_AM_SYSTEM_BANNERS_NAV_TIPS", "
+<ul>
+<li>اضافه , ویرایش و به روز رسانی شاخه , بنر‌ها و مشتریان.</li>
+</ul>
+");
+
+?>
