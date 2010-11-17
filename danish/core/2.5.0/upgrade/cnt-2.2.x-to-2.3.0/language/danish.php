d039ee00f483b965eaf8216f374f0c4540724a68
Michael Albertsen <culex@culex.dk>
2010-11-17 10:50:45 +0000

    Missing danish language files for upgrade

---
+++ danish/core/2.5.0/upgrade/cnt-2.2.x-to-2.3.0/language/danish.php
@@ -0,0 +1,15 @@
+<?php
+// $Id$
+// _LANGCODE: en
+// _CHARSET : UTF-8
+// Translator: Culex
+
+define( "_CONFIRM_UPGRADE_220", "
+Opdate skriptet vil migrere data fra eksisterende 'profile' modul.<br />
+Af-installer ikke det eksisterende 'profile' modul manuelt, da tilhørende data ikke vil blive migreret.<br /><br />
+Når opgraderingsprocessen er færdig venligst <stong>gå til moduleadministration for at opgradere 'profile' modulet</strong>, hvor data vil blive migreret fuldt.<br /><br />
+For at afbryde upgraderingen, <a href='index.php'>klik her</a>.<br /><br />
+Eller for at <a style='color:green; font-size:1.3em;' href='index.php?action=next&upd220=ok'>fortsætte</a>.
+");
+
+?>
