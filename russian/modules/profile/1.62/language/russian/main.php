89dc68e71da4cb166cd757837dc7a992d13843fa
andrey3761 <andrey3761@c17157d5-08f2-41e7-a131-df055f6a0da3>
2010-12-15 17:20:22 +0000

    Module Profile 1.62 ru.utf8

---
+++ russian/modules/profile/1.62/language/russian/main.php
@@ -0,0 +1,65 @@
+<?php
+// $Id: main.php 3899 2009-11-17 20:52:14Z trabis $
+// _LANGCODE: ru
+// _CHARSET : UTF-8
+// Translator: 2010-12-15 andrey3761
+
+define('_PROFILE_MA_VALIDATIONFAILED', 'Проверка не прошла, пожалуйста попробуйте ещё раз!');
+define("_PROFILE_MA_REGISTER_NOTGROUP", "Новый пользователь не зарегистрирован в соответствующих группах.");
+define("_PROFILE_MA_FINISH_LOGIN", "Ваша учетная запись создана успешно. Пожалуйста используйте имя и пароль введенные при регистрации.");
+define("_PROFILE_MA_REGISTER_FINISH", "Спасибо за регистрацию");
+define("_PROFILE_MA_REGISTER_STEPS", "Шаги регистрации:");
+
+define("_PROFILE_MA_DEFAULT", "Основные сведения");
+
+define("_PROFILE_MA_ERRORDURINGSAVE", "При сохранении возникли ошибки");
+define("_PROFILE_MA_NOSTEPSAVAILABLE", "Извините, на данный момент сайт не принимает новых регистраций");
+define("_PROFILE_MA_EXPIRED", "Процесс прерван. Пожалуйста, вернитесь назад, чтобы повторить попытку.");
+
+
+define("_PROFILE_MA_RECENTACTIVITY","Активность пользователя");
+define("_PROFILE_MA_THEME","Тема оформления");
+define("_PROFILE_MA_ACTIVATE","Активировать");
+define("_PROFILE_MA_DEACTIVATE","Деактивировать");
+define("_PROFILE_MA_SENDPM","Отправить сообщение");
+
+//changepass.php
+define("_PROFILE_MA_CHANGEPASSWORD", "Смена пароля");
+define("_PROFILE_MA_PASSWORDCHANGED", "Пароль успешно изменен");
+define("_PROFILE_MA_OLDPASSWORD", "Текущий пароль");
+define("_PROFILE_MA_NEWPASSWORD", "Новый пароль");
+define("_PROFILE_MA_WRONGPASSWORD", "Старый пароль не верен");
+
+//search.php
+define("_PROFILE_MA_SORTBY", "Сортировать по полю");
+define("_PROFILE_MA_ORDER", "Последовательность сортировки");
+define("_PROFILE_MA_PERPAGE", "Количество записей на странице");
+define("_PROFILE_MA_LATERTHAN", "%s позднее чем");
+define("_PROFILE_MA_EARLIERTHAN", "%s ранее чем");
+define("_PROFILE_MA_LARGERTHAN", "%s более чем");
+define("_PROFILE_MA_SMALLERTHAN", "%s менее чем");
+
+define("_PROFILE_MA_NOUSERSFOUND", "Пользователи не найдены");
+define("_PROFILE_MA_RESULTS", "Результаты поиска");
+define("_PROFILE_MA_SEARCH", "Поиск пользователя");
+
+//changemail.php
+define("_PROFILE_MA_CHANGEMAIL", "Смена e-mail адреса");
+define("_PROFILE_MA_NEWMAIL", "Новый e-mail адрес");
+
+define("_PROFILE_MA_NEWEMAIL", "Новый адрес электронной почты %s");
+define("_PROFILE_MA_EMAILCHANGED", "Ваш e-mail адрес успешно изменен");
+define("_PROFILE_MA_SITEDEFAULT", "Текущие шаблоны сайта");
+
+define("_PROFILE_MA_USERINFO","Профиль пользователя");
+define("_PROFILE_MA_REGISTER","Регистрационная форма");
+
+define("_PROFILE_MA_ACTUS", "Активных пользователей: %s");
+define("_PROFILE_MA_FOUNDUSER", "%s пользователей найдено");
+
+define("_PROFILE_VALIDATE", "Проверка формы");
+define("_PROFILE_VALIDATE_PASS", "<font color='#00FF00'>Проверка прошла успешно!</font>");
+define("_PROFILE_VALIDATE_FAIL", "<font color='#FF0000'>Ошибка при проверки!</font>");
+define("_PROFILE_VALIDATE_PASSKEYFAILED", "<font color='#FF0000'>60 Minutes has passed and you no longer have a valid pass key token, you need to refresh the page!</font>");
+
+?>
