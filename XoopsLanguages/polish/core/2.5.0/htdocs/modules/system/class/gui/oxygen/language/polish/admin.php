820de3bc4f5cef33f335ac5d1f909618819ae7ac
tstempko <tstempko@c17157d5-08f2-41e7-a131-df055f6a0da3>
2011-01-25 16:49:34 +0000

    Uzupelnienie tlumaczenia

---
+++ XoopsLanguages/polish/core/2.5.0/htdocs/modules/system/class/gui/oxygen/language/polish/admin.php
@@ -0,0 +1,86 @@
+<?php
+// $Id$
+// _LANGCODE: pl
+// _CHARSET : UTF-8
+// Translator: tstempko
+
+define('_AD_SYSOPTIONS','Opcje systemu');
+define('_AD_INSTALLEDMODULES','Zainstalowane moduły');
+
+define('_AD_XOOPSTHEMES','Motywy graficzne XOOPS');
+define('_AD_XOOPSMODULES','Moduły XOOPS');
+define('_AD_INTERESTSITES','Linki');
+define('_AD_LOCALSUPPORT','Wsparcie');
+define('_XOOPS_NEWS','Wiadomości Xoops');
+define('_XOOPS_WEBSITE','XOOPS');
+define('_SOURCEFORGE','Sourceforge');
+
+define('_MD_OXYGEN_VERSION','Wersja');
+define('_MD_OXYGEN_VERSION_XOOPS','Wersja XOOPS');
+define('_MD_OXYGEN_VERSION_PHP','Wersja PHP');
+define('_MD_OXYGEN_VERSION_MYSQL','Wersja mySQL');
+define('_MD_OXYGEN_Server_API','Serwer API');
+define('_MD_OXYGEN_OS','OS');
+define('_MD_OXYGEN_ABOUT','Informacje o XOOPS');
+define('_MD_OXYGEN_ABOUT_TEXT','XOOPS jest rozszerzalnym (X - eXtensible), zorientowanym obiektowo (OO- Object Oriented), łatwym w użyciu synamicznym systemem zarządzania treścią, napisanym w języku PHP. XOOPS jest idealnym narzędziem do tworzenia małych i dużych stron społecznościowych, wewnętrznych dtron firmowych, portali firmowych, portali korporacyjnych, weblogów, i wielu innych. Przeczytaj stronę<a href="http://www.xoops.org/modules/wfchannel/" rel="external">All about XOOPS</a> jeśli chcesz dowiedzieć się więcej.');
+define('_MD_OXYGEN_XOOPS_LINKS','Linki XOOPS');
+
+define('_MD_OXYGEN_XOOPSPROJECT','Projekt XOOPS');
+define('_MD_OXYGEN_LOCALSUPPORT','Wsparcie lokalne XOOPS');
+define('_MD_OXYGEN_XOOPSCORE','Jądro XOOPS');
+define('_MD_OXYGEN_XOOPSTHEME','Motywy graficzne XOOPS');
+define('_MD_OXYGEN_XOOPSWIKI','XOOPS Wiki');
+define('_MD_OXYGEN_XOOPSBOOKS','Książki XOOPS');
+define('_MD_OXYGEN_NEWMODULE','Nowe moduły');
+define('_MD_OXYGEN_XOOPSFAQ','XOOPS FAQ');
+define('_MD_OXYGEN_CODESVN','Kod SVN');
+define('_MD_OXYGEN_REPORTBUG','Zgłoś błąd');
+define('_MD_OXYGEN_XGIFTSHOP','Sklep z gadżetami XOOPS');
+define('_MD_OXYGEN_XDONATIONS','Wesprzyj (wpłać)');
+define('_MD_OXYGEN_XDONATIONS_TEXT','<h2>Potrzebujemy Twojej pomocy - Wesprzyj nas małą wpłatą!</h2>
+Jeśli podoba Ci się system XOOPS, i chciałbyś się jakoś odwdzięczyć, możesz dokonać właty na XOOPS Foundation! Skorzystaj z wpłat PayPal aby dokonać przelewuna konto fundacji XOOPS.<br />
+Twoja wpłat zostanie wykorzystana na promocję i rozwój systemu XOOPS. Gwarantowne.<br />
+Fundacja XOOPS Foundation jest zarejestrowana jako organizacja not-for-profit (pożytku społecznego) w USA, i ma na celu rozwój XOOPSa!<br />
+Wszyscy wpłacający otrzymają rangę Friend of XOOPS (Przyjaciela XOOPS). Dziękujemy wszystkim przyjaciołom za ich wpłaty i nieustające wsparcie<br />
+Aby dokonać wpłaty i uzyskać więcej informacji proszę kliknąć <a href="http://www.xoops.org/modules/xdonations/" rel="external">tutaj</a>');
+
+define('_MD_OXYGEN_SITEPREF','Właściwości');
+define('_MD_OXYGEN_GENERAL','Ustawienia ogólne');
+define('_MD_OXYGEN_USERSETTINGS','Ustawienia dotyczące użytkowników');
+define('_MD_OXYGEN_METAFOOTER','Meta Tagi i Stopka');
+define('_MD_OXYGEN_CENSOR','Opcje cenzury');
+define('_MD_OXYGEN_SEARCH','Opcje wyszukiwania');
+define('_MD_OXYGEN_MAILER','Ustawienia poczty');
+define('_MD_OXYGEN_AUTHENTICATION','Opcje uwierzytelniania');
+
+//Add for styles name
+define('_MD_LANG_SILVER','Srebrny');
+define('_MD_LANG_DARK','Ciemny');
+define('_MD_LANG_ORANGE','Pomarańczowy');
+
+//help
+define('_MD_AM_BANS_HELP','Zarządzaj twoimi banerami<br>z tego miejsca');
+define('_MD_AM_BKAD_HELP','Zarządzaj sytemem i ustawieniami bloków<br>które prezentują dodatkowe informacje tobie lub twoim użytkownikom');
+define('_MD_AM_ADGS_HELP','Dodaj/Edytuj grupy <br>i zarządzaj uprawnieniami <br> dla każdej grupy');
+define('_MD_AM_IMAGES_HELP','Utwórz Kategorię dla obrazów<br>i dodaj obrazy do kategorii');
+define('_MD_AM_MDAD_HELP','Instaluj/Odinstaluj moduły XOOPS.<br>Ustawiaj stan modułów ');
+define('_MD_AM_PREF_HELP','Zarządzaj indywidualnymi i ogólnymi <br>właściwościami modułów');
+define('_MD_AM_SMLS_HELP','Zarządzaj emotikonami (uśmieszkami) które<br>są używane w twoich postach, PM, komentarzach, itp.');
+define('_MD_AM_RANK_HELP','Zarządzaj rangami użytkowników');
+define('_MD_AM_USER_HELP','Dodaj/Edytuj użytkownika<br> i informacje o nim');
+define('_MD_AM_FINDUSER_HELP','Wyszukaj użytkownika w oparciu o dostępne <br>informacje o nim, np. email, nazwa użytkownika');
+define('_MD_AM_MLUS_HELP','Wyślij email lub PM (Prywatną Wiadomość) do wybranych użytkowników');
+define('_MD_AM_AVATARS_HELP','Dodaj avatarki dla użytkowników,<br>których mogą używać w swoich profilach');
+define('_MD_AM_TPLSETS_HELP','Modyfikuj/Personalizuj szablony XOOPS');
+define('_MD_AM_COMMENTS_HELP','Zarządzaj komentarzami użytkowników');
+
+//Add 10 des
+define('_MD_OXYGEN_XOOPS_LICENSE','Licencja XOOPS');
+define('_MD_OXYGEN_KEY','Klucz XOOPS');
+define('_MD_OXYGEN_LICENSE','Licencja');
+define('_MD_OXYGEN_RSS','Kanał RSS ');
+define('_MD_OXYGEN_POWERED_BY','Powered by');
+define('_MD_OXYGEN_SOURCEFORGE_TEXT','Kod XOOPS jest dostępny na SourceForge');
+define('_MD_OXYGEN_ADMINISTRATION','Administracja XOOPS');
+
+?>
---
+++ XoopsLanguages/polish/core/2.5.0/htdocs/modules/system/class/gui/oxygen/language/polish/localsupport.php
@@ -0,0 +1,41 @@
+<?php
+// $Id$
+// _LANGCODE: pl
+// _CHARSET : UTF-8
+// Translator: tstempko
+
+$menu = array();
+
+// sample for English  support
+/*
+$menu[] = array(
+    'link'      => 'http://www.xoops.org',
+    'title'     => 'XOOPS',
+    'absolute'  => 1,
+    'icon'      => XOOPS_URL . '/modules/system/class/gui/oxygen/images/xoops.png'
+);
+
+$menu[] = array(
+    'link'      => 'http://www.xoops.org',
+    'title'     => 'XOOPS',
+    'absolute'  => 1,
+    'icon'      => XOOPS_URL . '/modules/system/class/gui/oxygen/images/xoops.png'
+);
+
+$menu[] = array(
+    'link'      => 'http://www.xoops.org/modules/library/',
+    'title'     => _AD_XOOPSTHEMES,
+    'absolute'  => 1,
+    'icon'      => XOOPS_URL . '/modules/system/class/gui/oxygen/images/tweb.png'
+);
+
+$menu[] = array(
+    'link'      => 'http://www.xoops.org/modules/modules-search/',
+    'title'     => _AD_XOOPSMODULES,
+    'absolute'  => 1,
+    'icon'      => XOOPS_URL . '/modules/system/class/gui/oxygen/images/xoops.png'
+);
+*/
+
+return $menu;
+?>
