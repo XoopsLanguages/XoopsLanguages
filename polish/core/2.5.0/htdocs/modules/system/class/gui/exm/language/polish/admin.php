4a5301e8e98fefb93659b7c69ed3cd7708c42443
tstempko <tstempko@users.noreply.github.com>
2011-01-25 16:49:34 +0000

    Uzupelnienie tlumaczenia

---
+++ polish/core/2.5.0/htdocs/modules/system/class/gui/exm/language/polish/admin.php
@@ -0,0 +1,13 @@
+<?php
+// $Id$
+// _LANGCODE: pl
+// _CHARSET : UTF-8
+// Translator: tstempko
+
+define('_AD_SYSOPTIONS','Opcje systemu');
+define('_AD_INSTALLEDMODULES','Zainstalowane moduły');
+
+define('_AD_XOOPSTHEMES','Motywy graficzne');
+define('_AD_INTERESTSITES','Linki');
+
+?>
