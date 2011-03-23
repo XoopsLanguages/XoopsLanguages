8cce6231e6a4a0dc712f9d8ece7815434553d923
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2011-03-23 21:34:28 +0000

    Adding Reference Files for 2.5.0 Translation

---
+++ XoopsLanguages/english-Reference/core/2.5.0/htdocs/themes/zetagenesis/language/english/main.php
@@ -0,0 +1,52 @@
+<?php
+
+// Theme language definitions
+
+// $Id: main.php 5307 2010-09-18 10:08:22Z kris_fr $
+// _LANGCODE: en
+// _CHARSET : UTF-8
+// Translator: XOOPS Translation Team
+
+/* ------------------- userbar.html ------------------- */
+define("_THEME_REGISTER",           "User Registration");
+define("_THEME_INBOX",              "View your messages");
+define("_THEME_INBOX_NOTREAD",      "message(s) unread");
+define("_THEME_NOTIFICATIONS",      "View your notifications");
+define("_THEME_PROFILE",            "View your profile");
+define("_THEME_ADMINISTRATION",     "Administration");
+define("_THEME_LOGOUT",             "Click to logout");
+
+define("THEME_WELLCOME",				"Welcome");
+define("THEME_EDITPROFILE",				"Edit profil");
+define("THEME_NOTIFICATION",			"See notification");
+define("THEME_MESSAGE",					"See message");
+define("THEME_YOUHAVE",					"You have");
+define("THEME_NOTREAD",					"message no read");
+define("THEME_MYACCOUNT",				"My account");
+define("THEME_SEARCH",					"Search");
+define("THEME_DESC_SEARCH",				"Find on website");
+define("THEME_KEYWORDS",				"Keyword");
+define("THEME_UPTOP",					"Top page");
+define("THEME_USER",					"Users");
+
+/* ------------------- xo_footerstatic.html ------------------- */
+define("THEME_CPHOME",					"Administration");
+define("THEME_BANS",					"Manage banners");
+define("THEME_BKAD",					"Manage blocks");
+define("THEME_ADGS",					"Manage users groups");
+define("THEME_PREF",					"Settings");
+define("THEME_INSTALLEDMODULES",		"Manage module(s)");
+define("THEME_IMAGES",					"Images");
+define("THEME_MDAD",					"Modules");
+define("THEME_SMLS",					"Manage smilies");
+define("THEME_RANK",					"User Ranks");
+define("THEME_EUSER",					"Edit User(s)");
+define("THEME_FINDUSER",				"Find a user");
+define("THEME_MLUS",					"Send email");
+define("THEME_AVATARS",					"Manage avatars");
+define("THEME_TPLS",					"Manage templates");
+define("THEME_COMMENTS",				"Manage comments");
+define("THEME_ADMTOOLS",				"Tools");
+define("THEME_ADMCLEAN",				"Maintenance");
+define("THEME_SYSSETTING",				"System Module Settings");
+?>
