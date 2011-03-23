4bc5f8ddde1bed84544c6d9c8e0559d67ee220e1
andrey3761 <andrey3761@c17157d5-08f2-41e7-a131-df055f6a0da3>
2011-03-23 21:43:41 +0000

    XOOPS 2.5.1 RC: Russian translation is added.

---
+++ XoopsLanguages/russian/core/2.5.1/htdocs/language/russian/misc.php
@@ -0,0 +1,27 @@
+<?php
+// $Id: misc.php 4897 2010-06-19 02:55:48Z phppp $
+// _LANGCODE: ru
+// _CHARSET : UTF-8
+// Translator: 2010-10-09 andrey3761
+
+define('_MSC_YOURNAMEC','Ваше имя: ');
+define('_MSC_YOUREMAILC','Ваш E-mail: ');
+define('_MSC_FRIENDNAMEC','Имя друга: ');
+define('_MSC_FRIENDEMAILC','E-mail друга: ');
+define('_MSC_RECOMMENDSITE','Посоветовать этот сайт другу');
+// %s название вашего сайта
+define('_MSC_INTSITE','Интересный сайт: %s');
+define('_MSC_REFERENCESENT','Ссылка на наш сайт была передана Вашему другу. Спасибо!');
+define('_MSC_ENTERYNAME','Пожалуйста введите свое имя');
+define('_MSC_ENTERFNAME','Пожалуйста введите имя друга');
+define('_MSC_ENTERFMAIL','Пожалуйста введите E-mail адрес друга');
+define('_MSC_NEEDINFO','Пожалуйста заполните обязательные поля!');
+define('_MSC_INVALIDEMAIL1','Введенный Вами E-mail адрес не верен.');
+define('_MSC_INVALIDEMAIL2','Пожалуйста проверьте адрес и попробуйте снова.');
+define('_MSC_AVAVATARS','Доступные аватары');
+define('_MSC_SMILIES','Смайлики');
+define('_MSC_CLICKASMILIE','Нажмите на смайлик для вставки его в Ваше сообщение.');
+define('_MSC_CODE','Код');
+define('_MSC_EMOTION','Эмоция');
+
+?>
---
+++ XoopsLanguages/russian/core/2.5.1/htdocs/modules/system/language/russian/admin/userrank.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @copyright   The XOOPS Project http://sourceforge.net/projects/xoops/
+ * @license     http://www.fsf.org/copyleft/gpl.html GNU public license
+ * _LANGCODE    ru
+ * _CHARSET     UTF-8
+ * Translator   2010-11-11 andrey3761
+ * @version     $Id: userrank.php 5580 2010-10-21 19:38:48Z forxoops $
+ */
+ //Nav
+define("_AM_SYSTEM_USERRANK_NAV_MANAGER","Управление рангами");
+define("_AM_SYSTEM_USERRANK_NAV_MAIN","Список рангов");
+define("_AM_SYSTEM_USERRANK_NAV_ADD","Добавить новый ранг");
+define("_AM_SYSTEM_USERRANK_NAV_EDIT","Редактировать ранг");
+define("_AM_SYSTEM_USERRANK_NAV_DELETE","Удалить ранг");
+
+define("_AM_SYSTEM_USERRANK_ACTION","Действие");
+define("_AM_SYSTEM_USERRANK_ADD","Добавить новый ранг");
+define("_AM_SYSTEM_USERRANK_DELETE","Удалить ранг");
+define("_AM_SYSTEM_USERRANK_EDIT","Редактировать ранг");
+define("_AM_SYSTEM_USERRANK_IMAGE","Рисунок");
+define("_AM_SYSTEM_USERRANK_IMAGE_PATH", "Файлы существуют в: %s");
+define("_AM_SYSTEM_USERRANK_MAXPOST","Макс. сообщений");
+define("_AM_SYSTEM_USERRANK_MINPOST","Мин. сообщений");
+define("_AM_SYSTEM_USERRANK_OFF","выкл");
+define("_AM_SYSTEM_USERRANK_ON","вкл");
+define("_AM_SYSTEM_USERRANK_SAVE",_AM_SYSTEM_DBUPDATED);
+define("_AM_SYSTEM_USERRANK_SPECIAL","Специальный ранг");
+define("_AM_SYSTEM_USERRANK_SPECIAL_CAN","(Специальный ранг, может быть присвоен независимо от количества сообщений пользователем)");
+define("_AM_SYSTEM_USERRANK_SUREDEL", "Вы уверены, что хотите удалить этот ранг?");
+define("_AM_SYSTEM_USERRANK_TITLE","Наименование");
+define("_AM_SYSTEM_USERRANK_UPLOAD","Загрузить");
+
+// Tips
+define("_AM_SYSTEM_USERRANK_TIPS", "<ul><li>Сдесь Вы можете добавлять, изменять или удалять ранги пользователей.</li></ul>");
+define("_AM_SYSTEM_USERRANK_TIPS_FORM1","<ul><li>Разрешенные типы файлов: %s</li>");
+define("_AM_SYSTEM_USERRANK_TIPS_FORM2","<li>Максимально допустимый размер загружаемого файла: %s kb</li></ul>");
+
+?>
