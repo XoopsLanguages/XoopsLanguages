9900a3539d75ef2195571348ed9118359df72587
sabahan <sabahan@c17157d5-08f2-41e7-a131-df055f6a0da3>
2011-09-20 14:42:25 +0000

    update xoops 2.5.2 malay translation (unfinished)

---
+++ malaysian/core/2.5x/htdocs/modules/system/language/malay/admin/avatars.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @copyright   The XOOPS Project http://sourceforge.net/projects/xoops/
+ * @license     GNU GPL 2 (http://www.gnu.org/licenses/old-licenses/gpl-2.0.html)
+ * _LANGCODE    en
+ * _CHARSET     UTF-8
+ * @version     $Id$
+ */
+
+// Navigation
+define("_AM_SYSTEM_AVATAR_MANAGER","Avatars Manager");
+define("_AM_SYSTEM_AVATAR_MAIN","Main");
+define("_AM_SYSTEM_AVATAR_SYSTEM","System Avatars");
+define("_AM_SYSTEM_AVATAR_CUSTOM","Custom Avatars");
+define("_AM_SYSTEM_AVATAR_ADD","Add Avatar");
+define("_AM_SYSTEM_AVATAR_EDIT","Edit Avatar");
+define("_AM_SYSTEM_AVATAR_DELETE","Delete Avatar");
+
+// Infos
+define("_AM_SYSTEM_AVATAR_ERROR","Errors");
+define("_AM_SYSTEM_AVATAR_USERS","Users using this avatar");
+define("_AM_SYSTEM_AVATAR_USE_FILE","Choose files in: %s");
+define("_AM_SYSTEM_AVATAR_UPLOAD","Upload:");
+// Messages
+define("_AM_SYSTEM_AVATAR_FAILDEL","Failed deleting avatar %s from the database");
+define("_AM_SYSTEM_AVATAR_SUREDEL","Are you sure that you want to delete this avatar image ?");
+
+// Tips
+define("_AM_SYSTEM_AVATAR_TIPS","
+<ul id='newsticker' class='newsticker'>
+<li>Manage all System or cutom avatars</li>
+<li>Site members can optionally fashion their own online personal called avatars.<br />This option can be set off from module User preferences.</li>
+</ul>");
+
+?>
