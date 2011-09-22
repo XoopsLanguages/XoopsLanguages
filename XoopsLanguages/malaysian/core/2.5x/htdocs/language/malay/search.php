fa1208c0a68271f5060df5f806a78384480b8f7b
sabahan <sabahan@c17157d5-08f2-41e7-a131-df055f6a0da3>
2011-09-22 08:53:21 +0000

    Updating Malay Translation

--- XoopsLanguages/malaysian/core/2.5x/htdocs/language/malay/search.php
+++ XoopsLanguages/malaysian/core/2.5x/htdocs/language/malay/search.php
@@ -4,24 +4,24 @@
 // _CHARSET : UTF-8
 // Translator: XOOPS Translation Team
 //%%%%%%    File Name search.php    %%%%%
-define('_SR_SEARCH', 'Search');
-define('_SR_PLZENTER', 'Please enter all required data!');
-define('_SR_SEARCHRESULTS', 'Search Results');
-define('_SR_NOMATCH', 'No Match Found for your Query');
-define('_SR_FOUND', 'Found <strong>%s</strong> match(es)');
-define('_SR_SHOWING', '(Showing %d - %d)');
-define('_SR_ANY', 'Any (OR)');
-define('_SR_ALL', 'All (AND)');
-define('_SR_EXACT', 'Exact Match');
-define('_SR_SHOWALLR', 'Show all results');
-define('_SR_NEXT', 'Next >>');
-define('_SR_PREVIOUS', '<< Previous');
-define('_SR_KEYWORDS', 'Keyword(s)');
-define('_SR_TYPE', 'Type');
-define('_SR_SEARCHIN', 'Search in');
-define('_SR_KEYTOOSHORT', 'Keywords must be at least <strong>%s</strong> characters long');
-define('_SR_KEYIGNORE', 'Keywords shorter than <strong>%s</strong> characters will be ignored');
-define('_SR_SEARCHRULE', 'Seach Rule');
-define('_SR_IGNOREDWORDS', 'The following words are shorter than allowed minmum length (%u chars) and were not included in your search:');
+define('_SR_SEARCH', 'Carian');
+define('_SR_PLZENTER', 'Sila masukkan kata kunci carian anda !!');
+define('_SR_SEARCHRESULTS', 'Hasil Keputusan Carian');
+define('_SR_NOMATCH', 'Tiada padanan dengan kata kunci carian anda');
+define('_SR_FOUND', ' <strong>%s</strong> padanan dijumpai');
+define('_SR_SHOWING', '(Memaparkan %d - %d)');
+define('_SR_ANY', 'Atau (OR)');
+define('_SR_ALL', 'Semua (AND)');
+define('_SR_EXACT', 'Padanan Tepat');
+define('_SR_SHOWALLR', 'Tunjukkan semua hasil carian');
+define('_SR_NEXT', 'Seterusnya >>');
+define('_SR_PREVIOUS', '<< Sebelumnya');
+define('_SR_KEYWORDS', 'Kata Kunci Carian');
+define('_SR_TYPE', 'Jenis');
+define('_SR_SEARCHIN', 'Cari dalam');
+define('_SR_KEYTOOSHORT', 'Kata kunci carian mesti sekurang-kurangnya <strong>%s</strong> huruf');
+define('_SR_KEYIGNORE', 'Kata kunci carian pendek dari <strong>%s</strong> huruf akan diabaikan');
+define('_SR_SEARCHRULE', 'Peraturan carian');
+define('_SR_IGNOREDWORDS', 'Perkataan tersebut adalah lebih pendek dari panjang minimum (%u aksara) dan tidak dimasukkan dalam carian anda:');
 
 ?>
