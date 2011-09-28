b552f42efc142fe3ab4fe9f55c3e24c0eb687912
tstempko <tstempko@users.noreply.github.com>
2011-09-28 12:39:39 +0000

    fix: update paths to to ranks and smiles

--- polish/core/2.5.0/htdocs/install/language/polish/mysql.lang.data.sql
+++ polish/core/2.5.0/htdocs/install/language/polish/mysql.lang.data.sql
@@ -2,32 +2,32 @@
 # Dumping data for table `ranks`
 #
 
-INSERT INTO ranks VALUES (1, 'Przechodzeń', 0, 20, 0, 'rank3e632f95e81ca.gif');
-INSERT INTO ranks VALUES (2, 'Nowicjusz', 21, 40, 0, 'rank3dbf8e94a6f72.gif');
-INSERT INTO ranks VALUES (3, 'Bywalec', 41, 70, 0, 'rank3dbf8e9e7d88d.gif');
-INSERT INTO ranks VALUES (4, 'Stały bywalec', 71, 150, 0, 'rank3dbf8ea81e642.gif');
-INSERT INTO ranks VALUES (5, 'Znana osobistość', 151, 10000, 0, 'rank3dbf8eb1a72e7.gif');
-INSERT INTO ranks VALUES (6, 'Moderator', 0, 0, 1, 'rank3dbf8edf15093.gif');
-INSERT INTO ranks VALUES (7, 'Webmaster', 0, 0, 1, 'rank3dbf8ee8681cd.gif');
+INSERT INTO ranks VALUES (1, 'Przechodzeń', 0, 20, 0, 'ranks/rank3e632f95e81ca.gif');
+INSERT INTO ranks VALUES (2, 'Nowicjusz', 21, 40, 0, 'ranks/rank3dbf8e94a6f72.gif');
+INSERT INTO ranks VALUES (3, 'Bywalec', 41, 70, 0, 'ranks/rank3dbf8e9e7d88d.gif');
+INSERT INTO ranks VALUES (4, 'Stały bywalec', 71, 150, 0, 'ranks/rank3dbf8ea81e642.gif');
+INSERT INTO ranks VALUES (5, 'Znana osobistość', 151, 10000, 0, 'ranks/rank3dbf8eb1a72e7.gif');
+INSERT INTO ranks VALUES (6, 'Moderator', 0, 0, 1, 'ranks/rank3dbf8edf15093.gif');
+INSERT INTO ranks VALUES (7, 'Webmaster', 0, 0, 1, 'ranks/rank3dbf8ee8681cd.gif');
 
 #
 # Dumping data for table `smiles`
 #
 
-INSERT INTO smiles VALUES (1, ':-D', 'smil3dbd4d4e4c4f2.gif', 'Szeroko uśmiechnięty', 1);
-INSERT INTO smiles VALUES (2, ':-)', 'smil3dbd4d6422f04.gif', 'Uśmiechnięty', 1);
-INSERT INTO smiles VALUES (3, ':-(', 'smil3dbd4d75edb5e.gif', 'Smutny', 1);
-INSERT INTO smiles VALUES (4, ':-o', 'smil3dbd4d8676346.gif', 'Zdziwiony', 1);
-INSERT INTO smiles VALUES (5, ':-?', 'smil3dbd4d99c6eaa.gif', 'Zdezorientowany', 1);
-INSERT INTO smiles VALUES (6, '8-)', 'smil3dbd4daabd491.gif', 'Cool', 1);
-INSERT INTO smiles VALUES (7, ':lol:', 'smil3dbd4dbc14f3f.gif', 'Lol', 1);
-INSERT INTO smiles VALUES (8, ':-x', 'smil3dbd4dcd7b9f4.gif', 'Zły', 1);
-INSERT INTO smiles VALUES (9, ':-P', 'smil3dbd4ddd6835f.gif', 'Z języczkiem', 1);
-INSERT INTO smiles VALUES (10, ':oops:', 'smil3dbd4df1944ee.gif', 'Zawstydzony', 0);
-INSERT INTO smiles VALUES (11, ':cry:', 'smil3dbd4e02c5440.gif', 'Płaczący', 0);
-INSERT INTO smiles VALUES (12, ':evil:', 'smil3dbd4e1748cc9.gif', 'Wściekły', 0);
-INSERT INTO smiles VALUES (13, ':roll:', 'smil3dbd4e29bbcc7.gif', 'Rolling Eyes', 0);
-INSERT INTO smiles VALUES (14, ';-)', 'smil3dbd4e398ff7b.gif', 'Oczko', 0);
-INSERT INTO smiles VALUES (15, ':pint:', 'smil3dbd4e4c2e742.gif', 'Another pint of beer', 0);
-INSERT INTO smiles VALUES (16, ':hammer:', 'smil3dbd4e5e7563a.gif', 'ToolTimes at work', 0);
-INSERT INTO smiles VALUES (17, ':idea:', 'smil3dbd4e7853679.gif', 'Mam pomysł', 0);
+INSERT INTO smiles VALUES (1, ':-D', 'smilies/smil3dbd4d4e4c4f2.gif', 'Szeroko uśmiechnięty', 1);
+INSERT INTO smiles VALUES (2, ':-)', 'smilies/smil3dbd4d6422f04.gif', 'Uśmiechnięty', 1);
+INSERT INTO smiles VALUES (3, ':-(', 'smilies/smil3dbd4d75edb5e.gif', 'Smutny', 1);
+INSERT INTO smiles VALUES (4, ':-o', 'smilies/smil3dbd4d8676346.gif', 'Zdziwiony', 1);
+INSERT INTO smiles VALUES (5, ':-?', 'smilies/smil3dbd4d99c6eaa.gif', 'Zdezorientowany', 1);
+INSERT INTO smiles VALUES (6, '8-)', 'smilies/smil3dbd4daabd491.gif', 'Cool', 1);
+INSERT INTO smiles VALUES (7, ':lol:', 'smilies/smil3dbd4dbc14f3f.gif', 'Lol', 1);
+INSERT INTO smiles VALUES (8, ':-x', 'smilies/smil3dbd4dcd7b9f4.gif', 'Zły', 1);
+INSERT INTO smiles VALUES (9, ':-P', 'smilies/smil3dbd4ddd6835f.gif', 'Z języczkiem', 1);
+INSERT INTO smiles VALUES (10, ':oops:', 'smilies/smil3dbd4df1944ee.gif', 'Zawstydzony', 0);
+INSERT INTO smiles VALUES (11, ':cry:', 'smilies/smil3dbd4e02c5440.gif', 'Płaczący', 0);
+INSERT INTO smiles VALUES (12, ':evil:', 'smilies/smil3dbd4e1748cc9.gif', 'Wściekły', 0);
+INSERT INTO smiles VALUES (13, ':roll:', 'smilies/smil3dbd4e29bbcc7.gif', 'Rolling Eyes', 0);
+INSERT INTO smiles VALUES (14, ';-)', 'smilies/smil3dbd4e398ff7b.gif', 'Oczko', 0);
+INSERT INTO smiles VALUES (15, ':pint:', 'smilies/smil3dbd4e4c2e742.gif', 'Another pint of beer', 0);
+INSERT INTO smiles VALUES (16, ':hammer:', 'smilies/smil3dbd4e5e7563a.gif', 'ToolTimes at work', 0);
+INSERT INTO smiles VALUES (17, ':idea:', 'smilies/smil3dbd4e7853679.gif', 'Mam pomysł', 0);
