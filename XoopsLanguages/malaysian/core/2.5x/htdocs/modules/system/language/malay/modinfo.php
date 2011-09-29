0cd74f60657bdd441c4f438fb8cd9f5c8c561bcd
sabahan <sabahan@c17157d5-08f2-41e7-a131-df055f6a0da3>
2011-09-29 00:09:59 +0000

    Update Malay Translation

--- XoopsLanguages/malaysian/core/2.5x/htdocs/modules/system/language/malay/modinfo.php
+++ XoopsLanguages/malaysian/core/2.5x/htdocs/modules/system/language/malay/modinfo.php
@@ -7,50 +7,50 @@
 // Module Info
 
 // The name of this module
-define("_MI_SYSTEM_NAME","System");
+define("_MI_SYSTEM_NAME","Sistem");
 
 // A brief description of this module
-define("_MI_SYSTEM_DESC","For administration of core settings of the site.");
+define("_MI_SYSTEM_DESC","Untuk pentadbiran tetapan Core XOOPS.");
 
 // Names of blocks for this module (Not all module has blocks)
-define("_MI_SYSTEM_BNAME2","User Menu");
-define("_MI_SYSTEM_BNAME3","Login");
-define("_MI_SYSTEM_BNAME4","Search");
-define("_MI_SYSTEM_BNAME5","Waiting Contents");
-define("_MI_SYSTEM_BNAME6","Main Menu");
-define("_MI_SYSTEM_BNAME7","Site Info");
-define("_MI_SYSTEM_BNAME8", "Who is Online");
-define("_MI_SYSTEM_BNAME9", "Top Posters");
-define("_MI_SYSTEM_BNAME10", "New Members");
-define("_MI_SYSTEM_BNAME11", "Recent Comments");
+define("_MI_SYSTEM_BNAME2","Menu Pengguna");
+define("_MI_SYSTEM_BNAME3","Log Masuk");
+define("_MI_SYSTEM_BNAME4","Cari");
+define("_MI_SYSTEM_BNAME5","Kandungan Menunggu");
+define("_MI_SYSTEM_BNAME6","Menu Utama");
+define("_MI_SYSTEM_BNAME7","Info Laman");
+define("_MI_SYSTEM_BNAME8", "Siapa dalam talian");
+define("_MI_SYSTEM_BNAME9", "Pengirim Terbanyak");
+define("_MI_SYSTEM_BNAME10", "Ahli Baru");
+define("_MI_SYSTEM_BNAME11", "Komen Terbaru");
 // RMV-NOTIFY
-define("_MI_SYSTEM_BNAME12", "Notification Options");
+define("_MI_SYSTEM_BNAME12", "Pilihan Notifikasi");
 define("_MI_SYSTEM_BNAME13", "Themes");
 
 // Names of admin menu items
 define("_MI_SYSTEM_ADMENU1","Banners");
 define("_MI_SYSTEM_ADMENU2","Blocks");
-define("_MI_SYSTEM_ADMENU3","Groups");
+define("_MI_SYSTEM_ADMENU3","Kumpulan");
 define("_MI_SYSTEM_ADMENU5","Modules");
-define("_MI_SYSTEM_ADMENU6","Preferences");
+define("_MI_SYSTEM_ADMENU6","Pilihan Utama");
 define("_MI_SYSTEM_ADMENU7","Smilies");
-define("_MI_SYSTEM_ADMENU9","User Ranks");
-define("_MI_SYSTEM_ADMENU10","Edit User");
-define("_MI_SYSTEM_ADMENU11","Mail Users");
-define("_MI_SYSTEM_ADMENU12", "Find Users");
-define("_MI_SYSTEM_ADMENU13", "Images");
+define("_MI_SYSTEM_ADMENU9","Pangkat Pengguna");
+define("_MI_SYSTEM_ADMENU10","Edit Pengguna");
+define("_MI_SYSTEM_ADMENU11","Mel Pengguna");
+define("_MI_SYSTEM_ADMENU12", "Cari Pengguna");
+define("_MI_SYSTEM_ADMENU13", "Imej");
 define("_MI_SYSTEM_ADMENU14", "Avatars");
 define("_MI_SYSTEM_ADMENU15", "Templates");
-define("_MI_SYSTEM_ADMENU16", "Comments");
+define("_MI_SYSTEM_ADMENU16", "Komen");
 
 //Preference
-define("_MI_SYSTEM_PREFERENCE_BREAK_GENERAL","General Settings");
-define("_MI_SYSTEM_PREFERENCE_TIPS","Help online?");
-define("_MI_SYSTEM_PREFERENCE_TIPS_DSC","This gives you tips and online help");
-define("_MI_SYSTEM_PREFERENCE_ICONS","Icons");
+define("_MI_SYSTEM_PREFERENCE_BREAK_GENERAL","Tetapan Umum");
+define("_MI_SYSTEM_PREFERENCE_TIPS","Bantuan Dalam Talian?");
+define("_MI_SYSTEM_PREFERENCE_TIPS_DSC","Memberi anda tips dan bantuan dalam talian");
+define("_MI_SYSTEM_PREFERENCE_ICONS","Ikon");
 define("_MI_SYSTEM_PREFERENCE_BREADCRUMB","Breadcrumb");
 
-define("_MI_SYSTEM_PREFERENCE_BREAK_ACTIVE","Active Section");
+define("_MI_SYSTEM_PREFERENCE_BREAK_ACTIVE","Seksyen Aktif");
 define("_MI_SYSTEM_PREFERENCE_ACTIVE_AVATARS","Active Avatars");
 define("_MI_SYSTEM_PREFERENCE_ACTIVE_BANNERS","Active Banners");
 define("_MI_SYSTEM_PREFERENCE_ACTIVE_BLOCKSADMIN","");
