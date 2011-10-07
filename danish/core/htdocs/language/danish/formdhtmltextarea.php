b9f9f1a70cd9d4b6947b6f6e96ee18826d6b6692
Michael Albertsen <culex@culex.dk>
2011-10-07 10:47:10 +0000

    Danish translation for 2.5.3

---
+++ danish/core/htdocs/language/danish/formdhtmltextarea.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Language definitions for {@link XoopsFormDhtmlTextArea}
+ *
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code 
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright       The XOOPS Project http://sourceforge.net/projects/xoops/
+ * @license         GNU GPL 2 (http://www.gnu.org/licenses/old-licenses/gpl-2.0.html)
+ * @package         kernel
+ * @subpackage      form
+ * @since           2.3.0
+ * @author          Taiwen Jiang <phppp@users.sourceforge.net>
+ * @version         $Id: formdhtmltextarea.php 4941 2010-07-22 17:13:36Z beckmi $
+ */
+defined('XOOPS_ROOT_PATH') or die('Restricted access');
+
+// _LANGCODE: da
+// _CHARSET : UTF-8
+// Translator: XOOPS Translation Team
+/**
+ * Localized fonts 
+ */
+$GLOBALS['formtextdhtml_fonts'] = array(
+    'Arial' , 
+    'Courier' , 
+    'Georgia' , 
+    'Helvetica' , 
+    'Impact' , 
+    'Verdana' , 
+    'Haettenschweiler');
+/**
+ * Localized font sizes: 'font size value' => 'font size name'
+ */
+$GLOBALS['formtextdhtml_sizes'] = array(
+    'xx-small' => 'xx-Small' , 
+    'x-small' => 'x-Small' , 
+    'small' => 'Small' , 
+    'medium' => 'Medium' , 
+    'large' => 'Large' , 
+    'x-large' => 'x-Large' , 
+    'xx-large' => 'xx-Large');
+define("_XOOPS_FORM_ALT_URL", "URL");
+define("_XOOPS_FORM_ALT_EMAIL", "E-mail");
+define("_XOOPS_FORM_ALT_IMG", "Billeder");
+define("_XOOPS_FORM_ALT_IMAGE", "Inden i billeder");
+define("_XOOPS_FORM_ALT_SMILEY", "Smiley");
+define("_XOOPS_FORM_ALT_CODE", "Kildekode");
+define("_XOOPS_FORM_ALT_QUOTE", "Citat");
+define("_XOOPS_FORM_ALT_BOLD", "Fed");
+define("_XOOPS_FORM_ALT_ITALIC", "Kursiv");
+define("_XOOPS_FORM_ALT_UNDERLINE", "Understreget");
+define("_XOOPS_FORM_ALT_LINETHROUGH", "Gennemstreget");
+define("_XOOPS_FORM_ALT_ENTERHEIGHT", "Højde:");
+define("_XOOPS_FORM_ALT_ENTERWIDTH", "Bredde:");
+define("_XOOPS_FORM_ALT_LEFT", "Venstre");
+define("_XOOPS_FORM_ALT_RIGHT", "Højre");
+define("_XOOPS_FORM_ALT_CENTER", "Center");
+define("_XOOPS_FORM_ALTFLASH", "Flash");
+define("_XOOPS_FORM_ALTMMS", "MMS");
+define("_XOOPS_FORM_ALTRTSP", "Real Player");
+define("_XOOPS_FORM_ALTIFRAME", "IFRAME");
+define("_XOOPS_FORM_ALTWIKI", "WIKI link");
+define("_XOOPS_FORM_ENTERIFRAMEURL", "IFRAME URL:");
+define("_XOOPS_FORM_ENTERMMSURL", "RMMS URL:");
+define("_XOOPS_FORM_ENTERWMPURL", "WMP URL:");
+define("_XOOPS_FORM_ENTERFLASHURL", "FLASH URL:");
+define("_XOOPS_FORM_ENTERYOUTUBEURL", "Youtube URL:");
+define("_XOOPS_FORM_ENTERRTSPURL", "RTSP URL:");
+define("_XOOPS_FORM_ENTERWIKITERM", "Ordet der skal være knyttet til Wiki:");
+define("_XOOPS_FORM_ALTMP3", "MP3");
+define("_XOOPS_FORM_ENTERMP3URL", "MP3 URL");
+define("_XOOPS_FORM_ALT_CHECKLENGTH", "Check tekstlængde");
+define("_XOOPS_FORM_ALT_LENGTH", "Nuværende længde på indhold: %s");
+define("_XOOPS_FORM_ALT_LENGTH_MAX", "Maksimal længde:");
+define("_XOOPS_FORM_PREVIEW_CONTENT", "Klik på <strong>'. _PREVIEW. '</strong> for at se indholdet i aktion.");
+
+define("_XOOPS_FORM_ALTYOUTUBE", "Youtube");
+?>
+<?php // Translation done by xtransam & anderssk - 2010-10-11 22:40 ?>
---
+++ danish/core/htdocs/language/danish/logger.php
@@ -0,0 +1,59 @@
+<?php
+/*
+ You may not change or alter any portion of this comment or credits
+ of supporting developers from this source code or any supporting source code
+ which is considered copyrighted (c) material of the original comment or credit authors.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+/**
+ *  Xoops Language
+ *
+ * @copyright       The XOOPS Project http://sourceforge.net/projects/xoops/
+ * @license         GNU GPL 2 (http://www.gnu.org/licenses/old-licenses/gpl-2.0.html)
+ * @package         kernel
+ * @subpackage      Xoops Logger Language
+ * @since           2.4.0
+ * @author          trabis <lusopoemas@gmail.com>
+ * @version         $Id: logger.php 0000 01/08/2009 10:02:39 trabis $
+ */
+defined('XOOPS_ROOT_PATH') or die('Restricted access');
+
+// _LANGCODE: en
+// _CHARSET : UTF-8
+// Translator: XOOPS Translation Team
+define("_LOGGER_DEBUG", "Debug");
+define("_LOGGER_INCLUDED_FILES", "Inkluderede filer");
+define("_LOGGER_FILES", "%s filer");
+define("_LOGGER_MEM_USAGE", "Hukommelsesbrug");
+define("_LOGGER_MEM_ESTIMATED", "%s [Anslået]");
+define("_LOGGER_NONE", "Ingen");
+define("_LOGGER_ALL", "Alle");
+define("_LOGGER_ERRORS", "Fejl");
+define("_LOGGER_DEPRECATED", "Forældet");
+define("_LOGGER_QUERIES", "Forespørgsler");
+define("_LOGGER_BLOCKS", "Blokke");
+define("_LOGGER_EXTRA", "Ekstra");
+define("_LOGGER_TIMERS", "Timere");
+
+
+define("_LOGGER_TIMETOLOAD", "%s tog %s sekunder at indlæse.");
+
+define("_LOGGER_TOTAL", "Total");
+
+define("_LOGGER_NOT_CACHED", "Ikke cached");
+define("_LOGGER_CACHED", "Cachelagrede (regenererer hvert %s sekund)");
+
+define("_LOGGER_UNKNOWN", "Ukendt");
+define("_LOGGER_E_USER_NOTICE", "Bemærk");
+define("_LOGGER_E_USER_WARNING", "Advarsel");
+define("_LOGGER_E_USER_ERROR", "Fejl");
+define("_LOGGER_E_NOTICE", "Bemærk");
+define("_LOGGER_E_WARNING", "Advarsel");
+define("_LOGGER_E_STRICT", "Fuldstændigt");
+define("_LOGGER_FILELINE", "%s i filen %s linje %s");
+
+?><?php // Translation done by xtransam & anderssk - 2010-10-11 22:40 ?>
