22be88d4e4756859d46b6ba8e7e8c0ad0d936d1e
djculex <djculex@c17157d5-08f2-41e7-a131-df055f6a0da3>
2011-10-07 10:47:10 +0000

    Danish translation for 2.5.3

---
+++ XoopsLanguages/danish/core/htdocs/language/danish/mail_template/welcome.tpl
@@ -0,0 +1,15 @@
+Hej {X_UNAME},
+
+
+Du er nu registreret på {X_SITENAME} med brugernavnet "{X_UNAME}".
+Du er velkommen til at besøge{X_SITENAME} at: {X_SITEURL}
+
+{TERMSOFUSE}
+
+Hvis du har nogen problemer med at bruge "{X_SITENAME}", så kontakt os endelig via: {X_ADMINMAIL}
+
+Med venlig hilsen,
+
+-----------
+{X_SITENAME}: {X_SITEURL} 
+webmaster: {X_ADMINMAIL}
---
+++ XoopsLanguages/danish/core/htdocs/language/danish/timezone.php
@@ -0,0 +1,38 @@
+<?php
+// $Id: timezone.php 4897 2010-06-19 02:55:48Z phppp $
+// _LANGCODE: en
+// _CHARSET : UTF-8
+// Translator: XOOPS Translation Team
+//%%%%%     Time Zone   %%%%
+define("_TZ_GMTM12", "(GMT-12: 00) Eniwetok, Kwajalein");
+define("_TZ_GMTM11", "(GMT-11: 00) Midway Island, Samoa");
+define("_TZ_GMTM10", "(GMT-10: 00) Hawaii");
+define("_TZ_GMTM9", "(GMT-9: 00) Alaska");
+define("_TZ_GMTM8", "(GMT-8: 00) Pacific Time (USA og Canada)");
+define("_TZ_GMTM7", "(GMT-7: 00) Mountain Time (USA og Canada)");
+define("_TZ_GMTM6", "(GMT-6: 00) Central Time (USA og Canada), Mexico City");
+define("_TZ_GMTM5", "(GMT-5: 00) Eastern Time (USA og Canada), Bogota, Lima, Quito");
+define("_TZ_GMTM4", "(GMT-4: 00) Atlantic Time (Canada), Caracas, La Paz");
+define("_TZ_GMTM35", "(GMT-3: 30) Newfoundland");
+define("_TZ_GMTM3", "(GMT-3: 00) Brasilia, Buenos Aires, Georgetown");
+define("_TZ_GMTM2", "(GMT-2: 00) Mid-Atlantic");
+define("_TZ_GMTM1", "(GMT-1: 00) Azorerne, Kap Verde-øerne");
+define("_TZ_GMT0", "(GMT) Greenwich Mean Time, London, Dublin, Lissabon, Casablanca, Monrovia");
+define("_TZ_GMTP1", "(GMT +1:00) Amsterdam, Berlin, Rom, København, Bruxelles, Madrid, Paris");
+define("_TZ_GMTP2", "(GMT +2:00) Athen, Istanbul, Minsk, Helsinki, Jerusalem, Sydafrika");
+define("_TZ_GMTP3", "(GMT +3:00) Bagdad, Kuwait, Riyadh, Moskva, St. Petersborg");
+define("_TZ_GMTP35", "(GMT +3:30) Teheran");
+define("_TZ_GMTP4", "(GMT +4:00) Abu Dhabi, Muscat, Baku, Tbilisi");
+define("_TZ_GMTP45", "(GMT +4:30) Kabul");
+define("_TZ_GMTP5", "(GMT +5:00) Ekaterinburg, Islamabad, Karachi, Tasjkent");
+define("_TZ_GMTP55", "(GMT +5:30) Mumbai, Kolkata, Chennai, New Delhi");
+define("_TZ_GMTP6", "(GMT +6:00) Almaty, Dhaka, Colombo");
+define("_TZ_GMTP7", "(GMT +7:00) Bangkok, Hanoi, Jakarta");
+define("_TZ_GMTP8", "(GMT +8:00) Beijing, Perth, Singapore, Hong Kong, Urumqi, Taipei");
+define("_TZ_GMTP9", "(GMT +9:00) Tokyo, Seoul, Osaka, Sapporo, Yakutsk");
+define("_TZ_GMTP95", "(GMT +9:30) Adelaide, Darwin");
+define("_TZ_GMTP10", "(GMT +10:00) Brisbane, Canberra, Melbourne, Sydney, Guam, Vlasdiostok");
+define("_TZ_GMTP11", "(GMT +11:00) Magadan, Salomonøerne, Ny Kaledonien");
+define("_TZ_GMTP12", "(GMT +12:00) Auckland, Wellington, Fiji, Kamchatka, Marshall Island");
+
+?><?php // Translation done by xtransam & anderssk - 2010-10-11 22:40 ?>
