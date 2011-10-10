a0dc7a465207daf8b38e68e2e1840e6a5d212076
andrey3761 <andrey3761@c17157d5-08f2-41e7-a131-df055f6a0da3>
2011-10-10 19:38:40 +0000

    XOOPS 2.5.3 Funal: Russian translation is added.

---
+++ XoopsLanguages/russian/core/2.5.3/docs/README.txt
@@ -0,0 +1,17 @@
+XOOPS 2.5.3 Final
+
+The XOOPS Development Team is pleased to announce the release of XOOPS 2.5.3. .
+
+This 2.5 series builds on the XOOPS 2.4.x series with objectives of usability improvements instead of architecture or API changes. This release is reverting fix for bug ID: 2672723 Changed the code in Install to omits the SQL command: "ALTER DATABASE ... CHARACTER SET ..."
+
+If you have a fresh 2.5.2 install, you should reinstall it with 2.5.3.
+
+How to contribute
+-----------------------------------
+Bug report: http://sourceforge.net/tracker/?group_id=41586&atid=430840
+Patch and enhancement: http://sourceforge.net/tracker/?group_id=41586&atid=430842
+Feature design: http://sourceforge.net/tracker/?group_id=41586&atid=430843
+Release announcement: https://lists.sourceforge.net/lists/listinfo/xoops-announcement
+
+XOOPS Development Team
+October 3rd, 2011
---
+++ XoopsLanguages/russian/core/2.5.3/htdocs/modules/system/language/russian/admin/banners.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * @copyright   The XOOPS Project http://sourceforge.net/projects/xoops/
+ * @license     http://www.fsf.org/copyleft/gpl.html GNU public license
+ * _LANGCODE    ru
+ * _CHARSET     UTF-8
+ * Translator   2010-11-11 andrey3761
+ * @version     $Id: banners.php 5580 2010-10-21 19:38:48Z forxoops $
+ */
+
+//%%%%%%        Admin Module Name  Banners         %%%%%
+define("_AM_SYSTEM_BANNERS_DBUPDATED",_AM_SYSTEM_DBUPDATED);
+
+//Nav
+define("_AM_SYSTEM_BANNERS_NAV_MANAGER","Управление банерами");
+define("_AM_SYSTEM_BANNERS_NAV_MAIN","Список банеров и клиентов");
+define("_AM_SYSTEM_BANNERS_NAV_EDITBNR","Редактировать банер");
+define("_AM_SYSTEM_BANNERS_NAV_DELETEBNR","Удалить банер");
+define("_AM_SYSTEM_BANNERS_NAV_DELETEFINISHBNR","Удалить завершённые банеры");
+define("_AM_SYSTEM_BANNERS_NAV_ADDBNR","Добавить новый банер");
+define("_AM_SYSTEM_BANNERS_NAV_EDITADVCLI","Редактировать рекламму клиентов");
+define("_AM_SYSTEM_BANNERS_NAV_ADDNWCLI","Добавить нового клиента");
+define("_AM_SYSTEM_BANNERS_NAV_DELETECLI","Удалить клиента");
+
+define("_AM_SYSTEM_BANNERS_CURACTBNR","Активные баннеры");
+define("_AM_SYSTEM_BANNERS_BANNERID","Номер банера");
+define("_AM_SYSTEM_BANNERS_IMPRESION","Показов");
+define("_AM_SYSTEM_BANNERS_IMPLEFT","Показов осталось");
+define("_AM_SYSTEM_BANNERS_CLICKS","Нажатий");
+define("_AM_SYSTEM_BANNERS_NCLICKS","% нажатий");
+define("_AM_SYSTEM_BANNERS_CLINAME","Название клиента");
+define("_AM_SYSTEM_BANNERS_FUNCTION","Операции");
+define("_AM_SYSTEM_BANNERS_UNLIMIT","Неограниченно");
+define("_AM_SYSTEM_BANNERS_VIEW","Показов банеров");
+define("_AM_SYSTEM_BANNERS_EDIT","Редактировать");
+define("_AM_SYSTEM_BANNERS_DELETE","Удалить");
+define("_AM_SYSTEM_BANNERS_FINISHBNR","Законченные показы баннеров");
+define("_AM_SYSTEM_BANNERS_IMPD","Показов");
+define("_AM_SYSTEM_BANNERS_STARTDATE","Дата начала");
+define("_AM_SYSTEM_BANNERS_ENDDATE","Дата окончания");
+define("_AM_SYSTEM_BANNERS_ADVCLI","Рекламируемые клиенты");
+define("_AM_SYSTEM_BANNERS_ACTIVEBNR","Актиных банеров");
+define("_AM_SYSTEM_BANNERS_CONTNAME","Контактное имя");
+define("_AM_SYSTEM_BANNERS_CONTMAIL","Контактный E-mail");
+define("_AM_SYSTEM_BANNERS_CLINAMET","Имя клиента:");
+define("_AM_SYSTEM_BANNERS_ADDNWBNR","Добавление нового баннера");
+define("_AM_SYSTEM_BANNERS_IMPPURCHT","Показов заказано:");
+define("_AM_SYSTEM_BANNERS_IMGURLT","Адрес рисунка:");
+define("_AM_SYSTEM_BANNERS_CLICKURLT","Адрес нажатия:");
+define("_AM_SYSTEM_BANNERS_ADDBNR","Добавить банер");
+define("_AM_SYSTEM_BANNERS_ADDNWCLI","Добавить нового клиента");
+define("_AM_SYSTEM_BANNERS_CONTNAMET","Контактное имя:");
+define("_AM_SYSTEM_BANNERS_CONTMAILT","Контактный E-mail:");
+define("_AM_SYSTEM_BANNERS_CLILOGINT","Имя клиента для входа:");
+define("_AM_SYSTEM_BANNERS_CLIPASST","Пароль клиента:");
+define("_AM_SYSTEM_BANNERS_ADDCLI","Добавить клиента");
+define("_AM_SYSTEM_BANNERS_DELEBNR","Удалить баннер");
+define("_AM_SYSTEM_BANNERS_SUREDELE","Вы действительно хотите удалить этот баннер?");
+define("_AM_SYSTEM_BANNERS_NO","Нет");
+define("_AM_SYSTEM_BANNERS_YES","Да");
+define("_AM_SYSTEM_BANNERS_EDITBNR","Редактирование баннера");
+define("_AM_SYSTEM_BANNERS_ADDIMPT","Добавить дополнительные показы:");
+define("_AM_SYSTEM_BANNERS_PURCHT","Заказано:");
+define("_AM_SYSTEM_BANNERS_MADET","Сделано:");
+define("_AM_SYSTEM_BANNERS_CHGBNR","Сохранить изменения");
+define("_AM_SYSTEM_BANNERS_DELEADC","Удаление рекламируемого клиента");
+define("_AM_SYSTEM_BANNERS_SUREDELCLI","При этом вы удалите клиента <b>%s</b> и все его баннеры!!!");
+define("_AM_SYSTEM_BANNERS_NOBNRRUN","Этот клиент не имеет баннеров на Вашем сайте.");
+define("_AM_SYSTEM_BANNERS_WARNING","ПРЕДУПРЕЖДЕНИЕ!!!");
+define("_AM_SYSTEM_BANNERS_ACTBNRRUN","Этот клиент имеет АКТИВНЫЕ БАННЕРЫ, работающие на Вашем сайте:");
+define("_AM_SYSTEM_BANNERS_SUREDELBNR","Вы действительно хотите удалить этого клиента и ВСЕ его баннеры?");
+define("_AM_SYSTEM_BANNERS_EDITADVCLI","Редактирование рекламируемого клиента");
+define("_AM_SYSTEM_BANNERS_EXTINFO","Дополнительная информация:");
+define("_AM_SYSTEM_BANNERS_CHGCLI","Сохранить изменения");
+define("_AM_SYSTEM_BANNERS_USEHTML","Использовать HTML?");
+define("_AM_SYSTEM_BANNERS_CODEHTML","HTML-код:");
+
+// Tips
+define( "_AM_SYSTEM_BANNERS_NAV_TIPS", "
+<ul>
+<li>Добавление, редактирование и обновление категорий, баннеров и клиентов.</li>
+</ul>
+");
+
+?>
