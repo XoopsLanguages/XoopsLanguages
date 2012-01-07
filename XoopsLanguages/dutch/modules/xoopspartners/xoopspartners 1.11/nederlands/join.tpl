b75a7dced66a481050eba812d42d55561d61aaef
renetjuh <renetjuh@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-01-07 00:36:31 +0000

    Update NL bestanden

---
+++ XoopsLanguages/dutch/modules/xoopspartners/xoopspartners 1.11/nederlands/join.tpl
@@ -0,0 +1,17 @@
+Hallo.
+
+Er is een verzoek ontvangen om partner te worden van deze website {SITENAME}
+Het verzoek is verstuurd door de gebruiker {USER} met IP {IP}
+De informatie die verstrekt is middels het formulier is:
+
+Títel: {TITLE}
+URL:    {URL}
+Afbeelding: {IMAGE}
+Omschrijving:
+{DESCRIPTION}
+
+Het verzoek kan toegevoegd worden door onderstaande URL:
+{SITEURL}modules/{MODULENAME}/admin/main.php?op=partnersAdminAdd
+
+-----------
+{SITENAME} ({SITEURL})
---
+++ XoopsLanguages/dutch/modules/xoopspartners/xoopspartners 1.11/nederlands/modinfo.php
@@ -0,0 +1,58 @@
+<?php
+// $Id: modinfo.php,v 1.7 2004/09/11 10:37:47 onokazu Exp $
+//  ------------------------------------------------------------------------ //
+//                XOOPS - PHP Content Management System                      //
+//                    Copyright (c) 2000 XOOPS.org                           //
+//                       <http://www.xoops.org/>                             //
+//  ------------------------------------------------------------------------ //
+//  This program is free software; you can redistribute it and/or modify     //
+//  it under the terms of the GNU General Public License as published by     //
+//  the Free Software Foundation; either version 2 of the License, or        //
+//  (at your option) any later version.                                      //
+//                                                                           //
+//  You may not change or alter any portion of this comment or credits       //
+//  of supporting developers from this source code or any supporting         //
+//  source code which is considered copyrighted (c) material of the          //
+//  original comment or credit authors.                                      //
+//                                                                           //
+//  This program is distributed in the hope that it will be useful,          //
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of           //
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            //
+//  GNU General Public License for more details.                             //
+//                                                                           //
+//  You should have received a copy of the GNU General Public License        //
+//  along with this program; if not, write to the Free Software              //
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA //
+//  ------------------------------------------------------------------------ //
+
+define('_MI_XPARTNERS_NAME', 'Partners');
+define('_MI_XPARTNERS_DESC', 'Toon de partnersites in een blok en module');
+define('_MI_XPARTNERS_ADMIN', 'Partners beheren');
+define('_MI_XPARTNERS_ADMIN_ADD', 'Partner toevoegen');
+define('_MI_XPARTNERS_ID', 'ID');
+define('_MI_XPARTNERS_HITS', 'Klikken');
+define('_MI_XPARTNERS_TITLE', 'Titel');
+define('_MI_XPARTNERS_WEIGHT', 'Gewicht');
+define('_MI_XPARTNERS_RECLICK', 'Reclick tijd:');
+define('_MI_XPARTNERS_IMAGES', 'Afbeeldingen');
+define('_MI_XPARTNERS_TEXT', 'Tekst Links');
+define('_MI_XPARTNERS_BOTH', 'Beide');
+define('_MI_XPARTNERS_MLIMIT', 'Beperk de hoofdpagina tot xx entries: (0 = geen limiet)');
+define('_MI_XPARTNERS_MLIMITDSC', '');
+define('_MI_XPARTNERS_MSHOW', 'Weergeven op hoofdpagina:');
+define('_MI_XPARTNERS_MSCHODSC', '');
+define('_MI_XPARTNERS_MORDER', 'Sorteer inhoud hoofpagina op:');
+define('_MI_XPARTNERS_MORDERDSC', '');
+define('_MI_XPARTNERS_HOUR', '1 uur');
+define('_MI_XPARTNERS_3HOURS', '3 uur');
+define('_MI_XPARTNERS_5HOURS', '5 uur');
+define('_MI_XPARTNERS_10HOURS', '10 uur');
+define('_MI_XPARTNERS_DAY', '1 dag');
+
+//1.11
+// index.php
+define('_MI_XPARTNERS_MENU_01', 'Home');
+define('_MI_XPARTNERS_ADMIN_ABOUT', 'Over');
+define('_MI_XPARTNERS_ADMIN_HELP', 'Hulp');
+define('_MI_XPARTNERS_ADMIN_MANAGE', 'Overzicht Partners');
+define('_MI_XPARTNERS_ADMIN_ADDP', 'Partner toevoegen');
