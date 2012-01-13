93ba0ce46bad44dd9c351c03bbc37e027b785978
renetjuh <renetjuh@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-01-13 22:32:13 +0000

    Uploaded NL language files

---
+++ dutch/modules/xlanguage/xlanguage 3.03/nederlands/admin.php
@@ -0,0 +1,54 @@
+<?php 
+// $Id: admin.php,v 1.2 2004/09/27 20:06:50 phppp Exp $
+//  ------------------------------------------------------------------------ //
+//         Xlanguage: eXtensible Language Management For Xoops               //
+//             Copyright (c) 2004 Xoops China Community                      //
+//                    <http://www.xoops.org.cn/>                             //
+//  ------------------------------------------------------------------------ //
+//  This program is free software; you can redistribute it and/or modify     //
+//  it under the terms of the GNU General Public License as published by     //
+//  the Free Software Foundation; either version 2 of the License, or        //
+//  (at your option) any later version.                                      //
+//                                                                           //
+//  You may not change or alter any portion of this comment or credits       //
+//  of supporting developers from this source code or any supporting         //
+//  source code which is considered copyrighted (c) material of the          //
+//  original comment or credit authors.                                      //
+//                                                                           //
+//  This program is distributed in the hope that it will be useful,          //
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of           //
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            //
+//  GNU General Public License for more details.                             //
+//                                                                           //
+//  You should have received a copy of the GNU General Public License        //
+//  along with this program; if not, write to the Free Software              //
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA //
+//  ------------------------------------------------------------------------ //
+// Author: D.J.(phppp) php_pp@hotmail.com                                    //
+// URL: http://www.xoops.org.cn                                              //
+// ------------------------------------------------------------------------- //
+
+define('_AM_XLANG_CONFIG', 'XLanguage Instellingen');
+define('_AM_XLANG_DELETE_CFM', 'Deze taal verwijderen?');
+define('_AM_XLANG_DELETED', 'De gekozen taal is verwijderd');
+define('_AM_XLANG_SAVED', 'De gekozen taal is opgeslagen');
+define('_AM_XLANG_EDITLANG', 'Taal bewerken');
+define('_AM_XLANG_ADDLANG', 'Taal toevoegen');
+define('_AM_XLANG_CREATED', 'Het configbestand is aangemaakt');
+define('_AM_XLANG_CONFIGOK', 'Het configbestand is beschikbaar');
+define('_AM_XLANG_CONFIGNOTOK', 'Het confibestandis niet beschikbaar');
+define('_AM_XLANG_ADDBASE', 'Basistaal toevoegen');
+define('_AM_XLANG_ADDEXT', 'Uitgebreide taal toevoegen');
+define('_AM_XLANG_CREATECONFIG', 'Configbestand aanmaken');
+define('_AM_XLANG_ABOUT', 'Over deze module');
+define('_AM_XLANG_LANGLIST', 'Taallijst');
+define('_AM_XLANG_DESC', 'Omschrijving');
+define('_AM_XLANG_NAME', 'Naam');
+define('_AM_XLANG_CHARSET', 'karakterset');
+define('_AM_XLANG_CODE', 'Code');
+define('_AM_XLANG_IMAGE', 'Afbeelding');
+define('_AM_XLANG_WEIGHT', 'Gewicht');
+define('_AM_XLANG_BASE', 'Basistaal');
+// 3.03
+define('_AM_XLANG_ACTION', 'Actie');
+
---
+++ dutch/modules/xlanguage/xlanguage 3.03/nederlands/modinfo.php
@@ -0,0 +1,28 @@
+<?php
+// $Id: modinfo.php,v 1.2 2004/09/27 20:36:31 phppp Exp $
+// Module Info
+
+define("_MI_XLANGUAGE_NAME","xLanguage");
+define("_MI_XLANGUAGE_DESC","eXtensible multi-Language Management");
+
+define("_MI_XLANGUAGE_BNAME","Taalselectie");
+
+define("_MI_XLANGUAGE_ADMENU0","Talen beheren");
+define("_MI_XLANGUAGE_ADMENU1","Basistaal toevoegen");
+define("_MI_XLANGUAGE_ADMENU2","Ext. taal toevoegen");
+define("_MI_XLANGUAGE_ADMENU3","Over");
+
+define('_MI_XLANG_AUTHOR_INFO', "Informatie ontwikkelaar");
+define('_MI_XLANG_AUTHOR_NAME', "Ontwikkelaar");
+define('_MI_XLANG_AUTHOR_WEBSITE', "Website ontwikkelaar");
+define('_MI_XLANG_AUTHOR_EMAIL', "Ontwikkelaar e-mail sturen");
+define('_MI_XLANG_AUTHOR_CREDITS', "Credits");
+define('_MI_XLANG_MODULE_INFO', "Informatie Module-ontwikkeling");
+define('_MI_XLANG_MODULE_STATUS', "Ontwikkelstatus");
+define('_MI_XLANG_MODULE_DEMO', "Demo Site");
+define('_MI_XLANG_MODULE_SUPPORT', "Official support site");
+define('_MI_XLANG_RELEASE', "Release Date ");
+define('_MI_XLANG_AUTHOR_BUGFIXES', "Bug fix history");
+define('_MI_XLANG_MODULE_XOOPSVERSION', "Xoops Versie");
+//3.03
+define("_MI_XLANGUAGE_ADMENU_HOME","Home");
