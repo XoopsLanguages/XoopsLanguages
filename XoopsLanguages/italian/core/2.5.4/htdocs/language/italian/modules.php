784cba9daf46734699724a05f97df0432b009e71
txmodxoops <txmodxoops@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-02-06 06:13:15 +0000

    Added more defines for repetives variables

--- XoopsLanguages/italian/core/2.5.4/htdocs/language/italian/modules.php
+++ XoopsLanguages/italian/core/2.5.4/htdocs/language/italian/modules.php
@@ -1,16 +1,17 @@
 <?php
-// $Id: modules.php 5683 2012-02-06 07:03:22Z timgno $
+// $Id: modules.php 8887 2012-02-06 07:03:22Z timgno $
 // _LANGCODE: it
 // _CHARSET : UTF-8
 define("_ABOUT","Informazioni");
 define("_WEIGHT","Peso");
 define("_CATEGORY","Categoria");
 define("_CATEGORIES","Categorie");
-define("_SUBJECT","Oggetto");
-define("_MESSAGEICON","Icona del messaggio");
-define("_COMMENTS","Commenti");
-define("_POSTANON","Invia come anonimo");
-define("_DISABLESMILEY","Disabilita faccine");
-define("_DISABLEHTML","Disabilita tag HTML");
-define("_PREVIEW","Anteprima");
+define("_PERMISSION","Permesso");
+define("_PERMISSION","Permessi");
+define("_DASHBOARD","Cruscotto");
+define("_SUBMITTER","Inviato da");
+define("_CREATED","Creato");
+define("_PUBLISHED","Pubblicato");
+define("_PUBLISHER","Pubblicatore");
+define("_EXPIRED","Terminato");
 ?>
