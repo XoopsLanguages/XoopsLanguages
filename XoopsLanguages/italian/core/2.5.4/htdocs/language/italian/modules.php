17963f48e3cb5c5f90294aeaa8de2cbe01588232
txmodxoops <txmodxoops@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-02-06 06:07:05 +0000

    New defines for all modules forms

---
+++ XoopsLanguages/italian/core/2.5.4/htdocs/language/italian/modules.php
@@ -0,0 +1,16 @@
+<?php
+// $Id: modules.php 5683 2012-02-06 07:03:22Z timgno $
+// _LANGCODE: it
+// _CHARSET : UTF-8
+define("_ABOUT","Informazioni");
+define("_WEIGHT","Peso");
+define("_CATEGORY","Categoria");
+define("_CATEGORIES","Categorie");
+define("_SUBJECT","Oggetto");
+define("_MESSAGEICON","Icona del messaggio");
+define("_COMMENTS","Commenti");
+define("_POSTANON","Invia come anonimo");
+define("_DISABLESMILEY","Disabilita faccine");
+define("_DISABLEHTML","Disabilita tag HTML");
+define("_PREVIEW","Anteprima");
+?>
