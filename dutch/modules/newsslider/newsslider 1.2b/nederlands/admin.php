e714fb185e81d45fc125ec373c62381bb15ad283
renetjuh <renetjuh@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-02-07 23:33:30 +0000

    Upload dutch language files for newsslider 1.2b

---
+++ dutch/modules/newsslider/newsslider 1.2b/nederlands/admin.php
@@ -0,0 +1,15 @@
+﻿<?php
+/**
+ * $Id: admin.php v 1.0 11 Jan 2012 Yerres Exp $
+ * Module: newsslider
+ * Version: 1.1
+ * Release Date: 11 Jan 2012 Yerres
+ * Author: Yerres
+ * Licence: GNU
+ */
+
+define("_AM_NWS_INTRO", "Er kunnen zoveel nieuwssliderblokken toegevoegd worden via de blokkenadministratie als dat nodig is.");
+define("_AM_NWS_TAGS", "Nieuwsslider");
+define("_AM_NWS_INDEX", "Index");
+
+?>
