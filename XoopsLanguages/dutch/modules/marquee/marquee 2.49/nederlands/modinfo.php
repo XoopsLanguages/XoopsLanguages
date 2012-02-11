05d1e904d053bcd802b005eeb7b8368dc5cb84de
renetjuh <renetjuh@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-02-11 14:06:16 +0000

    Dutch Language files marquee 2.49

---
+++ XoopsLanguages/dutch/modules/marquee/marquee 2.49/nederlands/modinfo.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * ****************************************************************************
+ * marquee - MODULE FOR XOOPS
+ * Copyright (c) Hervé Thouzard (http://www.herve-thouzard.com)
+ *
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright       Hervé Thouzard (http://www.herve-thouzard.com)
+ * @license         http://www.fsf.org/copyleft/gpl.html GNU public license
+ * @package         marquee
+ * @author 			Hervé Thouzard (http://www.herve-thouzard.com)
+ *
+ * Version : $Id:
+ * ****************************************************************************
+ */
+define("_MI_MARQUEE_NAME",		"Scrollende tekst");
+define("_MI_MARQUEE_DESC",		"Hiermee zijn scrollende teksten (marquees) te maken");
+define("_MI_MARQUEE_BNAME_01",	"Scrollende tekst 01");
+define("_MI_MARQUEE_BDESC_01",	"Scrollende tekst 01 weergeven");
+define("_MI_MARQUEE_BNAME_02",	"Scrollende tekst 02");
+define("_MI_MARQUEE_BDESC_02",	"Scrollende tekst 02 weergeven");
+define("_MI_MARQUEE_BNAME_03",	"Scrollende tekst 03");
+define("_MI_MARQUEE_BDESC_03",	"Scrollende tekst 03 weergeven");
+define("_MI_MARQUEE_BNAME_04",	"Scrollende tekst 04");
+define("_MI_MARQUEE_BDESC_04",	"Scrollende tekst 04 weergeven");
+define("_MI_MARQUEE_MENU_01",	"Scrollende teksten");
+define("_MI_MARQUEE_USE_KIOVI", "Kiovi gebruiken?");
+define("_MI_MARQUEE_USE_KIOVI_DESC","Indien gedetecteerd wordt koivi gebruikt");
+define("_MI_MARQUEE_METHOD","Methode om te gebruiken");
+define("_MI_MARQUEE_METHOD_DESC","Wanneer de 'Scrollende tekst' methode is geselecteerd dan gebruikt de module de html marquee's code, Deze methode is niet compatibel met alle browsers maar biedt vele opties.<br>
+Wanneer de 'dhtml' methode is geselecteerd, dan werkt de scrollende tekst in meerdere browsers maar het behoefte meer code en de pagina en er zijn minder opties beschikbaar.");
+define("_MI_MARQUEE_MARQUEE","Scrollende tekst");
+define("_MI_MARQUEE_DHTML","Javascript");
+define("_MI_MARQUEE_DATEFORMAT","Datumformaat");
+define("_MI_MARQUEE_DATEFORMAT_DESC","Bekijk het datumformaat op http://fr.php.net/manual/en/function.date.php). Noot, De standaardwaarde wordt gebruikt als er niets is ingevuld.");
+define("_MI_MARQUE_ITEMSCOUNT","");
+define("_MI_MARQUE_ITEMSCOUNT_DESC","Wanneer de content van een andere module wordt gebruikt (en geen vaste tekst) om scrollende teksten te maken, dit is het aantal items op de lijst.");
+define("_MI_MARQUE_TITLELENGTH","Titel lengte");
+define("_MI_MARQUE_TITLELENGTH_DESC","Stel de maximale lengte in voor de titellengte van de scrollende teksten (0=geen limiet)");
+
+define("_MI_MARQUEE_TEXT_EDITOR", "Teksteditor gebruiken");
+define("_MI_MARQUEE_FORM_COMPACT","Compact");
+define("_MI_MARQUEE_FORM_DHTML","DHTML");
+define("_MI_MARQUEE_FORM_SPAW","Spaw Editor");
+define("_MI_MARQUEE_FORM_HTMLAREA","HtmlArea Editor");
+define("_MI_MARQUEE_FORM_FCK","FCK Editor");
+define("_MI_MARQUEE_FORM_KOIVI","Koivi Editor");
+define("_MI_MARQUEE_FORM_TINYEDITOR","TinyEditor");
+
+//2.48
+// index.php
+
+define('_MI_MARQUEE_MENU_00',"Home");
+define("_MI_MARQUEE_ADMIN_HOME_DESC","Terug naar Home");
+
+define("_MI_MARQUEE_ADMIN_ABOUT" , "Over");
+define("_MI_MARQUEE_ADMIN_ABOUT_DESC" , "Over deze module");
+
+?>
