00bd70cf5b0c73321f8cdeba76977d3785f2e7a5
renetjuh <renetjuh@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-02-11 14:06:16 +0000

    Dutch Language files marquee 2.49

---
+++ dutch/modules/marquee/marquee 2.49/nederlands/admin.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * ****************************************************************************
+ * marquee - MODULE FOR XOOPS
+ * Copyright (c) Hervé Thouzard (http://www.herve-thouzard.com)
+ *
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright       Hervé Thouzard (http://www.herve-thouzard.com)
+ * @license         http://www.fsf.org/copyleft/gpl.html GNU public license
+ * @package         marquee
+ * @author 			Hervé Thouzard (http://www.herve-thouzard.com)
+ *
+ * Version : $Id:
+ * ****************************************************************************
+ */
+define("_AM_MARQUEE_CONFIG","Scrollende teksten configuratie");
+define("_AM_MARQUEE_ID","ID");
+define("_AM_MARQUEE_GENERALSET", "Instellingen" );
+define("_AM_MARQUEE_MODULEADMIN","Module's administratie");
+define("_AM_MARQUEE_DIRECTION","Richting");
+define("_AM_MARQUEE_BEHAVIOUR","Gedrag");
+define("_AM_MARQUEE_STOP","Stop wanneer de muis erover beweegt");
+define("_AM_MARQUEE_CONTENT","Inhoud");
+define("_AM_MARQUEE_EDIT","Bewerken");
+define("_AM_MARQUEE_DELETE","Verwijderen");
+define("_AM_MARQUEE_DIRECTION1","links->rechts");
+define("_AM_MARQUEE_DIRECTION2","rechts->links");
+define("_AM_MARQUEE_DIRECTION3","beneden->boven");
+define("_AM_MARQUEE_DIRECTION4","boven->beneden");
+define("_AM_MARQUEE_BEHAVIOUR1","scrollen");
+define("_AM_MARQUEE_BEHAVIOUR2","schuiven");
+define("_AM_MARQUEE_BEHAVIOUR3","alternatief");
+define("_AM_MARQUEE_ADDMARQUEE","Scrollende tekst toevoegen");
+define("_AM_MARQUEE_ACTION","Actie");
+define("_AM_MARQUEE_LOOP","Loop");
+define("_AM_MARQUEE_INFINITELOOP","Oneindig");
+define("_AM_MARQUEE_ALIGN","Uitlijning");
+define("_AM_MARQUEE_ALIGN1","Boven");
+define("_AM_MARQUEE_ALIGN2","Beneden");
+define("_AM_MARQUEE_ALIGN3","Midden");
+define("_AM_MARQUEE_ADDBUTTON","Toevoegen");
+define("_AM_MARQUEE_RESETBUTTON","Annuleren");
+define("_AM_MARQUEE_ERROR_ADD_MARQUEE","ERROR, de verplichte velden zijn niet ingevuld.");
+define("_AM_MARQUEE_ADDED_OK","De scrollende tekst is succesvol toegevoegd.");
+define("_AM_MARQUEE_DBUPDATED","De database is succesvol bijgewerkt.");
+define("_AM_MARQUEE_RUSUREDEL","Zeker weten om dit item verwijderen?");
+define("_AM_MARQUEE_UPDATE","Bijwerken");
+define("_AM_MARQUEE_BGCOLOR","Achtergrondkleur <br />(Vergeet niet om een #<br /> te gebruiken om een kleurcode te specificeren)");
+define("_AM_MARQUEE_WIDTH","Breedte (in pixels of percentage)");
+define("_AM_MARQUEE_HEIGHT","Hoogte (in pixels)");
+define("_AM_MARQUEE_SCRAMOUNT","Scroll waarde");
+define("_AM_MARQUEE_HSPACE","Horizontale ruimte (in pixels)");
+define("_AM_MARQUEE_VSPACE","Verticale ruimte (in pixels)");
+define("_AM_MARQUEE_SCRDELAY","Vertraging tussen<br />twee bewegingen in miliseconden");
+define("_AM_MARQUEE_ERROR_MODIFY_DB","ERROR : fout tijdens het bijwerken van de database");
+define("_AM_MARQUEE_SOURCE","bron van de inhoud");
+define("_AM_MARQUEE_SOURCE_FIXED","een vaste tekst");
+
+//2.4.8
+
+// About.php
+define("_AM_MARQUEE_ABOUT_RELEASEDATE",              "Vrijgegeven: ");
+define("_AM_MARQUEE_ABOUT_UPDATEDATE",               "Bijgewerkt: ");
+define("_AM_MARQUEE_ABOUT_AUTHOR",                   "Auteur: ");
+define("_AM_MARQUEE_ABOUT_CREDITS",                  "Credits: ");
+define("_AM_MARQUEE_ABOUT_LICENSE",                  "Licentie: ");
+define("_AM_MARQUEE_ABOUT_MODULE_STATUS",            "Status: ");
+define("_AM_MARQUEE_ABOUT_WEBSITE",                  "Website: ");
+define("_AM_MARQUEE_ABOUT_AUTHOR_NAME",              "Naam auteur: ");
+define("_AM_MARQUEE_ABOUT_CHANGELOG",                "Wijzigingsbeheer");
+define("_AM_MARQUEE_ABOUT_MODULE_INFO",              "Module Info");
+define("_AM_MARQUEE_ABOUT_AUTHOR_INFO",              "Auteur Info");
+define("_AM_MARQUEE_ABOUT_DESCRIPTION",              "Omschrijving: ");
+
+// Configuration
+define("_AM_MARQUEE_CONFIG_CHECK","Configuratiecontrole");
+define("_AM_MARQUEE_CONFIG_PHP","Minimum PHP benodigd: %s (huidige versie is %s)");
+define("_AM_MARQUEE_CONFIG_XOOPS","Minimum XOOPS benodigd:  %s (huidige versie is %s)");
+
+//ModuleAdmin
+define('_AM_MODULEADMIN_MISSING','ERROR: De ModuleAdmin class ontbreekt. Installeer ModuleAdmin Class in de map /Frameworks (zie ook /docs/readme.txt)');
---
+++ dutch/modules/marquee/marquee 2.49/nederlands/extension.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * ****************************************************************************
+ * marquee - MODULE FOR XOOPS
+ * Copyright (c) Hervé Thouzard (http://www.herve-thouzard.com)
+ *
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright       Hervé Thouzard (http://www.herve-thouzard.com)
+ * @license         http://www.fsf.org/copyleft/gpl.html GNU public license
+ * @package         marquee
+ * @author 			Hervé Thouzard (http://www.herve-thouzard.com)
+ *
+ * Version : $Id:
+ * ****************************************************************************
+ */
+
+define("_EXT_MARQUEE_ALTWMP","Marquee toevoegen vanuit de Marquee module");
+define("_EXT_MARQUEE_TEXTID","Marquee's ID om weer te geven");
+define("_EXT_MARQUEE_TEXTTITLE","Marquee titel invoeren");
+?>
