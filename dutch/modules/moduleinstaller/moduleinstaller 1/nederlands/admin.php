cc1506c9a6ecd5c6a67fd476c7b5f4df03ac492d
renetjuh <renetjuh@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-02-26 16:59:04 +0000

    Upload NL language files moduleinstaller 1

---
+++ dutch/modules/moduleinstaller/moduleinstaller 1/nederlands/admin.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * ****************************************************************************
+ * marquee - MODULE FOR XOOPS
+ * Copyright (c) Hervé Thouzard (http://www.herve-thouzard.com)
+ *
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright       Hervé Thouzard (http://www.herve-thouzard.com)
+ * @license         http://www.fsf.org/copyleft/gpl.html GNU public license
+ * @package         marquee
+ * @author 			Hervé Thouzard (http://www.herve-thouzard.com)
+ *
+ * Version : $Id:
+ * ****************************************************************************
+ */
+define("_AM_MARQUEE_CONFIG","Marquees Configuration");
+define("_AM_MARQUEE_ID","ID");
+define("_AM_MARQUEE_GENERALSET", "Preferences" );
+define("_AM_MARQUEE_MODULEADMIN","Module's administration");
+define("_AM_MARQUEE_DIRECTION","Direction");
+define("_AM_MARQUEE_BEHAVIOUR","Behaviour");
+define("_AM_MARQUEE_STOP","Stop when mouse is over");
+define("_AM_MARQUEE_CONTENT","Content");
+define("_AM_MARQUEE_EDIT","Edit");
+define("_AM_MARQUEE_DELETE","Delete");
+define("_AM_MARQUEE_DIRECTION1","left->right");
+define("_AM_MARQUEE_DIRECTION2","right->left");
+define("_AM_MARQUEE_DIRECTION3","bottom->top");
+define("_AM_MARQUEE_DIRECTION4","top->bottom");
+define("_AM_MARQUEE_BEHAVIOUR1","scroll");
+define("_AM_MARQUEE_BEHAVIOUR2","slide");
+define("_AM_MARQUEE_BEHAVIOUR3","alternate");
+define("_AM_MARQUEE_ADDMARQUEE","Add a marquee");
+define("_AM_MARQUEE_ACTION","Action");
+define("_AM_MARQUEE_LOOP","Loop");
+define("_AM_MARQUEE_INFINITELOOP","Infinite");
+define("_AM_MARQUEE_ALIGN","Align");
+define("_AM_MARQUEE_ALIGN1","Top");
+define("_AM_MARQUEE_ALIGN2","Bottom");
+define("_AM_MARQUEE_ALIGN3","Middle");
+define("_AM_MARQUEE_ADDBUTTON","Add");
+define("_AM_MARQUEE_RESETBUTTON","Reset");
+define("_AM_MARQUEE_ERROR_ADD_MARQUEE","Error, the recquired fields have not been typed");
+define("_AM_MARQUEE_ADDED_OK","The marquee has been succesfully added");
+define("_AM_MARQUEE_DBUPDATED","The database has been sucesfully updated");
+define("_AM_MARQUEE_RUSUREDEL","Are you sure you want to delete this item ?");
+define("_AM_MARQUEE_UPDATE","Update");
+define("_AM_MARQUEE_BGCOLOR","Background color <br />(don't forget to put a #<br /> if you specify a color code)");
+define("_AM_MARQUEE_WIDTH","Width (in pixels or percent)");
+define("_AM_MARQUEE_HEIGHT","Height (in pixels)");
+define("_AM_MARQUEE_SCRAMOUNT","Scroll amount");
+define("_AM_MARQUEE_HSPACE","Horizontal space (in pixels)");
+define("_AM_MARQUEE_VSPACE","Vertical space (in pixels)");
+define("_AM_MARQUEE_SCRDELAY","Delay between<br />two moves in milliseconds");
+define("_AM_MARQUEE_ERROR_MODIFY_DB","Error while updating the database");
+define("_AM_MARQUEE_SOURCE","Content's source");
+define("_AM_MARQUEE_SOURCE_FIXED","A fixed text");
+
+//2.4.8
+
+// About.php
+define("_AM_MARQUEE_ABOUT_RELEASEDATE",              "Released: ");
+define("_AM_MARQUEE_ABOUT_UPDATEDATE",               "Updated: ");
+define("_AM_MARQUEE_ABOUT_AUTHOR",                   "Author: ");
+define("_AM_MARQUEE_ABOUT_CREDITS",                  "Credits: ");
+define("_AM_MARQUEE_ABOUT_LICENSE",                  "License: ");
+define("_AM_MARQUEE_ABOUT_MODULE_STATUS",            "Status: ");
+define("_AM_MARQUEE_ABOUT_WEBSITE",                  "Website: ");
+define("_AM_MARQUEE_ABOUT_AUTHOR_NAME",              "Author name: ");
+define("_AM_MARQUEE_ABOUT_CHANGELOG",                "Change Log");
+define("_AM_MARQUEE_ABOUT_MODULE_INFO",              "Module Info");
+define("_AM_MARQUEE_ABOUT_AUTHOR_INFO",              "Author Info");
+define("_AM_MARQUEE_ABOUT_DESCRIPTION",              "Description: ");
+
+// Configuration
+define("_AM_MARQUEE_CONFIG_CHECK","Configuration Check");
+define("_AM_MARQUEE_CONFIG_PHP","Minimum PHP required: %s (your version is %s)");
+define("_AM_MARQUEE_CONFIG_XOOPS","Minimum XOOPS required:  %s (your version is %s)");
+
+//ModuleAdmin
+define('_AM_MODULEADMIN_MISSING','Error: The ModuleAdmin class is missing. Please install the ModuleAdmin Class into /Frameworks (see /docs/readme.txt)');
