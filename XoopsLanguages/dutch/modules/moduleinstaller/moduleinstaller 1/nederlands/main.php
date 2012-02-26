6c7533c17d89d49185d981d8e030fba8a381d546
renetjuh <renetjuh@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-02-26 16:59:04 +0000

    Upload NL language files moduleinstaller 1

---
+++ XoopsLanguages/dutch/modules/moduleinstaller/moduleinstaller 1/nederlands/main.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Marquee module
+ *
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright	The XOOPS Project http://sourceforge.net/projects/xoops/
+ * @license             http://www.fsf.org/copyleft/gpl.html GNU public license
+ * @package	Marquee
+ * @since		2.5.0
+ * @author 	Michael Beck (Mamba) 
+ * @version	$Id $
+**/
+
+// upgrade old version
+define("_AM_MARQUEE_UPDATE_DONE","Done!");
+define("_AM_MARQUEE_UPDATE_DONE1","Click me for normal upgrade now!");
+define("_AM_MARQUEE_UPDATE_DONE2","After the process of updating, remember to delete the upgrade.php file in this module!!!");
+
+?>
---
+++ XoopsLanguages/dutch/modules/moduleinstaller/moduleinstaller 1/nederlands/modinfo.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * ****************************************************************************
+ * Installer - MODULE FOR XOOPS
+ * Copyright (c) Michael Beck (mamba)
+ *
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright       Michael Beck (mamba)
+ * @license         http://www.fsf.org/copyleft/gpl.html GNU public license
+ * @package         installer
+ * @author 			Michael Beck (mamba)
+ *
+ * Version : $Id:
+ * ****************************************************************************
+ */
+
+define("_MI_INSTALLER_NAME",		"Installer");
+define('_MI_INSTALLER_MENU_00',"Home");
+define("_MI_INSTALLER_MENU_01",	"Modules installeren");
+define('_MI_INSTALLER_MENU_02',"Modules de-installeren");
+define("_MI_INSTALLER_ADMIN_ABOUT" , "Over");
+define("_MI_INSTALLER_DESC" , "Module om andere modules te installeren of de-installeren");
