c1303d9afe7a2780b86b1164a37f6a2789d4eb12
txmodxoops <txmodxoops@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-03-19 10:34:01 +0000

    xLanguage Module Italian Translation

---
+++ italian/modules/xlanguage/language/italian/admin.php
@@ -0,0 +1,64 @@
+<?php 
+// $Id$
+//  ------------------------------------------------------------------------ //
+//         Xlanguage: eXtensible Language Management For Xoops               //
+//             Copyright (c) 2004 Xoops China Community                      //
+//                    <http://www.xoops.org.cn/>                             //
+//  ------------------------------------------------------------------------ //
+//  This program is free software; you can redistribute it and/or modify     //
+//  it under the terms of the GNU General Public License as published by     //
+//  the Free Software Foundation; either version 2 of the License, or        //
+//  (at your option) any later version.                                      //
+//                                                                           //
+//  You may not change or alter any portion of this comment or credits       //
+//  of supporting developers from this source code or any supporting         //
+//  source code which is considered copyrighted (c) material of the          //
+//  original comment or credit authors.                                      //
+//                                                                           //
+//  This program is distributed in the hope that it will be useful,          //
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of           //
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            //
+//  GNU General Public License for more details.                             //
+//                                                                           //
+//  You should have received a copy of the GNU General Public License        //
+//  along with this program; if not, write to the Free Software              //
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA //
+//  ------------------------------------------------------------------------ //
+// Author: D.J.(phppp) php_pp@hotmail.com                                    //
+// URL: http://www.xoops.org.cn                                              //
+// ------------------------------------------------------------------------- //
+
+define("_AM_XLANG_CONFIG", "Configurazione xLanguage");
+define("_AM_XLANG_DELETE_CFM", "Sei sicuro di voler eliminare la lingua?");
+define("_AM_XLANG_DELETED", "Il linguaggio &#232; stato eliminato");
+define("_AM_XLANG_SAVED", "Il linguaggio &#232; stato salvato");
+define("_AM_XLANG_EDITLANG", "Lingua");
+define("_AM_XLANG_ADDLANG", "Aggiungi lingua");
+define("_AM_XLANG_CREATED", "Il file di configurazione &#232; stato creato");
+define("_AM_XLANG_CONFIGOK", "File di configurazione disponibile");
+define("_AM_XLANG_CONFIGNOTOK", "File di configurazione non disponibile");
+define("_AM_XLANG_ADDBASE", "Aggiungi lingua di base");
+define("_AM_XLANG_ADDEXT", "Aggiungi lingua estesa");
+define("_AM_XLANG_CREATECONFIG", "Crea configurazione");
+define("_AM_XLANG_ABOUT", "Informazioni su questo modulo");
+define("_AM_XLANG_LANGLIST", "Lingua");
+define("_AM_XLANG_DESC", "Descrizione");
+define("_AM_XLANG_NAME", "Nome");
+define("_AM_XLANG_CHARSET", "Codifica dei caratteri");
+define("_AM_XLANG_CODE", "Codice");
+define("_AM_XLANG_IMAGE", "Immagine");
+define("_AM_XLANG_WEIGHT", "Peso");
+define("_AM_XLANG_BASE", "Protolingua");
+// 3.03
+define("_AM_XLANG_ACTION", "Azione");
+
+//ModuleAdmin
+define("_AM_MODULEADMIN_MISSING", "Errore: La classe ModuleAdmin manca. Si prega di installare la classe ModuleAdmin in /Frameworks (vedi / docs/readme.txt)");
+
+// Text for Admin footer
+define("_AM_XLANG_FOOTER", "&lt;div class='center smallsmall italic pad5'&gt; xLanguage &#232; mantenuta dalla &lt;a class='tooltip' rel='external' href='http://xoops.org/' title=&quot;Visita Community XOOPS&quot;&gt;community di Xoops&lt;/a&gt; &lt;/div&gt;");
+
+// Translation done by XTransam & admin (info@txmodxoops.org)
+// XTransam 1.2 is written by Chronolabs Co-op & The XOOPS Project - File Dumped on 2012-03-19 09:48
+
+?>
---
+++ italian/modules/xlanguage/language/italian/modinfo.php
@@ -0,0 +1,33 @@
+<?php
+// $Id$
+// Module Info
+
+define("_MI_XLANGUAGE_NAME", "xLanguage");
+define("_MI_XLANGUAGE_DESC", "Gestione Estensibile per multi-lingua");
+
+define("_MI_XLANGUAGE_BNAME", "Selezione della lingua");
+
+define("_MI_XLANGUAGE_ADMENU0", "Gestione Lingue");
+define("_MI_XLANGUAGE_ADMENU1", "Lingua Base");
+define("_MI_XLANGUAGE_ADMENU2", "Lingua Estesa");
+define("_MI_XLANGUAGE_ADMENU3", "Informazioni");
+
+define("_MI_XLANG_AUTHOR_INFO", "Informazioni per gli sviluppatori");
+define("_MI_XLANG_AUTHOR_NAME", "Sviluppatore");
+define("_MI_XLANG_AUTHOR_WEBSITE", "Sito Sviluppatore");
+define("_MI_XLANG_AUTHOR_EMAIL", "Email Sviluppatore");
+define("_MI_XLANG_AUTHOR_CREDITS", "Crediti");
+define("_MI_XLANG_MODULE_INFO", "Informazioni Sviluppatore Modulo");
+define("_MI_XLANG_MODULE_STATUS", "Stato dello sviluppo");
+define("_MI_XLANG_MODULE_DEMO", "Demo");
+define("_MI_XLANG_MODULE_SUPPORT", "Sito di supporto ufficiale");
+define("_MI_XLANG_RELEASE", "Data release");
+define("_MI_XLANG_AUTHOR_BUGFIXES", "Storia Bug fix");
+define("_MI_XLANG_MODULE_XOOPSVERSION", "Versione Xoops");
+//3.03
+define("_MI_XLANGUAGE_ADMENU_HOME", "Home page");
+
+// Translation done by XTransam & admin (info@txmodxoops.org)
+// XTransam 1.2 is written by Chronolabs Co-op & The XOOPS Project - File Dumped on 2012-03-19 09:48
+
+?>
