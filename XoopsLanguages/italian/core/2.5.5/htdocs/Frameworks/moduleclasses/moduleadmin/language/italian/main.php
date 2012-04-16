2ab9e2547e83cd6771a22844ce1811d3be1cf004
txmodxoops <txmodxoops@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-04-16 11:08:23 +0000

    Added Frameworks Folder italian language

---
+++ XoopsLanguages/italian/core/2.5.5/htdocs/Frameworks/moduleclasses/moduleadmin/language/italian/main.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Frameworks Module Admin
+ *
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright   Grégory Mage (Aka Mage)
+ * @license     GNU GPL 2 (http://www.gnu.org/licenses/old-licenses/gpl-2.0.html)
+ * @author      Grégory Mage (Aka Mage)
+ * @traslaction TXMod Xoops (Timgno)
+ */
+
+// Info
+define('_AM_MODULEADMIN_CONFIG', 'Controllo configurazione');
+define('_AM_MODULEADMIN_CONFIG_CHMOD', "La cartella '%s' deve essere con chmod '%s' ( ora &#232; impostato su %s ).'");
+define('_AM_MODULEADMIN_CONFIG_FOLDERKO', "La cartella '%s' non esiste");
+define('_AM_MODULEADMIN_CONFIG_FOLDEROK', "La cartella '%s' esiste");
+define('_AM_MODULEADMIN_CONFIG_PHP', 'PHP minimo richiesto: %s (la tua versione &#232; %s)');
+define('_AM_MODULEADMIN_CONFIG_XOOPS', 'XOOPS minimo richiesto: %s (la tua versione &#232; %s)');
+
+define('_AM_MODULEADMIN_CONFIG_DB','Versione minima richiesta: %s (la tua versione &#232; %s)');
+define('_AM_MODULEADMIN_CONFIG_ADMIN','ModuleAdmin minimo richiesto: %s (la tua versione &#232; %s)');
+
+// Header
+define('_AM_MODULEADMIN_MISSING','Errore: Non stai utilizzando il Frameworks \"admin module\". Devi installare questo Frameworks');
+
+define("_AM_MODULEADMIN_HOME", "Home");
+define("_AM_MODULEADMIN_DASHBOARD", "Dashboard");
+define("_AM_MODULEADMIN_ABOUT", "Informazioni");
+define("_AM_MODULEADMIN_PERMISSIONS", "Permessi");
+define("_AM_MODULEADMIN_ABOUT_AUTHOR","Autore: ");
+define("_AM_MODULEADMIN_ABOUT_CREDITS","Crediti: ");
+define("_AM_MODULEADMIN_ABOUT_LICENSE","Licenza: ");
+define("_AM_MODULEADMIN_ABOUT_AUTHOR_NAME","Nome Autore: ");
+define("_AM_MODULEADMIN_ABOUT_AUTHOR_INFO","Info Autore");
+
+// About
+define('_AM_MODULEADMIN_ABOUT_CHANGELOG', 'Cambiamenti Versioni');
+define('_AM_MODULEADMIN_ABOUT_DESCRIPTION', 'Descrizione');
+define('_AM_MODULEADMIN_ABOUT_AUTHORINFO','Informazioni Autore');
+define('_AM_MODULEADMIN_ABOUT_AUTHOREMAIL','Email:');
+define('_AM_MODULEADMIN_ABOUT_CREDITS','Crediti:');
+define('_AM_MODULEADMIN_ABOUT_MODULEINFO', 'Informazioni Modulo');
+define('_AM_MODULEADMIN_ABOUT_MODULESTATUS', 'Stato:');
+define('_AM_MODULEADMIN_ABOUT_UPDATEDATE', 'Aggiornato:');
+define('_AM_MODULEADMIN_ABOUT_RELEASEDATE', 'Rilasciato:');
+define('_AM_MODULEADMIN_ABOUT_WEBSITE', 'Sito Web:');
+
+?>
