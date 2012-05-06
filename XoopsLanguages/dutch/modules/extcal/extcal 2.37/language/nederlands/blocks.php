ce96a889c9701fd44c263a14aa1c01db4340706c
renetjuh <renetjuh@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-05-06 11:09:46 +0000

    Upload NL language files extcal 2.37

---
+++ XoopsLanguages/dutch/modules/extcal/extcal 2.37/language/nederlands/blocks.php
@@ -0,0 +1,30 @@
+<?php
+define("_MB_EXTCAL_BGCOLOR", "Achtergrondkleur");
+define("_MB_EXTCAL_CAT_TO_USE", "Category");
+define("_MB_EXTCAL_HEIGHT", "Height");
+define("_MB_EXTCAL_HORLOGE", "Clock");
+define("_MB_EXTCAL_HORLOGE_OPT", "Clocks options");
+define("_MB_EXTCAL_OPT_SHOW", "Show options");
+define("_MB_EXTCAL_OPT_SLIDE_SHOW", "Slide show");
+define("_MB_EXTCAL_WIDTH", "Width");
+define('_MB_EXTCAL_ALL_CAT', 'All categoriën');
+define('_MB_EXTCAL_CAT_TO_USE_DESC', 'Selecteer cagegorie waar evenementen bijhoren');
+define('_MB_EXTCAL_CURRENT', 'Huidig');
+define("_MB_EXTCAL_DISPLAY","Weergeven");
+define("_MB_EXTCAL_DISPLAY_IMG","Afbeelding weergeven");
+define("_MB_EXTCAL_DISPLAY_MONTH","Maand om weer te geven");
+define("_MB_EXTCAL_DISPLAY_SUBMIT_LINK","Toevoegen link weergeven");
+define("_MB_EXTCAL_EVENT","Evenementen");
+define("_MB_EXTCAL_IMG_CAT","Afbeeldingscategorie");
+define('_MB_EXTCAL_NEXT', 'Volgende');
+define('_MB_EXTCAL_PREVIEW', 'Voorbeeld');
+define('_MB_EXTCAL_PX', 'px');
+define('_MB_EXTCAL_SECONDES', 'seconden');
+define('_MB_EXTCAL_SS_HEIGHT', 'Slideshow hoogte');
+define('_MB_EXTCAL_SS_NB_PHOTOS', 'Slideshow vs fotos');
+define('_MB_EXTCAL_SS_PAUSE_TIME', 'Slideshow pause tijd');
+define('_MB_EXTCAL_SS_TRANS_TIME', 'Slideshow trans tijd');
+define('_MB_EXTCAL_SS_WIDTH', 'Slideshow breedte');
+define("_MB_EXTCAL_SUBMIT_LINK_TEXT","Toevoegen evenement");
+define("_MB_EXTCAL_TITLE_LENGTH","Titel lengte");
+?>
