10a8cd922870801b81bbfe1a9835290a52732f68
Christelle Gaulupeau <cesagxoops@gmail.com>
2012-05-17 00:11:54 +0000

    news module french place

---
+++ french/modules/news/smallworld/smallworld 1.15/smallworld/language/french_iso/js/variables.js
@@ -0,0 +1,63 @@
+/* Divers codes de langue à inclure dans l'entête
+Voir ceci pour vos paramètres : https://spreadsheets.google.com/spreadsheet/pub?key=0Ah0xU81penP1cDlwZHdzYWkyaERNc0xrWHNvTTA1S1E&gid=1 
+Traduit par Cesag (frxoops.org) le 5 Mai 2012 */
+
+var language = 'fr';
+var countryshort = 'fr';
+var countrylong = 'france';
+
+var _smallworldContinueToReg = "S'enregistrer";
+var _smallworldCancel = "Annuler";
+var _smallworldClose = "Fermer";
+
+var _smallworldCommentsMoreMore = "..Afficher";
+var _smallworldCommentsMoreLess = "..Cacher"; 
+
+var SmallworldGenderError = "Sélectionnez votre sexe";
+var SmallworldBirthdayError = "Renseignez votre date anniversaire";
+var SmallworldEmailError = "Renseignez votre adresse électronique";
+
+var SmallworldSavedSuccesMsg = "Enregistré avec succès";
+
+var SmallworldConfirmBtnFI = "Êtes-vous sûr ?";
+var SmallworldConfirmBtnYes = 'Oui !';
+var SmallworldConfirmBtnNo = 'Non !';
+
+var smallworldNewMessages = "Vous avez de nouveaux messages +";
+var smallworldDeletedMessages = "Les messages ont été supprimés. Le compteur est maintenant -"; 
+
+//Validation
+var SmallworldValidationBackButton = "Retour"; // Bouton de retour
+var SmallworldValidationForwardButton = "Suivant"; // Bouton suivant
+var SmallworldValidationNameErrorMsg = "Veuillez écrire votre nom de famille"; // Champ du nom réel
+var SmallworldValidationGenderErrorMsg = "Sélectionnez un sexe valide"; // Choix du sexe  
+var SmallworldValidationIntingenderErrorMsg = "Veuillez en cocher au moins un. La valeur par défaut est « aucun »"; // Orientation sexuelle
+var SmallworldValidationSearchrelatErrorMsg = "Veuillez en cocher au moins un. La valeur par défaut est « amis »"; // Relations recherchées
+var SmallworldValidationBirthdayErrorMsg = "Veuillez entrer une date valide"; // Anniversaire
+var SmallworldValidationEmailTitleErrorMsg = "Mauvais format d'adresse électronique";// Format d'adresse électronique
+var SmallworldValidationBirthplaceErrorMsg = "Entrez votre date de naissance"; // Date de naissance obligatoire
+var SmallworldValidationAdressErrorMsg = "Entrez le nom de la rue"; // Adresse
+var SmallworldValidationCityErrorMsg = "Renseignez la ville"; // Ville
+var SmallworldValidationCountryErrorMsg = "Renseignez votre pays"; // Pays
+var SmallworldValidationWebsiteErrorMsg = "Entrez votre site internet"; // Site internet
+var SmallworldSureDelete = "Êtes-vous sûr de vouloir supprimer cette mise à jour ? Il n'y aura pas de retour possible !"; // Supprimer les commentaires
+var SmallworldAdminSureDeleteUser = "Confirmer la suppression de cet utilisateur "; //Suppression des utilisateurs dans l'administration
+var SmallworldDialogStatsTitle = "Statistiques"; // Titre de la fenêtre de dialogue des statistiques
+// Partie confirmation selon les langues, pour jquery
+var args = 	{
+				'confirm'		:	false, 		    // Bouttons "ok" et "annuler"
+				'verify'		:	true,		    // Bouttons "oui" et "non"
+				'input'			:	false, 		    // Saisie de texte ("true" ou chaîne de texte par défaut)
+				'animate'		:	true,		    // Annimation habituelle ("true" ou un nombre, 400 par défaut)
+				'textOk'		:	'Ok',		    // Boutton "Ok", texte par défaut
+				'textCancel'	:	'Annuler',  	// Boutton "Annuler", texte par défaut
+				'textYes'		:	'Oui !',	    // Boutton "Oui !", texte par défaut
+				'textNo'		:	'Non !'	        // Boutton "Non !", texte par défaut
+			}
+// Variables pour la Galerie de photos
+var SmallworldPhotoPlaySlideshow = 'Lancer le Diaporama';            
+var SmallworldPhotoPauseSlideshow = 'Pause';
+var SmallworldPhotoPrevPhoto = '&lsaquo; Photos précédentes';
+var SmallworldPhotoNextPhoto = 'Photos suivantes &rsaquo;';
+var SmallworldPhotoNextLink = 'Suivant &rsaquo;';
+var SmallworldPhotoPrevLink = '&lsaquo; Précédent';
