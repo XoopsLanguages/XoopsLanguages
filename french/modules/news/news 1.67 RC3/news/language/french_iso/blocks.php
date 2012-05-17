c647d3b8adbea5e7c0d4c27f8911440d18033a50
Christelle Gaulupeau <cesagxoops@gmail.com>
2012-05-17 00:18:37 +0000

    Add news 1.67 RC3 french

---
+++ french/modules/news/news 1.67 RC3/news/language/french_iso/blocks.php
@@ -0,0 +1,58 @@
+<?php
+// $Id$
+define("_MB_NEWS_NOTYET","Il n'y a pas encore d'article du jour.");
+define("_MB_NEWS_TMRSI","Aujourd'hui l'article le plus lu est :");
+define("_MB_NEWS_ORDER","Ordre");
+define("_MB_NEWS_DATE","Date de publication");
+define("_MB_NEWS_HITS","Nombre de lectures");
+define("_MB_NEWS_DISP","Affichage");
+define("_MB_NEWS_ARTCLS","articles");
+define("_MB_NEWS_CHARS","Longueur du titre");
+define("_MB_NEWS_LENGTH"," caractères");
+define("_MB_TITLE","Titre");
+define("_MB_POSTED","Posté le");
+define("_MB_POSTER","Expéditeur");
+define("_MB_ACTION","Action");
+define("_MB_TOPIC","Sujet");
+define("_MB_DELETE","Effacer");
+define("_MB_NEWS_RESTRICTTOPICS", "Restreindre les sujets visibles par utilisateur ?");
+define("_MB_NEWS_TEASER", "Longueur du texte de l'article (0 pour ne pas avoir de texte)");
+define("_MB_NEWS_SPOTLIGHT", "Utiliser l'option Spotlight ");
+define("_MB_NEWS_FIRST", "--Premier élément--");
+define("_MB_NEWS_IMAGE", "Image pour Spotlight");
+define("_MB_SPOTLIGHT_TOPIC", "Sélectioner le(s) sujet(s) à utiliser");
+define("_MB_SPOTLIGHT_ALL_TOPICS", "Tous les sujets");
+define("_MB_NEWS_SPOTLIGHT_ARTICLE", "Sélectionnez les articles:");
+define("_MB_READMORE","En savoir plus...");
+define("_MB_NEWS_RATE","Cotation");
+define("_MB_NEWS_SHOW_NEWS_COUNT","Montrer le compteur d'articles ?");
+define("_MB_NEWS_SPOTLIGHT_TITLE", "A la une");
+define("_MB_NEWS_VIEW_TYPE1", "Classique");
+define("_MB_NEWS_VIEW_TYPE2", "Par onglets");
+define("_MB_NEWS_TAB_COLOR1", "Couleur de la barre du haut (c'est la ligne qui est juste sous le nom de l'onglet)");
+define("_MB_NEWS_TAB_COLOR2", "Couleur de fond du contenu de l'onglet courant");
+define("_MB_NEWS_TAB_COLOR3", "Couleur de l'entête de l'onglet en cours");
+define("_MB_NEWS_TAB_COLOR4", "Couleur des entêtes des onglets non sélectionnés");
+define("_MB_NEWS_TAB_COLOR5", "Couleur de survol des onglets");
+define("_MB_NEWS_WHAT_PUBLISH", "Que souhaitez vous publier<br />pour l'article à la une ?");
+define("_MB_NEWS_RECENT_NEWS", "Les articles récents (n'utilisez pas la liste ci-dessous)");
+define("_MB_NEWS_RECENT_SPECIFIC", "Un article spécifique (voir en dessous)");
+define("_MB_NEWS_DEFAULT_COLORS", "Laissez les zones vides pour utiliser les couleurs par défaut");
+
+// Added in version 1.50
+define("_MB_NEWS_CAL_YEAR", "Année");
+define("_MB_NEWS_CAL_MONTH", "Mois");
+define("_MB_NEWS_STARTING_DATE", "Date de début");
+define("_MB_NEWS_ENDING_DATE", "Date de fin");
+define("_MB_NEWS_UNTIL_TODAY", "ou jusqu'à aujourd'hui");
+define("_MB_NEWS_RECENT_FIRST", "Les plus récents en premier");
+define("_MB_NEWS_OLDER_FIRST", "Les plus anciens en premier");
+
+/**
+ * @translation     Communauté Francophone des Utilisateurs de Xoops
+ * @specification   _LANGCODE: fr
+ * @specification   _CHARSET: iso-8859-1
+ * Mis à jour par Cesag le 16 Mai 2012
+ * @version         $Id $
+**/
+?>
---
+++ french/modules/news/news 1.67 RC3/news/language/french_iso/main.php
@@ -0,0 +1,144 @@
+<?php
+// $Id$
+//%%%%%%		File Name index.php 		%%%%%
+define('_NW_PRINTER',"Version imprimable");
+define('_NW_SENDSTORY',"Envoyer cet article à un ami");
+define('_NW_READMORE',"Lire la suite...");
+define('_NW_COMMENTS',"Commentaires ?");
+define('_NW_ONECOMMENT',"1 commentaire");
+define('_NW_BYTESMORE',"%s octets en plus");
+define('_NW_NUMCOMMENTS',"%s commentaires");
+define('_NW_MORERELEASES', "Plus d'articles dans le sujet ");
+
+
+//%%%%%%		File Name submit.php		%%%%%
+define('_NW_SUBMITNEWS',"Proposer un article");
+define('_NW_TITLE',"Titre");
+define('_NW_TOPIC',"Sujet");
+define('_NW_THESCOOP',"Le Scoop");
+define('_NW_NOTIFYPUBLISH',"Notifier par mail lors de la publication");
+define('_NW_POST',"Publier");
+define('_NW_GO',"Ok");
+define('_NW_THANKS',"Merci pour votre participation."); //submission of news article
+
+define('_NW_NOTIFYSBJCT',"Des articles à publier sur le site"); // Notification mail subject
+define('_NW_NOTIFYMSG',"Hé! Vous avez de nouvelles propositions d'articles pour votre site."); // Notification mail message
+
+//%%%%%%		File Name archive.php		%%%%%
+define('_NW_NEWSARCHIVES',"Archives");
+define('_NW_ARTICLES',"Articles");
+define('_NW_VIEWS',"Lectures");
+define('_NW_DATE',"Date");
+define('_NW_ACTIONS',"Actions");
+define('_NW_PRINTERFRIENDLY',"Format imprimable");
+
+define('_NW_THEREAREINTOTAL',"Il y a %s article(s) au total");
+
+// %s is your site name
+define('_NW_INTARTICLE',"Article intéressant sur %s");
+define('_NW_INTARTFOUND',"Voici un article intéressant trouvé sur %s");
+
+define('_NW_TOPICC',"Sujet :");
+define('_NW_URL',"Site :");
+define('_NW_NOSTORY',"Désolé, l'article sélectionné n'existe pas.");
+
+//%%%%%%	File Name print.php 	%%%%%
+
+define('_NW_URLFORSTORY',"L'adresse de cet article est :");
+
+// %s represents your site name
+define('_NW_THISCOMESFROM',"Cet article provient de %s");
+
+// Added by Hervé
+define('_NW_ATTACHEDFILES',"Fichiers attachés : ");
+define('_NW_ATTACHEDLIB',"Cet article a des fichiers attachés");
+define('_NW_NEWSSAMEAUTHORLINK',"Articles du même auteur");
+define('_NW_NEWS_NO_TOPICS',"Désolé, mais il n'y actuellement aucun sujet, merci de le créer avant de soumettre un article");
+define('_NW_PREVIOUS_ARTICLE',"Article précédent");
+define('_NW_NEXT_ARTICLE',"Article suivant");
+define('_NW_OTHER_ARTICLES',"Autres articles");
+
+// Added by Hervé in version 1.3 for rating
+define('_NW_RATETHISNEWS',"Noter cet article");
+define('_NW_RATEIT',"Notez-le !");
+define('_NW_TOTALRATE',"Total des votes");
+define('_NW_RATINGLTOH',"Notation (du score le plus bas au score le plus haut)");
+define('_NW_RATINGHTOL',"Notation (du score le plus haut au score le plus bas)");
+define('_NW_RATINGC',"Note: ");
+define('_NW_RATINGSCALE',"L'échelle va de 1 à 10, 1 étant Pas Terrible quand 10 est Excellent.");
+define('_NW_BEOBJECTIVE',"Merci de rester objectif, si tout le monde reçoit une note de 1 ou 10, la notation n'est pas très utile.");
+define('_NW_DONOTVOTE',"Merci de ne pas voter pour vos propres participations.");
+define('_NW_RATING',"Notation");
+define('_NW_VOTE',"Vote");
+define('_NW_NORATING',"Aucune note sélectionnée.");
+define('_NW_USERAVG',"Note moyenne");
+define('_NW_DLRATINGS',"Note des articles (nombre total de vote: %s)");
+define('_NW_ONEVOTE',"1 vote");
+define('_NW_NUMVOTES',"%s votes");
+define('_NW_CANTVOTEOWN',"Vous ne pouvez pas  voter pour une ressource que vous avez proposé.<br />All votes are logged and reviewed.");
+define('_NW_VOTEDELETED',"Données de vote supprimées.");
+define('_NW_VOTEONCE',"Merci de ne pas  voter pour une même ressource plus d'une fois.");
+define('_NW_VOTEAPPRE',"Merci pour votre vote.");
+define('_NW_THANKYOU',"Merci d'avoir pris le temps de voter ici sur %s"); // %s is your site name
+define('_NW_RSSFEED',"Flux RSS");	// Warning, this text is included insided an Alt attribut (for a picture), so take care to the quotes
+define('_NW_AUTHOR',"Auteur");
+define('_NW_META_DESCRIPTION',"Description Meta");
+define('_NW_META_KEYWORDS',"Meta mots clés");
+define('_NW_MAKEPDF',"Créer un fichier PDF à partir de cet article");
+define('_MD_POSTEDON',"Publiée le : ");
+define('_NW_AUTHOR_ID',"ID de l'auteur");
+define('_NW_POST_SORRY',"Désolé mais soit il n'y a pas de sujets soit vous n'avez pas le droit de créer des articles dans les sujets existants. Si vous êtes les webmaster, allez dans les permissions et définissez les permissions de 'Soumettre'.");
+
+// Added in v 1.50
+define('_NW_LINKS',"Liens");
+define('_NW_PAGE',"Page");
+define('_NW_BOOKMARK_ME',"Signets Sociaux");
+define('_AM_NEWS_TOTAL',"Total de %u articles");
+define('_AM_NEWS_WHOS_WHO',"Annuaire des Auteurs");
+define('_NW_NEWS_LIST_OF_AUTHORS',"Voici une liste des auteurs de ce site, cliquez sur leur nom pour voir leurs articles");
+define('_AM_NEWS_TOPICS_DIRECTORY',"Répertoire des sujets");
+define('_NW_PAGE_AUTO_SUMMARY',"Page %d  %s");
+// Added in version 1.51
+define('_NW_BOOKMARK_TO_BLINKLIST',"Mettre en favoris dans Blinklist");
+define('_NW_BOOKMARK_TO_DELICIOUS',"Mettre en favoris dans del.icio.us");
+define('_NW_BOOKMARK_TO_DIGG',"Mettre en favoris dans Digg");
+define('_NW_BOOKMARK_TO_FARK',"Mettre en favoris dans Fark");
+define('_NW_BOOKMARK_TO_FURL',"Mettre en favoris dans Furl");
+define('_NW_BOOKMARK_TO_NEWSVINE',"Mettre en favoris dans Newsvine");
+define('_NW_BOOKMARK_TO_REDDIT',"Mettre en favoris dans Reddit");
+define('_NW_BOOKMARK_TO_SIMPY',"Mettre en favoris dans Simpy");
+define('_NW_BOOKMARK_TO_SPURL',"Mettre en favoris dans Spurl");
+define('_NW_BOOKMARK_TO_YAHOO',"Mettre en favoris dans Yahoo");
+
+// Added in version 1.56
+define('_NW_NOTYETSTORY',"Désolé, l'artcile sélectionné n'a pas encore été publié. Veuillez revenir plus tard.");
+define('_NW_SELECT_IMAGE', "Choisissez une image à joindre à l'article");
+define('_NW_CURENT_PICTURE', "Image actuelle");
+// Added in version 1.67
+define('_NW_BOOKMARK_TO_FACEBOOK', "Mettre en favoris dans Faceboom");
+define('_NW_BOOKMARK_TO_TWITTER', "Mettre en favoris dans Twitter");
+define('_NW_BOOKMARK_TO_SCRIPSTYLE', "Mettre en favoris dans Scripstyle");
+define('_NW_BOOKMARK_TO_STUMBLE', "Mettre en favoris dans Stumble");
+define('_NW_BOOKMARK_TO_TECHNORATI', "Mettre en favoris dans Technorati");
+define('_NW_BOOKMARK_TO_MIXX', "Mettre en favoris dans Mixx");
+define('_NW_BOOKMARK_TO_MYSPACE', "Mettre en favoris dans Myspace");
+define('_NW_BOOKMARK_TO_DESIGNFLOAT', "Mettre en favoris dans Designfloat");
+define('_NW_BOOKMARK_TO_BALATARIN', "Mettre en favoris dans Balatarin");
+define('_NW_BOOKMARK_TO_GOOLGEBUZZ', "Mettre en favoris dans Google Buzz");
+define('_NW_BOOKMARK_TO_GOOLGEREADER', "Mettre en favoris dans Google Reader");
+define('_NW_BOOKMARK_TO_GOOLGEBOOKMARKS', "Mettre en favoris dans Google Bookmarks");
+
+define('_NW_DELETE', "Supprimer");
+define('_NW_EDIT', "Editer");
+define('_NW_SUBTITLE', "Sous-titre : ");
+define('_NW_SELECT_IMAGE_DESC', "Description de l'image sélectionnée : ");
+
+/**
+ * @translation     Communauté Francophone des Utilisateurs de Xoops
+ * @specification   _LANGCODE: fr
+ * @specification   _CHARSET: iso-8859-1
+ * Mis à jour par Cesag le 16 Mai 2012
+ * @version         $Id $
+**/
+
+?>
