5e4d2a4f48f69cc8c74ff2dcdd34127345920b6b
Christelle Gaulupeau <cesagxoops@gmail.com>
2012-05-31 19:13:20 +0000

    Addition of French translation of MyMenus 1.4. Remove iso files.

---
+++ french/modules/mymenus/mymenus 1.4/mymenus/french/blocks.php
@@ -0,0 +1,19 @@
+<?php
+//  Author: Trabis
+//  URL: http://www.xuups.com
+//  E-Mail: lusopoemas@gmail.com
+
+define('_MB_MYMENUS_SELECT_MENU', "Sélectionnez le menu");
+define('_MB_MYMENUS_SELECT_MENU_DSC', "");
+define('_MB_MYMENUS_SELECT_SKIN', "Sélectionnez l'apparence");
+define('_MB_MYMENUS_SELECT_SKIN_DSC', "");
+define('_MB_MYMENUS_USE_THEME_SKIN', "Utiliser l'apparence du thème");
+define('_MB_MYMENUS_USE_THEME_SKIN_DSC', "Si le thème ne fournit pas d'apparence, la sélection ci-dessus sera utilisée");
+define('_MB_MYMENUS_UNIQUEID', "ID unique");
+define('_MB_MYMENUS_UNIQUEID_DSC', "Veuillez garder une ID unique pour chaque bloc. Elle est utilisée lorsque le menu est assigné au modèle");
+define('_MB_MYMENUS_DISPLAY_METHOD', "Procédé d'affichage");
+define('_MB_MYMENUS_DISPLAY_METHOD_DSC', 'Si vous choisissez de l\'attribuer au modèle, vous pouvez alors utiliser <{$xoops_menu_UNIQUEID"}> dans votre thème');
+define('_MB_MYMENUS_DISPLAY_METHOD_BLOCK', "Afficher ce bloc");
+define('_MB_MYMENUS_DISPLAY_METHOD_TEMPLATE', "Attribuer au modèle");
+
+define('_MB_MYMENUS_HOME', "Accueil");
