01aaef12bc9b8dd4ceed5ffb0773251722638037
cesag <cesag@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-05-31 03:49:31 +0000

    add of french translation of mysearch 1.2

---
+++ XoopsLanguages/french/modules/mysearch/mysearch 1.2/french/modinfo.php
@@ -0,0 +1,64 @@
+<?php
+//  ------------------------------------------------------------------------ //
+//                       mysearch - MODULE FOR XOOPS 2                        //
+//                  Copyright (c) 2005-2006 Instant Zero                     //
+//                     <http://xoops.instant-zero.com/>                      //
+// ------------------------------------------------------------------------- //
+//  This program is free software; you can redistribute it and/or modify     //
+//  it under the terms of the GNU General Public License as published by     //
+//  the Free Software Foundation; either version 2 of the License, or        //
+//  (at your option) any later version.                                      //
+//                                                                           //
+//  You may not change or alter any portion of this comment or credits       //
+//  of supporting developers from this source code or any supporting         //
+//  source code which is considered copyrighted (c) material of the          //
+//  original comment or credit authors.                                      //
+//                                                                           //
+//  This program is distributed in the hope that it will be useful,          //
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of           //
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            //
+//  GNU General Public License for more details.                             //
+//                                                                           //
+//  You should have received a copy of the GNU General Public License        //
+//  along with this program; if not, write to the Free Software              //
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA //
+//  ------------------------------------------------------------------------ //
+
+define('_MI_MYSEARCH_NAME',"Ma recherche");
+define('_MI_MYSEARCH_DESC',"Avec ce module, vous pouvez apprendre ce que cherchent les personnes sur votre site Web.");
+
+
+define('_MI_MYSEARCH_ADMMENU1',"Statistiques");
+define('_MI_MYSEARCH_ADMMENU2',"Trier");
+define('_MI_MYSEARCH_ADMMENU3',"Exportation");
+define('_MI_MYSEARCH_ADMMENU4',"Liste noire");
+define('_MI_MYSEARCH_ADMMENU5',"À propos");
+
+define('_MI_MYSEARCH_OPT0',"Nombre de recherches à afficher sur la page d'index du module");
+define('_MI_MYSEARCH_OPT0_DSC',"Sélectionnez le nombre de recherches que les utilisateurs peuvent voir sur la page d'index du module (0 = montrer rien)");
+
+define('_MI_MYSEARCH_OPT1',"Groupes que vous ne souhaitez pas enregistrer");
+define('_MI_MYSEARCH_OPT1_DSC',"Toutes les recherches faites par les personnes qui sont dans ces groupes ne seront pas enregistrées");
+
+define('_MI_MYSEARCH_OPT2',"Nombre de mots clés visibles dans l'administration");
+define('_MI_MYSEARCH_OPT2_DSC',"");
+
+define('_MI_MYSEARCH_BNAME1',"Dernières recherches");
+define('_MI_MYSEARCH_BNAME2',"Plus grands utilisateurs de la recherche");
+define('_MI_MYSEARCH_BNAME3',"Statistiques");
+define('_MI_MYSEARCH_BNAME4',"Recherche Ajax");
+
+// Added by Lankford on 2007/8/15
+define('_MI_MYSEARCH_DO_DEEP_SEARCH', "Activer la recherche « appronfondie » ?");
+define('_MI_MYSEARCH_DO_DEEP_SEARCH_DSC', "Vous désirez que votre page de résultats de recherche initiale indique combien de résultats ont été trouvés dans chaque module ?  Note : cette activation peut ralentir le processus de recherche !");
+define('_MI_MYSEARCH_INIT_SRCH_RSLTS', "Nombre de résultats de recherche initial : (pour la recherche « simple »)");
+define('_MI_MYSEARCH_INIT_SRCH_RSLTS_DSC', "Les recherches « simples » sont faites plus rapidement en limitant les résultats qui sont retournés pour chaque module sur la page de recherche initiale.");
+define('_MI_MYSEARCH_MDL_SRCH_RESULTS', "Nombre de résultats par page :");
+define('_MI_MYSEARCH_MDL_SRCH_RESULTS_DSC', "Cela détermine le nombre de visites par page qui sont apparues après l'exploration dans les résultats de la recherche d'un module particulier.");
+
+define('_MI_MYSEARCH_MIN_SEARCH', 'Longueur minimum du mot-clé');
+define('_MI_MYSEARCH_MIN_SEARCH_DSC', 'Entrer la longueur de mot-clé minimum que les utilisateurs sont tenus d\'entrer pour effectuer la recherche');
+
+define('_MI_MYSEARCH_ADMIN0',"Accueil");
+// Traduction faite par Cesag le 31 Mai 2012 pour frxoops.org
+?>
