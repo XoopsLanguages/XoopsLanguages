150baae97ee925b0fe188aa86fd1fd6fb93accc5
Christelle Gaulupeau <cesagxoops@gmail.com>
2012-05-31 03:49:31 +0000

    add of french translation of mysearch 1.2

---
+++ french/modules/mysearch/mysearch 1.2/french/main.php
@@ -0,0 +1,51 @@
+<?php
+//  ------------------------------------------------------------------------ //
+//                       mysearch - MODULE FOR XOOPS 2                        //
+//                  Copyright (c) 2005-2006 Instant Zero                     //
+//                     <http://xoops.instant-zero.com/>                      //
+// ------------------------------------------------------------------------- //
+//  This program is free software; you can redistribute it and/or modify     //
+//  it under the terms of the GNU General Public License as published by     //
+//  the Free Software Foundation; either version 2 of the License, or        //
+//  (at your option) any later version.                                      //
+//                                                                           //
+//  You may not change or alter any portion of this comment or credits       //
+//  of supporting developers from this source code or any supporting         //
+//  source code which is considered copyrighted (c) material of the          //
+//  original comment or credit authors.                                      //
+//                                                                           //
+//  This program is distributed in the hope that it will be useful,          //
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of           //
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            //
+//  GNU General Public License for more details.                             //
+//                                                                           //
+//  You should have received a copy of the GNU General Public License        //
+//  along with this program; if not, write to the Free Software              //
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA //
+//  ------------------------------------------------------------------------ //
+
+define('_MA_MYSEARCH_DATE',"Date");
+define('_MA_MYSEARCH_KEYWORD',"Mots recherchés");
+define('_MA_MYSEARCH_PAGE',"Page");
+
+define("_MA_MYSEARCH_SEARCH","Recherche");
+define("_MA_MYSEARCH_PLZENTER","Veuillez entrer toutes les données requises !");
+define("_MA_MYSEARCH_SEARCHRESULTS","Résultats de la recherche");
+define("_MA_MYSEARCH_NOMATCH","Aucune correspondance trouvée pour votre requête");
+define("_MA_MYSEARCH_FOUND","<b>%s</b> correspondance(s) trouvée(s)");
+define("_MA_MYSEARCH_SHOWING","(%d -%d affichés)");
+define("_MA_MYSEARCH_ANY","Quelques uns (OU)");
+define("_MA_MYSEARCH_ALL","Tous (ET)");
+define("_MA_MYSEARCH_EXACT","Correspondance exacte");
+define("_MA_MYSEARCH_SHOWALLR","Afficher tous les résultats");
+define("_MA_MYSEARCH_NEXT","Suivant >>");
+define("_MA_MYSEARCH_PREVIOUS","<< Précédent");
+define("_MA_MYSEARCH_KEYWORDS","Mots clés");
+define("_MA_MYSEARCH_TYPE","Type");
+define("_MA_MYSEARCH_SEARCHIN","Rechercher dans");
+define('_MA_MYSEARCH_KEYTOOSHORT', 'Les mots clés doivent avoir au moins <b>%s</b> caractères');
+define('_MA_MYSEARCH_KEYIGNORE', 'Les mots clés plus courts que <b>%s</b> caractères seront ignorés');
+define('_MA_MYSEARCH_SEARCHRULE', 'Mode de recherche');
+define('_MA_MYSEARCH_IGNOREDWORDS', 'Les mots suivants sont plus courts que le minimum de la longueur permise (%u signes) et n\'étaient pas inclus dans votre recherche :');
+// Traduction faite par Cesag le 31 Mai 2012 pour frxoops.org
+?>
