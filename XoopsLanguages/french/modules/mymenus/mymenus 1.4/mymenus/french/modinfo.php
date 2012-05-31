86dcb25d29fc7e8128f6782a6052c29945957a39
cesag <cesag@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-05-31 19:13:20 +0000

    Addition of French translation of MyMenus 1.4. Remove iso files.

---
+++ XoopsLanguages/french/modules/mymenus/mymenus 1.4/mymenus/french/modinfo.php
@@ -0,0 +1,27 @@
+<?php
+//  Author: Trabis
+//  URL: http://www.xuups.com
+//  E-Mail: lusopoemas@gmail.com
+
+// Module Info
+define('_MI_MYMENUS_MD_NAME', "Mes menus");
+define('_MI_MYMENUS_MD_DESC', "Menus pour votre site");
+
+// Blocks
+define('_MI_MYMENUS_BLK', "Bloc du menu");
+define('_MI_MYMENUS_BLK_DSC', "");
+
+// Admin menu
+define('_MI_MYMENUS_MENUSMANAGER', "Gestionnaire de menus");
+define('_MI_MYMENUS_MENUMANAGER', "Gestionnaire de menus");
+define('_MI_MYMENUS_ABOUT', "À propos");
+
+//Configs
+define('_MI_MENUS_CONF_ASSIGN_METHOD', "Afttribuer ce mode");
+define('_MI_MENUS_CONF_ASSIGN_METHOD_DSC', "Méthode d'attribution des css et js à l'en-tête");
+define('_MI_MENUS_CONF_ASSIGN_METHOD_XOOPSTPL', "XoopsTpl");
+define('_MI_MENUS_CONF_ASSIGN_METHOD_XOTHEME', "XoTheme");
+
+//1.4
+    define('_MI_MYMENUS_ADMMENU0', 'Accueil');
+
