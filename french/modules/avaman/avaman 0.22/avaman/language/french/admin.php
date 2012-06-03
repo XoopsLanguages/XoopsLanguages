eaaf9962bb382928390fdfc6e9b4a7549fed3da9
Christelle Gaulupeau <cesagxoops@gmail.com>
2012-06-03 12:13:54 +0000

    Avaman 0.22 french translation

---
+++ french/modules/avaman/avaman 0.22/avaman/language/french/admin.php
@@ -0,0 +1,45 @@
+<?php
+/*
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+/**
+ * Avaman module
+ *
+ * @copyright    The XOOPS Project (http://www.xoops.org)
+ * @license   {@link http://www.gnu.org/licenses/gpl-2.0.html GNU Public License}
+ * @package    Avaman
+ * @since      2.5.0
+ * @author     GIJOE
+ * @version    $Id $
+ */
+
+define('_AM_AVAMAN_DBUPDATED' , 'Mise à jour avec succès' ) ;
+define('_AM_AVAMAN_FILEUPLOADED' , 'Les fichiers %s sont enregistrés avec succès' ) ;
+define('_AM_AVAMAN_UPLOAD' , 'Téléchargement de fichiers image (vous pouvez également télécharger plusieurs fichiers en archive zip ou tar.gz)' ) ;
+define('_AM_AVAMAN_ERR_INVALIDARCHIVE' , "L'archive ne peut pas être extraite." ) ;
+define('_AM_AVAMAN_INVALIDEXT' , 'Type de fichier non valide' ) ;
+
+define('_AM_AVAMAN_TH_ID' , 'ID' ) ;
+define('_AM_AVAMAN_TH_FILE' , 'Fichier' ) ;
+define('_AM_AVAMAN_TH_AVATARNAME' , 'Nom' ) ;
+define('_AM_AVAMAN_TH_CREATED' , 'Créé' ) ;
+define('_AM_AVAMAN_TH_DISPLAY' , 'Affichage' ) ;
+define('_AM_AVAMAN_TH_WEIGHT' , 'Poids' ) ;
+define('_AM_AVAMAN_TH_USERS' , 'Utilisateurs' ) ;
+define('_AM_AVAMAN_TH_DELETE' , 'Supprimer' ) ;
+define('_AM_AVAMAN_CB_SELECTALL' , 'Sélectionner/désélectionner tout' ) ;
+
+define('_AM_AVAMAN_TH_CODE' , 'Code' ) ;
+define('_AM_AVAMAN_TH_EMOTION' , 'Emotion' ) ;
+define('_AM_AVAMAN_TH_SMILEDISPLAY' , 'Montrer en zone de texte' ) ;
+
+//ModuleAdmin
+define('_AM_AVAMAN_MODULEADMIN_MISSING','Erreur : La classe ModuleAdmin est manquante. Veuillez installer la classe ModuleAdmin dans /Frameworks (voir /docs/readme.txt)');
+// Traduction par Cesag pour frxoops.org  le 03 Juin 2012
---
+++ french/modules/avaman/avaman 0.22/avaman/language/french/modinfo.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+/**
+ * Avaman module
+ *
+ * @copyright    The XOOPS Project (http://www.xoops.org)
+ * @license   {@link http://www.gnu.org/licenses/gpl-2.0.html GNU Public License}
+ * @package    Avaman
+ * @since      2.5.0
+ * @author     GIJOE
+ * @version    $Id $
+ */
+
+define( '_MI_AVAMAN_MODULENAME' , 'AVAMAN' ) ;
+define( '_MI_AVAMAN_MODULEDESC' , 'Gestion des avatars du système et des smileys' ) ;
+
+define( '_MI_AVAMAN_AVATARMANAGER' , 'Gestionnaire d\'avatars' ) ;
+define( '_MI_AVAMAN_SMILIESMANAGER' , 'Gestionnaire de Smileys' ) ;
+// Traduction par Cesag pour frxoops.org  le 03 Juin 2012
