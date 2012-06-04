fd2c5b6530816efbf6de393bdaeb7da3f391127c
cesag <cesag@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-06-04 01:20:45 +0000

    Defacer 1.1 french translation.

---
+++ XoopsLanguages/french/modules/defacer/defacer 1.1/defacer/language/french/modinfo.php
@@ -0,0 +1,31 @@
+<?php
+define("_MI_DEFACER_MD_NAME", "Defacer");
+define("_MI_DEFACER_MD_DSC", "Ce module permet à votre site de changer les thèmes, les métadonnées et les permissions d'accès des groupes, pour chaque page donnée.");
+
+define("_MI_DEFACER_PAGEMANAGER", "Gestionnaire de page");
+define("_MI_DEFACER_THEMEMANAGER", "Gestionnaire de thème");
+define("_MI_DEFACER_METAMANAGER", "Gestionnaire de métadonnées");
+define("_MI_DEFACER_PERMISSIONMANAGER", "Gestionnaire d'autorisations");
+define("_MI_DEFACER_ABOUT", "À propos");
+
+define("_MI_DEFACER_DISDEFACER", "Désactiver Defacer");
+define("_MI_DEFACER_DISDEFACER_DSC", "Cela va désactiver toutes les actions de Defacer");
+
+define("_MI_DEFACER_DISTHEMES", "Désactiver le changement de thème");
+define("_MI_DEFACER_DISTHEMES_DSC", "Cela va désactiver toutes les actions des thèmes Defacer");
+
+define("_MI_DEFACER_DISMETAS", "Désactiver la modification des métadonnées");
+define("_MI_DEFACER_DISMETAS_DSC", "Cela va désactiver toutes les actions des métadonnées Defacer");
+
+define("_MI_DEFACER_DISPERMISSIONS", "Désactiver les changements de permission");
+define("_MI_DEFACER_DISPERMISSIONS_DSC", "Cela va désactiver toutes les actions d'autorisation de Defacer");
+
+define("_MI_DEFACER_XOOPS_URL", "Lien internet de votre site Xoops");
+define("_MI_DEFACER_XOOPS_URL_DSC", "Veuillez fournir le lien de votre site sans la barre oblique de fin.");
+
+define("_MI_DEFACER_ENABLE_REDIRECT", "Activer le système de redirection JGrowl");
+define("_MI_DEFACER_ENABLE_REDIRECT_DSC", "Activez ceci, afin de remplacer les pages de redirection par défaut (xoops 2.4 > seulement)");
+
+//1.1
+define("_MI_DEFACER_HOME", "Accueil");
+// Traduction faite par Cesag pour frxoops.org le 04 Juin 2012.
