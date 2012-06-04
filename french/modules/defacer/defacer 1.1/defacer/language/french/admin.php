6b4ba43bcabe5a40754bd1f4df0faa57ba1c1637
Christelle Gaulupeau <cesagxoops@gmail.com>
2012-06-04 01:20:45 +0000

    Defacer 1.1 french translation.

---
+++ french/modules/defacer/defacer 1.1/defacer/language/french/admin.php
@@ -0,0 +1,50 @@
+<?php
+define("_AM_DEFACER_MODULEADMIN","Module d'Administration");
+define("_AM_DEFACER_GENERALSET","Paramètres généraux");
+
+define("_AM_DEFACER_ID","ID");
+define("_AM_DEFACER_MODULE","Module");
+define("_AM_DEFACER_TITLE","Titre");
+define("_AM_DEFACER_URL","Lien internet");
+define("_AM_DEFACER_STATUS","Statut");
+define("_AM_DEFACER_QUICKLAUNCH","Lancement rapide");
+
+define("_AM_DEFACER_CHANGESTATUS","Cliquez pour changer de statut");
+define("_AM_DEFACER_DBUPDATED","Base de données mise à jour");
+define("_AM_DEFACER_ERROR","Désolé, une erreur s'est produite");
+define("_AM_DEFACER_RUDEL","Êtes-vous sûr de vouloir le supprimer ?");
+define("_AM_DEFACER_NOTFOUND","Non trouvé");
+
+define("_AM_DEFACER_MODDEACTIVE","Module non actif");
+define("_AM_DEFACER_SELECTMODULE_ERR","Veuillez d'abord sélectionner un module");
+define("_AM_DEFACER_SELECTPAGE_ERR","Veuillez d'abord sélectionner une page");
+
+define("_AM_DEFACER_PAGEMAN","Gestionnaire de pages");
+define("_AM_DEFACER_PAGE","Page");
+define("_AM_DEFACER_PAGE_MODULE","Page du module");
+define("_AM_DEFACER_PAGE_TITLE","Titre de la page");
+define("_AM_DEFACER_PAGE_URL","Lien internet de la page");
+define("_AM_DEFACER_PAGE_URL_DESC","Le lien internet est relatif à la racine (système) ou au module.<br /> vous pouvez entrer un caractère générique '*' pour faire tout correspondre.");
+define("_AM_DEFACER_PAGE_DISPLAY","Affichage de la page");
+
+define("_AM_DEFACER_THEMEMAN","Gestionnaire de thème");
+define("_AM_DEFACER_THEME","Thème");
+
+define("_AM_DEFACER_METAMAN","Gestionnaire de métadonnées");
+define("_AM_DEFACER_META_SITENAME","Nom du site");
+define("_AM_DEFACER_META_PAGETITLE","Titre de la page");
+define("_AM_DEFACER_META_SLOGAN","Slogan du site");
+define("_AM_DEFACER_META_KEYWORDS","Mots clés méta");
+define("_AM_DEFACER_META_DESCRIPTION","Description méta");
+
+define("_AM_DEFACER_PERMISSIONMAN","Gestionnaire d'autorisations");
+define("_AM_DEFACER_PERMISSION_GROUPS","Groupes autorisés");
+
+//1.1
+
+//ModuleAdmin
+define('_AM_DEFACER_MODULEADMIN_MISSING','Erreur : La classe ModuleAdmin est manquante. Veuillez installer la classe ModuleAdmin dans /Frameworks (voir /docs/readme.txt)');
+
+// Text for Admin footer
+//define("_AM_DEFACER_FOOTER", "<div class='center smallsmall italic pad5'>Defacer est maintenu par la <a class='tooltip' rel='external' href='http://xoops.org/' title='Visit XOOPS Community'>Communauté XOOPS</a></div>");
+// Traduction faite par Cesag pour frxoops.org le 04 Juin 2012.
