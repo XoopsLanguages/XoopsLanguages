ab4f98714c5b9c667b549e2d9acaba8b0d58f657
Christelle Gaulupeau <cesagxoops@gmail.com>
2012-06-05 02:06:35 +0000

    Birthday 2.3RC2 french translation.

---
+++ french/modules/birthday/birthday 2.3RC2/birthday/language/french/blocks.php
@@ -0,0 +1,14 @@
+<?php
+define("_MB_BD_TITRE","Joyeux anniversaire");
+define("_MB_BD_DESC","Montre les anniversaires des membres");
+define("_MD_DB_FELICI","Bon anniversaire à :");
+define("_MB_BD_TODAY","pour ses ");
+define("_MB_BD_YEARSOLD"," ans");
+define("_MB_BD_NOBIRTHDAY","Pas d'anniversaires aujourd'hui.");
+define("_MB_BD_SEND","Envoyez un message privé à ");
+define("_MB_BD_SEND2"," pour son anniversaire.");
+define("_MB_BD_EDIT","Editez votre anniversaire");
+define("_MB_BIRTHDAY_MAX_ITEMS", "Nombre maximum d'éléments (0 = pas de limite)");
+define("_MB_BIRTHDAY_SHOW_MORE", "Voir plus d'anniversaires");
+// Traduction faite par Cesag pour frxoops.org le 05 Juin 2012.
+?>
