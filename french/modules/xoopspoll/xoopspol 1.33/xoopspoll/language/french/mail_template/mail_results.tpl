a2bf716d1445c08e367c83bf27417b55ef0428ff
Christelle Gaulupeau <cesagxoops@gmail.com>
2012-06-24 13:23:33 +0000

    Xoops Poll 1.33 Final french translation

---
+++ french/modules/xoopspoll/xoopspol 1.33/xoopspoll/language/french/mail_template/mail_results.tpl
@@ -0,0 +1,13 @@
+Bonjour {X_UNAME},
+
+Votre sondage "{POLL_QUESTION}" a expiré.
+Le sondage a débuté le {POLL_START} et s'est terminé le {POLL_END}.
+Il y a eu {POLL_VOTERS} votants et {POLL_VOTES} votes au total.
+
+Vous pouvez voir les résultats à l'URL suivante :
+{SITEURL}modules/xoopspoll/pollresults.php?poll_id={POLL_ID}
+
+-----------
+{SITENAME} ({SITEURL}) 
+Le Webmestre
+{ADMINMAIL}
---
+++ french/modules/xoopspoll/xoopspol 1.33/xoopspoll/language/french/main.php
@@ -0,0 +1,34 @@
+<?php
+// $Id$
+
+//%%%%%%	File Name pollresults.php 	%%%%%
+define("_PL_TOTALVOTES","Total des Votes : %s");
+define("_PL_TOTALVOTERS","Total des Votants : %s");
+
+//%%%%%%	File Name index.php 	%%%%%
+define("_PL_POLLSLIST","Liste des sondages");
+define("_PL_ALREADYVOTED", "Désolé, vous avez déjà voté une fois.");
+define("_PL_THANKSFORVOTE","Merci pour votre vote !");
+define("_PL_SORRYEXPIRED", "Désolé, mais le sondage a expiré.");
+define("_PL_YOURPOLLAT", "%s, votre sondage sur %s"); // 1st %s is user name, 2nd %s is site name
+define("_PL_PREV", "Précédent");
+define("_PL_NEXT", "Suivant");
+define("_PL_POLLQUESTION", "Question du sondage");
+define("_PL_VOTERS", "Nombre total de votants");
+define("_PL_VOTES", "Total de votes");
+define("_PL_EXPIRATION", "Expiration");
+define("_PL_EXPIRED", "Expiré");
+define("_PL_MUSTLOGIN", "Désolé, mais vous devez vous connecter pour voter à ce sondage.");
+
+//%%%%%%	File Name xoopspollrenderer.php 	%%%%%
+// %s represents date
+define("_PL_ENDSAT","Se termine le %s");
+define("_PL_ENDEDAT","S'est terminé à %s");
+define("_PL_VOTE","Votez !");
+define("_PL_RESULTS","Résultats");
+// 1.32
+//file /admin/index.php
+define("_MD_XOOPSPOLL_DASHBOARD","Tableau de bord des sondages de Xoops");
+define("_MD_XOOPSPOLL_TOTALPOLLS","Total des sondages : <strong>%s</strong> ");
+define("_MD_XOOPSPOLL_TOTALACTIVE","Sondages actifs : <strong>%s</strong> ");
+define("_MD_XOOPSPOLL_TOTALNONACTIVE","Sondages expirés : <strong>%s</strong> ");
