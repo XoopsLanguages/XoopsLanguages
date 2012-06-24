d0ec72176409c850c8aadb5d30ccecb93567e958
cesag <cesag@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-06-24 13:23:33 +0000

    Xoops Poll 1.33 Final french translation

---
+++ XoopsLanguages/french/modules/xoopspoll/xoopspol 1.33/xoopspoll/language/french/blocks.php
@@ -0,0 +1,10 @@
+<?php
+// $Id$
+// Blocks
+
+define("_PL_WILLEXPIRE","Le sondage va fermer à");
+define("_PL_HASEXPIRED","Sondage fermé à");
+define("_PL_COMMENTS","Commentaires");
+define("_PL_COMMENT","Commentaire");
+
+?>
---
+++ XoopsLanguages/french/modules/xoopspoll/xoopspol 1.33/xoopspoll/language/french/modinfo.php
@@ -0,0 +1,32 @@
+<?php
+// $Id$
+// Module Info
+
+// The name of this module
+define("_MI_POLLS_NAME","Sondage XOOPS");
+
+// A brief description of this module
+define("_MI_POLLS_DESC","Module de sondages qui s'affiche aussi dans un bloc");
+
+// Names of blocks for this module (Not all module has blocks)
+define("_MI_POLLS_BNAME1","Sondages");
+
+// Names of admin menu items
+define("_MI_POLLS_ADMENU1","Liste des sondages");
+define("_MI_POLLS_ADMENU2","Ajouter un sondage");
+
+//Module properties
+define("_MI_POLL_LIMITBYIP","Restreindre le droit de vote à la même IP");
+define("_MI_POLL_LIMITBYIPD","");
+define("_MI_POLL_LIMITBYUID","Restreindre le vote par le même utilisateur");
+define("_MI_POLL_LIMITBYUIDD","");
+
+// index.php
+define("_MI_POLLS_HOME",                  "Accueil");
+define("_MI_POLLS_ADMIN_ABOUT",                  "À propos");
+define("_MI_POLLS_ADMIN_HELP",                 "Aide");
+
+//1.33
+// lookup host
+define("_MI_POLLS_LOOKUPHOST","Afficher le nom d'hôte au lieu de l'adresse IP");
+define("_MI_POLLS_LOOKUPHOSTDESC","Liste des noms d'hôte au lieu des adresses IP dans l'affichage des journaux de sondage. Si Nslookup est utilisé, cela peut prendre plus de temps pour afficher les noms.");
