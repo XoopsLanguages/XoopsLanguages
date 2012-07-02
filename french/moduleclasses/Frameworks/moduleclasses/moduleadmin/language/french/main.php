f84875874984c67c27214b29736ea16f8c7de8dc
Christelle Gaulupeau <cesagxoops@gmail.com>
2012-07-02 15:49:28 +0000

    French moduleclasses translation

---
+++ french/moduleclasses/Frameworks/moduleclasses/moduleadmin/language/french/main.php
@@ -0,0 +1,52 @@
+﻿<?php
+/**
+ * Frameworks Module Admin
+ *
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright   Grégory Mage (Aka Mage)
+ * @license     GNU GPL 2 (http://www.gnu.org/licenses/old-licenses/gpl-2.0.html)
+ * @author      Grégory Mage (Aka Mage)
+ */
+
+/* french translation: karim@xoops-buch.de */
+
+//
+
+// Info
+define("_AM_MODULEADMIN_CONFIG","Préférences");
+define("_AM_MODULEADMIN_CONFIG_CHMOD","Le dossier '%s' doit avoir CHMOD '%s'(a actuellement %s).");
+define("_AM_MODULEADMIN_CONFIG_FOLDERKO","Le dossier '%s' n'existe pas");
+define("_AM_MODULEADMIN_CONFIG_FOLDEROK","Le dossier '%s' existe");
+define("_AM_MODULEADMIN_CONFIG_PHP","Vous avez besoin de PHP version %s au minimum (votre Version utilisée actuellement est %s)");
+define("_AM_MODULEADMIN_CONFIG_XOOPS","Vous avez besoin de XOOPS version %s (votre Version utilisée actuellement est %s)");
+
+define('_AM_MODULEADMIN_CONFIG_DB','version minimum requise : %s (votre version est %s)');
+define('_AM_MODULEADMIN_CONFIG_ADMIN','Minimum ModuleAdmin required: %s (your version is %s)');
+
+// About
+define("_AM_MODULEADMIN_ABOUT_CHANGELOG","Histoire des modifications");
+define("_AM_MODULEADMIN_ABOUT_MODULEINFO","Infos module");
+define("_AM_MODULEADMIN_ABOUT_MODULESTATUS","Statut:");
+define("_AM_MODULEADMIN_ABOUT_UPDATEDATE","Date de dernière mise à jour:");
+define("_AM_MODULEADMIN_ABOUT_WEBSITE","Site:");
+
+define("_AM_MODULEADMIN_ABOUT_RELEASEDATE","Released: ");
+define("_AM_MODULEADMIN_ABOUT_AUTHOR","Author: ");
+define("_AM_MODULEADMIN_ABOUT_CREDITS","Credits: ");
+define("_AM_MODULEADMIN_ABOUT_LICENSE","License: ");
+define("_AM_MODULEADMIN_ABOUT_AUTHOR_NAME","Author name: ");
+define("_AM_MODULEADMIN_ABOUT_AUTHOR_INFO","Author Info");
+
+define("_AM_MODULEADMIN_HOME", "Home");
+define("_AM_MODULEADMIN_DASHBOARD", "Dashboard");
+define("_AM_MODULEADMIN_ABOUT", "About");
+define("_AM_MODULEADMIN_PERMISSIONS", "Permissions");
+
+//ModuleAdmin
+define("_AM_MODULEADMIN_ADMIN_FOOTER", "<div class='center smallsmall italic pad5'>Ce module est maintenu par la <a class='tooltip' rel='external' href='http://xoops.org/' title='Visitez la Communauté XOOPS'>Communauté XOOPS</a></div>");
