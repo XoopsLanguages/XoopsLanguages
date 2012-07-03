4d75d9b82e58203ee56f9353b6377e8ed6b52a01
txmodxoops <txmodxoops@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-07-03 06:34:57 +0000

    Added italian language

---
+++ XoopsLanguages/italian/modules/boox/language/italian/admin.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * ****************************************************************************
+ * boox - MODULE FOR XOOPS
+ * Copyright (c) Hervé Thouzard of Instant Zero (http://www.instant-zero.com)
+ *
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright       Hervé Thouzard of Instant Zero (http://www.instant-zero.com)
+ * @license         http://www.gnu.org/licenses/gpl-2.0.html GNU Public License
+ * @package         boox
+ * @author 			Hervé Thouzard of Instant Zero (http://www.instant-zero.com)
+ *
+ * Version : $Id:
+ * ****************************************************************************
+ */
+define("_AM_BOOX_DBUPDATED", "Il database &#232; stato aggiornato con successo");
+define("_AM_BOOX_GENERALSET", "Opzioni del modulo");
+define("_AM_BOOX_MODULEADMIN", "Admin del modulo");
+define("_AM_BOOX_EDIT", "modificare");
+define("_AM_BOOX_DELETE", "Elimina");
+define("_AM_BOOX_ADD", "Aggiungi");
+define("_AM_BOOX_CONFIRM", "Siete sicuri ?");
+define("_AM_BOOX_FILES", "File");
+define("_AM_BOOX_FILE", "File");
+define("_AM_BOOX_FILENAME", "Nome file");
+define("_AM_BOOX_ACTION", "Azione");
+define("_AM_BOOX_COMMAND", "Comando da includere nel tuo tema");
+define("_AM_BOOX_ERROR1", "Specificare un nome di file.");
+define("_AM_BOOX_ERROR2", "Errore, Impossibile salvare il file %s");
+define("_AM_BOOX_ERROR3", "Errore, Impossibile leggere il file %s");
+define("_AM_BOOX_CONTENT", "Contenuto");
+//ModuleAdmin
+define("_AM_BOOX_MODULEADMIN_MISSING", "Errore: La classe ModuleAdmin manca. Si prega di installare la classe ModuleAdmin in /Frameworks (vedi /docs/readme.txt)");
+// Translation done by XTransam & admin (info@txmodxoops.org)
+// XTransam 1.2 is written by Chronolabs Co-op & The XOOPS Project - File Dumped on 2012-07-03 06:02
+?>
---
+++ XoopsLanguages/italian/modules/boox/language/italian/modinfo.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * ****************************************************************************
+ * boox - MODULE FOR XOOPS
+ * Copyright (c) Hervé Thouzard of Instant Zero (http://www.instant-zero.com)
+ *
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright       Hervé Thouzard of Instant Zero (http://www.instant-zero.com)
+ * @license         http://www.gnu.org/licenses/gpl-2.0.html GNU Public License
+ * @package         boox
+ * @author 			Hervé Thouzard of Instant Zero (http://www.instant-zero.com)
+ *
+ * Version : $Id:
+ * ****************************************************************************
+ */
+define("_MI_BOOX_NAME", "Boox");
+define("_MI_BOOX_DESC", "Creare blocchi per Xoops");
+define("_MI_BOOX_OPT0", "Selezionare il luogo dove si desidera memorizzare i file");
+define("_MI_BOOX_OPT0_DSC", "Tutti i file creati verranno archiviati in questo luogo.");
+define("_MI_BOOX_FORM_OPTIONS", "Opzione Modulo");
+define("_MI_BOOX_FORM_COMPACT", "Compattare");
+// Added in version 1.1
+define("_MI_BOOX_OPT1", "Compattare contenuto prima di salvare?");
+define("_MI_BOOX_OPT1_DSC", "Gli spazi saranno rimossi prima che il file viene salvato");
+//1.71
+define("_MI_BOOX_MENU_00", "Dashboard");
+define("_MI_BOOX_MENU_01", "Boox Manager");
+define("_MI_BOOX_ADMIN_ABOUT", "Informazioni");
+// Translation done by XTransam & admin (info@txmodxoops.org)
+// XTransam 1.2 is written by Chronolabs Co-op & The XOOPS Project - File Dumped on 2012-07-03 06:02
+?>
