0b6ad8abc071cbc9f9109473b428e680b0093310
cesag <cesag@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-07-07 13:29:57 +0000

    Adding Extcal 2.37 beta 3 french translation.

---
+++ XoopsLanguages/french/modules/extcal/extcal 2.37/extcal/language/french/blocks.php
@@ -0,0 +1,31 @@
+<?php
+define('_MB_EXTCAL_BGCOLOR', "Couleur de fond");
+define('_MB_EXTCAL_CAT_TO_USE', "Catégories");
+define('_MB_EXTCAL_HEIGHT', "Hauteur");
+define('_MB_EXTCAL_HORLOGE', "Horloge");
+define('_MB_EXTCAL_HORLOGE_OPT', "Paramètres de l'horloge");
+define('_MB_EXTCAL_OPT_SHOW', "Options d'affichage");
+define('_MB_EXTCAL_OPT_SLIDE_SHOW', "Diaporama");
+define('_MB_EXTCAL_WIDTH', "Largeur");
+define('_MB_EXTCAL_ALL_CAT', "Toutes les catégories");
+define('_MB_EXTCAL_CAT_TO_USE_DESC', "Sélectionner les catégories dans lesquelles les évènements seront intégrés");
+define('_MB_EXTCAL_CURRENT', "Actuel");
+define('_MB_EXTCAL_DISPLAY', "Afficher");
+define('_MB_EXTCAL_DISPLAY_IMG', "Afficher l'image");
+define('_MB_EXTCAL_DISPLAY_MONTH', "Mois à afficher");
+define('_MB_EXTCAL_DISPLAY_SUBMIT_LINK', "Afficher le lien de soumission");
+define('_MB_EXTCAL_EVENT', "évènements");
+define('_MB_EXTCAL_IMG_CAT', "Image de la catégorie");
+define('_MB_EXTCAL_NEXT', "Suivant");
+define('_MB_EXTCAL_PREVIEW', "Précédent");
+define('_MB_EXTCAL_PX', "px");
+define('_MB_EXTCAL_SECONDES', "secondes");
+define('_MB_EXTCAL_SS_HEIGHT', "Hauteur du diaporama");
+define('_MB_EXTCAL_SS_NB_PHOTOS', "Nombre de photos du diaporama");
+define('_MB_EXTCAL_SS_PAUSE_TIME', "Temps de pause du diaporama");
+define('_MB_EXTCAL_SS_TRANS_TIME', "Temps de transition du diaporama");
+define('_MB_EXTCAL_SS_WIDTH', "Largeur du diaporama");
+define('_MB_EXTCAL_SUBMIT_LINK_TEXT', "Soumettre un évènement");
+define('_MB_EXTCAL_TITLE_LENGTH', "Longueur du titre");
+
+?>
