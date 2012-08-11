130c1ccb2a513fd9f82cd00a6a44ce012ba29fdc
Christelle Gaulupeau <cesagxoops@gmail.com>
2012-08-11 23:18:17 +0000

    Updating extGallery 1.11 RC french translation.

---
+++ french/modules/extgallery/extgallery 1.11/extgallery/language/french/main.php
@@ -0,0 +1,129 @@
+<?php
+define("_MD_EXTGALLERY_CATEGORIESALBUMS","Cat&eacute;gories / Albums");
+define("_MD_EXTGALLERY_ALBUMS","Albums");
+define("_MD_EXTGALLERY_NBALBUMS","Nb Albums");
+define("_MD_EXTGALLERY_NBPHOTOS","Nb Photos");
+define("_MD_EXTGALLERY_DESC","Description");
+define("_MD_EXTGALLERY_SEND_ECARD","Envoyer une eCarte");
+define("_MD_EXTGALLERY_EXTRA_INFO","Information supplémentaire");
+define("_MD_EXTGALLERY_NOPERM","D&eacute;sol&eacute;, vous n'avez pas le droit d'effectuer cette action");
+define("_MD_EXTGALLERY_PHOTO_TITLE","Titre");
+define("_MD_EXTGALLERY_PHOTO_WEIGHT","Poids");
+define("_MD_EXTGALLERY_RSS","Galerie RSS");
+define("_MD_EXTGALLERY_ALBUMRSS","Album RSS");
+// Album page
+define("_MD_EXTGALLERY_HITS","Vues");
+define("_MD_EXTGALLERY_COMMENTS","Commentaires");
+define("_MD_EXTGALLERY_RATING_SCORE","Résultat des votes");
+define("_MD_EXTGALLERY_INFODATE","Date :");
+// Photo page
+define("_MD_EXTGALLERY_PREVIEW","Pr&eacute;c&eacute;dent");
+define("_MD_EXTGALLERY_NEXT","Suivant");
+define("_MD_EXTGALLERY_OF","sur");
+define("_MD_EXTGALLERY_VOTE_FOR_THIS_PHOTO","Voter pour cette photo");
+define("_MD_EXTGALLERY_PHOTO_INFORMATION","Informations sur la photo");
+define("_MD_EXTGALLERY_RESOLUTION","R&eacute;solution");
+define("_MD_EXTGALLERY_PIXELS","pixels");
+define("_MD_EXTGALLERY_VIEW","Vue");
+define("_MD_EXTGALLERY_FILE_SIZE","Taille du fichier");
+define("_MD_EXTGALLERY_ADDED","Ajout&eacute;e");
+define("_MD_EXTGALLERY_SCORE","Note");
+define("_MD_EXTGALLERY_VOTES","votes");
+define("_MD_EXTGALLERY_DOWNLOAD_ORIG","T&eacute;l&eacute;charger l'original");
+define("_MD_EXTGALLERY_DOWNLOADS","téléchargements");
+define("_MD_EXTGALLERY_SENDS","envois");
+define("_MD_EXTGALLERY_ALL_PHOTO_BY","toutes les photos de");
+define("_MD_EXTGALLERY_SUBMITTER","propri&eacute;taire");
+// Send eCard page
+define("_MD_EXTGALLERY_TO","Pour");
+define("_MD_EXTGALLERY_FROM","De");
+define("_MD_EXTGALLERY_FROM_NAME","Exp&eacute;diteur (nom)");
+define("_MD_EXTGALLERY_FROM_EMAIL","Exp&eacute;diteur (courrier électronique)");
+define("_MD_EXTGALLERY_TO_NAME","Destinataire (nom)");
+define("_MD_EXTGALLERY_TO_EMAIL","Destinataire (courrier électronique)");
+define("_MD_EXTGALLERY_GREETINGS","Cordialement");
+define("_MD_EXTGALLERY_SECURITY","Test de s&eacute;curit&eacute;");
+// View eCard page and mail
+define("_MD_EXTGALLERY_CLICK_FOR_MORE","Cliquez ici pour voir plus de photos !");
+define("_MD_EXTGALLERY_ECARD_TITLE","Une carte électronique de %s pour vous");
+// Public upload page
+define("_MD_EXTGALLERY_PUBLIC_UPLOAD","Envoyer une photo dans un album publique");
+define("_MD_EXTGALLERY_PHOTO","Photo");
+define('_MD_EXTGALLERY_NOT_AN_ALBUM',"Vous devez s&eacute;lectionner un album valide");
+define('_MD_EXTGALLERY_UPLOAD_ERROR',"Erreur durant l'envoi");
+// Applet upload page
+// Lang for the photo upload applet. Available language value :
+// br, cz, de, dk, en, eo, es, fr, hr, hu, il, it, ja, nl, no, pl, pt, pt_BR, ru, se, si, sk, tr, us, zh, zh_TW
+define('_MD_EXTGALLERY_APPLET_LANG',"fr");
+// Public modify page
+define("_MD_EXTGALLERY_MODIFY_PHOTO","Modifiez cette photo");
+define("_MD_EXTGALLERY_CATEGORY","Cat&eacute;gorie");
+define("_MD_EXTGALLERY_PHOTO_UPDATED","Photo mise &agrave; jour");
+// Public rating page
+define("_MD_EXTGALLERY_VOTE_THANKS","Merci pour votre vote");
+define("_MD_EXTGALLERY_ALREADY_VOTED","Vous avez d&eacute;j&agrave; vot&eacute; pour cette photo");
+// Public send eCard page
+define("_MD_EXTGALLERY_ECARD_SENT","Votre carte électronique a &eacute;t&eacute; envoy&eacute;e");
+define("_MD_EXTGALLERY_CAPTCHA_ERROR","Le code de s&eacute;curit&eacute; est incorrect. Veuillez r&eacute;essayer.");
+// Public Upload photo page
+define("_MD_EXTGALLERY_PHOTO_UPLOADED","Photo envoy&eacute;e. Merci pour votre participation.");
+define("_MD_EXTGALLERY_PHOTO_PENDING","Votre photo doit &ecirc;tre approuv&eacute; par un administrateur. Merci pour votre participation.");
+// Public user's album
+define("_MD_EXTGALLERY_USERS_SUB_PHOTO_ALBUM"," : toutes ses photos");
+// Public Slideshow
+define("_MD_EXTGALLERY_MOREINFO","Plus");
+define("_MD_EXTGALLERY_GFIC_DOWNLOAD","Télécharger l'image d'origine"); 
+define("_MD_EXTGALLERY_GFIC_PLAY","Lire"); 
+define("_MD_EXTGALLERY_GFIC_PAUSE","Pause"); 
+define("_MD_EXTGALLERY_GFIC_NEXT","Suivant"); 
+define("_MD_EXTGALLERY_GFIC_PREVIOUS","Précédent"); 
+define("_MD_EXTGALLERY_GFIC_NEXTP","Suiv &rsaquo;"); 
+define("_MD_EXTGALLERY_GFIC_PREVIOUSP","&lsaquo; Préc"); 
+// Public bookmarks
+define("_MD_EXTGALLERY_BOOKMARK_ME", "Recommandez-moi");
+define("_MD_EXTGALLERY_BOOKMARK_TO_BLINKLIST", "Recommander sur Blinklist");
+define("_MD_EXTGALLERY_BOOKMARK_TO_DELICIOUS", "Recommander sur del.icio.us");
+define("_MD_EXTGALLERY_BOOKMARK_TO_DIGG", "Recommander sur Digg");
+define("_MD_EXTGALLERY_BOOKMARK_TO_FARK", "Recommander sur Fark");
+define("_MD_EXTGALLERY_BOOKMARK_TO_FURL", "Recommander sur Furl");
+define("_MD_EXTGALLERY_BOOKMARK_TO_NEWSVINE", "Recommander sur Newsvine");
+define("_MD_EXTGALLERY_BOOKMARK_TO_REDDIT", "Recommander sur Reddit");
+define("_MD_EXTGALLERY_BOOKMARK_TO_SIMPY", "Recommander sur Simpy");
+define("_MD_EXTGALLERY_BOOKMARK_TO_SPURL", "Recommander sur Spurl");
+define("_MD_EXTGALLERY_BOOKMARK_TO_YAHOO", "Recommander sur to Yahoo");
+define("_MD_EXTGALLERY_BOOKMARK_TO_FACEBOOK", "Recommander sur Facebook");
+define("_MD_EXTGALLERY_BOOKMARK_TO_TWITTER", "Recommander sur Twitter");
+define("_MD_EXTGALLERY_BOOKMARK_TO_SCRIPSTYLE", "Recommander sur Scripstyle");
+define("_MD_EXTGALLERY_BOOKMARK_TO_STUMBLE", "Recommander sur Stumble");
+define("_MD_EXTGALLERY_BOOKMARK_TO_TECHNORATI", "Recommander sur Technorati");
+define("_MD_EXTGALLERY_BOOKMARK_TO_MIXX", "Recommander sur Mixx");
+define("_MD_EXTGALLERY_BOOKMARK_TO_MYSPACE", "Recommander sur Myspace");
+define("_MD_EXTGALLERY_BOOKMARK_TO_DESIGNFLOAT", "Recommander sur Designfloat");
+define("_MD_EXTGALLERY_BOOKMARK_TO_BALATARIN", "Recommander sur Balatarin");
+define("_MD_EXTGALLERY_BOOKMARK_TO_GOOGLEPLUS", "Recommander sur Google Plus");
+define("_MD_EXTGALLERY_BOOKMARK_TO_GOOGLEREADER", "Recommander sur Google Reader");
+define("_MD_EXTGALLERY_BOOKMARK_TO_GOOGLEBOOKMARKS", "Recommander sur Google Bookmarks");
+// Public order and sort
+define("_MD_EXTGALLERY_ORDERBY","Ordonner par : ");
+define("_MD_EXTGALLERY_ORDERBY_DATEASC","Date, nouvelles photos en premier");
+define("_MD_EXTGALLERY_ORDERBY_DATEDESC","Date, anciennes photos en premier");
+define("_MD_EXTGALLERY_ORDERBY_TITREASC","Nom, A à Z");
+define("_MD_EXTGALLERY_ORDERBY_TITREDESC","Nom, Z à A");
+define("_MD_EXTGALLERY_ORDERBY_HITSASC","Vues, les plus populaires en premier");
+define("_MD_EXTGALLERY_ORDERBY_HITSDESC","Vues, les moins populaires en premier");
+define("_MD_EXTGALLERY_ORDERBY_NOTEASC","Vote, les mieux notées en premier");
+define("_MD_EXTGALLERY_ORDERBY_NOTEDESC","Vote, les moins bien notées en premier");
+define("_MD_EXTGALLERY_SORTDATE","Date");
+define("_MD_EXTGALLERY_SORTDATEASC","Anciennes photos en premier");
+define("_MD_EXTGALLERY_SORTDATEDESC","Nouvelles photos en premier");
+define("_MD_EXTGALLERY_SORTNAME","Nom");
+define("_MD_EXTGALLERY_SORTNAMEASC","Nom, A à Z");
+define("_MD_EXTGALLERY_SORTNAMEDESC","Nom, Z à A");
+define("_MD_EXTGALLERY_SORTHITS","Vues");
+define("_MD_EXTGALLERY_SORTHITSASC","Moins populaires en premier");
+define("_MD_EXTGALLERY_SORTHITSDESC","Les plus populaires en premier");
+define("_MD_EXTGALLERY_SORTNOTE","Note");
+define("_MD_EXTGALLERY_SORTNOTEASC","Moins bien notées en premier");
+define("_MD_EXTGALLERY_SORTNOTEDESC","Mieux notées en premier");
+// Traduction par Cesag pour frxoops.org le 11 Août 2012.
+?>
