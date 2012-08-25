566723772b7a5fbb53a4616195c275650b4ebd5c
renetjuh <renetjuh@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-08-25 18:29:14 +0000

    Upload NL language files mastop_go2 1.01

---
+++ XoopsLanguages/dutch/modules/mastop_go2/mastop_go2 1.01/nederlands/blocks.php
@@ -0,0 +1,22 @@
+<?php
+### =============================================================
+### Mastop InfoDigital - Paixão por Internet
+### =============================================================
+### Arquivo de Tradução em inglês para Informações de Blocos
+### =============================================================
+### Developer: Fernando Santos (topet05), fernando@mastop.com.br
+### Copyright: Mastop InfoDigital © 2003-2007
+### -------------------------------------------------------------
+### www.mastop.com.br
+### =============================================================
+### $Id: blocks.php,v 1.1 2007/05/01 21:11:11 topet05 Exp $
+### =============================================================
+define("MGO_BLO_MODDIR", "mastop_go2");
+define("MGO_BLO_SHOW_SECTION", "Sectie weergeven");
+define("MGO_BLO_ALTURA", "Hoogte van het blok (pixels)");
+define("MGO_BLO_SETAS", "Navigatiepijlen weergeven?");
+define("MGO_BLO_BARRA", "Tekstbar weergeven?");
+define("MGO_BLO_DELAY", "Transitietijd tussen spotlights (seconden)");
+define("MGO_BLO_BGCOLOR", "Tekstbarkleur");
+define("MGO_BLO_TXTCOLOR", "Tekstkleur");
+define("MGO_BLO_TRANSP", "Tekstbar Transparantie");
---
+++ XoopsLanguages/dutch/modules/mastop_go2/mastop_go2 1.01/nederlands/main.php
@@ -0,0 +1,31 @@
+<?php
+### =============================================================
+### Mastop InfoDigital - Paixão por Internet
+### =============================================================
+### Arquivo de Tradução  em inglês para a Home do Módulo
+### =============================================================
+### Developer: Fernando Santos (topet05), fernando@mastop.com.br
+### Copyright: Mastop InfoDigital © 2003-2007
+### -------------------------------------------------------------
+### www.mastop.com.br
+### =============================================================
+### $Id: main.php,v 1.2 2007/05/02 00:27:32 topet05 Exp $
+### =============================================================
+define("MGO_MAI_DESC", "Promote us! Use the form below to create the code to put in your site.");
+define("MGO_MAI_FORM_TITLE", "Code Generator");
+define("MGO_MAI_COPY_PASTE", "Kopieer de onderstaande code en plak deze in je site om de bovenstaande spotlights weer te geven.");
+define("MGO_MAI_SECTION", "Sectie");
+define("MGO_MAI_WIDTH", "Schijnwerper breedte");
+define("MGO_MAI_HEIGHT", "Schijnwerper hoogte (pixels)");
+define("MGO_MAI_SETAS", "Navigatiepijlen weergeven?");
+define("MGO_MAI_BARRA", "Show Text Bar?");
+define("MGO_MAI_DELAY", "Transitie tijd tussen afbeeldingen<br /> (seconden)");
+define("MGO_MAI_BARCOLOR", "Text Bar kleur");
+define("MGO_MAI_TEXTCOLOR", "Tekstkleur");
+define("MGO_MAI_TRANSP", "Text Bar Transparantie");
+define("MGO_MAI_GENERATE", "Genereer!");
+define("MGO_MAI_ALIGN", "Uitlijnen");
+define("MGO_MAI_ALIGN_MIDDLE", "Midden");
+define("MGO_MAI_ALIGN_LEFT", "Links");
+define("MGO_MAI_ALIGN_RIGHT", "Rechts");
+define("MGO_MAI_SEC_404", "Er is geen actieve spoplight in sectie %s.");
