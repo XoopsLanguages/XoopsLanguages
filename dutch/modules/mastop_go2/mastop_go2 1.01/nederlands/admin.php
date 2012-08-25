9afaf036193e567a60621b00ffedaba8fea13d8b
renetjuh <renetjuh@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-08-25 18:29:14 +0000

    Upload NL language files mastop_go2 1.01

---
+++ dutch/modules/mastop_go2/mastop_go2 1.01/nederlands/admin.php
@@ -0,0 +1,95 @@
+<?php
+### =============================================================
+### Mastop InfoDigital - Paixão por Internet
+### =============================================================
+### Arquivo de Tradução em ingls para Administração
+### =============================================================
+### Developer: Fernando Santos (topet05), fernando@mastop.com.br
+### Copyright: Mastop InfoDigital © 2003-2007
+### -------------------------------------------------------------
+### www.mastop.com.br
+### =============================================================
+### $Id: admin.php,v 1.1 2007/05/01 21:11:11 topet05 Exp $
+### =============================================================
+// Geral
+define("MGO_ADM_SEC", "Secties");
+define("MGO_ADM_GO2", "Spotlights");
+define("MGO_ADM_GER", "Beheer");
+define("MGO_ADM_ID", "ID");
+define("MGO_ADM_NOME", "Naam");
+define("MGO_ADM_IMAGEM", "Afbeelding");
+define("MGO_ADM_ATIVO", "Actief");
+define("MGO_ADM_SHOWHIDEMENU", "Toon/verberg menu");
+define("MGO_ADM_PORPAGINA", "records per pagina");
+define("MGO_ADM_FILTROS", "Filters");
+define("MGO_ADM_ACAO", "Actie");
+define("MGO_ADM_SEMRESULT", "Geen record gevonden!");
+define("MGO_ADM_GRP_ERR_SEL", "Selecteer een record!");
+define("MGO_ADM_GRP_ACTION", "Group acties: ");
+define("MGO_ADM_GRP_DEL", "Verwijder selectie");
+define("MGO_ADM_GRP_DEL_SURE", "Zeker weten om de geselecteerde records te verwijderen?");
+define("MGO_ADM_SELECIONE", "Selecteren");
+define("MGO_ADM_SEARCH", "Zoeken");
+define("MGO_ADM_EXIBIR", "Weergeven ");
+define("MGO_ADM_EXIBINDO", "Weergeven van %u tot %u of <b>%u</b> records.");
+define("MGO_ADM_SUCESS_ADD", "Informatie succesvol toegevoegd");
+define("MGO_ADM_SUCESS_UPD", "Informatie bijgewerkt");
+define("MGO_ADM_SUCESS_DEL", "Informatie succesvol verwijderd");
+define("MGO_ADM_SUCESS2", "Informatie bijgewerkt");
+define("MGO_ADM_403", "Het is niet toegestaan om deze pagina te bekijken!");
+define("MGO_ADM_404", "ERROR: Record niet gevonden!");
+define("MGO_ADM_DB_ERRO", "ERROR: Opslaan in de database mislukt!");
+define("MGO_ADM_BLOCKS", "Blokken");
+define("MGO_ADM_IMAGEMS", "Afbeeldingen");
+define("MGO_ADM_GRUPOS", "Groepen");
+define("MGO_ADM_SECTION", "Sectie");
+define("MGO_ADM_DSTAC", "Spotlight");
+define("MGO_ADM_FEEDBACK", "Feedback");
+define("MGO_ADM_FEEDBACKN", "Feedback versturen");
+define("MGO_ADM_CHKVERSION", "Controleren voor update");
+
+
+// sec.php
+define("MGO_ADM_SEC_TITULO", "Sectie beheer");
+define("MGO_ADM_SEC_NEW", "Sectie toevoegen");
+define("MGO_ADM_SEC_EDIT", "Sectie bewerken");
+define("MGO_ADM_SEC_CONFIRMA_DEL", "Zeker weteom om sectie <b># %u</b> - %s  te verwijderen gezamelijk met alle bijbehorende spotlights?");
+
+// go2.php
+define("MGO_ADM_GO2_30_NOME", "Tekst");
+define("MGO_ADM_GO2_30_LINK", "URL");
+define("MGO_ADM_GO2_10_ACESSOS", "Klikken");
+define("MGO_ADM_GO2_11_TARGET", "Open URL in");
+define("MGO_ADM_GO2_11_TARGET_0", "Zelfde venster");
+define("MGO_ADM_GO2_11_TARGET_1", "Nieuw venster");
+define("MGO_ADM_GO2_TITULO", "Spotlights Administratie");
+define("MGO_ADM_GO2_NEW", "Spotlight toevoegen");
+define("MGO_ADM_GO2_EDIT", "Spotlight bewerken");
+define("MGO_ADM_GO2_CONFIRMA_DEL", "Zeker weten om spotlight '# %u - %s' te verwijderen?");
+define("MGO_ADM_GO2_ATIVA_SEL", "Selectie activeren");
+define("MGO_ADM_GO2_DESATIVA_SEL", "Selectie de-activeren");
+define("MGO_ADM_GO2_ZERA_COUNT", "Klikken legen");
+
+// feedback.php
+define("MGO_ADM_YNAME", "Je Naam");
+define("MGO_ADM_YEMAIL", "Je E-mail");
+define("MGO_ADM_YSITE", "website");
+define("MGO_ADM_FEEDTYPE", "Feedbacktype<br /><span style='font-size:10px; font-weight:normal'>* Verklaringen worden geplaatst op de Mastop Go2 hoofdpagina</span>");
+define("MGO_ADM_TBUGS", "Fout");
+define("MGO_ADM_TESTIMONIAL", "Verklaring");
+define("MGO_ADM_TSUGGESTION", "Suggesties");
+define("MGO_ADM_TFEATURES", "Middelen");
+define("MGO_ADM_TOTHERS", "Anders");
+define("MGO_ADM_DESC", "Omschrijving");
+define("MGO_ADM_FEEDSUCCESS", "Reactie succesvol verstuurd!");
+
+//1.01
+define("_AM_TOPPAGE", "Toppagina");
+define("_AM_ALLPAGES", "Alle pagina's");
+define("_AM_BADMIN", "Pagina");
+define("_AM_TITLE", "Titel");
+define("_AM_SIDE", "Zijde");
+define("_AM_WEIGHT", "Gewicht");
+define("_AM_VISIBLE", "Zichtbaar");
+define("_AM_VISIBLEIN", "Zichtbaar in");
+define("_AM_ACTION", "Actie");
---
+++ dutch/modules/mastop_go2/mastop_go2 1.01/nederlands/modinfo.php
@@ -0,0 +1,38 @@
+<?php
+### =============================================================
+### Mastop InfoDigital - Paixão por Internet
+### =============================================================
+### Arquivo de Tradução em inglês para Informações do Módulo
+### =============================================================
+### Developer: Fernando Santos (topet05), fernando@mastop.com.br
+### Copyright: Mastop InfoDigital © 2003-2007
+### -------------------------------------------------------------
+### www.mastop.com.br
+### =============================================================
+### $Id: modinfo.php,v 1.1 2007/05/01 21:11:11 topet05 Exp $
+### =============================================================
+// xoops_version.php
+define("MGO_MOD_NOME", "Mastop Go2");
+define("MGO_MOD_DESC", "A Spotlights Module");
+define("MGO_MOD_DIR", "mastop_go2");
+define("MGO_MOD_TABELA0", "mgo_sec_section");
+define("MGO_MOD_TABELA1", "mgo_go2_go2");
+define("MGO_MOD_BLOCOS", "Blocks");
+
+define("MGO_MOD_BLOCO1", "Spotlights");
+define("MGO_MOD_BLOCO1_DESC", "Blok om spotlights weer te geven");
+define("MGO_MOD_BLOCO1_FILE", "mgo_go2.bloco.php");
+define("MGO_MOD_BLOCO1_SHOW", "mgo_go2_exibe");
+define("MGO_MOD_BLOCO1_EDIT", "mgo_go2_edita");
+define("MGO_MOD_BLOCO1_TEMPLATE", "mgo_go2.block.tpl.html");
+
+define("MGO_MOD_DSTAC_IMG", "Spotlights Afbeeldingen");
+define("MGO_MOD_DSTAC_IMG_DES", "Selecteer de afbeeldingenbibliotheek waar de spotlight afbeeldingen worden bewaard<br />
+Selecteer door gebruik te maken van <B>CTRL</B>.<br />
+<p style='font-weight:bold; color:red; width: 550px'>Wanneer er een afbeeldingsbibliotheek is toegevoegd na de installatie van deze module, update het dan zodat de categorie ook wordt weergegeven.</p>");
+// admin/menu.php
+define('MGO_MOD_MENU_SEC','Secties');
+define('MGO_MOD_MENU_GO2','Spotlights');
+define("MGO_ADM_HOME", "Home");
+define("MGO_ADM_ABOUT", "Over");
+?>
