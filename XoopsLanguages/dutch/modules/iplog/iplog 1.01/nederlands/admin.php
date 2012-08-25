aaa3670f0c44f4010889af148c2ac5a64019a4d7
renetjuh <renetjuh@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-08-25 18:26:06 +0000

    Upload NL language files iplog 1.01

---
+++ XoopsLanguages/dutch/modules/iplog/iplog 1.01/nederlands/admin.php
@@ -0,0 +1,44 @@
+<?php
+// $Id: admin.php 4751 2010-05-01 15:35:45Z trabis $
+// _LANGCODE: nl
+// _CHARSET : UTF-8
+// Translator: NLXOOPS, Flipse 
+
+define("_AM_IPLOG_SECONDS", "Seconden");
+
+define("_AM_IPLOG_TH_IP_ID", "IP ID");
+define("_AM_IPLOG_TH_UNAME", "Gebruikersnaam");
+define("_AM_IPLOG_TH_IP", "IP");
+define("_AM_IPLOG_TH_COUNTRY_CODE", "Land Code");
+define("_AM_IPLOG_TH_NAME", "IP Land");
+define("_AM_IPLOG_TH_REGION", "IP Regio");
+define("_AM_IPLOG_TH_CONTINENT", "IP Continent");
+define("_AM_IPLOG_TH_NETWORK_ADDY", "NetBIOS");
+define("_AM_IPLOG_TH_PROXY_IP", "Proxy IP");
+define("_AM_IPLOG_TH_USERAGENT", "User Agent");
+define("_AM_IPLOG_TH_START", "Start Sessie");
+define("_AM_IPLOG_TH_END", "Einde Sessie");
+define("_AM_IPLOG_TH_ONLINE", "Tijd Online");
+define("_AM_IPLOG_TH_ACTIONS", "Aktie");
+
+define("_AM_IPLOG_LOG_H1", "Bezoekers IP Logfile");
+define("_AM_IPLOG_LOG_P", "Deze logfile toont de recente bezoekers, het door hen gebruikte IP adres, Netbios en land/ regio/ continent van herkomst");
+
+//Version 1.72
+//Dashboard
+define('_AM_IPLOG_ADMIN_COUNTS_BY_COUNTRY', 'Gebruikers sessies per Land');
+define('_AM_IPLOG_ADMIN_COUNTS_BY_REGION', 'Gebruikers sessies per Regio');
+define('_AM_IPLOG_ADMIN_COUNTS_BY_CONTINENT', 'Gebruikers sessies per Continent');
+define('_AM_IPLOG_ADMIN_SUM_BY_COUNTRY', 'Totaal minuten per Land');
+define('_AM_IPLOG_ADMIN_SUM_BY_REGION', 'Totaal minuten per Regio');
+define('_AM_IPLOG_ADMIN_SUM_BY_CONTINENT', 'Totaal minuten per Continent');
+define('_AM_IPLOG_ADMIN_AVG_BY_COUNTRY', 'Minuten gemiddeld per Land');
+define('_AM_IPLOG_ADMIN_AVG_BY_REGION', 'Minuten gemiddeld per Regio');
+define('_AM_IPLOG_ADMIN_AVG_BY_CONTINENT', 'Minuten gemiddeld per Continent');
+
+//About
+define('_AM_IPLOG_ABOUT_MAKEDONATE', 'Doneer een bijdrage aan Chronolabs co-op, de ontwikkelaar van deze module');
+
+define('_AM_MSG_LOG_DELETE', 'Wilt u dit adres echt verwijderen?');
+define('_AM_MSG_LOG_DELETED', 'IP adres succesvol verwijderd!');
+
