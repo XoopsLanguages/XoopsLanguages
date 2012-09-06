e58559f9c42935f19ef60e171917c818afb458ff
Michael Albertsen <culex@culex.dk>
2012-09-06 16:47:41 +0000

    Dk language for default theme

---
+++ danish/core/2.6.0 Alpha-1/htdocs/modules/system/themes/default/language/danish/admin.php
@@ -0,0 +1,80 @@
+<?Php
+// $ Id $
+
+// Hurtig adgang menu
+define("_DEFAULT_QUICK", "Hurtig adgang");
+define("_DEFAULT_CPHOME", 'Kontrolpanel');
+define("_DEFAULT_NEWS", "Xoops Nyheder");
+
+// Moduler menuen
+define("_DEFAULT_MANAGE", "Administrer");
+
+// Preferences Menu
+define("_DEFAULT_SITEPREF", "Opsætning");
+define("_DEFAULT_SYSOPTIONS", "System indstillinger");
+define("_DEFAULT_GENERAL", "Generelle indstillinger");
+define("_DEFAULT_USERSETTINGS", "Bruger Info-indstillinger");
+define("_DEFAULT_METAFOOTER", "Metatags og sidefod");
+define("_DEFAULT_CENSOR", "Censurereringsindstillinger");
+define("_DEFAULT_SEARCH", "Search indstillinger");
+define("_DEFAULT_MAILER", "Mail indstillinger");
+define("_DEFAULT_AUTHENTICATION", "Sikkerhedsindstillinger");
+define("_DEFAULT_MODULESETTINGS", "System Modulindstillinger");
+
+
+// Control Panel
+define("_DEFAULT_DASHBORD", "Dashboard");
+define("_DEFAULT_DASHBORD_DESC", "kontroller og administrere dit websted");
+define("_DEFAULT_SYSTEM_TOOL", "Systemværktøjer");
+define("_DEFAULT_SYSTEM_EXTENSION", "System Udvidelser");
+define("_DEFAULT_INSTALLEDMODULES", "Installerede moduler");
+define("_DEFAULT_INSTALLEDEXTENSIONS", "Installerede Udvidelser");
+define("_DEFAULT_NO_MODULE", "Der er ingen moduler installeret.");
+
+define("_DEFAULT_XOOPSTHEMES", "XOOPS Temaer");
+define("_DEFAULT_XOOPSMODULES", "XOOPS Modules");
+define("_DEFAULT_INTERESTSITES", "Links");
+define("_DEFAULT_LOCALSUPPORT", "Local Support");
+
+define("_DEFAULT_WEBSITE", "Fællesskabets Site");
+define("_DEFAULT_SOURCEFORGE", "Sourceforge");
+
+define("_DEFAULT_VERSION", "Version");
+define("_DEFAULT_VERSION_XOOPS", "XOOPS Version");
+define("_DEFAULT_VERSION_PHP", "PHP Version");
+define("_DEFAULT_VERSION_MYSQL", "MySQL version");
+define("_DEFAULT_Server_API", "Server API");
+define("_DEFAULT_OS", "OS");
+define("_DEFAULT_ABOUT", "Om XOOPS");
+define("_DEFAULT_ABOUT_TEXT", 'Læs <a href="http://www.xoops.org/modules/wfchannel/" rel="external"> Alt om XOOPS </a> side for flere detaljer.');
+define("_DEFAULT_XOOPS_LINKS", "XOOPS Links");
+
+define("_DEFAULT_XOOPSPROJECT", "XOOPS Project");
+define("_DEFAULT_XOOPSCORE", "XOOPS Core");
+define("_DEFAULT_XOOPSTHEME", "XOOPS Temaer");
+define("_DEFAULT_XOOPSWIKI", "XOOPS Wiki");
+define("_DEFAULT_XOOPSBOOKS", "XOOPS Books");
+define("_DEFAULT_NEWMODULE", "nye moduler");
+define("_DEFAULT_XOOPSFAQ", "XOOPS FAQ");
+define("_DEFAULT_CODESVN", "Code SVN");
+define("_DEFAULT_REPORTBUG", "Rapport Bug");
+
+
+
+// Tilføj til stilarter navn
+define("_DEFAULT_SILVER", "Silver");
+define("_DEFAULT_DARK", "Dark");
+define("_DEFAULT_ORANGE", "Orange");
+
+// Tilføj 10 des
+define("_DEFAULT_RSS", "RSS Feed");
+define("_DEFAULT_ADMINISTRATION", "XOOPS Administration");
+define("_DEFAULT_UPTOP", "Up Top");
+
+// Tilføj hjælp
+define("_DEFAULT_HELP_1", "Hvordan kan jeg lave indhold?");
+define("_DEFAULT_HELP_DESC_1", "For at lave indhold skal du først installere nogle moduler. Efter dette, kan du udgive dine artikler. For at installere moduler skal du bruge <a href='modules/system/help.php?mid=1&page=modulesadmin'> denne artikel </a> og for at downloade moduler gå til: <a href='http://www.xoops.org' rel='external'> XOOPS websted </a> ");
+define("_DEFAULT_HELP_2", "Hvad er en Block?");
+define("_DEFAULT_HELP_DESC_2", "Blokke er diskrete sektioner af indhold, der kan oprettes og konfigureres i admin interface. Brugerdefinerede blokke kan oprettes og er typisk bestående af tekst, grafik og billeder. Indholdet i disse blokke kan være formateret individuelt eller arve stedet formatering (<a href='modules/system/help.php?mid=1&page=blocksadmin'> mere ... </ a>) ").;
+define("_DEFAULT_HELP_3", "Hvordan kan jeg finde mere hjælp?");
+define("_DEFAULT_HELP_DESC_3", "Hvis du har brug for mere hjælp og information til at bruge XOOPS kan du bruge <a href='modules/system/help.php'> hjælpesider </a> i systemet modul eller brug <a href ='http://www.xoops.org' rel = 'eksterne'> XOOPS Lokale support sites </a>");
