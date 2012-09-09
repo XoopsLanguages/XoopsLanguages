0ee112564d45ecdb8a3f8df1e7dce36b4d243d57
djculex <djculex@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-09-09 18:20:46 +0000

    missing define

--- XoopsLanguages/danish/core/2.6.0 Alpha-1/htdocs/modules/system/language/danish/admin/images.php
+++ XoopsLanguages/danish/core/2.6.0 Alpha-1/htdocs/modules/system/language/danish/admin/images.php
@@ -2,9 +2,11 @@
 /**
  * @copyright   The XOOPS Project http://sourceforge.net/projects/xoops/
  * @license     http://www.fsf.org/copyleft/gpl.html GNU public license
- * _LANGCODE    en
+ * _LANGCODE    da
  * _CHARSET     UTF-8
  * @version     $Id: images.php 5014 2010-08-23 08:08:45Z forxoops $
+ * @translation Michael Albertsen (culex)
+
  */
 
 // Navigation
@@ -14,6 +16,8 @@ define("_AM_SYSTEM_IMAGES_IMGLIST","Billed liste");
 
 // Main
 define("_AM_SYSTEM_IMAGES_ADDCAT","Tilføj kategori");
+define("_AM_SYSTEM_IMAGES_EDITCAT","Rediger kategori");
+
 define("_AM_SYSTEM_IMAGES_ADDIMG","Tilføj billede");
 define("_AM_SYSTEM_IMAGES_EDITIMG","Redigér billede");
 define("_AM_SYSTEM_IMAGES_CATLIST","Kategori liste");
@@ -31,12 +35,15 @@ define("_AM_SYSTEM_IMAGES_ASFILE","Opbevares som filer (i uploads mappe) <br />"
 define("_AM_SYSTEM_IMAGES_IMGCATNAME","Kategori navn:");
 define("_AM_SYSTEM_IMAGES_IMGCATRGRP","Vælg grupper til billed vedligeholdelse: <br /><br />Dette er grupper der har rettighed til at benytte billed værktøjet til at vælge billeder, men ikke uploade. Webmaster har automatisk denne adgang.");
 define("_AM_SYSTEM_IMAGES_IMGCATWGRP","Vælg grupper med tilladelse til at uploade billeder: <br /><br />Typisk for moderatorer og administrator");
+
+
 define('_AM_SYSTEM_IMAGES_IMGCATDISPLAY',"Vis denne kategori?" );
 define("_AM_SYSTEM_IMAGES_IMGCATSTRTYPE","Billederne er uploadet til:");
 define("_AM_SYSTEM_IMAGES_STRTYOPENG","Dette kan ikke ændres bagefter!");
 define("_AM_SYSTEM_IMAGES_IMGCATWEIGHT","Visnings rækkefølge i billed værktøjet:");
 define("_AM_SYSTEM_IMAGES_OFF","Vis i billede værktøj");
 define("_AM_SYSTEM_IMAGES_ON","Vises ikke i billede værktøjet");
+define("_AM_SYSTEM_IMAGES_URL","Vis billed URL");
 
 // Messages
 define("_AM_SYSTEM_IMAGES_RUDELIMG","Er du sikker på at du vil slette denne billeder fil?");
@@ -45,9 +52,9 @@ define("_AM_SYSTEM_IMAGES_RUDELIMGCAT","Er du sikker på at du vil slette denne 
 define("_AM_SYSTEM_IMAGES_FAILDEL","Det mislykkedes at slette billedet %s fra databasen");
 define("_AM_SYSTEM_IMAGES_FAILDELCAT","Kunne ikke slette billed-kategori %s fra databasen");
 define("_AM_SYSTEM_IMAGES_FAILUNLINK","Det mislykkedes at slette billedet %s fra serverens bibliotek");
+define("_AM_SYSTEM_IMAGES_SCATDELNG","Kan ikke slette denne kategori");
 
 // Tips
 define("_AM_SYSTEM_IMAGES_TIPS","<ul><li> Administrer kategorier af billeder og brugernes tilladelser </li></ul>");
 
 ?>
-<?php // Translation done by xtransam & anderssk - 2010-10-19 07:12 ?>
