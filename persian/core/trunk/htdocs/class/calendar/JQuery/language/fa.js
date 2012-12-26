181fdf160454d271ff4ea2f4206314575383bae5
irmtfan <irmtfan@c17157d5-08f2-41e7-a131-df055f6a0da3>
2012-12-26 05:46:29 +0000

    add xoops 2.5.5 persian language package

--- persian/core/trunk/htdocs/class/calendar/JQuery/language/fa.js
+++
@@ -1,8 +0,0 @@
-/* French initialisation for the jQuery UI date picker plugin. */
-/* Written by Keith Wood (kbwood@virginbroadband.com.au) and Stéphane Nahmani (sholby@sholby.net). */
-jQuery(function($){
-	$.datepicker.regional['fa'] = {
-		dateFormat: 'yy-mm-dd', firstDay: 6,
-		isRTL: true};
-	$.datepicker.setDefaults($.datepicker.regional['fa']);
-});
