84880a7c0a3d0818189f6efd8b0643decc7d1099
irmtfan <irmtfan@c17157d5-08f2-41e7-a131-df055f6a0da3>
2013-04-29 06:08:38 +0000

    - bug fix: http://sourceforge.net/p/xoops/bugs/1226/ disable CaricFoto js in class/textsanitizer/image/config.php - bug fix: http://sourceforge.net/p/xoops/bugs/1225/ xoopsUserTheme is not regenerate if remember me was checked in login in include/common.php - bug fix: http://sourceforge.net/p/xoops/bugs/1261/ footer is not loaded after header in header.php - bug fix: http://sourceforge.net/p/xoops/bugs/1220/ xoops_getConfigOption('language') is wrongly return in include/functions.php around line 926 - show real name instead of uname in modules/system/blocks/system_blocks.php - change default savecopy to true in modules/pm/pmlite.php - increasing the uname size from 25 to 50 in kernel/user.php - add EmailLogin hack in include/checklogin.php

---
+++ persian/core/trunk/htdocs/include/checklogin.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * XOOPS authentication/authorization
+ *
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright       The XOOPS Project http://sourceforge.net/projects/xoops/
+ * @license         GNU GPL 2 (http://www.gnu.org/licenses/old-licenses/gpl-2.0.html)
+ * @package         core
+ * @since           2.0.0
+ * @version         $Id: checklogin.php 10931 2013-01-27 18:17:51Z dhcst $
+ * @todo            Will be refactored
+ */
+defined('XOOPS_ROOT_PATH') or die('Restricted access');
+
+xoops_loadLanguage('user');
+
+$uname = !isset($_POST['uname']) ? '' : trim($_POST['uname']);
+$pass = !isset($_POST['pass']) ? '' : trim($_POST['pass']);
+if ($uname == '' || $pass == '') {
+    redirect_header(XOOPS_URL.'/user.php', 1, _US_INCORRECTLOGIN);
+    exit();
+}
+
+$member_handler =& xoops_gethandler('member');
+$myts =& MyTextsanitizer::getInstance();
+
+include_once $GLOBALS['xoops']->path('class/auth/authfactory.php');
+
+xoops_loadLanguage('auth');
+// EmailLogin begin irmtfan
+if( strstr( $_POST['uname'] , '@' ) ) { @list( $uname ) = $xoopsDB->fetchRow( $xoopsDB->query( "SELECT uname FROM ".$xoopsDB->prefix('users')." WHERE email='".$myts->addSlashes($_POST['uname'])."'" ) ) ; } 
+// EmailLogin end irmtfan
+// Place here the Auth class calls //**** BEGIN AUTH ****
+
+$xoopsAuth =& XoopsAuthFactory::getAuthConnection($myts->addSlashes($uname));
+$user = $xoopsAuth->authenticate($myts->addSlashes($uname), $myts->addSlashes($pass));
+
+if (false != $user) {
+    if (0 == $user->getVar('level')) {
+        redirect_header(XOOPS_URL.'/index.php', 5, _US_NOACTTPADM);
+        exit();
+    }
+    if ($xoopsConfig['closesite'] == 1) {
+        $allowed = false;
+        foreach ($user->getGroups() as $group) {
+            if (in_array($group, $xoopsConfig['closesite_okgrp']) || XOOPS_GROUP_ADMIN == $group) {
+                $allowed = true;
+                break;
+            }
+        }
+        if (!$allowed) {
+            redirect_header(XOOPS_URL.'/index.php', 1, _NOPERM);
+            exit();
+        }
+    }
+    $user->setVar('last_login', time());
+    if (!$member_handler->insertUser($user)) {
+    }
+    // Regenrate a new session id and destroy old session
+    $GLOBALS["sess_handler"]->regenerate_id(true);
+    $_SESSION = array();
+    $_SESSION['xoopsUserId'] = $user->getVar('uid');
+    $_SESSION['xoopsUserGroups'] = $user->getGroups();
+    $user_theme = $user->getVar('theme');
+    if (in_array($user_theme, $xoopsConfig['theme_set_allowed'])) {
+        $_SESSION['xoopsUserTheme'] = $user_theme;
+    }
+
+    // Set cookie for rememberme
+    if (!empty($xoopsConfig['usercookie'])) {
+        if (!empty($_POST["rememberme"])) {
+            setcookie($xoopsConfig['usercookie'], $_SESSION['xoopsUserId'] . '{-}' . md5($user->getVar('pass') . XOOPS_DB_NAME . XOOPS_DB_PASS . XOOPS_DB_PREFIX), time() + 31536000, '/', XOOPS_COOKIE_DOMAIN, 0);
+        } else {
+            setcookie($xoopsConfig['usercookie'], 0, -1, '/', XOOPS_COOKIE_DOMAIN, 0);
+        }
+    }
+
+    if (!empty($_POST['xoops_redirect']) && !strpos($_POST['xoops_redirect'], 'register')) {
+        $xoops_redirect = trim(rawurldecode($_POST['xoops_redirect']));
+        $parsed = parse_url(XOOPS_URL);
+        $url = isset($parsed['scheme']) ? $parsed['scheme'].'://' : 'http://';
+        if (isset( $parsed['host'])) {
+            $url .= $parsed['host'];
+            if (isset( $parsed['port'])) {
+                $url .= ':' . $parsed['port'];
+            }
+        } else {
+            $url .= $_SERVER['HTTP_HOST'];
+        }
+        if (@$parsed['path']) {
+            if (strncmp($parsed['path'], $xoops_redirect, strlen( $parsed['path']))) {
+                $url .= $parsed['path'];
+            }
+        }
+        $url .= $xoops_redirect;
+    } else {
+        $url = XOOPS_URL . '/index.php';
+    }
+
+    // RMV-NOTIFY
+    // Perform some maintenance of notification records
+    $notification_handler =& xoops_gethandler('notification');
+    $notification_handler->doLoginMaintenance($user->getVar('uid'));
+
+    redirect_header($url, 1, sprintf(_US_LOGGINGU, $user->getVar('uname')), false);
+} else if (empty($_POST['xoops_redirect'])) {
+    redirect_header(XOOPS_URL . '/user.php', 5, $xoopsAuth->getHtmlErrors());
+} else {
+    redirect_header(XOOPS_URL . '/user.php?xoops_redirect=' . urlencode(trim($_POST['xoops_redirect'])), 5, $xoopsAuth->getHtmlErrors(), false);
+}
+exit();
+
+?>
