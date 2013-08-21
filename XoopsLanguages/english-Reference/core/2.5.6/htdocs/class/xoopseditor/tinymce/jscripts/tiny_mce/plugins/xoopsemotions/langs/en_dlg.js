9141f2223439c7d306e30e78d54eb68148622958
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2013-08-21 07:38:42 +0000

    Updating 2.5.6 English reference for translators

---
+++ XoopsLanguages/english-Reference/core/2.5.6/htdocs/class/xoopseditor/tinymce/jscripts/tiny_mce/plugins/xoopsemotions/langs/en_dlg.js
@@ -0,0 +1,10 @@
+/**
+ * @version     $Id$
+ */
+
+tinyMCE.addI18n('en.xoopsemotions_dlg',{
+    title : 'Insert Xoops Emoticons',
+    tab_emotionsbrowser: 'Emoticons',
+    tab_emotionsadmin: 'Add Emoticons',
+    error_noemotions: 'No emotions in database !!!'
+});
---
+++ XoopsLanguages/english-Reference/core/2.5.6/htdocs/class/xoopseditor/tinymce/jscripts/tiny_mce/plugins/xoopsmlcontent/langs/en.js
@@ -0,0 +1,9 @@
+/**
+ * @version     $Id$
+ */
+
+tinyMCE.addI18n('en.xoopsmlcontent',{
+    desc : 'Insert multi-language content',
+    delta_width : '0',
+    delta_height : '0'
+});
