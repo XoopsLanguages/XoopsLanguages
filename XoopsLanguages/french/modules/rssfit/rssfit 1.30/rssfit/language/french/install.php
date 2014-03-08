0fd350d394d41fbe3d99615a2397cad8846ba4cd
cesag <cesag@c17157d5-08f2-41e7-a131-df055f6a0da3>
2014-03-08 06:59:18 +0000

    Move Rssfit french folder to french languages ​​folder

---
+++ XoopsLanguages/french/modules/rssfit/rssfit 1.30/rssfit/language/french/install.php
@@ -0,0 +1,16 @@
+<?php
+// $Id: install.php 12344 2014-03-03 01:32:43Z beckmi $
+define('_INTRO_TITLE', 'Syndiquer ce site');
+define('_INTRO_CONTENT', '<a href="rss.php"><img src="images/rss.gif" alt="RSS feed" /></a> <a href="rss.php"> <img src="images/xml.gif" alt="RSS feed" /></a>
+
+{SITENAME} alimente son propre <a href="rss.php">Flux RSS</a> au format XML. Les entr&eacute;es et descriptions sont mises &agrave; jour automatiquement et sont rapidement lisibles au sein de votre lecteur RSS.
+
+Si vous n\'&egrave;tes pas encore familliaris&eacute; avec le RSS, c\'est un <a href="http://blogs.law.harvard.edu/tech/rss">format web standard</a> de publication d\'ent&ecirc;te de news. Les flux RSS peuvent &ecirc;tre rapidement lus avec des logiciels sp&eacute;cifiques et facilement incorpor&eacute;s dans des blogs ou d\'autres contenus web.
+
+Nous vous recommandons comme logiciels lecteurs RSS <a href="http://ranchero.com/netnewswire/#lite">NetNewsWire Lite</a> (Mac OS X), <a href="http://sourceforge.net/projects/feedreader">Feedreader</a> (Windows) et <a href="http://www.disobey.com/amphetadesk/">AmphetaDesk</a> (multi-platforme), ou tout simplement en &eacute;tablissant une recherche avec <a href="http://www.google.com/search?ie=UTF-8&oe=UTF-8&q=RSS+reader">RSS reader</a> sur Google.
+
+Voici les liens URLs des sous-sections de flux :
+{SUB}
+Les flux RSS de ce site sont g&eacute;n&eacute;r&eacute;s par le module xoops <a href="http://www.brandycoke.com/products/rssfit/">RSSFit</a>.');
+define('_INSTALL_INTRO_SUB', '<a href="{URL}">{TITLE}</a>: {DESC}<br /><a href="{URL}">{URL}</a><br /><br />');
+
