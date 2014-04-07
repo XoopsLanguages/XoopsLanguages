9ffedd708f14436bb5cb9ab1fb23cf87f8579217
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2014-04-07 10:17:35 +0000

    Creating English branch for XOOPS 2.5.7

---
+++ XoopsLanguages/english-Reference/core/2.5.7/htdocs/class/xoopseditor/tinymce/jscripts/tiny_mce/plugins/xoopsimagemanager/langs/en.js
@@ -0,0 +1,14 @@
+/**
+ * @version     $Id$
+ */
+
+tinyMCE.addI18n('en.xoopsimagemanager',{
+    desc : 'Xoops Advanced Imagemanager',
+    delta_width : '0',
+    delta_height : '0'
+});
+tinyMCE.addI18n('en.xoopsimagebrowser',{
+    desc : 'Xoops Imagebrowser',
+    delta_width : '0',
+    delta_height : '0'
+});
