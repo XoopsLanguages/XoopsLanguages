628cb30bbccbfc28068776fb91b2d6ef201b1ecd
luciorota <luciorota@c17157d5-08f2-41e7-a131-df055f6a0da3>
2014-06-15 13:43:07 +0000

    xoops 2.5.7 italian language pack - beta1

---
+++ XoopsLanguages/italian/core/2.5.7/htdocs/modules/system/language/italian/admin/smilies.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @copyright   The XOOPS Project http://sourceforge.net/projects/xoops/
+ * @license     http://www.fsf.org/copyleft/gpl.html GNU public license
+ * _LANGCODE    it
+ * _CHARSET     UTF-8
+ * @version     $Id$
+ */
+//Translator: Xoops Italia Team - www.xoopsitalia.org
+//Last translation update: 19/05/2013
+
+//Nav
+define("_AM_SYSTEM_SMILIES_NAV_MANAGER","Gestione Faccine");
+define("_AM_SYSTEM_SMILIES_NAV_LIST","Elenco faccine");
+define("_AM_SYSTEM_SMILIES_NAV_ADD","Nuova faccina");
+define("_AM_SYSTEM_SMILIES_NAV_EDIT","Modifica faccina");
+define("_AM_SYSTEM_SMILIES_NAV_DELETE","Eliminazione faccina");
+
+// Tips
+define("_AM_SYSTEM_SMILIES_NAV_TIPS","<ul><li>Permette aggiungere, aggiornare o eliminare le faccine</li></ul>");
+define("_AM_SYSTEM_SMILIES_NAV_TIPS_FORM1","<ul><li>Tipi di file autorizzati (mime types): %s</li>");
+define("_AM_SYSTEM_SMILIES_NAV_TIPS_FORM2","<li>Dimensione massima del file: %s kb</li></ul>");
+
+//Infos
+define("_AM_SYSTEM_SMILIES_ACTION","Azione");
+define("_AM_SYSTEM_SMILIES_ADD","Nuova faccina");
+define("_AM_SYSTEM_SMILIES_CODE","Combinazione");
+define("_AM_SYSTEM_SMILIES_DELETE","Elimina");
+define("_AM_SYSTEM_SMILIES_DESCRIPTION","Descrizione");
+define("_AM_SYSTEM_SMILIES_DISPLAY","Visibile");
+define("_AM_SYSTEM_SMILIES_EDIT","Modifica faccina");
+define("_AM_SYSTEM_SMILIES_FILE","File");
+define("_AM_SYSTEM_SMILIES_IMAGE_PATH","Percorso del file: %s");
+define("_AM_SYSTEM_SMILIES_OFF","Visibile nei form");
+define("_AM_SYSTEM_SMILIES_ON","Non visibile nei form");
+define("_AM_SYSTEM_SMILIES_SMILIE","Immagine");
+define("_AM_SYSTEM_SMILIES_SUREDEL","Si &egrave; sicuri di voler eliminare questa faccina?");
+define("_AM_SYSTEM_SMILIES_UPLOADS","Caricamento ");
+
+define("_AM_SYSTEM_SMILIES_SAVE",_AM_SYSTEM_DBUPDATED);
---
+++ XoopsLanguages/italian/core/2.5.7/htdocs/modules/system/themes/default/language/italian/localsupport.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * @copyright   The XOOPS Project http://sourceforge.net/projects/xoops/
+ * @license     http://www.fsf.org/copyleft/gpl.html GNU public license
+ * _LANGCODE    it
+ * _CHARSET     UTF-8
+ * @version     $Id$
+ */
+//Translator: Xoops Italia Team - www.xoopsitalia.org
+//Last translation update: 19/05/2013
+
+$menu[0] = array(
+    'link'      => 'http://www.xoopsitalia.org',
+    'title'     => 'XOOPS ITALIA',
+    'absolute'  => 1,
+    'icon'      => 'http://www.xoopsitalia.org/favicon.ico'
+);
+
+$menu[1] = array(
+    'link'      => 'http://www.xoopsitalia.org/modules/newbb/',
+    'title'     => 'Forum di supporto',
+    'absolute'  => 1,
+    'icon'      => 'http://www.xoopsitalia.org/favicon.ico'
+);
+
+$menu[2] = array(
+    'link'      => 'http://www.xoopsitalia.org/modules/newbb/viewforum.php?forum=11',
+    'title'     => 'Traduzioni',
+    'absolute'  => 1,
+    'icon'      => 'http://www.xoopsitalia.org/favicon.ico'
+);
+
+$menu[3] = array(
+);
+
+
+// sample for English  support
+/*
+$menu[] = array(
+    'link'      => 'http://www.xoops.org',
+    'title'     => 'XOOPS',
+    'absolute'  => 1,
+    'icon'      => XOOPS_ADMINTHEME_URL . '/default/images/xoops.png'
+);
+
+$menu[] = array(
+    'link'      => 'http://www.xoops.org',
+    'title'     => 'XOOPS',
+    'absolute'  => 1,
+    'icon'      => XOOPS_ADMINTHEME_URL . '/default/images/xoops.png'
+);
+
+$menu[] = array(
+    'link'      => 'http://www.xoops.org/modules/library/',
+    'title'     => _AD_XOOPSTHEMES,
+    'absolute'  => 1,
+    'icon'      => XOOPS_ADMINTHEME_URL . '/default/images/tweb.png'
+);
+
+$menu[] = array(
+    'link'      => 'http://www.xoops.org/modules/modules-search/',
+    'title'     => _AD_XOOPSMODULES,
+    'absolute'  => 1,
+    'icon'      => XOOPS_ADMINTHEME_URL . '/default/images/xoops.png'
+);
+*/
+return $menu;
