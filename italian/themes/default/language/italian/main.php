caf47ce3244884fc0798e37a9e3c5476a8aafe0a
txmodxoops <txmodxoops@c17157d5-08f2-41e7-a131-df055f6a0da3>
2014-06-28 21:02:47 +0000

    Added italian language for default theme

---
+++ italian/themes/default/language/italian/main.php
@@ -0,0 +1,125 @@
+<?php
+// generality
+define('THEME_NAME', "Default");
+define('THEME_DESC', "nascita di sviluppo");
+define('THEME_DESIGNER', "Disegno da : <a href='http://www.xoops.org/' title='XOOPS Project Community'>XOOPS Community</a>");
+define('THEME_UPTOP', "Parte Superiore");
+define('THEME_VALIDCSS', "Questo tema template ho stato W3C Validato per CSS 1 e 2");
+define('THEME_VALIDXHTML', "Questo tema template ho stato W3C Validato per XHTML 1.01");
+define('THEME_LICENSE', "See Creative Commons Licence");
+define('THEME_W3C', "Valid W3C - XHTML 1.01 and CSS 1/2/3");
+
+// userbar.html
+define('THEME_REGISTER', "Registrazione utente");
+define('THEME_REMEMBER', " Ricordi da me?");
+define('THEME_PSEUDO', " Nome utente");
+define('THEME_DESC_PSEUDO', " Invia utente");
+define('THEME_PASSWORD', " Password");
+define('THEME_DESC_PASSWORD', " Invia password");
+
+define('THEME_WELLCOME', "Benvenuto");
+define('THEME_PROFILE', "Mostra profilo");
+define('THEME_NOTIFICATION', "Notifiche");
+define('THEME_MESSAGE', "Messaggi in arrivo");
+define('THEME_YOUHAVE', "Tu hai");
+define('THEME_NOTREAD', "nuovi messaggi");
+define('THEME_MYACCOUNT', "Mio profilo");
+
+// searchbar.html
+define('THEME_SEARCH', "Cerca");
+define('THEME_DESC_SEARCH', "Ricerca questo sito");
+define('THEME_KEYWORDS', "Invia keywords");
+
+// language.html
+define('THEME_ENGLISH', "English");
+define('THEME_FRENCH', "Français");
+define('THEME_SPAIN', "Español");
+define('THEME_DEUTCH', "Deutsch");
+define('THEME_ITALIA', "Italiano");
+define('THEME_PORTUGUESE', "Português");
+define('THEME_ROMANIAN', "Româneste");
+define('THEME_RUSSE', "Russian");
+define('THEME_JAPAN', "Japanese");
+define('THEME_CHINESE', "Chinese");
+define('THEME_KOREAN', "Korean");
+define('THEME_HOLLAND', "Dutch");
+define('THEME_FARSI', "Farsi");
+define('THEME_ARAB', "Arab");
+define('THEME_HEBRAIC', "Hebrew");
+define('THEME_BREITZ', "Breitz");
+define('THEME_BASQUE', "Euskadi");
+
+// globalnav.html
+define('THEME_CONTACT', "Contatto");
+define('THEME_CONTACT_DESC', "Contattaci?");
+define('THEME_SITEMAP', "Sitemap");
+define('THEME_RSS', "RSS Feeds");
+
+define('THEME_NAVTITLE', "Navigatione");
+define('THEME_HOME', "Home");
+define('THEME_HOME_DESC', "Back to home");
+
+define('THEME_MODULE1', "Notizie");
+define('THEME_MODULE1_DESC', "Vedi le notizie");
+
+define('THEME_MODULE1_SUB1', "Category 1"); //*** the SUB are for sub-menu in globalnav.html 'dynamic'
+define('THEME_MODULE1_SUB2', "Category 2"); //*** the SUB are for sub-menu in globalnav.html 'dynamic'
+define('THEME_MODULE1_SUB3', "Category 3"); //*** the SUB are for sub-menu in globalnav.html 'dynamic'
+define('THEME_MODULE1_SUB4', "Category 4"); //*** the SUB are for sub-menu in globalnav.html 'dynamic'
+define('THEME_MODULE1_SUB5', "Category 5"); //*** the SUB are for sub-menu in globalnav.html 'dynamic'
+
+define('THEME_MODULE2', "BBS");
+define('THEME_MODULE2_DESC', "Visita il nostro forum");
+
+define('THEME_MODULE3', "Galleria");
+define('THEME_MODULE3_DESC', "Visita la nostra galleria della foto");
+
+define('THEME_MODULE3_SUB1', "Gallery 1"); //*** the SUB are for sub-menu in globalnav.html 'dynamic'
+define('THEME_MODULE3_SUB2', "Gallery 2"); //*** the SUB are for sub-menu in globalnav.html 'dynamic'
+define('THEME_MODULE3_SUB3', "Gallery 3"); //*** the SUB are for sub-menu in globalnav.html 'dynamic'
+define('THEME_MODULE3_SUB4', "Gallery 4"); //*** the SUB are for sub-menu in globalnav.html 'dynamic'
+define('THEME_MODULE3_SUB5', "Gallery 5"); //*** the SUB are for sub-menu in globalnav.html 'dynamic'
+
+define('THEME_MODULE4', "Film");
+define('THEME_MODULE4_DESC', "Vedi i nostri film");
+
+define('THEME_MODULE4_SUB1', "Movie 1");
+define('THEME_MODULE4_SUB2', "Movie 2"); //*** the SUB are for sub-menu in globalnav.html 'dynamic'
+define('THEME_MODULE4_SUB3', "Movie 3"); //*** the SUB are for sub-menu in globalnav.html 'dynamic'
+define('THEME_MODULE4_SUB4', "Movie 4"); //*** the SUB are for sub-menu in globalnav.html 'dynamic'
+define('THEME_MODULE4_SUB5', "Movie 5"); //*** the SUB are for sub-menu in globalnav.html 'dynamic'
+
+define('THEME_MODULE5', "Archivio Link");
+define('THEME_MODULE5_DESC', "Visita i nostri link web");
+
+define('THEME_MODULE6', "Downloads");
+define('THEME_MODULE6_DESC', "Download il nostro limite");
+
+define('THEME_MODULE7', "XoopsInfo");
+define('THEME_MODULE7_DESC', "Uso Modulo XoopsInfo ");
+
+define('THEME_MODULE8', "Protector");
+define('THEME_MODULE8_DESC', "Uso Modulo Protector");
+
+// noms des modules dans globalnav.html en mode anonyme
+define('THEME_MODULE1a', "Accreditamenti");
+define('THEME_MODULE1a_DESC', "Accreditamenti del Sito");
+define('THEME_MODULE2a', "Localizzazione");
+define('THEME_MODULE2a_DESC', "Localizzali su una mappa");
+
+// menu Admin in globalnav.html 'Dynamic'
+define('THEME_ADMIN', "Amministrazione");
+define('THEME_ADMIN_DESC', "Vai all'Administrazione");
+define('THEME_ADMIN_PREFERENCES', "Preferenze Del Sito");
+define('THEME_ADMIN_PREFERENCES_DESC', "Vai alle preferenze del sito");
+define('THEME_ADMIN_GENERAL', "Regolazioni Generali");
+define('THEME_ADMIN_GENERAL_DESC', "Vai alle Regolazioni Generali");
+define('THEME_ADMIN_MODULES', "Moduli");
+define('THEME_ADMIN_MODULES_DESC', "Vai all'Administrazione dei Moduli");
+define('THEME_ADMIN_IMAGES', "Immagini");
+define('THEME_ADMIN_IMAGES_DESC', "Vai all'Administrazione delle Immagini");
+
+// accessibilitybar.html
+define('THEME_ACCESS_PLUS', "Dimensione in su !");
+define('THEME_ACCESS_MINUS', "Dimensione giù !");
+?>
