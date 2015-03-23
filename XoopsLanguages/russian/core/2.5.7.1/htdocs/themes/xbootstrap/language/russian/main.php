8248559df82a96f8a0cc1debd8e36ccc4944986a
andrey3761 <andrey3761@c17157d5-08f2-41e7-a131-df055f6a0da3>
2015-03-23 15:22:16 +0000

    XOOPS 2.5.7.1: Russian translate added

---
+++ XoopsLanguages/russian/core/2.5.7.1/htdocs/themes/xbootstrap/language/russian/main.php
@@ -0,0 +1,30 @@
+<?php
+
+//Nav Menu
+define("THEME_HOME", "Главная");
+define("THEME_MODULE1", "О нас");
+define("THEME_MODULE2", "Новости");
+define("THEME_MODULE3", "Форум");
+define("THEME_MODULE4", "Контакт");
+define("THEME_SEARCH_TEXT", "Найти...");
+define("THEME_SEARCH_BUTTON", "вперёд");
+
+//Slider
+define("THEME_READMORE", "Читать далее");
+
+//Home Message
+define("THEME_ABOUTUS", "О нас");
+define("THEME_LEARNINGMORE", "Подробное обучение...");
+
+//NewBB
+define("THEME_NEWBB_TOPIC", "Больше");
+define("THEME_FORUM_SPONSORBY", "Спонсор: ");
+define("THEME_GOTOTHEFORUM", "На форум");
+define("THEME_FORUM_DESCRIPTION", "Описание");
+define("THEME_NEWBB_SEARCH_FORUM", "Поиск по форуму...");
+define("THEME_NEWBB_SEARCH_TOPIC", "Поиск по разделам...");
+define("THEME_FORUM_DESC", "Об этом форуме");
+
+//Block login
+define("THEME_LOGIN", "Ваш ник");
+define("THEME_PASS", "Ваш пароль");
