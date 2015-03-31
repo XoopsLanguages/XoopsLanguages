008fcb1f1c0baaf2144845f210ea8c751b085824
beckmi <beckmi@c17157d5-08f2-41e7-a131-df055f6a0da3>
2015-03-31 01:30:00 +0000

    branch for 2.5.7.1

---
+++ danish/core/2.5.7.1/htdocs/Frameworks/moduleclasses/moduleadmin/language/danish/main.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Frameworks Module Admin
+ *
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright   Grégory Mage (Aka Mage)
+ * @license     GNU GPL 2 (http://www.gnu.org/licenses/old-licenses/gpl-2.0.html)
+ * @author      Grégory Mage (Aka Mage)
+ * @translation Michael Albertsen (Aka culex)
+ */
+
+// Info
+define('_AM_MODULEADMIN_CONFIG','Configurationscheck');
+define('_AM_MODULEADMIN_CONFIG_CHMOD',"Mappen '%s' skal være med chmod '%s' (den er nuværende sat til %s).'");
+define('_AM_MODULEADMIN_CONFIG_FOLDERKO',"Mappen '%s' findes ikke");
+define('_AM_MODULEADMIN_CONFIG_FOLDEROK',"mappen '%s' findes");
+define('_AM_MODULEADMIN_CONFIG_PHP','Minimum PHP krævet: %s (din version er %s)');
+define('_AM_MODULEADMIN_CONFIG_XOOPS','Minimum XOOPS krævet: %s (din version er %s)');
+
+define('_AM_MODULEADMIN_CONFIG_DB','minimum version krævet: %s (din version er %s)');
+define('_AM_MODULEADMIN_CONFIG_ADMIN','Minimum ModuleAdmin krævet: %s (din version er %s)');
+
+// About
+define('_AM_MODULEADMIN_ABOUT_CHANGELOG','Ændringslog');
+define('_AM_MODULEADMIN_ABOUT_DESCRIPTION','Beskrivelse');
+define('_AM_MODULEADMIN_ABOUT_MODULEINFO','Modul-info');
+define('_AM_MODULEADMIN_ABOUT_MODULESTATUS','Status:');
+define('_AM_MODULEADMIN_ABOUT_UPDATEDATE','Opdatet:');
+define('_AM_MODULEADMIN_ABOUT_WEBSITE',"Website:");
+
+define("_AM_MODULEADMIN_ABOUT_RELEASEDATE","Udgivet: ");
+define("_AM_MODULEADMIN_ABOUT_AUTHOR","Forfatter: ");
+define("_AM_MODULEADMIN_ABOUT_CREDITS","Kredit: ");
+define("_AM_MODULEADMIN_ABOUT_LICENSE","Licens: ");
+define("_AM_MODULEADMIN_ABOUT_AUTHOR_NAME","Forfatternavn: ");
+define("_AM_MODULEADMIN_ABOUT_AUTHOR_INFO","Forfatterinfo");
+
+define("_AM_MODULEADMIN_HOME","Hjem");
+define("_AM_MODULEADMIN_DASHBOARD","Dashboard");
+define("_AM_MODULEADMIN_ABOUT","Om");
+define("_AM_MODULEADMIN_PERMISSIONS","Tilladelser");
+
+//ModuleAdmin
+define("_AM_MODULEADMIN_ADMIN_FOOTER","<div class='center smallsmall italic pad5'>Dette modul opretholdes af <a class='tooltip' rel='external' href='http://xoops.org/' title='Besøg XOOPS Community'>XOOPS Community</a></div>");
---
+++ danish/core/2.5.7.1/htdocs/class/xoopseditor/tinymce/tinymce/jscripts/tiny_mce/plugins/advhr/langs/da_dlg.js
@@ -0,0 +1,5 @@
+tinyMCE.addI18n('da.advhr_dlg',{
+width:"Bredde",
+size:"Højde",
+noshade:"Ingen skygge"
+});
