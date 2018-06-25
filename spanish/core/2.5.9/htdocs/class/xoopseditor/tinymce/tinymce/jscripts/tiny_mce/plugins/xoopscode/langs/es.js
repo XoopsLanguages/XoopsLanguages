8946f0f99b895c49a125065828069dcce40de607
osw17 <osw17@hotmail.com>
2018-06-25 17:37:51 -0600

    Add Files

---
+++ spanish/core/2.5.9/htdocs/class/xoopseditor/tinymce/tinymce/jscripts/tiny_mce/plugins/xoopscode/langs/es.js
@@ -0,0 +1,3 @@
+tinyMCE.addI18n('es.xoopscode',{
+    code_desc:"Insertar código"
+});
---
+++ spanish/core/2.5.9/htdocs/class/xoopseditor/tinymce/tinymce/jscripts/tiny_mce/plugins/xoopsimagemanager/langs/es.js
@@ -0,0 +1,13 @@
+/**
+ */
+
+tinyMCE.addI18n('es.xoopsimagemanager',{
+    desc : 'Gestor Avanzado de Imágenes de Xoops',
+    delta_width : '0',
+    delta_height : '0'
+});
+tinyMCE.addI18n('es.xoopsimagebrowser',{
+    desc : 'Explorador de Imágenes de Xoops',
+    delta_width : '0',
+    delta_height : '0'
+});
